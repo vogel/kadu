62c89f49edb9298517e71a631fec7676c132b544
Tomasz Chiliński <chilek@chilan.com>
2002-09-12 21:04:39 +0000

    nowy plik
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@213 fea5205d-6df5-0310-bef7-ea67e47da9a5

---
+++ libgadu/acconfig.h
@@ -0,0 +1,31 @@
+/* $Id: acconfig.h,v 1.1 2002/09/12 21:04:39 chilek Exp $ */
+
+/* define if you want "wap" support */
+#undef WITH_WAP
+
+/* define if you have ncurses */
+#undef HAVE_NCURSES
+
+/* define if you have readline */
+#undef HAVE_READLINE
+
+/* define if you have C99-compatible vsnprintf() */
+#undef HAVE_C99_VSNPRINTF
+
+/* define if you have python */
+#undef WITH_PYTHON
+
+/* define if you want ui_ncurses */
+#undef WITH_UI_NCURSES
+
+/* define if you want ui_readline */
+#undef WITH_UI_READLINE
+
+/* define if you want default ui_readline */
+#undef WITH_FORCE_READLINE
+
+/* define if you want VOIP support */
+#undef HAVE_VOIP
+
+/* define if you have zlib for compressed logs */
+#undef HAVE_ZLIB
