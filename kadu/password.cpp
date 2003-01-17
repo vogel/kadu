6d96a48d1f85d378f4ddbf203c7b7a2d66f27170
Tomasz Chiliński <chilek@chilan.com>
2003-01-17 01:11:53 +0000

    - wersja probna.
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@570 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- kadu/password.cpp
+++ kadu/password.cpp
@@ -2,17 +2,96 @@
 #include "kadu.h"
 #include "password.h"
 
-void remindPassword() {
-    struct gg_http *h = gg_remind_passwd(config.uin, 0);
-    struct gg_pubdir *dupa;
-
-    dupa = (gg_pubdir *) h->data;
-
-    if (h && dupa->success)
-        fprintf(stderr,"KK remindPassword(): Done\n");
-    else
-        fprintf(stderr,"KK remindPassword(): Problem!\n");
-
-    gg_free_remind_passwd(h);
+void remindPassword::remindPassword() {
+	fprintf(stderr, "KK remindPassword::remindPassword()\n");
+	snr = snw = NULL;
+	h = NULL;
 }
 
+remindPassword::~remindPassword() {
+	fprintf(stderr, "KK remindPassword::~remindPassword()\n");
+	
+	deleteSocketNotifiers();
+	if (h) {
+		gg_remind_passwd_free(h);
+		h = NULL;
+		}
+}
+
+void remindPassword::start() {
+	fprintf(stderr, "KK remindPassword::start()\n");
+	if (!(h = gg_remind_passwd(config.uin, 1))) {
+		deleteLater();
+		return;
+		}
+
+	createSocketNotifiers();
+}
+
+void remindPassword::createSocketNotifiers() {
+	fprintf(stderr, "KK remindPassword::createSocketNotifiers()\n");
+
+	snr = new QSocketNotifier(h->fd, QSocketNotifier::Read, kadu);
+	QObject::connect(snr, SIGNAL(activated(int)), this, SLOT(dataReceived()));
+	
+	snw = new QSocketNotifier(h->fd, QSocketNotifier::Write, kadu);
+	QObject::connect(snw, SIGNAL(activated(int)), this, SLOT(dataSent()));
+}
+
+void remindPassword::deleteSocketNotifiers() {
+	fprintf(stderr, "KK remindPassword::deleteSocketNotifiers()\n");
+	if (snr) {
+		snr->setEnabled(false);
+		snr->deleteLater();
+		snr = NULL;
+		}
+	if (snw) {
+		snw->setEnabled(false);
+		snw->deleteLater();
+		snw = NULL;
+		}
+}
+
+void remindPassword::dataReceived() {
+	fprintf(stderr, "KK remindPassword::dataReceived()\n");
+	if (h->check && GG_CHECK_READ)
+		socketEvent();
+}
+
+void remindPassword::dataSent() {
+	fprintf(stderr, "KK remindPassword::dataSent()\n");
+	snw->setEnabled(false);
+	if (h->check && GG_CHECK_WRITE)
+		socketEvent();
+}
+
+void remindPassword::socketEvent() {
+	fprintf(stderr, "KK remindPassword::socketEvent()\n");
+	if (gg_remind_passwd_watch_fd(h) == -1) {
+		deleteSocketNotifiers();
+		fprintf(stderr, "KK remindPassword::socketEvent(): error reminding password!\n");
+		deleteLater();
+		return;
+		}
+	switch (h->state) {
+		case GG_STATE_CONNECTING:
+			fprintf(stderr, "KK remindPassword::socketEvent(): changing QSocketNotifiers.\n");
+			deleteSocketNotifiers();
+			createSocketNotifiers();
+		case GG_STATE_ERROR:
+			fprintf(stderr, "KK remindPassword::socketEvent(): error reminding password!\n");
+			deleteSocketNotifiers();
+			deleteLater();
+			break;
+		case GG_STATE_DONE:
+			fprintf(stderr, "KK remindPassword::socketEvent(): success!\n");
+			deleteSocketNotifiers();
+			deleteLater();
+			break;
+		default:
+			if (h->check && GG_CHECK_WRITE)
+				snw->setEnabled(true);
+		}
+}
+
+#include "password.moc"
--- kadu/password.h
+++ kadu/password.h
@@ -1,10 +1,33 @@
 #ifndef PASSWORD_H
 #define PASSWORD_H
 
-#include <qvaluelist.h>
-#include <qdialog.h>
-#include <qlineedit.h>
+#include <qobject.h>
+#include <qsocketnotifier.h>
 
-void remindPassword();
+class remindPassword : public QObject {
+	Q_OBJECT:
+
+	public:
+		remindPassword();
+		~remindPassword();
+
+	private:
+		void deleteSocketNotifiers();
+		void createSocketNotifiers();
+
+		struct gg_http *h;
+		struct gg_pubdir *p;
+
+		QSocketNotifier *snr;
+		QSocketNotifier *snw;
+
+	public slots:
+		void start();
+
+	private slots:
+		void socketEvent();
+		void dataReceived();
+		void dataSent();
+};
 
 #endif
