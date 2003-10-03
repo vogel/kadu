be89dca7665eed4fa0d91db1ff20df2386f0e9d0
Tomasz Chiliński <chilek@chilan.com>
2003-10-03 20:25:10 +0000

    s/E-mail/New email/
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@1366 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- kadu/password.cpp
+++ kadu/password.cpp
@@ -127,7 +127,7 @@ changePassword::changePassword(QDialog *parent, const char *name) : QDialog(pare
 	
 	tokenimage = new ImageWidget(this);
 
-	QLabel *l_email = new QLabel(tr("E-mail"),this);
+	QLabel *l_email = new QLabel(tr("New email"),this);
 	emailedit = new QLineEdit(this);
 
 	QLabel *l_newpwd = new QLabel(tr("New password"),this);
