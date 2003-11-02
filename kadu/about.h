3a422e9c55efdf73dae818d233730a8f89b48b7c
Tomasz Chiliński <chilek@chilan.com>
2003-11-02 23:54:48 +0000

    przebudowane okno "O programie"
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@1529 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- kadu/about.h
+++ kadu/about.h
@@ -1,35 +1,25 @@
 #ifndef ABOUT_H
 #define ABOUT_H
 
-#include <qdialog.h>
-#include <qlabel.h>
-#include <qpixmap.h>
-#include <qsimplerichtext.h>
-#include <qtimer.h>
+#include <qtabdialog.h>
 
 /*
 	Okno z informacjami o programie
 */
-class About : public QDialog {
+class About : public QTabDialog {
 	Q_OBJECT
 
 public:
 	About();
-	~About();
-
-private slots:
-	virtual void paintEvent(QPaintEvent *);
-	void scrollText();
 
 private:
-	int posy;
-	int w,h;
-	QLabel *m_pfield;
-	QPixmap *m_ppix_text;
-	QPixmap *m_pPixmap;
-	QPixmap *m_pBackground;
-	QSimpleRichText * doc;
-	QTimer * timer;
+	void addTab1();
+	void addTab2();
+	void addTab3();
+	void addTab4();
+	void addTab5();
+
+	QString loadFile(const QString &name);
 };
 
 #endif
