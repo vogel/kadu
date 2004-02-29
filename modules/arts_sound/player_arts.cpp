f77bb332209a8430f10b866a76b800b404b4103f
Adrian Smarzewski <adrian@kadu.net>
2004-02-29 21:10:53 +0000

    modul arts_sound
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@1788 fea5205d-6df5-0310-bef7-ea67e47da9a5

---
+++ modules/arts_sound/player_arts.cpp
@@ -0,0 +1,102 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include "player_arts.h"
+#include "debug.h"
+#include "config_dialog.h"
+#include <string>
+
+SoundManager* sound_manager;
+
+extern "C" void arts_sound_info(ModuleInfo* i)
+{
+    i->description="Arts sound server support module";
+    i->author="Joi";
+    i->depends+="sound";
+}
+
+extern "C" int arts_sound_init()
+{
+	kdebugf();
+
+	sound_manager=soundManager();
+	if(sound_manager==NULL)
+		return 1;
+
+	slotsObj=new ArtsPlayerSlots();
+	if (slotsObj->server.isNull())
+	{
+		delete slotsObj;
+		return -1;
+	}
+
+	QObject::connect(sound_manager, SIGNAL(playOnTestSound(const QString &, bool, double)),
+					 slotsObj, SLOT(playTestSound(const QString &, bool, double)));
+	QObject::connect(sound_manager, SIGNAL(playOnMessage(UinsList, const QString &, const QString &, bool, double)),
+					 slotsObj, SLOT(playMessage(UinsList, const QString &, const QString &, bool, double)));
+	QObject::connect(sound_manager, SIGNAL(playOnChat(UinsList, const QString &, const QString &, bool, double)),
+					 slotsObj, SLOT(playChat(UinsList, const QString &, const QString &, bool, double)));
+	QObject::connect(sound_manager, SIGNAL(playOnNotify(const uin_t, const QString &, bool, double)),
+					 slotsObj, SLOT(playNotify(const uin_t, const QString &, bool, double)));
+
+	return 0;
+}
+extern "C" void arts_sound_close()
+{
+	kdebugf();
+
+	QObject::disconnect(sound_manager, SIGNAL(playOnTestSound(const QString &, bool, double)),
+						slotsObj, SLOT(playTestSound(const QString &, bool, double)));
+	QObject::disconnect(sound_manager, SIGNAL(playOnMessage(UinsList, const QString &, const QString &, bool, double)),
+						slotsObj, SLOT(playMessage(UinsList, const QString &, const QString &, bool, double)));
+	QObject::disconnect(sound_manager, SIGNAL(playOnChat(UinsList, const QString &, const QString &, bool, double)),
+						slotsObj, SLOT(playChat(UinsList, const QString &, const QString &, bool, double)));
+	QObject::disconnect(sound_manager, SIGNAL(playOnNotify(const uin_t, const QString &, bool, double)),
+						slotsObj, SLOT(playNotify(const uin_t, const QString &, bool, double)));
+	delete slotsObj;
+	slotsObj=NULL;
+}
+
+ArtsPlayerSlots::ArtsPlayerSlots()
+{
+	kdebugf();
+	server=Arts::Reference("global:Arts_SimpleSoundServer");
+}
+
+void ArtsPlayerSlots::play(const QString &s, bool volCntrl, double vol)
+{
+	kdebugf();
+	if (!server.isNull())
+		server.play(std::string(s.ascii()));
+}
+
+void ArtsPlayerSlots::playTestSound(const QString &s, bool volCntrl, double vol)
+{
+	kdebugf();
+	play(s);
+}
+
+void ArtsPlayerSlots::playMessage(UinsList senders, const QString &sound, const QString &msg, bool volCntrl, double vol)
+{
+	kdebugf();
+	play(sound);
+}
+
+void ArtsPlayerSlots::playChat(UinsList senders, const QString &sound, const QString &msg, bool volCntrl, double vol)
+{
+	kdebugf();
+	play(sound);
+}
+
+void ArtsPlayerSlots::playNotify(const uin_t uin, const QString &sound, bool volCntrl, double vol)
+{
+	kdebugf();
+	play(sound);
+}
+
+ArtsPlayerSlots *slotsObj;
---
+++ modules/arts_sound/player_arts.h
@@ -0,0 +1,30 @@
+#ifndef PLAYER_ARTS_H
+#define PLAYER_ARTS_H
+#include <qobject.h>
+#include <qstring.h>
+#include <soundserver.h>
+#include "../sound/sound.h"
+#include "misc.h"
+
+using namespace Arts;
+
+class ArtsPlayerSlots : public QObject
+{
+	Q_OBJECT
+		Dispatcher disp;
+	public:
+		ArtsPlayerSlots();
+		SimpleSoundServer server;
+	private slots:
+		void play(const QString &s, bool volCntrl=false, double vol=1);
+		
+		void playTestSound(const QString &s, bool volCntrl, double vol);
+		void playMessage(UinsList senders, const QString &sound, const QString &msg, bool volCntrl, double vol);
+		void playChat(UinsList senders, const QString &sound, const QString &msg, bool volCntrl, double vol);
+		void playNotify(const uin_t uin, const QString &sound, bool volCntrl, double vol);
+};
+
+extern ArtsPlayerSlots *slotsObj;
+
+#endif
+
