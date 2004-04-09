ba7c02bb2d1dedef4df9e47c71dae4b8c6c196e6
Marcin Ślusarz <joi@kadu.net>
2004-04-09 00:28:39 +0000

    tłumaczenia
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@1975 fea5205d-6df5-0310-bef7-ea67e47da9a5

---
+++ modules/autoaway/translations/autoaway_pl.ts
@@ -0,0 +1,21 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>@default</name>
+    <message>
+        <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enable autoaway</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Set status to away after </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Check idle every </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
