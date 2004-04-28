74f6da6b9f834c338b1b8474761d095903cf844b
Marcin Ślusarz <joi@kadu.net>
2004-04-28 22:46:53 +0000

    konfiguracja statusów w autoresponderze
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@2090 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- modules/autoresponder/autoresponder.cpp
+++ modules/autoresponder/autoresponder.cpp
@@ -38,12 +38,20 @@ AutoResponder::AutoResponder() : QObject()
 		QT_TRANSLATE_NOOP("@default", "Autoanswer text:"),
 		"Autotext",
 		QT_TRANSLATE_NOOP("@default", "Thanks for your message. User is not currently available."));
+	ConfigDialog::addLabel("Autoresponder", "Autoresponder options", QT_TRANSLATE_NOOP( "@default","Choose status:" ));
+	ConfigDialog::addCheckBox(config, "Autoresponder", "Autoresponder options", QT_TRANSLATE_NOOP( "@default","Status invisible" ),"StatusInvisible",false );
+	ConfigDialog::addCheckBox(config, "Autoresponder", "Autoresponder options", QT_TRANSLATE_NOOP( "@default","Status busy" ),"StatusBusy",true );
+	ConfigDialog::addCheckBox(config, "Autoresponder", "Autoresponder options", QT_TRANSLATE_NOOP( "@default","Status available" ),"StatusAvailable",false );
 }
 
 AutoResponder::~AutoResponder()
 {
 	QObject::disconnect(&event_manager,SIGNAL(chatMsgReceived1(UinsList,const QString&,time_t,bool&)),
 		this,SLOT(chatReceived(UinsList,const QString&,time_t)));
+	ConfigDialog::removeControl("Autoresponder", "Choose status:");
+	ConfigDialog::removeControl("Autoresponder", "Status invisible");
+	ConfigDialog::removeControl("Autoresponder", "Status busy");
+	ConfigDialog::removeControl("Autoresponder", "Status available");
 	ConfigDialog::removeControl("Autoresponder", "Autoanswer text:");
 	ConfigDialog::removeControl("Autoresponder", "Autoresponder options");
 	ConfigDialog::removeTab("Autoresponder");
@@ -51,19 +59,27 @@ AutoResponder::~AutoResponder()
 	delete config;
 }
 
-void AutoResponder::chatReceived(UinsList senders,const QString& msg,time_t time)
+void AutoResponder::chatReceived(UinsList senders, const QString& msg, time_t time)
 {
-	int status = getActualStatus();
-
 	if (msg.left(5)!="KADU ")
-		if(status == (GG_STATUS_FRIENDS_MASK | GG_STATUS_BUSY)
-			|| status == (GG_STATUS_FRIENDS_MASK | GG_STATUS_BUSY_DESCR)
-			|| status == GG_STATUS_BUSY 
-			|| status == GG_STATUS_BUSY_DESCR) {
-			
+	{
+		int status=getActualStatus() & ~GG_STATUS_FRIENDS_MASK; //pozbywamy siê flagi "Tylko dla przyjació³"
+		
+		bool respond=config->readBoolEntry("Autoresponder", "StatusInvisible") &&
+					(status==GG_STATUS_INVISIBLE || status==GG_STATUS_INVISIBLE_DESCR);
+
+		if (!respond)
+			respond=config->readBoolEntry("Autoresponder", "StatusBusy") &&
+					(status==GG_STATUS_BUSY || status==GG_STATUS_BUSY_DESCR);
+
+		if (!respond)
+			respond=config->readBoolEntry("Autoresponder", "StatusAvailable") &&
+					(status==GG_STATUS_AVAIL || status==GG_STATUS_AVAIL_DESCR);
+
+		if (respond)
 			gadu->sendMessage(senders, unicode2cp(tr("KADU AUTORESPONDER:")+"\n"+
 							config->readEntry("Autoresponder", "Autotext")));
-		}
+	}
 
 }
 
