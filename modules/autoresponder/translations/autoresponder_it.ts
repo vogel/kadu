8c92d07f77125bffb2e778c38f6488b3c068a031
Roman Krzystyniak <Ron_K@tlen.pl>
2004-05-04 11:01:01 +0000

    Aktualizacja
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@2116 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- modules/autoresponder/translations/autoresponder_it.ts
+++ modules/autoresponder/translations/autoresponder_it.ts
@@ -1,17 +1,44 @@
 <!DOCTYPE TS><TS>
 <context>
+    <name>@default</name>
+    <message>
+        <source>Autoresponder</source>
+        <translation>Riposta automatica</translation>
+    </message>
+    <message>
+        <source>Autoresponder options</source>
+        <translation>Configuracione riposta automatica</translation>
+    </message>
+    <message>
+        <source>Autoanswer text:</source>
+        <translation>Riposta:</translation>
+    </message>
+    <message>
+        <source>Thanks for your message. User is not currently available.</source>
+        <translation>Grazie per il messaggio, lo leggo più tardi, adesso sono occupato.</translation>
+    </message>
+    <message>
+        <source>Choose status:</source>
+        <translation>Scegliere status:</translation>
+    </message>
+    <message>
+        <source>Status invisible</source>
+        <translation>Non presente</translation>
+    </message>
+    <message>
+        <source>Status busy</source>
+        <translation>Occupato</translation>
+    </message>
+    <message>
+        <source>Status available</source>
+        <translation>Accessibile</translation>
+    </message>
+</context>
+<context>
     <name>AutoResponder</name>
     <message>
-        <source>Autoresponder started</source>
-        <translation>Autoresponder attivato</translation>
-    </message>
-    <message>
-        <source>Autoresponder stopped</source>
-        <translation>Autoresponder disattivato</translation>
-    </message>
-    <message>
-        <source>KADU AUTORESPONDER: Thanks for your message. User is not currently available.</source>
-        <translation>RISPOSTA AUTOMATICA: Grazie per il messaggio, lo leggo più tardi, adesso sono occupato.</translation>
+        <source>KADU AUTORESPONDER:</source>
+        <translation>RISPOSTA AUTOMATICA:</translation>
     </message>
 </context>
 </TS>
--- modules/autoresponder/translations/autoresponder_pl.ts
+++ modules/autoresponder/translations/autoresponder_pl.ts
@@ -1,17 +1,44 @@
 <!DOCTYPE TS><TS>
 <context>
+    <name>@default</name>
+    <message>
+        <source>Autoresponder</source>
+        <translation>Autoodpowiedż</translation>
+    </message>
+    <message>
+        <source>Autoresponder options</source>
+        <translation>Opcje autoodpowiedzi</translation>
+    </message>
+    <message>
+        <source>Autoanswer text:</source>
+        <translation>Tekst autoodpowiedzi: </translation>
+    </message>
+    <message>
+        <source>Thanks for your message. User is not currently available.</source>
+        <translation>Dziękuję za wiadomość. Jestem w tej chwili niedostepny.</translation>
+    </message>
+    <message>
+        <source>Choose status:</source>
+        <translation>Wybór statusu:</translation>
+    </message>
+    <message>
+        <source>Status invisible</source>
+        <translation>Status niedostępny</translation>
+    </message>
+    <message>
+        <source>Status busy</source>
+        <translation>Status zajęty</translation>
+    </message>
+    <message>
+        <source>Status available</source>
+        <translation>Status dostępny</translation>
+    </message>
+</context>
+<context>
     <name>AutoResponder</name>
     <message>
-        <source>Autoresponder started</source>
-        <translation>Autoresponder załadowany</translation>
-    </message>
-    <message>
-        <source>Autoresponder stopped</source>
-        <translation>Autoresponder zatrzymany</translation>
-    </message>
-    <message>
-        <source>KADU AUTORESPONDER: Thanks for your message. User is not currently available.</source>
-        <translation>AUTOODPOWIEDZ: Dziekuje za wiadomosc przeczytam ja pozniej, teraz jestem nieobecny.</translation>
+        <source>KADU AUTORESPONDER:</source>
+        <translation>KADU AUTOODPOWIEDŹ:</translation>
     </message>
 </context>
 </TS>
