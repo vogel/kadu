2daf66780fdc91d55e31f5da5faa3f9fa7001378
Michał Podsiadlik <michal@kadu.net>
2004-05-17 18:32:00 +0000

    nauka wymowy: chrząszcz brzmi w trzczcinie w Szczebrzeszynie czyli next łatka KID-a :)
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@2223 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- kadu/personal_info.cpp
+++ kadu/personal_info.cpp
@@ -8,10 +8,9 @@
  ***************************************************************************/
 
 #include <qlabel.h>
-#include <qpushbutton.h>
 #include <qmessagebox.h>
 #include <qvbox.h>
-#include <qlayout.h>
+#include <qvgroupbox.h>
 
 #include <stdlib.h>
 
@@ -22,80 +21,148 @@
 #include "personal_info.h"
 
 PersonalInfoDialog::PersonalInfoDialog(QDialog *parent, const char *name)
-	: QDialog (parent, name)
 {
 	kdebugf();
-
-	resize(400, 150);
-	setCaption(tr("Personal Information"));	
 	setWFlags(Qt::WDestructiveClose);
+	setCaption(tr("Personal Information"));
 	
-	QGridLayout *GridLayout = new QGridLayout(this, 8, 3, 10, 10);
-
-	QVBox *NicknameBox = new QVBox(this);
-	/*QLabel *NicknameLabel =*/ new QLabel(tr("Nickname"), NicknameBox);
-	NicknameEdit = new QLineEdit(NicknameBox);
-	GridLayout->addMultiCellWidget(NicknameBox, 0, 1, 0, 0);
-
-	QVBox *NameBox = new QVBox(this);
-	/*QLabel *NameLabel =*/ new QLabel(tr("Name"), NameBox);
-	NameEdit = new QLineEdit(NameBox);
-	GridLayout->addMultiCellWidget(NameBox, 0, 1, 1, 1);
+	// create main QLabel widgets (icon and app info)
+	QVBox *left=new QVBox(this);
+	left->setMargin(10);
+	left->setSpacing(10);
 	
-	QVBox *SurnameBox = new QVBox(this);
-	/*QLabel *SurnameLabel =*/ new QLabel(tr("Surname"), SurnameBox);
-	SurnameEdit = new QLineEdit(SurnameBox);
-	GridLayout->addMultiCellWidget(SurnameBox, 0, 1, 2, 2);
+	QLabel *l_icon = new QLabel(left);
+	QWidget *blank=new QWidget(left);
+	blank->setSizePolicy(QSizePolicy(QSizePolicy::Maximum, QSizePolicy::Expanding));
+	
+	QVBox *center=new QVBox(this);
+	center->setMargin(10);
+	center->setSpacing(10);
+	
+	QLabel *l_info = new QLabel(center);
+	l_icon->setPixmap(icons_manager.loadIcon("PersonalInformationWindowIcon"));
+	l_info->setText(tr("This dialog box allows you to manage your personal information, for example your "
+				"name, surname or age."));
+	l_info->setAlignment(Qt::WordBreak);
+	// end create main QLabel widgets (icon and app info)
+	
+	// our QVGroupBox
+	QVGroupBox *vgb_info = new QVGroupBox(center);
+	vgb_info->setTitle(tr("Your personal information"));
+	center->setStretchFactor(vgb_info, 1);
+	// end our QGroupBox
+	
+	// create our info-fields
+	QHBox *hb_nicknamesurname = new QHBox(vgb_info);
+	QVBox *vb_nickname = new QVBox(hb_nicknamesurname);
+	QVBox *vb_name = new QVBox(hb_nicknamesurname);
+	QVBox *vb_surname = new QVBox(hb_nicknamesurname);
+	hb_nicknamesurname->setSpacing(3);
+	vb_nickname->setSpacing(3);
+	vb_name->setSpacing(3);
+	vb_surname->setSpacing(3);
+	
+	new QLabel(tr("Nickname"), vb_nickname);
+	le_nickname = new QLineEdit(vb_nickname);
+	new QLabel(tr("Name"), vb_name);
+	le_name = new QLineEdit(vb_name);
+	new QLabel(tr("Surname"), vb_surname);
+	le_surname = new QLineEdit(vb_surname);
+	
+	//
+	QHBox *hb_genderbirthyearcity = new QHBox(vgb_info);
+	QVBox *vb_gender = new QVBox(hb_genderbirthyearcity);
+	QVBox *vb_birthyear = new QVBox(hb_genderbirthyearcity);
+	QVBox *vb_city = new QVBox(hb_genderbirthyearcity);
+	hb_genderbirthyearcity->setSpacing(3);
+	vb_gender->setSpacing(3);
+	vb_birthyear->setSpacing(3);
+	vb_city->setSpacing(3);
+	
+	new QLabel(tr("Gender"), vb_gender);
+	cb_gender = new QComboBox(vb_gender);
+	cb_gender->insertItem("");	
+	cb_gender->insertItem(tr("Male"));
+	cb_gender->insertItem(tr("Female"));
+	cb_gender->setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Maximum));
+	hb_genderbirthyearcity->setStretchFactor(cb_gender, 1);
+	new QLabel(tr("Birthyear"), vb_birthyear);
+	le_birthyear = new QLineEdit(vb_birthyear);
+	hb_genderbirthyearcity->setStretchFactor(le_birthyear, 1);
+	new QLabel(tr("City"), vb_city);
+	le_city = new QLineEdit(vb_city);
+	hb_genderbirthyearcity->setStretchFactor(le_city, 1);
+	
+	//
+	QHBox *hb_fnamefcity = new QHBox(vgb_info);
+	QVBox *vb_fname = new QVBox(hb_fnamefcity);
+	QVBox *vb_fcity = new QVBox(hb_fnamefcity);
+	QVBox *vb_blank = new QVBox(hb_fnamefcity);
+	hb_fnamefcity->setSpacing(3);
+	vb_fname->setSpacing(3);
+	vb_fcity->setSpacing(3);
+	vb_blank->setSpacing(3);
+	
+	new QLabel(tr("Family Name"), vb_fname);
+	le_familyname = new QLineEdit(vb_fname);
+	new QLabel(tr("Family City"), vb_fcity);
+	le_familycity = new QLineEdit(vb_fcity);
+	QWidget *w_blankfill = new QWidget(vb_blank);
+	w_blankfill->setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Maximum));
+	//
+	
+	// buttons
+	QHBox *bottom=new QHBox(center);
+	QWidget *blank2=new QWidget(bottom);
+	bottom->setSpacing(5);
+	blank2->setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Maximum));
+	pb_save = new QPushButton(icons_manager.loadIcon("SavePersonalInfoButton"), tr("&Save"), bottom, "save");
+	QPushButton *pb_reload = new QPushButton(icons_manager.loadIcon("ReloadPersonalInfoButton"), tr("&Reload"), bottom, "save");
+	QPushButton *pb_close = new QPushButton(icons_manager.loadIcon("CloseWindow"), tr("&Close"), bottom, "close");
+	// end buttons
+	
+	connect(pb_close, SIGNAL(clicked()), this, SLOT(close()));
+	connect(pb_save, SIGNAL(clicked()), this, SLOT(saveButtonClicked()));
+	connect(pb_reload, SIGNAL(clicked()), this, SLOT(reloadInfo()));
+	
+	reloadInfo();
+	
+	kdebugf2();
+ 	loadGeometry(this, "General", "PersonalInfoDialogGeometry", 0, 0, 450, 400);
+}
 
-	QVBox *GenderBox = new QVBox(this);
-	/*QLabel *GenderLabel =*/ new QLabel(tr("Gender"), GenderBox);
-	GenderCombo = new QComboBox(GenderBox);
-	GenderCombo->insertItem("");	
-	GenderCombo->insertItem(tr("Male"));
-	GenderCombo->insertItem(tr("Female"));
-	GridLayout->addMultiCellWidget(GenderBox, 2, 3, 0, 0);
+PersonalInfoDialog::~PersonalInfoDialog()
+{
+	kdebugf();
+	saveGeometry(this, "General", "PersonalInfoDialogGeometry");
+}
 
-	QVBox *BirthyearBox = new QVBox(this);
-	/*QLabel *BirthyearLabel =*/ new QLabel(tr("Birthyear"), BirthyearBox);
-	BirthyearEdit = new QLineEdit(BirthyearBox);
-	GridLayout->addMultiCellWidget(BirthyearBox, 2, 3, 1, 1);
+void PersonalInfoDialog::keyPressEvent(QKeyEvent *ke_event)
+{
+	if (ke_event->key() == Qt::Key_Escape)
+		close();
+	if ((ke_event->key() == Qt::Key_Return || ke_event->key() == Qt::Key_Enter))
+		saveButtonClicked();
+}
 
-	QVBox *CityBox = new QVBox(this);
-	/*QLabel *CityLabel =*/ new QLabel(tr("City"), CityBox);
-	CityEdit = new QLineEdit(CityBox);
-	GridLayout->addMultiCellWidget(CityBox, 2, 3, 2, 2);
-
-	QVBox *FamilyNameBox = new QVBox(this);
-	/*QLabel *FamilyNameLabel =*/ new QLabel(tr("Family Name"), FamilyNameBox);
-	FamilyNameEdit = new QLineEdit(FamilyNameBox);
-	GridLayout->addMultiCellWidget(FamilyNameBox, 4, 5, 0, 0);
-
-	QVBox *FamilyCityBox = new QVBox(this);
-	/*QLabel *FamilyCityLabel =*/ new QLabel(tr("Family City"), FamilyCityBox);
-	FamilyCityEdit = new QLineEdit(FamilyCityBox);
-	GridLayout->addMultiCellWidget(FamilyCityBox, 4, 5, 1, 1);
-
-	QPushButton* OkButton = new QPushButton(tr("&OK"), this);
-	GridLayout->addWidget(OkButton, 7, 1);
-	connect(OkButton, SIGNAL(clicked()), this, SLOT(OkButtonClicked()));
-
-	QPushButton* CancelButton = new QPushButton(tr("&Cancel"), this);
-	GridLayout->addWidget(CancelButton, 7, 2);	
-	connect(CancelButton, SIGNAL(clicked()), this, SLOT(reject()));
-
-	if (getCurrentStatus() != GG_STATUS_NOT_AVAIL) {
+void PersonalInfoDialog::reloadInfo()
+{
+	if (getCurrentStatus() != GG_STATUS_NOT_AVAIL) 
+	{
 		gg_pubdir50_t req;
 		req = gg_pubdir50_new(GG_PUBDIR50_READ);
 		seq = gg_pubdir50(sess, req);
 		connect(&event_manager, SIGNAL(pubdirReplyReceived(gg_pubdir50_t)),
 			this, SLOT(fillFields(gg_pubdir50_t)));
 		gg_pubdir50_free(req);
+		pb_save->setEnabled(true);
 		State = READING;
 	}
-	kdebugf2();
+	else
+		pb_save->setEnabled(false);
 }
 
-void PersonalInfoDialog::OkButtonClicked()
+void PersonalInfoDialog::saveButtonClicked()
 {
 	kdebugf();
 	if (getCurrentStatus() == GG_STATUS_NOT_AVAIL)
@@ -103,13 +170,13 @@ void PersonalInfoDialog::OkButtonClicked()
 
 	char *nick, *first, *last, *city, *born, *family_name, *family_city;
 
-	nick = NicknameEdit->text().length() ? strdup(unicode2cp(NicknameEdit->text()).data()) : NULL;
-	first = NameEdit->text().length() ? strdup(unicode2cp(NameEdit->text()).data()) : NULL;
-	last = SurnameEdit->text().length() ? strdup(unicode2cp(SurnameEdit->text()).data()) : NULL;
-	city = CityEdit->text().length() ? strdup(unicode2cp(CityEdit->text()).data()) : NULL;
-	born = BirthyearEdit->text().length() ? strdup(unicode2cp(BirthyearEdit->text()).data()) : NULL;
-	family_name = FamilyNameEdit->text().length() ? strdup(unicode2cp(FamilyNameEdit->text()).data()) : NULL;
-	family_city = FamilyCityEdit->text().length() ? strdup(unicode2cp(FamilyCityEdit->text()).data()) : NULL;
+	nick = le_nickname->text().length() ? strdup(unicode2cp(le_nickname->text()).data()) : NULL;
+	first = le_name->text().length() ? strdup(unicode2cp(le_name->text()).data()) : NULL;
+	last = le_surname->text().length() ? strdup(unicode2cp(le_surname->text()).data()) : NULL;
+	city = le_city->text().length() ? strdup(unicode2cp(le_city->text()).data()) : NULL;
+	born = le_birthyear->text().length() ? strdup(unicode2cp(le_birthyear->text()).data()) : NULL;
+	family_name = le_familyname->text().length() ? strdup(unicode2cp(le_familyname->text()).data()) : NULL;
+	family_city = le_familycity->text().length() ? strdup(unicode2cp(le_familycity->text()).data()) : NULL;
 
 	gg_pubdir50_t req;
 	req = gg_pubdir50_new(GG_PUBDIR50_WRITE);
@@ -129,8 +196,8 @@ void PersonalInfoDialog::OkButtonClicked()
 		gg_pubdir50_add(req, GG_PUBDIR50_CITY, (const char *)city);
 		free(city);
 		}
-	if (GenderCombo->currentItem())
-		gg_pubdir50_add(req, GG_PUBDIR50_GENDER, QString::number(GenderCombo->currentItem()).latin1());
+	if (cb_gender->currentItem())
+		gg_pubdir50_add(req, GG_PUBDIR50_GENDER, QString::number(cb_gender->currentItem()).latin1());
 	if (born) {
 		gg_pubdir50_add(req, GG_PUBDIR50_BIRTHYEAR, (const char *)born);
 		free(born);
@@ -180,24 +247,24 @@ void PersonalInfoDialog::fillFields(gg_pubdir50_t res)
 			family_name = gg_pubdir50_get(res, 0, GG_PUBDIR50_FAMILYNAME);
 			family_city = gg_pubdir50_get(res, 0, GG_PUBDIR50_FAMILYCITY);
 			if (first)
-				NameEdit->setText(cp2unicode((unsigned char *)first));
+				le_name->setText(cp2unicode((unsigned char *)first));
 			if (last)
-				SurnameEdit->setText(cp2unicode((unsigned char *)last));
+				le_surname->setText(cp2unicode((unsigned char *)last));
 			if (nick)
-				NicknameEdit->setText(cp2unicode((unsigned char *)nick));
+				le_nickname->setText(cp2unicode((unsigned char *)nick));
 			if (born && strcmp(born, "0"))
-				BirthyearEdit->setText(cp2unicode((unsigned char *)born));
+				le_birthyear->setText(cp2unicode((unsigned char *)born));
 			if (city)
-				CityEdit->setText(cp2unicode((unsigned char *)city));
+				le_city->setText(cp2unicode((unsigned char *)city));
 			if (family_name)
-				FamilyNameEdit->setText(cp2unicode((unsigned char *)family_name));
+				le_familyname->setText(cp2unicode((unsigned char *)family_name));
 			if (family_city)
-				FamilyCityEdit->setText(cp2unicode((unsigned char *)family_city));
-			GenderCombo->setCurrentItem(gender ? atoi(gender) : 0);
+				le_familycity->setText(cp2unicode((unsigned char *)family_city));
+			cb_gender->setCurrentItem(gender ? atoi(gender) : 0);
 			break;
 		case WRITTING:
 			kdebug("PersonalInfoDialog::fillFields(): Done writing info.\n");
-			accept();
+			close();
 			break;
 		case READY:
 			break;
--- kadu/personal_info.h
+++ kadu/personal_info.h
@@ -1,40 +1,45 @@
 #ifndef PERSONAL_INFO_H
 #define PERSONAL_INFO_H
 
-#include <qdialog.h>
+#include <qhbox.h>
 #include <qlineedit.h>
 #include <qcombobox.h>
+#include <qpushbutton.h>
 #include "libgadu.h"
 
 /**
 	Dialog umo¿liwiaj±cy zarz±dzanie w³asnymi danymi w katalogu
 	publicznym
 **/
-class PersonalInfoDialog : public QDialog
+class PersonalInfoDialog : public QHBox
 {
 	Q_OBJECT
 
 	private:
-		QLineEdit* NicknameEdit;
-		QLineEdit* NameEdit;
-		QLineEdit* SurnameEdit;
-		QComboBox* GenderCombo;
-		QLineEdit* BirthyearEdit;
-		QLineEdit* CityEdit;
-		QLineEdit* FamilyNameEdit;
-		QLineEdit* FamilyCityEdit;
+		QLineEdit* le_nickname;
+		QLineEdit* le_name;
+		QLineEdit* le_surname;
+		QComboBox* cb_gender;
+		QLineEdit* le_birthyear;
+		QLineEdit* le_city;
+		QLineEdit* le_familyname;
+		QLineEdit* le_familycity;
+		QPushButton *pb_save;
 		enum DialogState {READY, READING, WRITTING};
 		DialogState State;
 		uint32_t seq;
 
 	private slots:
-		void OkButtonClicked();
+		void saveButtonClicked();
+		void keyPressEvent(QKeyEvent *);
+		void reloadInfo();
 
 	protected:
 		virtual void closeEvent(QCloseEvent * e);
 
 	public:
 		PersonalInfoDialog(QDialog *parent=0, const char *name=0);
+		~PersonalInfoDialog();
 
 	public slots:
 		void fillFields(gg_pubdir50_t res);
