ffab45dcf84e9c6607e8a997de9b04078f245a6a
Roman Krzystyniak <Ron_K@tlen.pl>
2004-07-21 14:30:38 +0000

    Nowe funkcjie
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@2462 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- modules/autoaway/translations/autoaway_it.ts
+++ modules/autoaway/translations/autoaway_it.ts
@@ -17,5 +17,33 @@
         <source>Check idle every </source>
         <translation>Controlla  Iddle ogni</translation>
     </message>
+    <message>
+        <source>Enable autoinvisible</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enable autodisconnect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Set status to invisible after </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Disconnect after </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enable AutoStatus</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Autorestore status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Auto change status</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 </TS>
--- modules/autoaway/translations/autoaway_pl.ts
+++ modules/autoaway/translations/autoaway_pl.ts
@@ -17,5 +17,33 @@
         <source>Check idle every </source>
         <translation> Sprawdź bezczynność co:</translation>
     </message>
+    <message>
+        <source>Enable autoinvisible</source>
+        <translation>Włącz autoniedostępny</translation>
+    </message>
+    <message>
+        <source>Enable autodisconnect</source>
+        <translation>Włącz autorozłączenie</translation>
+    </message>
+    <message>
+        <source>Set status to invisible after </source>
+        <translation>Ustaw status niedostępny po </translation>
+    </message>
+    <message>
+        <source>Disconnect after </source>
+        <translation>Rozłącz po </translation>
+    </message>
+    <message>
+        <source>Enable AutoStatus</source>
+        <translation>Włącz AutoStatus</translation>
+    </message>
+    <message>
+        <source>Autorestore status</source>
+        <translation>Autoprzywracanie statusu</translation>
+    </message>
+    <message>
+        <source>Auto change status</source>
+        <translation>Autozmiana statusu</translation>
+    </message>
 </context>
 </TS>
