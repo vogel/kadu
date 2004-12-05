5e0f35538c501515a9b9afcaac3df85b55a4a90b
Marcin Ślusarz <joi@kadu.net>
2004-12-05 19:55:51 +0000

    poprawka idiotycznego błędu
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@2866 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- modules/encryption/encryption.cpp
+++ modules/encryption/encryption.cpp
@@ -251,7 +251,7 @@ void EncryptionManager::sendMessageFilter(const UinsList& uins, QCString& msg, b
 	Chat* chat=chat_manager->findChatByUins(uins);
 	if (uins.count()==1 && EncryptionEnabled[chat])
 	{
-		char *msg_c = sim_message_encrypt((const unsigned char *)msg_c, uins[0]);
+		char *msg_c = sim_message_encrypt((const unsigned char*)(const char*)msg, uins[0]);
 		if (msg_c==NULL)
 		{
 			kdebugm(KDEBUG_ERROR, "sim_message_encrypt returned NULL! sim_errno=%d sim_strerror=%s\n", sim_errno, sim_strerror(sim_errno));
