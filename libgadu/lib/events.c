afcd47d6d38b6e8ca06073ec6da19d550d05fa57
Marcin Ślusarz <joi@kadu.net>
2005-01-06 23:12:13 +0000

    poprawki w obsłudze proxy (z cvsu libgadu)
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@2970 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- libgadu/lib/events.c
+++ libgadu/lib/events.c
@@ -1,4 +1,4 @@
-/* $Id: events.c,v 1.40 2004/11/01 13:51:44 adrian Exp $ */
+/* $Id: events.c,v 1.41 2005/01/06 23:12:13 joi Exp $ */
 
 /*
  *  (C) Copyright 2001-2003 Wojtek Kaniewski <wojtekka@irc.pl>
@@ -1156,8 +1156,14 @@ struct gg_event *gg_watch_fd(struct gg_session *sess)
 			/* je¶li mamy proxy, wy¶lijmy zapytanie. */
 			if (sess->proxy_addr && sess->proxy_port) {
 				char buf[100], *auth = gg_proxy_auth();
+				struct in_addr addr;
 
-				snprintf(buf, sizeof(buf), "CONNECT %s:%d HTTP/1.0\r\n", inet_ntoa(*((struct in_addr*) &sess->server_addr)), sess->port);
+				if (sess->server_addr)
+					addr.s_addr = sess->server_addr;
+				else
+					addr.s_addr = sess->hub_addr;
+
+				snprintf(buf, sizeof(buf), "CONNECT %s:%d HTTP/1.0\r\n", inet_ntoa(addr), sess->port);
 
 				gg_debug(GG_DEBUG_MISC, "// gg_watch_fd() proxy request:\n//   %s", buf);
 				
