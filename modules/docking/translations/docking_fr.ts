52accb659b67fafc1712528bd217d19d661984ad
Roman Krzystyniak <Ron_K@tlen.pl>
2005-01-06 10:49:12 +0000

    Aktualizacja FR
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@2963 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- modules/docking/translations/docking_fr.ts
+++ modules/docking/translations/docking_fr.ts
@@ -3,11 +3,11 @@
     <name>@default</name>
     <message>
         <source>Show tooltip in tray</source>
-        <translation type="unfinished"></translation>
+        <translation>Montrez la marque d&apos;outils sur le plateau</translation>
     </message>
     <message>
         <source>Start docked</source>
-        <translation type="unfinished"></translation>
+        <translation>Début accouplé</translation>
     </message>
 </context>
 <context>
@@ -15,21 +15,24 @@
     <message>
         <source>Left click - hide/show window
 Middle click or Left click- open message</source>
-        <translation type="unfinished"></translation>
+        <translation>click gauche - cache/montre fenêtre
+click milieu ou gauche - ouvre message</translation>
     </message>
     <message>
         <source>
 
 Current status:
 %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Statut actuel:
+%1</translation>
     </message>
     <message>
         <source>
 
 Description:
 %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Description:
+%2</translation>
     </message>
 </context>
 </TS>
