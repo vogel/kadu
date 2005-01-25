b932a928fe9189f17042069d462890224807c58c
Roman Krzystyniak <Ron_K@tlen.pl>
2005-01-25 04:39:57 +0000

    Nowe wersje jezykowe
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@3003 fea5205d-6df5-0310-bef7-ea67e47da9a5

---
+++ modules/alsa_sound/translations/alsa_sound_fr.ts
@@ -0,0 +1,13 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>@default</name>
+    <message>
+        <source>Output device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ALSA device:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
