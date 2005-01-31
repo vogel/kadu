d22d6ccd1afb3ee575a06f942b5c491b0e1e45e6
Paweł Płuciennik <pawel_p@kadu.net>
2005-01-31 20:58:59 +0000

    dokumentacja
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@3018 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- kadu/chat.h
+++ kadu/chat.h
@@ -53,11 +53,20 @@ class ChatMessage
 		Sformatowana wiadomo¶æ (razem z <p> lub <table>)
 	**/
 	QString message;
-
+	/**
+		Konstruktor ustawiaj±cy nick, niesformatowan± wiadomo¶æ,
+		czy wiadomo¶æ pochodzi od nas, datê wiadomo¶ci,
+		datê nadania wiadomo¶ci przez serwer
+	**/
 	ChatMessage(const QString &nick, const QString &unformattedMessage, bool myMessage, QDateTime date, QDateTime sdate=QDateTime());
+	/**
+		Konstruktor ustawiaj±cy sformatowan± wiadomo¶æ, kolor t³a i liter wiadomo¶ci
+	**/
 	ChatMessage(const QString &formattedMessage, const QColor &bgColor=Qt::white, const QColor &textColor=Qt::black);
 };
-
+/**
+	Klasa zarz±dzaj±ca oknami Chat
+**/
 class ChatManager : public QObject
 {
 	Q_OBJECT
@@ -80,10 +89,25 @@ class ChatManager : public QObject
 			Zwraca liste otwartych okien Chat.
 		**/
 		const ChatList& chats();
+		/**
+			Zwraca wska¼nik do okna z list± u¿ytkowników uins
+			@param uins lista u¿ytkowników
+			@return wska¼nik do okna je¶li istnieje w przeciwnym wypadku zwraca NULL
+		**/
 		Chat* findChatByUins(UinsList uins);
+		/**
+			zwraca warto¶æ w³asno¶ci "name" okna okre¶lonego przez uins
+			@param uins lista u¿ytkowników identyfikuj±ca okno
+			@param name nazwa w³asno¶ci
+			@return zwraca warto¶æ w³asno¶ci je¶li okre¶lone okno istnieje,
+			je¶li nie to tworzy tak± w³asno¶æ (ustawia na pust±)
+		**/
 		QVariant& getChatProperty(const UinsList &uins, const QString &name);
 		
 	public slots:
+		/**
+			Slot informuj±cy o odebraniu wiadomo¶ci
+		**/
 		void chatMsgReceived(UinsList senders,const QString& msg,time_t time,bool& grab);
 
 		/**
@@ -92,17 +116,45 @@ class ChatManager : public QObject
 			z historii ma sie pojawic w oknie.
 		**/
 		int openChat(UinsList senders,time_t time=0);		
-
+		/**
+			Wpisuje zakolejkowane wiadomo¶ci do okna z u¿ytkownikami "uins"
+		**/
 		void openPendingMsgs(UinsList uins);
+		/**
+			Wpisuje wszystkie zakolejkowane wiadomo¶ci do odpowiednich okien
+		**/
 		void openPendingMsgs();
+		/*
+			Niebardzo rozumiem tej funkcji (czemu jest uin i uins)	
+		*/
 		void sendMessage(UinType uin,UinsList selected_uins);
+		/**
+			Zamyka wszystkie okna chat
+		**/
 		void closeAllWindows();
-
+		/**
+			Dodaje okno do managera
+			@param chat wska¼nik do okna ktore chcemy dodaæ
+			@return zwraca numer naszego okna po zarejestrowaniu
+		**/
 		int registerChat(Chat* chat);
+		/**
+			Wyrejestrowuje okno z managera
+			Zapisuje w³asno¶ci okna
+			wysy³a sygna³ chatDestroying i chatDestroyed
+		**/
 		void unregisterChat(Chat* chat);
-
+		/**
+			Od¶wie¿a tytu³y wszystkich okien
+		**/
 		void refreshTitles();
+		/**
+			Od¶wie¿a tytu³y okien które zawieraj± uin
+		**/
 		void refreshTitlesForUin(UinType uin);
+		/**
+			Od¶wie¿a kolory i czcionki we wszystkich oknach
+		**/
 		void changeAppearance();
 		/**
 			pozwala przypisaæ okre¶lonemu czatowi (nawet je¿eli on jeszcze nie istnieje)
@@ -138,7 +190,9 @@ extern ChatManager* chat_manager;
 
 class EmoticonSelector;
 class ColorSelector;
-
+/**
+	Klasa umo¿liwiaj±ca wpisywanie wiadomo¶ci
+**/
 class CustomInput : public QMultiLineEdit
 {
 	Q_OBJECT
@@ -159,10 +213,21 @@ class CustomInput : public QMultiLineEdit
 
 	public slots:
 		void paste();
+		/**
+			Ustawia/wy³±cza automatyczne wysy³anie wiadomo¶ci
+		**/
 		void setAutosend(bool);
 
 	signals:
+		/**
+			Sygna³ zostaje wys³any kiedy naci¶niêto 
+			klawisz wys³ania wiadomo¶ci
+		**/
 		void sendMessage();
+		/**
+			Sygna³ zostaje wys³any gdy naci¶niêto specjalny klawisz (skrót)
+			odpowiadaj±cy np KEY_BOLD
+		**/
 		void specialKeyPressed(int key);
 		/**
 			Dowolny przycisk klawiatury zosta³ naci¶niêty.
@@ -176,6 +241,10 @@ class CustomInput : public QMultiLineEdit
 		void keyReleased(QKeyEvent* e, CustomInput* sender);
 };
 
+/**
+	Klasa wykorzystana do oddzielenia listy u¿ytkowników od historii
+	i pola do wpisywania wiadomo¶ci
+**/
 class KaduSplitter : public QSplitter
 {
 	protected:
@@ -270,17 +339,56 @@ class Chat : public QWidget
 		static void initModule();
 		Chat(UinsList uins, QWidget* parent = 0, const char* name = 0);
 		~Chat();
+		/**
+			Rejestruje przycisk w oknie chat, ustawia funkcje do obs³ugi 
+			sygna³u "clicked"
+			@param name nazwa przycisku aby moc siê do niego odwo³aæ
+			@param receiver obiekt który zawiera funkcje do obs³ugi zdarzenia
+			@param slot funkcja która bêdzie obs³ugiwaæ zdarzenie "clicked"
+		**/
 		static void registerButton(const QString& name, QObject* receiver, const QString& slot);
+		/**
+			Wyrejestrowuje przycisk ze wszystkich okien
+			@param name nazwa przycisku do wyrejestrowania
+		**/
 		static void unregisterButton(const QString& name);
+		/**
+			Zwraca wska¼nik do przycisku 
+			@param name nazwa przycisku
+			@return wska¼nik do przycisku, je¶li przycisk nie istnieje
+			zwraca NULL
+		**/
 		QPushButton* button(const QString& name);
-
+		/**
+			Formatuje wszystkie wiadomo¶ci na html'a,
+			dodaje kolory t³a i czcionek
+			@param msgs lista wiadomo¶ci do sformatowania
+		**/
 		void formatMessages(QValueList<ChatMessage *> &msgs);
+		/**
+			Formatuje wiadomo¶æ na html'a
+			@param msg wiadomo¶æ która bêdzie sformatowana
+			@param myBgColor kolor który bêdzie ustawiony jako t³o
+			w naszych wiadomo¶ciach
+			@param usrBgColor kolor, który bêdzie ustawiony jako t³o
+			wiadomo¶ci naszego rozmówcy
+			@param myFontColor kolor naszej czcionki
+			@param usrFontColor kolor czcionki naszego rozmówcy
+			@param style styl ikonek 
+		**/
 		void formatMessage(ChatMessage &msg,
 				QColor myBgColor=QColor(), QColor usrBgColor=QColor(),
 				QColor myFontColor=QColor(), QColor usrFontColor=QColor(),
 				EmoticonsStyle style=(EmoticonsStyle)config_file.readNumEntry("Chat","EmoticonsStyle"));
-		
+		/**
+			Dodaje wiadomo¶æ do okna
+		**/
 		void checkPresence(UinsList, const QString&, time_t, QValueList<ChatMessage *> &messages);
+		/**
+			Wpisuje do okna historiê rozmowy
+			@param senders lista u¿ytkowników identyfikuj±cych okno
+			@param time ..
+		**/
 		void writeMessagesFromHistory(UinsList, time_t);
 		/**
 			Zwraca listê numerów rozmowców.
@@ -306,29 +414,99 @@ class Chat : public QWidget
 		const QString& title();
 
 	public slots:
+		/**
+			Slot zmieniaj±cy kolory i czcionki w oknie
+		**/
 		void changeAppearance();
+		/**
+			Slot ustawiaj±cy tytu³ okna zgodnie z konfiguracj±
+		**/
 		void setTitle();
+		/**
+			Slot dodaje ikonke do pola wpisywania wiadomo¶ci
+			@param emot ikonka np. <lol>
+		**/
 		void addEmoticon(QString);
+		/**
+			Slot dodaj wiadomo¶ci do okna
+			@param messages lista wiadomo¶ci
+		**/
 		void scrollMessages(const QValueList<ChatMessage *> &);
+		/**
+			Slot informuj±cy o nowej wiadomo¶ci 
+			mruganie je¶li okno jest nieaktywne
+		**/
 		void alertNewMessage();
-
+		/**
+			Slot wywo³ywany po naci¶niêciu przycisku historii
+		**/
 		void HistoryBox();
+		/**
+			Slot wywo³ywany po naci¶niêciu przycisku do wysy³ania wiadomo¶ci
+		**/
 		void sendMessage();
+		/**
+			Slot wywo³ywany po naci¶nieciu przycisku anulowania
+			wysy³aniu wiadomo¶ci
+		**/
 		void cancelMessage();
+		/**
+			Slot wpisuj±cy wiadomo¶æ do okna
+			@see sendMessage
+		**/
 		void writeMyMessage();
+		/**
+		    	Slot zmieniaj±cy tytu³ 
+		    	@see setTitle
+		**/
 		void changeTitle();
+		/**
+			Slot wywo³ywany przy naci¶niêciu przycisku pogrubienia
+		**/
 		void toggledBold(bool on);
+		/**
+			Slot wywo³ywany przy naci¶niêciu przycisku kursywy
+		**/
 		void toggledItalic(bool on);
+		/**
+			Slot wywo³ywany przy naci¶niêciu przycisku podkre¶lenia
+		**/
 		void toggledUnderline(bool on);
+		/**
+			Slot wywo³ywany kiedy pozycja kursosa zmieni³a siê
+		**/
 		void curPosChanged(int para, int pos);
+		/**
+			Slot zostaje wywo³any gdy naci¶niêto specjalny klawisz (skrót)
+			odpowiadaj±cy np KEY_BOLD
+		**/
 		void specialKeyPressed(int key);
+		/**
+			Slot zostaje wywo³wany gdy u¿ytkownik zmieni³ kolor czcionki
+			któr± bêdzie pisa³
+		**/
 		void colorChanged(const QColor& color);
+		/**
+			Slot zostaje wywo³any przy zamykaniu okna wyboru ikonek
+		**/
 		void colorSelectorAboutToClose();
-		
+		/**
+			Slot zostaje wywo³any gdy nasza wiadomo¶æ zosta³a zablokowana
+			przez serwer
+		**/
 		void messageBlockedSlot(int seq, UinType uin);
+		/**
+			Slot zostaje wywo³any gdy skrzynka na wiadomo¶ci
+			zosta³a zape³niona
+		**/
 		void messageBoxFullSlot(int seq, UinType uin);
+		/**
+			Slot zostaje wywo³any gdy wiadomo¶æ nie zosta³a dostarczona
+		**/
 		void messageNotDeliveredSlot(int seq, UinType uin);
-
+		/**
+			Slot zostaje wywo³any gdy wiadomo¶æ zosta³a poprawnie wys³ana
+		**/
 		void messageAcceptedSlot(int seq, UinType uin);
 		
 	signals:
@@ -369,6 +547,9 @@ class ColorSelectorButton : public QPushButton
 		ColorSelectorButton(QWidget* parent, const QColor& qcolor, int width=1, const char *name=0);
 
 	signals:
+		/**
+			Sygna³ zostaje wys³any po wybraniu koloru
+		**/
 		void clicked(const QColor& color);
 };
 
@@ -386,10 +567,19 @@ class ColorSelector : public QWidget
 		ColorSelector(const QColor &defColor, QWidget* parent = 0, const char* name = 0);
 
 	public slots:
+		/**
+		  	Slot wyrównuje pozycjê do widgeta
+		**/
 		void alignTo(QWidget* w);
 
 	signals:
+		/**
+			Sygna³ jest emitowany gdy zamykany jest wybór kolorów
+		**/
 		void aboutToClose();
+		/**
+			Sygna³ jest emitowany gdy wybrano kolor
+		**/
 		void colorSelect(const QColor&);
 };
 
@@ -403,20 +593,60 @@ class ChatSlots :public QObject
 		ChatSlots(QObject* parent=0, const char* name=0);
 
 	public slots:
+		/**
+			Slot jest wywo³ywany gdy otwierane jest okno konfiguracji
+		**/
 		void onCreateConfigDialog();
+		/**
+			Slot jest wywo³ywany gdy zamykane jest okno konfiguracji
+		**/
 		void onDestroyConfigDialog();
+		/**
+			Slot jest wywo³ywany gdy wybierany jest styl ikonek
+		**/
 		void chooseEmoticonsStyle(int index);
+		/**
+			Slot jest wywo³ywany gdy wybierane jest czy otwierane okno
+			ma byæ pusty czy nie
+		**/
 		void onPruneChat(bool toggled);
+		/**
+		**/
 		void onFoldLink(bool toggled);
+		/**
+			Slot jest wywo³ywany gdy wybierany jest maksymalny czas
+			blokowania zamkniêcia okna po otrzymaniu wiadomo¶ci
+		**/
 		void onBlockClose(bool toggled);
+		/**
+			Slot jest wywo³ywany gdy zmieniono kolor,
+			aby uaktualniæ podgl±d 
+		**/
 		void chooseColor(const char* name, const QColor& color);
+		/**
+			Slot jest wywo³ywany gdy zmieniono czcionke,
+			aby uaktualniæ podgl±d 
+		**/
 		void chooseFont(const char* name, const QFont& font);
-
+		/**
+			Slot jest wywo³ywany gdy wybrano z listy przegl±darkê
+		**/
 		void findAndSetWebBrowser(int selectedBrowser);
+		/**
+			Slot jest wywo³ywany gdy aktywowane pole do wpisania opcji
+		**/
 		void findAndSetBrowserOption(int selectedOption);
-
+		/**
+			Slot ustawia przegl±darki wraz z opcjami
+		**/
 		static void initBrowserOptions(QComboBox *browserCombo, QComboBox *browserOptionsCombo, QLineEdit *browserPath);
+		/**
+			Slot jest wywo³ywany gdy wybrano z listy przegl±darkê
+		**/
 		static void findBrowser(int selectedBrowser, QComboBox *browserCombo, QComboBox *browserOptionsCombo, QLineEdit *browserPath);
+		/**
+			Slot ustawia przegl±darki wraz z opcjami
+		**/
 		static void setBrowserOption(int selectedOption, QLineEdit *browserPathEdit, int chosenBrowser);
 };
 
--- kadu/config_file.h
+++ kadu/config_file.h
@@ -27,61 +27,268 @@ class ConfigFile {
 
 	public:
 		/**
-			otwiera plik filename i go wczytuje
+			Otwiera plik filename i go wczytuje
 		**/
 		ConfigFile(const QString &filename);
 		
 		/**
-			zapisuje na dysk zawarto¶æ konfiguracji
+			Zapisuje na dysk zawarto¶æ konfiguracji
 		**/
 		void sync() const;
 		
 		/**
-			zapisuje do wybranego pliku kopiê konfiguracji
+			Zapisuje do wybranego pliku kopiê konfiguracji
 		**/
 		void saveTo(const QString &filename) const;
 		
 		/**
-			zwraca ca³± sekcjê "name"
+			Zwraca ca³± sekcjê "name"
 		**/
 		QMap<QString, QString>& getGroupSection(const QString& name);
 		
 		/**
-			zwraca listê sekcji
+			Zwraca listê sekcji
 		**/
 		QStringList getGroupList() const;
-
+		/**
+			Zapisuje warto¶æ "value" do grupy "group" w pole "name"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param value warto¶æ QString do wpisania
+		**/
 		void writeEntry(const QString &group,const QString &name, const QString &value);
+		/**
+			Zapisuje warto¶æ "value" do grupy "group" w pole "name"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param value warto¶æ char* do wpisania
+		**/
 		void writeEntry(const QString &group,const QString &name, const char *value);
+		/**
+			Zapisuje warto¶æ "value" do grupy "group" w pole "name"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param value warto¶æ int do wpisania
+		**/
 		void writeEntry(const QString &group,const QString &name, const int value);
+		/**
+			Zapisuje warto¶æ "value" do grupy "group" w pole "name"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param value warto¶æ double do wpisania
+		**/
 		void writeEntry(const QString &group,const QString &name, const double value);
+		/**
+			Zapisuje warto¶æ "value" do grupy "group" w pole "name"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param value warto¶æ bool do wpisania
+		**/
 		void writeEntry(const QString &group,const QString &name, const bool value);
+		/**
+			Zapisuje warto¶æ "value" do grupy "group" w pole "name"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param value warto¶æ QRect do wpisania
+		**/
 		void writeEntry(const QString &group,const QString &name, const QRect &value);
+		/**
+			Zapisuje warto¶æ "value" do grupy "group" w pole "name"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param value warto¶æ QSize do wpisania
+		**/
 		void writeEntry(const QString &group,const QString &name, const QSize &value);
+		/**
+			Zapisuje warto¶æ "value" do grupy "group" w pole "name"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param value warto¶æ QColor do wpisania
+		**/
 		void writeEntry(const QString &group,const QString &name, const QColor &value);
+		/**
+			Zapisuje warto¶æ "value" do grupy "group" w pole "name"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param value warto¶æ QFont do wpisania
+		**/
 		void writeEntry(const QString &group,const QString &name, const QFont &value);
+		/**
+			Zapisuje warto¶æ "value" do grupy "group" w pole "name"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param value warto¶æ QPoint do wpisania
+		**/
 		void writeEntry(const QString &group,const QString &name, const QPoint &value);
-
+		/**
+			Zwraca warto¶æ pola "name" z grupy "group"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param def domy¶lna warto¶æ pola
+			@return warto¶æ danego pola, je¶li nie istnieje zwracana
+			jest warto¶æ "def"
+		**/
 		QString readEntry(const QString &group,const QString &name, const QString &def = QString::null) const;
+		/**
+			Zwraca warto¶æ pola "name" z grupy "group"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param def domy¶lna warto¶æ pola
+			@return warto¶æ danego pola, je¶li nie istnieje zwracana
+			jest warto¶æ "def"
+		**/
 		int readNumEntry(const QString &group,const QString &name, int def = 0) const;
+		/**
+			Zwraca warto¶æ pola "name" z grupy "group"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param def domy¶lna warto¶æ pola
+			@return warto¶æ danego pola, je¶li nie istnieje zwracana
+			jest warto¶æ "def"
+		**/
 		unsigned int readUnsignedNumEntry(const QString &group,const QString &name, unsigned int def = 0) const;
+		/**
+			Zwraca warto¶æ pola "name" z grupy "group"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param def domy¶lna warto¶æ pola
+			@return warto¶æ danego pola, je¶li nie istnieje zwracana
+			jest warto¶æ "def"
+		**/
 		double readDoubleNumEntry(const QString &group,const QString &name, double def = 0.0) const;
+		/**
+			Zwraca warto¶æ pola "name" z grupy "group"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param def domy¶lna warto¶æ pola
+			@return warto¶æ danego pola, je¶li nie istnieje zwracana
+			jest warto¶æ "def"
+		**/
 		bool readBoolEntry(const QString &group,const QString &name, bool def = false) const;
+		/**
+			Zwraca warto¶æ pola "name" z grupy "group"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param def domy¶lna warto¶æ pola
+			@return warto¶æ danego pola, je¶li nie istnieje zwracana
+			jest warto¶æ "def"
+		**/
 		QRect readRectEntry(const QString &group,const QString &name, const QRect *def = 0L) const;
+		/**
+			Zwraca warto¶æ pola "name" z grupy "group"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param def domy¶lna warto¶æ pola
+			@return warto¶æ danego pola, je¶li nie istnieje zwracana
+			jest warto¶æ "def"
+		**/
 		QSize readSizeEntry(const QString &group,const QString &name, const QSize *def = 0L) const;
+		/**
+			Zwraca warto¶æ pola "name" z grupy "group"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param def domy¶lna warto¶æ pola
+			@return warto¶æ danego pola, je¶li nie istnieje zwracana
+			jest warto¶æ "def"
+		**/
 		QColor readColorEntry(const QString &group,const QString &name, const QColor *def = 0L) const;
+		/**
+			Zwraca warto¶æ pola "name" z grupy "group"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param def domy¶lna warto¶æ pola
+			@return warto¶æ danego pola, je¶li nie istnieje zwracana
+			jest warto¶æ "def"
+		**/
 		QFont readFontEntry(const QString &group,const QString &name, const QFont *def = 0L) const;
+		/**
+			Zwraca warto¶æ pola "name" z grupy "group"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param def domy¶lna warto¶æ pola
+			@return warto¶æ danego pola, je¶li nie istnieje zwracana
+			jest warto¶æ "def"
+		**/
 		QPoint readPointEntry(const QString &group,const QString &name, const QPoint *def = 0L) const;
-		
+		/**
+			Tworzy pole "name" w grupie "group" je¶li ono nie istnieje
+			i ustawia warto¶æ na "value"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param value warto¶æ QPoint do wpisania
+		**/
 		void addVariable(const QString &group,const QString &name, const QString &value);
+		/**
+			Tworzy pole "name" w grupie "group" je¶li ono nie istnieje
+			i ustawia warto¶æ na "value"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param value warto¶æ QPoint do wpisania
+		**/
 		void addVariable(const QString &group,const QString &name, const char *value);
+		/**
+			Tworzy pole "name" w grupie "group" je¶li ono nie istnieje
+			i ustawia warto¶æ na "value"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param value warto¶æ QPoint do wpisania
+		**/
 		void addVariable(const QString &group,const QString &name, const int value);
+		/**
+			Tworzy pole "name" w grupie "group" je¶li ono nie istnieje
+			i ustawia warto¶æ na "value"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param value warto¶æ QPoint do wpisania
+		**/
 		void addVariable(const QString &group,const QString &name, const double value);
+		/**
+			Tworzy pole "name" w grupie "group" je¶li ono nie istnieje
+			i ustawia warto¶æ na "value"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param value warto¶æ QPoint do wpisania
+		**/
 		void addVariable(const QString &group,const QString &name, const bool value);
+		/**
+			Tworzy pole "name" w grupie "group" je¶li ono nie istnieje
+			i ustawia warto¶æ na "value"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param value warto¶æ QPoint do wpisania
+		**/
 		void addVariable(const QString &group,const QString &name, const QRect &value);
+		/**
+			Tworzy pole "name" w grupie "group" je¶li ono nie istnieje
+			i ustawia warto¶æ na "value"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param value warto¶æ QPoint do wpisania
+		**/
 		void addVariable(const QString &group,const QString &name, const QSize &value);
+		/**
+			Tworzy pole "name" w grupie "group" je¶li ono nie istnieje
+			i ustawia warto¶æ na "value"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param value warto¶æ QPoint do wpisania
+		**/
 		void addVariable(const QString &group,const QString &name, const QColor &value);
+		/**
+			Tworzy pole "name" w grupie "group" je¶li ono nie istnieje
+			i ustawia warto¶æ na "value"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param value warto¶æ QPoint do wpisania
+		**/
 		void addVariable(const QString &group,const QString &name, const QFont &value);
+		/**
+			Tworzy pole "name" w grupie "group" je¶li ono nie istnieje
+			i ustawia warto¶æ na "value"
+			@param group nazwa grupy
+			@param name nazwa pola w grupie
+			@param value warto¶æ QPoint do wpisania
+		**/
 		void addVariable(const QString &group,const QString &name, const QPoint &value);
 };
 
