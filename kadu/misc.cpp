9da40a0234a1cfe935ebc08483a11df6fa8860d5
Marcin Ślusarz <joi@kadu.net>
2005-03-22 13:35:40 +0000

    formatowanie kodu
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@3146 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- kadu/misc.cpp
+++ kadu/misc.cpp
@@ -1514,9 +1514,9 @@ IconsManager *icons_manager_ptr;
 HttpClient::HttpClient()
 {
 	kdebugf();
-	connect(&Socket,SIGNAL(connected()),this,SLOT(onConnected()));
-	connect(&Socket,SIGNAL(readyRead()),this,SLOT(onReadyRead()));
-	connect(&Socket,SIGNAL(connectionClosed()),this,SLOT(onConnectionClosed()));
+	connect(&Socket, SIGNAL(connected()), this, SLOT(onConnected()));
+	connect(&Socket, SIGNAL(readyRead()), this, SLOT(onReadyRead()));
+	connect(&Socket, SIGNAL(connectionClosed()), this, SLOT(onConnectionClosed()));
 	kdebugf2();
 }
 
@@ -1526,10 +1526,10 @@ void HttpClient::onConnected()
 	QString query = PostData.isEmpty() ? "GET": "POST";
 	query += " ";
 
-	if (Path.left(7)!="http://" && config_file.readBoolEntry("Network", "UseProxy"))
+	if (Path.left(7) != "http://" && config_file.readBoolEntry("Network", "UseProxy"))
 		query += "http://" + Host;
 
-	if ((Path.isEmpty() || Path[0] != '/') && Path.left(7)!="http://")
+	if ((Path.isEmpty() || Path[0] != '/') && Path.left(7) != "http://")
 		query += '/';
 
 
@@ -1540,26 +1540,26 @@ void HttpClient::onConnected()
 //	query += "Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8\r\n";
 //	query += "Connection: keep-alive\r\n";
 	if (!Referer.isEmpty())
-		query += "Referer: "+Referer+"\r\n";
+		query += "Referer: " + Referer + "\r\n";
 	if (!Cookies.isEmpty())
 	{
 		query += "Cookie: ";
 		CONST_FOREACH(it, Cookies)
 		{
-			query += it.key() + "=" +it.data();
+			query += it.key() + "=" + it.data();
 			++it;
-			if (it!=Cookies.end())
-				query+="; ";
+			if (it != Cookies.end())
+				query += "; ";
 			--it;
 		}
-		query+="\r\n";
+		query += "\r\n";
 	}
 	if (!PostData.isEmpty())
 	{
 		query += "Content-Type: application/x-www-form-urlencoded\r\n";
 		query += "Content-Length: " + QString::number(PostData.size()) + "\r\n";
 	}
-	query+="\r\n";
+	query += "\r\n";
 	if (!PostData.isEmpty())
 		query += QString(PostData);
 	kdebugm(KDEBUG_NETWORK|KDEBUG_INFO, "HttpClient: Sending query:\n%s\n", query.local8Bit().data());
@@ -1570,57 +1570,57 @@ void HttpClient::onConnected()
 void HttpClient::onReadyRead()
 {
 	kdebugf();
-	int size=Socket.bytesAvailable();
-	kdebugm(KDEBUG_NETWORK|KDEBUG_INFO, "HttpClient: Data Block Retreived: %i bytes\n",size);
+	int size = Socket.bytesAvailable();
+	kdebugm(KDEBUG_NETWORK|KDEBUG_INFO, "HttpClient: Data Block Retreived: %i bytes\n", size);
 	// Dodaj nowe dane do starych
 	char buf[size];
-	Socket.readBlock(buf,size);
+	Socket.readBlock(buf, size);
 	//
 //	kdebugm(KDEBUG_NETWORK|KDEBUG_INFO, "%s\n",buf);
 	//
-	int old_size=Data.size();
-	Data.resize(old_size+size);
-	for(int i=0; i<size; ++i)
-		Data[old_size+i]=buf[i];
+	int old_size = Data.size();
+	Data.resize(old_size + size);
+	for(int i = 0; i < size; ++i)
+		Data[old_size + i] = buf[i];
 	// Jesli nie mamy jeszcze naglowka
-	if(!HeaderParsed)
+	if (!HeaderParsed)
 	{
 		kdebugm(KDEBUG_NETWORK|KDEBUG_INFO, "HttpClient: Trying to parse header\n");
 		// Kontynuuj odczyt jesli naglowek niekompletny
-		QString s=QString(Data);
-		int p=s.find("\r\n\r\n");
-		if(p<0)
+		QString s = QString(Data);
+		int p = s.find("\r\n\r\n");
+		if (p < 0)
 			return;
 		// Dostalismy naglowek,
 		kdebugm(KDEBUG_NETWORK|KDEBUG_INFO, "HttpClient: Http header found:\n%s\n", s.local8Bit().data());
-		HeaderParsed=true;
+		HeaderParsed = true;
 		// Wyci±gamy status
 		QRegExp status_regexp("HTTP/1\\.[01] (\\d+)");
-		if(status_regexp.search(s)<0)
+		if (status_regexp.search(s) < 0)
 		{
 			Socket.close();
 			emit error();
 			return;
 		}
-		StatusCode=status_regexp.cap(1).toInt();
+		StatusCode = status_regexp.cap(1).toInt();
 		kdebugm(KDEBUG_NETWORK|KDEBUG_INFO, "HttpClient: StatusCode: %i\n", StatusCode);
 		// StatusCode 302 oznacza przekierowanie.
-		if (StatusCode==302)
+		if (StatusCode == 302)
 		{
 			QRegExp location_regexp("Location: ([^\\r\\n]+)");
-			if(location_regexp.search(s)<0)
+			if (location_regexp.search(s) < 0)
 			{
 				Socket.close();
 				emit error();
 				return;
 			}
-			QString location=location_regexp.cap(1);
-			kdebugm(KDEBUG_NETWORK|KDEBUG_INFO, "Jumping to %s\n",location.local8Bit().data());
+			QString location = location_regexp.cap(1);
+			kdebugm(KDEBUG_NETWORK|KDEBUG_INFO, "Jumping to %s\n", location.local8Bit().data());
 			// czekamy. zbyt szybkie przekierowanie konczy sie
 			// czasem petla. nie wiem dlaczego.
-			QTime* t=new QTime();
+			QTime* t = new QTime();
 			t->start();
-			while (t->elapsed()<500) {};
+			while (t->elapsed() < 500) {};
 			delete t;
 			//
 			emit redirected(location);
@@ -1629,50 +1629,50 @@ void HttpClient::onReadyRead()
 		}
 		// Wyci±gamy Content-Length
 		QRegExp cl_regexp("Content-Length: (\\d+)");
-		ContentLengthNotFound=cl_regexp.search(s)<0;
-		if(ContentLengthNotFound)
+		ContentLengthNotFound = cl_regexp.search(s) < 0;
+		if (ContentLengthNotFound)
 		{
-			ContentLength=0;
+			ContentLength = 0;
 			kdebugm(KDEBUG_NETWORK|KDEBUG_INFO, "HttpClient: Content-Length not found. We will wait for connection to close.");
 		}
 		else
 		{
-			ContentLength=cl_regexp.cap(1).toUInt();
+			ContentLength = cl_regexp.cap(1).toUInt();
 			kdebugm(KDEBUG_NETWORK|KDEBUG_INFO, "HttpClient: Content-Length: %i bytes\n",ContentLength);
 		}
 
 		// Wyciagamy ewentualne cookie (dla uproszczenia tylko jedno)
 		QRegExp cookie_regexp("Set-Cookie: ([^=]+)=([^;]+);");
-		if(cookie_regexp.search(s)>=0)
+		if (cookie_regexp.search(s) >= 0)
 		{
-			QString cookie_name=cookie_regexp.cap(1);
-			QString cookie_val=cookie_regexp.cap(2);
-			Cookies.insert(cookie_name,cookie_val);
-			kdebugm(KDEBUG_NETWORK|KDEBUG_INFO, "HttpClient: Cookie retreived: %s=%s\n",cookie_name.local8Bit().data(),cookie_val.local8Bit().data());
+			QString cookie_name = cookie_regexp.cap(1);
+			QString cookie_val = cookie_regexp.cap(2);
+			Cookies.insert(cookie_name, cookie_val);
+			kdebugm(KDEBUG_NETWORK|KDEBUG_INFO, "HttpClient: Cookie retreived: %s=%s\n", cookie_name.local8Bit().data(), cookie_val.local8Bit().data());
 		}
 		// Wytnij naglowek z Data
-		int header_size=p+4;
-		int new_data_size=Data.size()-header_size;
-		for(int i=0; i<new_data_size; ++i)
-			Data[i]=Data[header_size+i];
+		int header_size = p + 4;
+		int new_data_size = Data.size() - header_size;
+		for(int i = 0; i < new_data_size; ++i)
+			Data[i] = Data[header_size+i];
 		Data.resize(new_data_size);
 		kdebugm(KDEBUG_NETWORK|KDEBUG_INFO, "HttpClient: Header parsed and cutted off from data\n");
-		kdebugm(KDEBUG_NETWORK|KDEBUG_INFO, "HttpClient: Header size: %i bytes\n",header_size);
-		kdebugm(KDEBUG_NETWORK|KDEBUG_INFO, "HttpClient: New data block size: %i bytes\n",new_data_size);
+		kdebugm(KDEBUG_NETWORK|KDEBUG_INFO, "HttpClient: Header size: %i bytes\n", header_size);
+		kdebugm(KDEBUG_NETWORK|KDEBUG_INFO, "HttpClient: New data block size: %i bytes\n", new_data_size);
 		// Je¶li status jest 100 - Continue to czekamy na dalsze dane
 		// (uniewa¿niamy ten nag³owek i czekamy na nastêpny)
-		if (StatusCode==100)
+		if (StatusCode == 100)
 		{
-			HeaderParsed=false;
+			HeaderParsed = false;
 			return;
 		}
 	}
 	// Kontynuuj odczyt jesli dane niekompletne
 	// lub je¶li mamy czekaæ na connection close
-	if(ContentLength>Data.size()||ContentLengthNotFound)
+	if (ContentLength > Data.size() || ContentLengthNotFound)
 		return;
 	// Mamy cale dane
-	kdebugm(KDEBUG_NETWORK|KDEBUG_INFO, "HttpClient: All Data Retreived: %i bytes\n",Data.size());
+	kdebugm(KDEBUG_NETWORK|KDEBUG_INFO, "HttpClient: All Data Retreived: %i bytes\n", Data.size());
 	Socket.close();
 	emit finished();
 	kdebugf2();
@@ -1681,7 +1681,7 @@ void HttpClient::onReadyRead()
 void HttpClient::onConnectionClosed()
 {
 	kdebugf();
-	if(HeaderParsed&&ContentLengthNotFound)
+	if (HeaderParsed && ContentLengthNotFound)
 		emit finished();
 	else
 		emit error();
@@ -1706,24 +1706,24 @@ void HttpClient::get(const QString &path)
 			config_file.readEntry("Network", "ProxyHost"),
 			config_file.readNumEntry("Network", "ProxyPort"));
 	else
-		Socket.connectToHost(Host,80);
+		Socket.connectToHost(Host, 80);
 }
 
-void HttpClient::post(const QString &path,const QByteArray& data)
+void HttpClient::post(const QString &path, const QByteArray& data)
 {
 	Referer = Path;
 	Path = path;
 	Data.resize(0);
 	PostData.duplicate(data);
 	HeaderParsed = false;
-	Socket.connectToHost(Host,80);
+	Socket.connectToHost(Host, 80);
 }
 
 void HttpClient::post(const QString &path,const QString& data)
 {
 	QByteArray PostData;
-	PostData.duplicate(data.local8Bit().data(),data.length());
-	post(path,PostData);
+	PostData.duplicate(data.local8Bit().data(), data.length());
+	post(path, PostData);
 }
 
 int HttpClient::status() const
