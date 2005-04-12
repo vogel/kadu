46bbbaa21d160733090ae822a4d7d48bca6d5de3
Roman Krzystyniak <Ron_K@tlen.pl>
2005-04-12 16:04:20 +0000

    Aktualizacja
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@3222 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- modules/desktop_docking/translations/desktop_docking_de.ts
+++ modules/desktop_docking/translations/desktop_docking_de.ts
@@ -19,26 +19,26 @@
     </message>
     <message>
         <source>Background color</source>
-        <translation type="unfinished"></translation>
+        <translation>Hintergrundfarbe</translation>
     </message>
     <message>
         <source>Enable Move entry in docklet&apos;s menu</source>
-        <translation type="unfinished"></translation>
+        <translation>Erlaube Verschieben der Einträge im Kontrolleisten-Menü</translation>
     </message>
     <message>
         <source>Move</source>
-        <translation type="unfinished"></translation>
+        <translation>Verschieben</translation>
     </message>
     <message>
         <source>Enable icon moving on desktop. After pressing move cursor over docking icon and then move. Press any mouse key when the icon is in right place.</source>
-        <translation type="unfinished"></translation>
+        <translation>Schaltet das Verschieben des Symbols auf dem Desktop ein. Nach dem Klickt fahren Sie über das Kadu-Eindock-Symbol und verschieben Sie es. Drücken Sie irgendeine Taste der Maus, wenn sich das Kadu-Eindock-Symbol auf dem richtigen Platz befindet.</translation>
     </message>
 </context>
 <context>
     <name>DesktopDockWindow</name>
     <message>
         <source>Move</source>
-        <translation type="unfinished"></translation>
+        <translation>Verschiebe</translation>
     </message>
 </context>
 </TS>
