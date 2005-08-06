116b60e44335740140d0b79281c5a2b4ad12ed00
Roman Krzystyniak <Ron_K@tlen.pl>
2005-08-06 19:54:03 +0000

    Pierwsze tłumaczenie modułu
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@3455 fea5205d-6df5-0310-bef7-ea67e47da9a5

---
+++ modules/migration/translations/migration_fr.ts
@@ -0,0 +1,102 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>MigrationDialog</name>
+    <message>
+        <source>Migration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Migration steps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Migration in progress ...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Finish</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Kadu detected, that you were using EKG, GnuGadu or
+older version of Kadu before. Would you like to try
+to import your settings from %1?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Step 1: Migrating settings to kadu directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Step 1: Settings migrated to kadu directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Migration process completed. You can remove
+%1directory
+(backup will be a good idea) or leave it for other applications.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error migrating data!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cannot start migration process!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Step 2: Migrating user list to kadu.conf.xml</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Step 2: User list migrated to kadu.conf.xml</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Contact list migrated to kadu.conf.xml.You can remove
+%1 and
+%2 now
+(backup will be a good idea).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Step 3: Migrating ignored list to kadu.conf.xml</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Step 3: Ignored list migrated to kadu.conf.xml</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ignored contact list migrated to kadu.conf.xml.
+You can remove %1 now
+(backup will be a good idea).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Step 4: Migrating config files to kadu.conf.xml</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Step 4: Config files migrated to kadu.conf.xml</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Configuration files migrated to kadu.conf.xml.
+You can remove following files now:
+%1
+(backup will be a good idea).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Migration complete. Kadu will be closed now.
+Please click Finish and than run Kadu again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Migration complete.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ modules/migration/translations/migration_pl.ts
@@ -0,0 +1,118 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>MigrationDialog</name>
+    <message>
+        <source>Migration</source>
+        <translation>Migracja</translation>
+    </message>
+    <message>
+        <source>Migration steps</source>
+        <translation>Kroki migracji</translation>
+    </message>
+    <message>
+        <source>Migration in progress ...</source>
+        <translation>Trwa migracja ...</translation>
+    </message>
+    <message>
+        <source>&amp;Finish</source>
+        <translation>&amp;Koniec</translation>
+    </message>
+    <message>
+        <source>Kadu detected, that you were using EKG, GnuGadu or
+older version of Kadu before. Would you like to try
+to import your settings from %1?</source>
+        <translation>Kadu odkryło, że używałeś EKG, GnuGadu lub starszej
+wersji Kadu. Czy chcesz importować swoje ustawienia z:
+%1?</translation>
+    </message>
+    <message>
+        <source>Step 1: Settings migrated to kadu directory</source>
+        <translation>Krok 1: Migracja ustawień  do katalogu .kadu</translation>
+    </message>
+    <message>
+        <source>Migration process completed. You can remove
+%1directory
+(backup will be a good idea) or leave it for other applications.</source>
+        <translation>Proces migracji ukończony. Możesz usunąć katalog:
+%1
+(zrobienie kopi zapasowej będzie dobrym pomysłem)
+lub pozostawić go dla innch aplikacji.</translation>
+    </message>
+    <message>
+        <source>Error migrating data!</source>
+        <translation>Błąd migracji danych!</translation>
+    </message>
+    <message>
+        <source>Cannot start migration process!</source>
+        <translation>Proces migracji nie może zostać rozpoczęty!</translation>
+    </message>
+    <message>
+        <source>Step 2: Migrating user list to kadu.conf.xml</source>
+        <translation>Krok 2: Migrowanie listy kontaktów do kadu.xml.conf</translation>
+    </message>
+    <message>
+        <source>Step 2: User list migrated to kadu.conf.xml</source>
+        <translation>Krok 2: Migracja listy kontaktów do kadu.conf.xml</translation>
+    </message>
+    <message>
+        <source>Contact list migrated to kadu.conf.xml.You can remove
+%1 and
+%2 now
+(backup will be a good idea).</source>
+        <translation>Lista kontaktów została przeniesiona do kadu.conf.xml.
+Możesz teraz usunąć:
+%1
+%2
+(Zrobienie kopi zapasowej to dobry pomysł).</translation>
+    </message>
+    <message>
+        <source>Step 3: Migrating ignored list to kadu.conf.xml</source>
+        <translation>Krok 3: Migrowanie listy ignorowanych do kadu.xml.conf</translation>
+    </message>
+    <message>
+        <source>Step 3: Ignored list migrated to kadu.conf.xml</source>
+        <translation>Krok 3: Migracja listy ignorowanych do kadu.xml.conf</translation>
+    </message>
+    <message>
+        <source>Ignored contact list migrated to kadu.conf.xml.
+You can remove %1 now
+(backup will be a good idea).</source>
+        <translation>Lista ignorowanych kontaktów przeniesiona do kadu.conf.xml
+Możesz teraz usunąć:
+%1
+(Zrobienie kopi zapasowej to dobry pomysł).</translation>
+    </message>
+    <message>
+        <source>Step 4: Migrating config files to kadu.conf.xml</source>
+        <translation>Krok 4: Migrowanie plików konfiguracyjnych do kadu.conf.xml</translation>
+    </message>
+    <message>
+        <source>Configuration files migrated to kadu.conf.xml.
+You can remove following files now:
+%1
+(backup will be a good idea).</source>
+        <translation>Pliki konfiguracyjne przeniesione do kadu.conf.xml
+Możesz teraz usunąć nastepujące pliki:
+%1
+(Zrobienie kopi zapasowej to dobry pomysł).</translation>
+    </message>
+    <message>
+        <source>Migration complete. Kadu will be closed now.
+Please click Finish and than run Kadu again.</source>
+        <translation>Migracja skończona. Kadu zostanie teraz zamknięte.
+Proszę kliknąć na &quot;Koniec&quot; i uruchomić Kadu ponownie.</translation>
+    </message>
+    <message>
+        <source>Migration complete.</source>
+        <translation>Migracja ukończona.</translation>
+    </message>
+    <message>
+        <source>Step 1: Migrating settings to kadu directory</source>
+        <translation>Krok 1: Przenoszenie ustawień do katalogu .kadu</translation>
+    </message>
+    <message>
+        <source>Step 4: Config files migrated to kadu.conf.xml</source>
+        <translation>Krok 4: Migracja plików konfiguracyjnych do kadu.conf.xml</translation>
+    </message>
+</context>
+</TS>
