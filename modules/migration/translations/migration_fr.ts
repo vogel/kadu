26c0194f6d07835ff10e97ea13c981d07483c6b9
Roman Krzystyniak <Ron_K@tlen.pl>
2005-08-24 18:26:29 +0000

    Poczatek tlumaczenia modulu na FR
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@3482 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- modules/migration/translations/migration_fr.ts
+++ modules/migration/translations/migration_fr.ts
@@ -3,39 +3,45 @@
     <name>MigrationDialog</name>
     <message>
         <source>Migration</source>
-        <translation type="unfinished"></translation>
+        <translation>Migration</translation>
     </message>
     <message>
         <source>Migration steps</source>
-        <translation type="unfinished"></translation>
+        <translation> Les étapes de migration</translation>
     </message>
     <message>
         <source>Migration in progress ...</source>
-        <translation type="unfinished"></translation>
+        <translation>Migration en cours ...</translation>
     </message>
     <message>
         <source>&amp;Finish</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Fin</translation>
     </message>
     <message>
         <source>Kadu detected, that you were using EKG, GnuGadu or
 older version of Kadu before. Would you like to try
 to import your settings from %1?</source>
-        <translation type="unfinished"></translation>
+        <translation>Kadu a découvert que vous utilisez EKG, GnuGadu,
+ou une version ancienne de Kadu, voulez vous 
+importer vos paramètres avec: %1?</translation>
     </message>
     <message>
         <source>Step 1: Migrating settings to kadu directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Vos paramètres sont en cours de la migration dans le catalogue .kadu</translation>
     </message>
     <message>
         <source>Step 1: Settings migrated to kadu directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Étape 1: Vos paramètres ont été transférés dans le catalogue .kadu</translation>
     </message>
     <message>
         <source>Migration process completed. You can remove
 %1directory
 (backup will be a good idea) or leave it for other applications.</source>
-        <translation type="unfinished"></translation>
+        <translation>La procedure de migration est terminé.
+Vous pouvez  éliminer le catalogue:
+%1
+(veuillez procéder à une sauvegarde, 
+cela peut servir pour une autre application).</translation>
     </message>
     <message>
         <source>Error migrating data!</source>
