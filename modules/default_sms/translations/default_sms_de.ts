ce6c62e7cf97aab3555ffcc1df1cdd68f71fa89d
Roman Krzystyniak <Ron_K@tlen.pl>
2005-10-05 09:15:02 +0000

    Dokonczenie translacji DE
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/branches/0.4@3558 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- modules/default_sms/translations/default_sms_de.ts
+++ modules/default_sms/translations/default_sms_de.ts
@@ -66,18 +66,18 @@
     </message>
     <message>
         <source>You don&apos;t have enough tokens</source>
-        <translation type="unfinished"></translation>
+        <translation>Ihr Guthaben ist verbraucht</translation>
     </message>
 </context>
 <context>
     <name>SmsGatewaySlots</name>
     <message>
         <source>Sponsored</source>
-        <translation type="unfinished"></translation>
+        <translation>Gesponsort</translation>
     </message>
     <message>
         <source>OmnixMultimedia</source>
-        <translation type="unfinished"></translation>
+        <translation>OmnixMultimedia</translation>
     </message>
 </context>
 <context>
