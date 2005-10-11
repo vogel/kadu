2309a7ced53b5739a94c573def56a55a27eeefb6
Marcin Ślusarz <joi@kadu.net>
2005-10-11 00:05:31 +0000

    Era - numery zaczynające się od 886
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/branches/0.4@3572 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- modules/default_sms/default_sms.cpp
+++ modules/default_sms/default_sms.cpp
@@ -300,7 +300,8 @@ bool SmsEraGateway::isNumberCorrect(const QString& number)
 		((number[0]=='6'&&((QChar(number[2])-'0')%2)==0) ||
 		(number[0]=='8' && number[1]=='8' && number[2]=='8') ||
 		(number[0]=='8' && number[1]=='8' && number[2]=='9') ||
-		(number[0]=='8' && number[1]=='8' && number[2]=='0'));
+		(number[0]=='8' && number[1]=='8' && number[2]=='0') ||
+		(number[0]=='8' && number[1]=='8' && number[2]=='6'));
 }
 
 void SmsEraGateway::httpRedirected(QString link)
