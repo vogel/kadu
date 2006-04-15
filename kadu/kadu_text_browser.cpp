92ddc947485f142a4948489562e644d355a98e28
Marcin Ślusarz <joi@kadu.net>
2006-04-15 21:50:18 +0000

    english comments, formatting
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@3810 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- kadu/kadu_text_browser.cpp
+++ kadu/kadu_text_browser.cpp
@@ -22,7 +22,7 @@
 #include "message_box.h"
 
 KaduTextBrowser::KaduTextBrowser(QWidget *parent, const char *name)
-	: QTextBrowser(parent, name),QToolTip(viewport()),level(0)
+	: QTextBrowser(parent, name), QToolTip(viewport()), level(0)
 {
 	kdebugf();
 
@@ -62,7 +62,7 @@ void KaduTextBrowser::maybeTip(const QPoint &c)
 {
 	if (!highlightedlink.isEmpty())
 		kdebugmf(KDEBUG_INFO, "link %s (X,Y)=%d,%d\n", highlightedlink.local8Bit().data(), c.x(), c.y());
-	tip(QRect(c.x()-20,c.y()-5,40,10), highlightedlink);
+	tip(QRect(c.x() - 20, c.y() - 5, 40, 10), highlightedlink);
 }
 
 void KaduTextBrowser::linkHighlighted(const QString & link)
@@ -89,12 +89,12 @@ QPopupMenu *KaduTextBrowser::createPopupMenu(const QPoint &point)
 {
 	kdebugf();
 	anchor = anchorAt(point);
-	anchor.replace("%2520", "%20");//obej¶cie b³êdu w Qt, patrz HtmlDocument::convertUrlsToHtml()
+	anchor.replace("%2520", "%20");//workaround for bug in Opera, see: HtmlDocument::convertUrlsToHtml()
 	image = imageAt(point);
 	if (!image.isEmpty())
 		kdebugm(KDEBUG_INFO, "image: %s\n", image.local8Bit().data());
 
-	QPopupMenu* popupmenu = QTextBrowser::createPopupMenu(point);
+	QPopupMenu *popupmenu = QTextBrowser::createPopupMenu(point);
 
 	if (!anchor.isEmpty())
 		popupmenu->insertItem(tr("Copy link &location"), this, SLOT(copyLinkLocation()), CTRL+Key_L, -1, 0);
@@ -108,15 +108,14 @@ QPopupMenu *KaduTextBrowser::createPopupMenu(const QPoint &point)
 void KaduTextBrowser::drawContents(QPainter * p, int clipx, int clipy, int clipw, int cliph)
 {
 	/*
-		z nie do koñca wiadomych przyczyn, Qt czasami wpada w pêtle i drawContents
-		jeszcze	raz wywo³uje sam± siebie, co powoduje wypisanie:
+		for unknown reasons, QTextBrowser::drawContents() sometimes invokes itself, which produces:
 			QPixmap::operator=: Cannot assign to pixmap during painting
 			QPaintDevice: Cannot destroy paint device that is being painted
-		oraz zawieszenie Kadu (http://www.kadu.net/forum/viewtopic.php?t=2486)
+		and freeze of application (http://www.kadu.net/forum/viewtopic.php?t=2486)
 	*/
 //	kdebugmf(KDEBUG_INFO, "level: %d\n", level);
 	++level;
-	if (level==1)
+	if (level == 1)
 	{
 //		kdebugm(KDEBUG_INFO, "x:%d y:%d w:%d h:%d\n", clipx, clipy, clipw, cliph);
 		QTextBrowser::drawContents(p, clipx, clipy, clipw, cliph);
@@ -128,7 +127,7 @@ void KaduTextBrowser::drawContents(QPainter * p, int clipx, int clipy, int clipw
 void KaduTextBrowser::hyperlinkClicked(const QString &link) const
 {
 	if (link.startsWith("www."))
-		openWebBrowser("http://"+link);
+		openWebBrowser("http://" + link);
 	else
 		openWebBrowser(link);
 }
@@ -140,15 +139,15 @@ void KaduTextBrowser::copy()
 	int paraFrom, indexFrom, paraTo, indexTo;
 	getSelection(&paraFrom, &indexFrom, &paraTo, &indexTo);
 //	kdebugm(KDEBUG_DUMP, "selection: %d %d %d %d\n", paraFrom, indexFrom, paraTo, indexTo);
-	if (paraFrom==paraTo && indexFrom==indexTo)
+	if (paraFrom == paraTo && indexFrom == indexTo)
 		return;
 
-	QString txt=selectedText();
+	QString txt = selectedText();
 //	kdebugm(KDEBUG_DUMP, "%d    plain:%d rich:%d auto:%d log:%d\n", textFormat(), Qt::PlainText, Qt::RichText, Qt::AutoText, Qt::LogText);
 //	kdebugm(KDEBUG_DUMP, "\n%s\n----------------------\n", txt.local8Bit().data());
 
-	//czasem siê to cholerstwo pojawia gdy dostajemy ca³y dokument (bug w qt!),
-	//wiêc wypada³oby pozbyæ siê wszystkich zbêdnych tagów...
+	// sometimes we get all document (bug in Qt!)
+	// so it would be nice to remove all these tags ;)
 	txt.remove("<html>");
 	txt.remove("</html>");
 
@@ -168,8 +167,8 @@ void KaduTextBrowser::copy()
 	txt.replace("<br/>", "\n");
 	txt.replace("<br />", "\n");
 
-	//usuwamy wszystkie znane tagi htmla, które mog± siê pojawiæ w chacie
-	//nie mo¿na u¿yæ po prostu <[^>]+>, bo za³api± siê te¿ emotikony typu <rotfl>
+	// remove all known html tags, which can show up in Chat
+	// we cannot use <[^>]+> because it would catch emoticons like <rotfl>
 	txt.remove(QRegExp("<![^>]+>"));//<!--StartFragment-->
 
 	txt.remove("<p>");
@@ -194,7 +193,7 @@ void KaduTextBrowser::copy()
 
 	txt.remove("<hr>");
 
-	//specjalnie traktujemy obrazki, mo¿e u¿ytkownik domy¶li siê o co tu chodzi :P
+	// treat images special
 	txt.replace(QRegExp("<img gg_crc=([0-9]*) gg_sender=([0-9]*) gg_size=([0-9]*) src=[^>]+>"), "\\2-\\3-\\1-*");
 	txt.replace(QRegExp("<img src=([^>]+)>"), "\\1");
 
@@ -207,7 +206,7 @@ void KaduTextBrowser::copy()
 
 //	kdebugm(KDEBUG_DUMP, "result: \n%s\n\n", txt.local8Bit().data());
 
-	QClipboard *clipboard=QApplication::clipboard();
+	QClipboard *clipboard = QApplication::clipboard();
 	clipboard->setText(txt, QClipboard::Clipboard);
 	clipboard->setText(txt, QClipboard::Selection);
 	kdebugf2();
