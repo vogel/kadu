c693d386fb8e004182f5d2ea221cbf7571eb5d5e
Marcin Ślusarz <joi@kadu.net>
2006-12-25 02:30:44 +0000

    macosx: creating universal binary
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@4067 fea5205d-6df5-0310-bef7-ea67e47da9a5

---
+++ varia/scripts/lipo_kadu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# skrypt ma za zadanie stworzenie wersji universal binary z wszystkich binarnych plików u¿ywanych przez Kadu
+# autorzy: max & joi
+# licencja: gpl v2 lub wy¿sza
+
+KINTEL=~/Desktop/kaduintel/Kadu.app
+KPPC=~/Desktop/kaduppc/Kadu.app
+KUB=~/Desktop/Kadu.app
+
+lipo $KINTEL/Contents/MacOS/kadu $KPPC/Contents/MacOS/kadu -create -output $KUB/Contents/MacOS/kadu
+lipo $KINTEL/Contents/MacOS/playsound $KPPC/Contents/MacOS/playsound -create -output $KUB/Contents/MacOS/playsound
+lipo $KINTEL/Contents/Frameworks/libsndfile.1.dylib $KPPC/Contents/Frameworks/libsndfile.1.dylib -create -output $KUB/Contents/Frameworks/libsndfile.1.dylib
+
+#na makintelu te biblioteki s± UB, wiêc nie trzeba nic lipowaæ
+cp $KINTEL/Contents/Frameworks/libssl.*.dylib $KUB/Contents/Frameworks/
+cp $KINTEL/Contents/Frameworks/libcrypto.*.dylib $KUB/Contents/Frameworks/
+
+for i in $KINTEL/kadu/modules/*.dylib; do
+	MODNAME=`basename $i`
+	lipo $KINTEL/kadu/modules/$MODNAME $KPPC/kadu/modules/$MODNAME -create -output $KUB/kadu/modules/$MODNAME
+done
