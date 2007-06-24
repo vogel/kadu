eeef0dc9d521253ae005106e2a673b7cfb5df351
Rafał Malinowski <malinowskirafal@wp.pl>
2007-06-24 22:49:57 +0000

    wrzucona zaległa funkcja
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@4357 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- kadu-core/themes.h
+++ kadu-core/themes.h
@@ -9,31 +9,33 @@
 class Themes : public QObject
 {
 	Q_OBJECT
-	private:
-		QStringList ThemesList;
-		QStringList ThemesPaths;
-		QStringList additional;
-		QString ConfigName, Name, ActualTheme;
-		QMap<QString, QString> entries;
-		QStringList getSubDirs(const QString& path) const;
-		QString fixFileName(const QString& path,const QString& fn) const;
 
-	public:
-		Themes(const QString& name, const QString& configname, const char *cname = 0);
-		QStringList defaultKaduPathsWithThemes() const;
-		const QStringList &themes() const;
-		const QString &theme() const;
-		const QStringList &paths() const;
-		const QStringList &additionalPaths() const;
-		QString themePath(const QString& theme = QString::null) const;
-		QString getThemeEntry(const QString& name) const;
-	public slots:
-		void setTheme(const QString& theme);
-		void setPaths(const QStringList& paths);
+	QStringList ThemesList;
+	QStringList ThemesPaths;
+	QStringList additional;
+	QString ConfigName, Name, ActualTheme;
+	QMap<QString, QString> entries;
+	QStringList getSubDirs(const QString& path) const;
+	QString fixFileName(const QString& path,const QString& fn) const;
 
-	signals:
-		void themeChanged(const QString& theme);
-		void pathsChanged(const QStringList& list);
+public:
+	Themes(const QString& name, const QString& configname, const char *cname = 0);
+	QStringList defaultKaduPathsWithThemes() const;
+	const QStringList &themes() const;
+	const QString &theme() const;
+	const QStringList &paths() const;
+	const QStringList &additionalPaths() const;
+	QString themePath(const QString& theme = QString::null) const;
+	QString getThemeEntry(const QString& name) const;
+	const QMap<QString, QString> & getEntries() { return entries; }
+
+public slots:
+	void setTheme(const QString& theme);
+	void setPaths(const QStringList& paths);
+
+signals:
+	void themeChanged(const QString& theme);
+	void pathsChanged(const QStringList& list);
 };
 
 #endif
