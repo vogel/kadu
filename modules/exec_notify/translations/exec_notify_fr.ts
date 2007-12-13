47a376b48ae05e08b43d9417f4f43ea53c4d35a0
Roman Krzystyniak <Ron_K@tlen.pl>
2007-12-13 17:18:03 +0000

    Aktualizacjia plików *.ts do wersji 0.6
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@4660 fea5205d-6df5-0310-bef7-ea67e47da9a5

---
+++ modules/exec_notify/translations/exec_notify_fr.ts
@@ -0,0 +1,22 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>@default</name>
+    <message>
+        <source>Exec</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ExecConfigurationWidget</name>
+    <message>
+        <source>Syntax: #{action} - action, #{protocol} - protocol, %ids - uins, %s - status, %d - description, %i - ip, %n - nick,
+ %a - altnick, %f - first name %r - surname,  %m - mobile, %u - uin, %g - group,
+ %o - return _space_ if user doesn&apos;t have us in userlist  %v - revDNS, %p - port %e - email %x - max image size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Command</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
