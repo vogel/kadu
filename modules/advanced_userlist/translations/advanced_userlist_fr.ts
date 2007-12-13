9ed79bb2fb0847e7a3e6cf046654a8e8dee4d191
Roman Krzystyniak <Ron_K@tlen.pl>
2007-12-13 17:32:40 +0000

    Aktualizacjia plików *.ts do wersji 0.6
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@4661 fea5205d-6df5-0310-bef7-ea67e47da9a5

---
+++ modules/advanced_userlist/translations/advanced_userlist_fr.ts
@@ -0,0 +1,29 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>AdvancedUserList</name>
+    <message>
+        <source>Number of pending messages)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Priorities</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Priority</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sorting functions</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
