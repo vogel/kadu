4f1cbfa04fee343d84268ad832c66ff038dd1ec6
Roman Krzystyniak <Ron_K@tlen.pl>
2007-12-13 17:35:18 +0000

    Aktualizacjia plików *.ts do wersji 0.6
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@4662 fea5205d-6df5-0310-bef7-ea67e47da9a5

---
+++ modules/xosd_notify/translations/xosd_notify_de.ts
@@ -0,0 +1,103 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>@default</name>
+    <message>
+        <source>XOSD</source>
+        <translation>XOSD</translation>
+    </message>
+</context>
+<context>
+    <name>XOSDConfigurationWidget</name>
+    <message>
+        <source>Please select font size! (pxlsz)</source>
+        <translation>Wählen Sie bitte die Schriftgröße aus! (px)</translation>
+    </message>
+    <message>
+        <source>Top left</source>
+        <translation>Oben links</translation>
+    </message>
+    <message>
+        <source>Top</source>
+        <translation>Oben</translation>
+    </message>
+    <message>
+        <source>Top right</source>
+        <translation>Oben rechts</translation>
+    </message>
+    <message>
+        <source>Left</source>
+        <translation>Links</translation>
+    </message>
+    <message>
+        <source>Center</source>
+        <translation>Mittig</translation>
+    </message>
+    <message>
+        <source>Right</source>
+        <translation>Rechts</translation>
+    </message>
+    <message>
+        <source>Bottom left</source>
+        <translation>Unten links</translation>
+    </message>
+    <message>
+        <source>Bottom</source>
+        <translation>Unten</translation>
+    </message>
+    <message>
+        <source>Bottom right</source>
+        <translation>Rechts unten</translation>
+    </message>
+    <message>
+        <source>Test</source>
+        <translation>Test</translation>
+    </message>
+    <message>
+        <source>Select font</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Positions</source>
+        <translation>Position</translation>
+    </message>
+    <message>
+        <source>Timeout</source>
+        <translation>Ausblenden nach</translation>
+    </message>
+    <message>
+        <source>Outline offset</source>
+        <translation>Rahmen-Verschiebung</translation>
+    </message>
+    <message>
+        <source>Shadow offset</source>
+        <translation>Schatten-Verschiebung</translation>
+    </message>
+    <message>
+        <source>Font color</source>
+        <translation>Schriftartfarbe</translation>
+    </message>
+    <message>
+        <source>Outline color</source>
+        <translation>Rahmenfarbe</translation>
+    </message>
+    <message>
+        <source>Shadow color</source>
+        <translation>Schattenfarbe</translation>
+    </message>
+    <message>
+        <source>Testing configuration</source>
+        <translation>Konfigurationstest</translation>
+    </message>
+</context>
+<context>
+    <name>XOSDNotify</name>
+    <message>
+        <source>Status changed</source>
+        <translation>Statusänderung</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;%a&lt;/b&gt; changed status to &lt;i&gt;%s&lt;/i&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ modules/xosd_notify/translations/xosd_notify_fr.ts
@@ -0,0 +1,103 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>@default</name>
+    <message>
+        <source>XOSD</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>XOSDConfigurationWidget</name>
+    <message>
+        <source>Please select font size! (pxlsz)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Top left</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Top</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Top right</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Left</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Center</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Right</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bottom left</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bottom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bottom right</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Test</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select font</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Positions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Timeout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Outline offset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Shadow offset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Font color</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Outline color</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Shadow color</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Testing configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>XOSDNotify</name>
+    <message>
+        <source>Status changed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;%a&lt;/b&gt; changed status to &lt;i&gt;%s&lt;/i&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
