84e42269cd3dc52fb5cdaf8666bea22442009334
Rafał Malinowski <malinowskirafal@wp.pl>
2007-12-19 20:55:05 +0000

    poprawka dla BSD
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@4689 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- modules/dcc/file_transfer_manager.cpp
+++ modules/dcc/file_transfer_manager.cpp
@@ -7,7 +7,10 @@
  *                                                                         *
  ***************************************************************************/
 
-#include <fcntl.h> 
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #include <qmessagebox.h>
 
