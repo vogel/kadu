01c40124e5cf65d5213a10804d172f49297ff3a8
Dawid Stawiarski <neeo@kadu.net>
2008-01-29 22:19:07 +0000

    [955] - odbieramy obrazki przy statusie ukryty
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@4849 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- kadu-core/kadu.cpp
+++ kadu-core/kadu.cpp
@@ -2048,7 +2048,7 @@ void Kadu::createDefaultConfiguration()
 	config_file.addVariable("Chat", "OpenChatOnMessageWhenOnline", false);
 	config_file.addVariable("Chat", "SaveOpenedWindows", true);
 	config_file.addVariable("Chat", "ReceiveMessages", true);
-	config_file.addVariable("Chat", "ReceiveImagesDuringInvisibility", false);
+	config_file.addVariable("Chat", "ReceiveImagesDuringInvisibility", true);
 	config_file.addVariable("Chat", "RememberPosition", true);
 	config_file.addVariable("Chat", "ShowEditWindowLabel", true);
 
