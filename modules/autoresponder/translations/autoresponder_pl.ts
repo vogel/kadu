9c4395e64b3a10b4569d23bea993bd88d41fffa6
Rafał Malinowski <malinowskirafal@wp.pl>
2008-02-17 15:53:01 +0000

    poprawka tłumaczenia
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@4900 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- modules/autoresponder/translations/autoresponder_pl.ts
+++ modules/autoresponder/translations/autoresponder_pl.ts
@@ -1,11 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="pl">
+<defaultcodec></defaultcodec>
 <context>
     <name>@default</name>
     <message>
         <location filename="../.configuration-ui-translations.cpp" line="3"/>
         <source>Chat</source>
-        <translation>Rozmowa</translation>
+        <translation>Okno rozmowy</translation>
     </message>
     <message>
         <location filename="../.configuration-ui-translations.cpp" line="6"/>
