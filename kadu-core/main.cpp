37cd8bfd1624ebd3210192bd4e68814e6042419c
Dawid Stawiarski <neeo@kadu.net>
2008-03-17 13:08:27 +0000

    poprawka kompilacji na FreeBSD
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/branches/0.6.0.1@4972 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- kadu-core/main.cpp
+++ kadu-core/main.cpp
@@ -337,6 +337,10 @@ int main(int argc, char *argv[])
 					kdebugm(KDEBUG_INFO, "l_type: %d, l_pid: %d\n", lock_str->l_type, lock_str->l_pid);
 					if (ggnumber)
 					{
+#ifdef __FreeBSD__
+						typedef union sigval sigval_t;
+#endif /* __FreeBSD__ */
+
 						sigval_t v;
 						v.sival_int = ggnumber;
 						sigqueue(lock_str->l_pid, OPEN_CHAT_SIGNAL, v);
