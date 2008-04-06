12a7f7a2cd45a339b80cce6b7f3c1848d66f28b0
Roman Krzystyniak <Ron_K@tlen.pl>
2008-04-06 19:35:55 +0000

    tlumaczenia de
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@5028 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- modules/sound/translations/sound_de.ts
+++ modules/sound/translations/sound_de.ts
@@ -1,82 +1,66 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS><TS>
 <context>
     <name>@default</name>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="8"/>
         <source>Sound</source>
         <translation>Klang</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="4"/>
         <source>Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Benachrichtigungen</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="11"/>
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>Allgemein</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="14"/>
         <source>Use sounds</source>
-        <translation type="unfinished"></translation>
+        <translation>Benutze Klänge</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="18"/>
         <source>Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Thema</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="21"/>
         <source>Sound theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Klang-Thema</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="24"/>
         <source>Sound paths</source>
-        <translation type="unfinished"></translation>
+        <translation>Pfad zu den Klängen</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="34"/>
         <source>Volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Lautstärke</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="31"/>
         <source>Enable volume control (player must support it)</source>
-        <translation type="unfinished"></translation>
+        <translation>Lautstärkekontolle benutzen (wenn das Abspiel-Programm dies unterstützt)</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="38"/>
         <source>Samples</source>
-        <translation type="unfinished"></translation>
+        <translation>Testklänge</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="41"/>
         <source>Test sample playing</source>
-        <translation type="unfinished"></translation>
+        <translation>Testklang abspielen</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="44"/>
         <source>Test sample recording</source>
-        <translation type="unfinished"></translation>
+        <translation>Testklang-Aufnahme</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="47"/>
         <source>Test full duplex</source>
-        <translation type="unfinished"></translation>
+        <translation>Teste full-duplex</translation>
     </message>
 </context>
 <context>
     <name>SoundConfigurationWidget</name>
     <message>
-        <location filename="../sound_slots.cpp" line="33"/>
         <source>Test</source>
         <translation>Test</translation>
     </message>
     <message>
-        <location filename="../sound_slots.cpp" line="37"/>
         <source>Sound file</source>
         <translation>Klangdatei</translation>
     </message>
@@ -84,7 +68,6 @@
 <context>
     <name>SoundManager</name>
     <message>
-        <location filename="../sound.cpp" line="280"/>
         <source>Custom</source>
         <translation>Eigene Einstellungen</translation>
     </message>
@@ -92,54 +75,44 @@
 <context>
     <name>SoundSlots</name>
     <message>
-        <location filename="../sound_slots.cpp" line="90"/>
         <source>Unmute sounds</source>
         <translation>Klänge einschalten</translation>
     </message>
     <message>
-        <location filename="../sound_slots.cpp" line="89"/>
         <source>Mute sounds</source>
         <translation>Klänge ausschalten</translation>
     </message>
     <message>
-        <location filename="../sound_slots.cpp" line="145"/>
         <source>Opening test sample file failed.</source>
         <translation>Öffnen des Beispielklangs fehlgeschlagen.</translation>
     </message>
     <message>
-        <location filename="../sound_slots.cpp" line="152"/>
         <source>Reading test sample file failed.</source>
         <translation>Lesen des Beispielklangs fehlgeschlagen.</translation>
     </message>
     <message>
-        <location filename="../sound_slots.cpp" line="273"/>
         <source>Opening sound device failed.</source>
         <translation>Öffnen des Sound-Gerätes fehlgeschlagen.</translation>
     </message>
     <message>
-        <location filename="../sound_slots.cpp" line="173"/>
         <source>Testing sample playing. You should hear some sound now.</source>
         <translation>Testklang wird abgespielt. Sie sollten jetzt einen Klang hören.</translation>
     </message>
     <message>
-        <location filename="../sound_slots.cpp" line="212"/>
         <source>Testing sample recording. Please talk now (3 seconds).</source>
-        <translation>Test der Klangaufnahem. Sprechen Sie bitte jetzt (3 Sekunden lang).</translation>
+        <translation>Aufnahme des Testklangs. Sprechen Sie bitte jetzt (3 Sekunden lang).</translation>
     </message>
     <message>
-        <location filename="../sound_slots.cpp" line="240"/>
         <source>You should hear your recorded sample now.</source>
         <translation>Jetzt sollten Sie die Aufnahme hören.</translation>
     </message>
     <message>
-        <location filename="../sound_slots.cpp" line="281"/>
         <source>Testing fullduplex. Please talk now.
 You should here it with one second delay.</source>
-        <translation>Teste full duplex. Bitte Sprechen.
+        <translation>Teste full duplex. Bitte sprechen Sie.
 Sie sollten sich mir einer Sekunde Verspätung hören.</translation>
     </message>
     <message>
-        <location filename="../sound_slots.cpp" line="233"/>
         <source>Cannot open sound device for playing!</source>
         <translation>Kann das Sound-Gerät für&apos;s Abspielen nicht öffnen!</translation>
     </message>
