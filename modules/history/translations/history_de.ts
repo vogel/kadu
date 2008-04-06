2140130c049f8918e97e2606cc50b07ea5973d35
Roman Krzystyniak <Ron_K@tlen.pl>
2008-04-06 19:37:53 +0000

    tlumaczenia de
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@5031 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- modules/history/translations/history_de.ts
+++ modules/history/translations/history_de.ts
@@ -1,277 +1,227 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS><TS>
 <context>
     <name>@default</name>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="53"/>
         <source>Chat</source>
-        <translation type="unfinished"></translation>
+        <translation>Chat</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="6"/>
         <source>History</source>
-        <translation type="unfinished">Verlauf</translation>
+        <translation>Verlauf</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="9"/>
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>Allgemein</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="12"/>
         <source>Save history</source>
-        <translation type="unfinished"></translation>
+        <translation>Verlauf speichern</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="14"/>
         <source>Save messages in history</source>
-        <translation type="unfinished"></translation>
+        <translation>Speichere Nachrichten im Verlauf</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="18"/>
         <source>Status changes</source>
-        <translation type="unfinished"></translation>
+        <translation>Zustandsänderungen</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="21"/>
         <source>Don&apos;t show status changes</source>
-        <translation type="unfinished"></translation>
+        <translation>Zeige keine Zustandsänderungen</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="23"/>
         <source>Don&apos;t show status changes in history</source>
-        <translation type="unfinished"></translation>
+        <translation>Zeige keine Zustandsänderungen im Verlauf</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="26"/>
         <source>Don&apos;t save status changes</source>
-        <translation type="unfinished"></translation>
+        <translation>Speichere keine Zusandsänderungen</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="28"/>
         <source>Don&apos;t save status changes in history</source>
-        <translation type="unfinished"></translation>
+        <translation>Speichere keine Zusandsänderungen im Verlauf</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="32"/>
         <source>Citation</source>
-        <translation type="unfinished"></translation>
+        <translation>Zitation</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="35"/>
         <source>Message citation in chat window</source>
-        <translation type="unfinished"></translation>
+        <translation>Anzeige alter Nachrichten im Chat-Fenster</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="38"/>
         <source>Don&apos;t cite messages older than</source>
-        <translation type="unfinished"></translation>
+        <translation>Zeige keine Nachrichten, die älter sind als</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="41"/>
         <source>.</source>
-        <translation type="unfinished"></translation>
+        <translation>.</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="47"/>
         <source>Shortcuts</source>
-        <translation type="unfinished"></translation>
+        <translation>Kürzel</translation>
     </message>
     <message>
-        <location filename="../.configuration-ui-translations.cpp" line="56"/>
         <source>View history</source>
-        <translation type="unfinished"></translation>
+        <translation>Zeige Verlauf</translation>
     </message>
 </context>
 <context>
     <name>HistoryDialog</name>
     <message>
-        <location filename="../history_dialog.cpp" line="77"/>
         <source>History</source>
         <translation>Verlauf</translation>
     </message>
     <message>
-        <location filename="../history_dialog.cpp" line="98"/>
         <source>&amp;Find</source>
         <translation>&amp;Suche</translation>
     </message>
     <message>
-        <location filename="../history_dialog.cpp" line="99"/>
         <source>Find &amp;next</source>
         <translation>Suche &amp;nächstes Vorkommen</translation>
     </message>
     <message>
-        <location filename="../history_dialog.cpp" line="100"/>
         <source>Find &amp;previous</source>
         <translation>Suche &amp;vorheriges Vorkommen</translation>
     </message>
     <message>
-        <location filename="../history_dialog.cpp" line="216"/>
         <source>Online</source>
         <translation>Erreichbar</translation>
     </message>
     <message>
-        <location filename="../history_dialog.cpp" line="220"/>
         <source>Busy</source>
         <translation>Beschäftigt</translation>
     </message>
     <message>
-        <location filename="../history_dialog.cpp" line="224"/>
         <source>Invisible</source>
         <translation>Versteckt</translation>
     </message>
     <message>
-        <location filename="../history_dialog.cpp" line="228"/>
         <source>Offline</source>
         <translation>Unerreichbar</translation>
     </message>
     <message>
-        <location filename="../history_dialog.cpp" line="84"/>
         <source>Uins</source>
         <translation>GG-Nummern</translation>
     </message>
     <message>
-        <location filename="../history_dialog.cpp" line="91"/>
         <source>Show status changes</source>
-        <translation type="unfinished"></translation>
+        <translation>Zeige Zustandsänderungen</translation>
     </message>
     <message>
-        <location filename="../history_dialog.cpp" line="231"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Unbekannt</translation>
     </message>
 </context>
 <context>
     <name>HistoryManager</name>
     <message>
-        <location filename="../history.cpp" line="324"/>
         <source>Clear history?</source>
-        <translation type="unfinished"></translation>
+        <translation>Verlauf leeren?</translation>
     </message>
     <message>
-        <location filename="../history.cpp" line="325"/>
         <source>Yes</source>
-        <translation type="unfinished"></translation>
+        <translation>Ja</translation>
     </message>
     <message>
-        <location filename="../history.cpp" line="325"/>
         <source>No</source>
-        <translation type="unfinished"></translation>
+        <translation>Nein</translation>
     </message>
 </context>
 <context>
     <name>HistoryModule</name>
     <message>
-        <location filename="../history_module.cpp" line="75"/>
         <source>Show history</source>
         <translation>Zeige Verlauf</translation>
     </message>
     <message>
-        <location filename="../history_module.cpp" line="335"/>
         <source>History</source>
-        <translation type="unfinished">Verlauf</translation>
+        <translation>Verlauf</translation>
     </message>
     <message>
-        <location filename="../history_module.cpp" line="336"/>
         <source>Clear history</source>
-        <translation type="unfinished">Verlauf leeren</translation>
+        <translation>Verlauf leeren</translation>
     </message>
     <message>
-        <location filename="../history_module.cpp" line="140"/>
         <source>%1 day(s) %2 hour(s)</source>
-        <translation type="unfinished">%1 Tage %2 Stunden</translation>
+        <translation>%1 Tage %2 Stunden</translation>
     </message>
     <message>
-        <location filename="../history_module.cpp" line="354"/>
         <source>The following users were deleted:
-%0Do you want to remove history as well?</source>
-        <translation type="unfinished"></translation>
+%0
+Do you want to remove history as well?</source>
+        <translation>Die folgenen Kontate werden entfernt:
+%0
+Wollen Sie den Verlauf dieser leeren?</translation>
     </message>
 </context>
 <context>
     <name>HistorySearchDialog</name>
     <message>
-        <location filename="../history_search_dialog.cpp" line="39"/>
         <source>Search history</source>
         <translation>Suche im Verlauf</translation>
     </message>
     <message>
-        <location filename="../history_search_dialog.cpp" line="67"/>
         <source>&amp;From:</source>
         <translation>&amp;Von:</translation>
     </message>
     <message>
-        <location filename="../history_search_dialog.cpp" line="90"/>
         <source>day</source>
         <translation>Tag</translation>
     </message>
     <message>
-        <location filename="../history_search_dialog.cpp" line="93"/>
         <source>month</source>
         <translation>Monat</translation>
     </message>
     <message>
-        <location filename="../history_search_dialog.cpp" line="96"/>
         <source>year</source>
         <translation>Jahr</translation>
     </message>
     <message>
-        <location filename="../history_search_dialog.cpp" line="99"/>
         <source>hour</source>
         <translation>Stunde</translation>
     </message>
     <message>
-        <location filename="../history_search_dialog.cpp" line="102"/>
         <source>minute</source>
         <translation>Minute</translation>
     </message>
     <message>
-        <location filename="../history_search_dialog.cpp" line="86"/>
         <source>&amp;To:</source>
         <translation>&amp;Bis:</translation>
     </message>
     <message>
-        <location filename="../history_search_dialog.cpp" line="104"/>
         <source>Find Criteria</source>
         <translation>Suchkriterien</translation>
     </message>
     <message>
-        <location filename="../history_search_dialog.cpp" line="105"/>
         <source>&amp;Pattern</source>
         <translation>&amp;Muster</translation>
     </message>
     <message>
-        <location filename="../history_search_dialog.cpp" line="106"/>
         <source>&amp;Status</source>
         <translation>&amp;Zustand</translation>
     </message>
     <message>
-        <location filename="../history_search_dialog.cpp" line="112"/>
         <source>Pattern</source>
         <translation>Muster</translation>
     </message>
     <message>
-        <location filename="../history_search_dialog.cpp" line="114"/>
         <source>Status</source>
         <translation>Zustand</translation>
     </message>
     <message>
-        <location filename="../history_search_dialog.cpp" line="119"/>
         <source>&amp;Reverse find</source>
         <translation>&amp;Suche rückwärts</translation>
     </message>
     <message>
-        <location filename="../history_search_dialog.cpp" line="121"/>
         <source>&amp;Find</source>
         <translation>&amp;Suche</translation>
     </message>
     <message>
-        <location filename="../history_search_dialog.cpp" line="122"/>
         <source>Reset</source>
         <translation>Zurücksetzen</translation>
     </message>
     <message>
-        <location filename="../history_search_dialog.cpp" line="123"/>
         <source>&amp;Cancel</source>
         <translation>&amp;Abbruch</translation>
     </message>
