e3e19f12d980d6afcf7ba4fde641f5f0fdeaf131
Rafał Malinowski <malinowskirafal@wp.pl>
2008-04-27 23:09:25 +0000

    porzadki w "action.h"
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@5094 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- kadu-core/action.h
+++ kadu-core/action.h
@@ -1,18 +1,8 @@
 #ifndef ACTION_H
 #define ACTION_H
 
-#include <qglobal.h>
-
-#include <q3mainwindow.h>
-#include <qmap.h>
-#include <q3popupmenu.h>
-#include <qstring.h>
-#include <q3valuelist.h>
-//Added by qt3to4:
 #include <QAction>
-#include <QPixmap>
-
-#include "usergroup.h"
+#include <QIcon>
 
 class ChatWidget;
 class KaduMainWindow;
@@ -91,98 +81,6 @@ public:
 	ActionType type() { return Type; }
 
 };
-
-// class Action : public QAction
-// {
-// 	Q_OBJECT
-// 
-// public:
-/*
-private:
-	QString IconName;
-	QString Text;
-	QKeySequence KeySeq0, KeySeq1; //!< skróty klawiszowe wywo³uj±ce akcjê
-	bool ToggleAction;
-	QString OnIcon;
-	QString OnText;
-	const char *Slot;
-	QList<ToolButton*> ToolButtons;
-	bool ToggleState;
-	ActionType Type;*/
-
-// 	Action(const Action &) {}
-// 	Action &operator = (const Action &) {return *this;}
-
-// private slots:
-// 	void toolButtonClicked();
-// 	void toolButtonDestroyed(QObject* obj);
-
-// public:
-// 	Action(const QString& icon, const QString& text, const char *name, ActionType Type,
-// 		QKeySequence Seq0 = QKeySequence(), QKeySequence Seq1 = QKeySequence());
-// 	virtual ~Action();
-
-// 	QString iconName() { return IconName; }
-// 	QString text() { return Text; }
-// 	bool toggleAction() { return ToggleAction; }
-// 	bool toggleState() { return ToggleState; }
-// 	QString onIcon() { return OnIcon; }
-// 	QString onText() { return OnText; }
-// 	ActionType type() { return Type; }
-// 	QKeySequence keySeq0() { return KeySeq0; }
-// 	QKeySequence keySeq1() { return KeySeq1; }
-
-// 	void setToggleAction(bool toggle);
-	/**
-		action works just like toggled but using two shapes
-		(pictures and texts)
-	**/
-// 	void setOnShape(const QString& icon, const QString& text);
-// 	void buttonAddedToToolbar(ToolBar *toolBar, ToolButton *button);
-// 	int addToPopupMenu(Q3PopupMenu* menu, bool connect_signal = true);
-// 	QList<ToolButton*> toolButtonsForUserListElements(
-// 		const UserListElements& users);
-
-//  	bool isChecked(const UserListElements &users);
-//  	void setChecked(const UserListElements &users, bool on);
-// 	void setAllChecked(bool on);
-//	void setPixmaps(const QPixmap& pixmap);
-// 	void setPixmaps(const UserListElements& users, const QPixmap& pixmap);
-// 	void setIcons(const UserListElements& users, const QIcon& icon);
-// 	void refreshIcons();
-// 	void setTexts(const UserListElements& users, const QString& text);
-	/**
-		Sets enabled state of all buttons in dockareas that has specified
-		parent.
-	**/
-// 	void setEnabled(QWidget* parent, bool enabled);
-	/**
-		Sets slot of dockarea's parent that will be called when action
-		is activated. You should ensure that class of parent's class
-		is known using setDockAreaGroupRestriction().
-	**/
-// 	void setSlot(const char *slot);
-	/**
-		Activate action
-	**/
-// 	void activate(const UserGroup* users);
-// 
-// 	ActionType actionType() const;
-
-// signals:
-// 	void addedToToolbar(const UserGroup* users, ToolButton* button, ToolBar* toolbar);
-// 	void addedToToolbar(ToolButton* button, ToolBar* toolbar);
-
-	/**
-		is_on jest zawsze ustawione na false jesli akcja nie jest typu "toggle".
-	**/
-// 	void activated(const UserGroup* users, const QWidget* source, bool is_on);
-
-	/**
-		Sygna³ jest emitowany po zmianie zestawu ikon
-	**/
-// 	void iconsRefreshed();
-// };
 
 class Actions : public QObject
 {
--- kadu-core/kadu_main_window.h
+++ kadu-core/kadu_main_window.h
@@ -14,6 +14,7 @@
 #include <QMainWindow>
 
 #include "action.h"
+#include "userlist.h"
 
 class QContextMenuEvent;
 
