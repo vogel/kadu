22ef24d92d90018a14a741dbf5f83ac6ff1f12eb
Tomasz Rostański <rozteck@interia.pl>
2008-09-24 21:01:07 +0000

    aktualizacja skryptu create_macosx_bundle.sh (mathel)
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@5594 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- varia/scripts/create_macosx_bundle.sh
+++ varia/scripts/create_macosx_bundle.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 # destination directory, Kadu.app will be created here
 # katalog docelowy, w nim zostanie utworzony katalog Kadu.app
-DEST=~/Desktop
+DEST=~/Desktop/kadu
 
-QTDIR=~/Desktop/root/qt
+QTDIR=/usr/local/Trolltech/Qt-4.4.1
+LQTDIR=~/Desktop/kadu/qt
 
 # prefix of compiled libsndfile and openssl
-# prefiks skompilowanych bibliotek libsndfile i openssl
-SNDFILEPATH=~/Desktop/root/libsndfile
+# prefiks skompilowanych bibliotek libsndfile i libgadu
+SNDFILEPATH=~/Desktop/kadu/libsndfile
+LIBGADU=~/Desktop/kadu/libgadu
 
 #OPENSSLPATH=~/Desktop/root/openssl
 OPENSSLPATH=/usr
 # version of openssl, without letters
 # wersja openssla, bez liter
-SSLVER=0.9.7
+SSLVER=0.9.8
 
 #libao nie radzi sobie z mackiem jak trzeba
 #AOPATH=~/Desktop/root/libao
 #AOVER=2.1.3
 
-INSTALLED_DIR=~/Desktop/root/kadu
+INSTALLED_DIR=/usr/local/
 
 ICONS_PKG=ng_big_icons.tar.bz2
 ICONS_LINK=http://www.kadu.net/~neeo/kadu/macosx/$ICONS_PKG
@@ -37,11 +39,11 @@ if [ ! -f VERSION ]; then
 	exit
 fi
 
-if [ ! -f ./kadu-core/kadu ]; then
-	echo "compile Kadu first!"
-	echo "wpierw skompiluj Kadu!"
-	exit
-fi
+#if [ ! -f ./kadu-core/kadu ]; then
+#	echo "compile Kadu first!"
+#	echo "wpierw skompiluj Kadu!"
+#	exit
+#fi
 
 if [ ! -f ${INSTALLED_DIR}/bin/kadu ]; then
 	echo "do make install first!"
@@ -49,7 +51,7 @@ if [ ! -f ${INSTALLED_DIR}/bin/kadu ]; then
 	exit
 fi
 
-if [ ! -f ${QTDIR}/include/qwidget.h ]; then
+if [ ! -f ${QTDIR}/include/Qt/qwidget.h ]; then
 	echo "wrong QTDIR"
 	echo "QTDIR zle ustawione"
 	exit
@@ -129,8 +131,8 @@ fi
 
 RSC_DIR=${CNT_DIR}/Resources
 mkdir ${RSC_DIR}
-cp kadu/kadu.icns $RSC_DIR/
-cp kadu/hi48-app-kadu.png ${DEST}/Kadu.app/kadu/kadu.png
+cp kadu.icns $RSC_DIR/
+cp hi48-app-kadu.png ${DEST}/Kadu.app/kadu/kadu.png
 
 FM_DIR=${CNT_DIR}/Frameworks
 mkdir ${FM_DIR}
@@ -144,10 +146,52 @@ if [ -f ${OPENSSLPATH}/lib/libcrypto.${SSLVER}.dylib ]; then
 	install_name_tool -id @executable_path/../Frameworks/libcrypto.${SSLVER}.dylib ${FM_DIR}/libcrypto.${SSLVER}.dylib
 fi
 
-if [ -f ${QTDIR}/lib/libqt-mt.3.dylib ]; then
+#if [ -f ${QTDIR}/lib/libQtCLucene.4.4.1.dylib ]; then
+#	echo "log: copying qt library"
+#	cp ${QTDIR}/lib/libQtCLucene.4.4.1.dylib ${FM_DIR}
+#	install_name_tool -id @executable_path/../Frameworks/libQtCLucene.4.4.1.dylib ${FM_DIR}/libQtCLucene.4.4.1.dylib
+#fi
+
+if [ -f ${QTDIR}/lib/Qt3Support.framework/versions/4/Qt3Support ]; then
 	echo "log: copying qt library"
-	cp ${QTDIR}/lib/libqt-mt.3.dylib ${FM_DIR}
-	install_name_tool -id @executable_path/../Frameworks/libqt-mt.3.dylib ${FM_DIR}/libqt-mt.3.dylib
+	cp ${QTDIR}/lib/Qt3Support.framework/versions/4/Qt3Support ${FM_DIR}
+	install_name_tool -id @executable_path/../Frameworks/Qt3Support ${FM_DIR}/Qt3Support
+fi
+
+if [ -f ${QTDIR}/lib/QtGui.framework/versions/4/QtGui ]; then
+	echo "log: copying qt library"
+	cp ${QTDIR}/lib/QtGui.framework/versions/4/QtGui ${FM_DIR}
+	install_name_tool -id @executable_path/../Frameworks/QtGui ${FM_DIR}/QtGui
+fi
+
+if [ -f ${LQTDIR}/lib/QtWebKit.framework/versions/4/QtWebKit ]; then
+	echo "log: copying qt library"
+	cp ${LQTDIR}/lib/QtWebKit.framework/versions/4/QtWebKit ${FM_DIR}
+	install_name_tool -id @executable_path/../Frameworks/QtWebKit ${FM_DIR}/QtWebKit
+fi
+
+if [ -f ${QTDIR}/lib/QtXml.framework/versions/4/QtXml ]; then
+	echo "log: copying qt library"
+	cp ${QTDIR}/lib/QtXml.framework/versions/4/QtXml ${FM_DIR}
+	install_name_tool -id @executable_path/../Frameworks/QtXml ${FM_DIR}/QtXml
+fi
+
+if [ -f ${QTDIR}/lib/QtSql.framework/versions/4/QtSql ]; then
+	echo "log: copying qtSql library"
+	cp ${QTDIR}/lib/QtSql.framework/versions/4/QtSql ${FM_DIR}
+	install_name_tool -id @executable_path/../Frameworks/QtSql ${FM_DIR}/QtSql
+fi
+
+if [ -f ${QTDIR}/lib/QtNetwork.framework/versions/4/QtNetwork ]; then
+	echo "log: copying qt library"
+	cp ${QTDIR}/lib/QtNetwork.framework/versions/4/QtNetwork ${FM_DIR}
+	install_name_tool -id @executable_path/../Frameworks/QtNetwork ${FM_DIR}/QtNetwork	
+fi
+
+if [ -f ${LQTDIR}/lib/QtCore.framework/versions/4/QtCore ]; then
+	echo "log: copying qt library"
+	cp ${LQTDIR}/lib/QtCore.framework/versions/4/QtCore ${FM_DIR}
+	install_name_tool -id @executable_path/../Frameworks/QtWebKit ${FM_DIR}/QtCore
 fi
 
 if [ -f ${SNDFILEPATH}/lib/libsndfile.1.dylib ]; then
@@ -156,9 +200,41 @@ if [ -f ${SNDFILEPATH}/lib/libsndfile.1.dylib ]; then
 	install_name_tool -id @executable_path/../Frameworks/libsndfile.1.dylib ${FM_DIR}/libsndfile.1.dylib
 fi
 
+if [ -f ${LIBGADU}/lib/libgadu.3.dylib ]; then
+	echo "log: copying libgadu library"
+	cp ${LIBGADU}/lib/libgadu.3.dylib ${FM_DIR}
+	install_name_tool -id @executable_path/../Frameworks/libgadu.3.dylib ${FM_DIR}/libgadu.3.dylib
+fi
+
+cd ${FM_DIR}
+install_name_tool -change ${QTDIR}/lib/QtSql.framework/Versions/4/QtSql @executable_path/../Frameworks/QtSql ./QtCore
+install_name_tool -change ${QTDIR}/lib/QtSql.framework/Versions/4/QtSql @executable_path/../Frameworks/QtSql ./Qt3Support
+install_name_tool -change ${QTDIR}/lib/QtXml.framework/Versions/4/QtXml @executable_path/../Frameworks/QtXml ./Qt3Support
+install_name_tool -change ${QTDIR}/lib/QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui ./Qt3Support
+install_name_tool -change ${QTDIR}/lib/QtNetwork.framework/Versions/4/QtNetwork @executable_path/../Frameworks/QtNetwork ./Qt3Support
+install_name_tool -change ${QTDIR}/lib/QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore ./Qt3Support
+install_name_tool -change ${QTDIR}/lib/QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore ./QtSql
+install_name_tool -change ${QTDIR}/lib/QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore ./QtXml
+install_name_tool -change ${QTDIR}/lib/QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore ./QtGui
+install_name_tool -change ${QTDIR}/lib/QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore ./QtWebKit
+install_name_tool -change ${LQTDIR}/lib/QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore ./QtWebKit
+install_name_tool -change ${QTDIR}/lib/QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore ./QtNetwork
+install_name_tool -change ${QTDIR}/lib/QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui ./QtWebKit
+install_name_tool -change ${QTDIR}/lib/QtNetwork.framework/Versions/4/QtNetwork @executable_path/../Frameworks/QtNetwork ./QtWebKit
+
 cd ${MACOS_DIR}
 echo "log: changing library bindings"
-install_name_tool -change libqt-mt.3.dylib @executable_path/../Frameworks/libqt-mt.3.dylib ./kadu
+install_name_tool -change ${QTDIR}/lib/QtSql.framework/Versions/4/QtSql @executable_path/../Frameworks/QtSql ./kadu
+install_name_tool -change ${LQTDIR}/lib/QtSql.framework/Versions/4/QtSql @executable_path/../Frameworks/QtSql ./kadu
+install_name_tool -change ${QTDIR}/lib/QtNetwork.framework/Versions/4/QtNetwork @executable_path/../Frameworks/QtNetwork ./kadu
+install_name_tool -change ${QTDIR}/lib/QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui ./kadu
+install_name_tool -change ${QTDIR}/lib/QtXml.framework/Versions/4/QtXml @executable_path/../Frameworks/QtXml ./kadu
+install_name_tool -change ${QTDIR}/lib/Qt3Support.framework/Versions/4/Qt3Support @executable_path/../Frameworks/Qt3Support ./kadu
+install_name_tool -change ${LQTDIR}/lib/QtWebKit.framework/Versions/4/QtWebKit @executable_path/../Frameworks/QtWebKit ./kadu
+install_name_tool -change ${LQTDIR}/lib/QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore ./kadu
+
+#install_name_tool -change libQtCLucene.4.4.1.dylib @executable_path/../Frameworks/libQtCLucene.4.4.1.dylib ./kadu
+install_name_tool -change ${LIBGADU}/lib/libgadu.3.dylib @executable_path/../Frameworks/libgadu.3.dylib ./kadu
 install_name_tool -change ${SNDFILEPATH}/lib/libsndfile.1.dylib @executable_path/../Frameworks/libsndfile.1.dylib ./kadu
 install_name_tool -change ${OPENSSLPATH}/lib/libssl.${SSLVER}.dylib @executable_path/../Frameworks/libssl.${SSLVER}.dylib ./kadu
 install_name_tool -change ${OPENSSLPATH}/lib/libcrypto.${SSLVER}.dylib @executable_path/../Frameworks/libcrypto.${SSLVER}.dylib ./kadu
