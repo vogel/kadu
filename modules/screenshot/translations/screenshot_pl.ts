5981c24290b62f47228d04af7f1ba53ce0935419
Rafał Malinowski <malinowskirafal@wp.pl>
2008-10-01 23:54:41 +0000

    screenshot module ported
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/branches/0.6.5@5632 fea5205d-6df5-0310-bef7-ea67e47da9a5

---
+++ modules/screenshot/translations/screenshot_pl.ts
@@ -0,0 +1,206 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="pl">
+<defaultcodec></defaultcodec>
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="15"/>
+        <source>File format</source>
+        <translation>Format pliku</translation>
+    </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="17"/>
+        <source>Select image format, which screenshots will be stored and sent in.</source>
+        <translation>Wybierz format obrazka, w jakim ma zostać zapisany zrzut.</translation>
+    </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="27"/>
+        <source>-1 for autodetection, 0 for best file compression, 100 for best image quality.</source>
+        <translation>-1 to autodetekcja, 0 to najlepsza kompresja, 100 to najlepsza jakość.</translation>
+    </message>
+    <message>
+        <location filename="" line="7471221"/>
+        <source>Screnshots directory path</source>
+        <translation type="obsolete">Ścieżka do katalogu zrzutów</translation>
+    </message>
+    <message>
+        <location filename="" line="7471221"/>
+        <source>Screnshots filename prefix</source>
+        <translation type="obsolete">Prefiks nazwy pliku zrzutów</translation>
+    </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="40"/>
+        <source>Screenshots directory path</source>
+        <translation>Ścieżka do katalogu ze zrzutami</translation>
+    </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="43"/>
+        <source>Screenshots filename prefix</source>
+        <translation>Prefiks nazwy pliku zrzutów ekranu</translation>
+    </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="25"/>
+        <source>Screenshots quality</source>
+        <translation>Jakość obrazka</translation>
+    </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="30"/>
+        <source>Paste [IMAGE] clause automatically</source>
+        <translation>Wklejaj polecenie [IMAGE] automatycznie</translation>
+    </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="32"/>
+        <source>When enabled, module will automatically paste ready to send [IMAGE] clause in chat window.</source>
+        <translation>Kiedy włączone, moduł automatycznie wklei polecenie [IMAGE] do okna rozmowy, aby było gotowe do wysłania.</translation>
+    </message>
+    <message>
+        <location filename="" line="7471221"/>
+        <source>Shots directory size control</source>
+        <translation type="obsolete">Kontrola rozmiaru katalogu ze zrzutami</translation>
+    </message>
+    <message>
+        <location filename="" line="7471221"/>
+        <source>Enable size warnings</source>
+        <translation type="obsolete">Włącz ostrzeżenia o rozmiarze</translation>
+    </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="48"/>
+        <source>When enabled, module will check for shots directory size and warn you if it gets size specified below.</source>
+        <translation>Kiedy włączone, moduł będzie sprawdzał rozmiar katalogu zrzutów i ostrzeże cię, jeśli będzie on większy niż ten podany niżej.</translation>
+    </message>
+    <message>
+        <location filename="" line="7471221"/>
+        <source>Directory size to warn (in KBytes)</source>
+        <translation type="obsolete">Rozmiar katalogu dla ostrzeżeń (w KiloBajtach)</translation>
+    </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="53"/>
+        <source>Define, how many kbytes can shots take in the directory before module will warn you.</source>
+        <translation>Określa, ile kilobajtów mogą zajmować zrzuty w katalogu, zanim moduł cię ostrzeże.</translation>
+    </message>
+    <message>
+        <location filename="" line="7471221"/>
+        <source>Warning style</source>
+        <translation type="obsolete">Sposób ostrzegania</translation>
+    </message>
+    <message>
+        <location filename="" line="7471221"/>
+        <source>Hint mode means a standard Kadu hint. Dialog window mode means standart information window.</source>
+        <translation type="obsolete">Tryb ostrzegania dymkiem oznacza standardowy dymek Kadu. Okno dialogowe oznacza standardowe okno informacyjne.</translation>
+    </message>
+    <message>
+        <location filename="" line="7471221"/>
+        <source>Warn with hint</source>
+        <translation type="obsolete">Ostrzegaj dymkiem</translation>
+    </message>
+    <message>
+        <location filename="" line="7471221"/>
+        <source>Warn with dialog window</source>
+        <translation type="obsolete">Ostrzegaj oknem dialogowym</translation>
+    </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="20"/>
+        <source>Force .jpg extension for JPEG format</source>
+        <translation>Wymuś rozszerzenie .jpg dla formatu JPEG</translation>
+    </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="22"/>
+        <source>Forces to use short .jpg extension for files in JPEG format. Some other instant messagers needs it.</source>
+        <translation>Wymusza użycie krótkiego rozszerzenia .jpg dla plików w formacie JPEG. Niektóre komunikatory tego wymagają.</translation>
+    </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="4"/>
+        <source>Screenshot</source>
+        <translation>Screenshot</translation>
+    </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="8"/>
+        <source>General</source>
+        <translation>Ogólne</translation>
+    </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="12"/>
+        <source>General settings</source>
+        <translation>Ogólne</translation>
+    </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="37"/>
+        <source>Directory</source>
+        <translation>Katalog</translation>
+    </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="46"/>
+        <source>Enable directory size control</source>
+        <translation>Kontroluj rozmiar katalogu</translation>
+    </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="51"/>
+        <source>Maximum directory size</source>
+        <translation>Maksymalny rozmiar katalogu</translation>
+    </message>
+</context>
+<context>
+    <name>ScreenShot</name>
+    <message>
+        <location filename="" line="7471221"/>
+        <source>Send ScreenShot</source>
+        <translation type="obsolete">Wyślij zrzut</translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="317"/>
+        <source>Image size is bigger than maximal image size
+set by all of conference contacts.</source>
+        <translation>Rozmiar obrazka większy niż maksymalny rozmiar obrazka
+ustawiony przez wszystkich uczestników konferencji.</translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="325"/>
+        <source>Image size is bigger than maximal image size
+set by some of conference contacts:
+%1.</source>
+        <translation>Rozmiar obrazka większy niż maksymalny rozmiar obrazka
+ustawiony przez niektórych uczestników konferencji:
+%1.</translation>
+    </message>
+    <message>
+        <location filename="" line="7471221"/>
+        <source>Image size is bigger than maximal image size set by %1.</source>
+        <translation type="obsolete">Rozmiar obrazka większy niż maksymalny rozmiar obrazka ustawiony przez %1.</translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="337"/>
+        <source>Image size is bigger than maximal image size set by %1. Send it anyway?</source>
+        <translation>Rozmiar obrazka większy niż maksymalny rozmiar ustawiony przez %1. Wysłać mimo to?</translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="107"/>
+        <source>Simple shot</source>
+        <translation>Zwykły zrzut</translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="108"/>
+        <source>With chat window hidden</source>
+        <translation>Z ukrytym oknem rozmowy</translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="115"/>
+        <source>ScreenShot</source>
+        <translation>ScreenShot</translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="109"/>
+        <source>Window shot</source>
+        <translation>Zrzut okna</translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="500"/>
+        <source>ScreenShot size limit</source>
+        <translation>Limit rozmiaru zrzutów</translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="501"/>
+        <source>Images size limit exceed: %1 KB</source>
+        <translation>Limit rozmiaru zrzutów został przekroczony: %1 KB</translation>
+    </message>
+</context>
+</TS>
