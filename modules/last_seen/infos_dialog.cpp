98f2d585d8f4735065a3da5fa2e6c054b8744471
Tomasz Rostański <rozteck@interia.pl>
2008-10-08 21:20:53 +0000

    Module last_seen added
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/branches/0.6.5@5671 fea5205d-6df5-0310-bef7-ea67e47da9a5

---
+++ modules/last_seen/infos_dialog.cpp
@@ -0,0 +1,72 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <QtCore/QStringList>
+#include <QtCore/QAbstractItemModel>
+#include <QtGui/QTreeWidget>
+#include <QtGui/QLayout>
+#include <QtGui/QPushButton>
+#include <QtNetwork/QHostAddress>
+
+#include "infos_dialog.h"
+
+#include "modules.h"
+#include "userlist.h"
+#include "debug.h"
+
+
+InfosDialog::InfosDialog(const LastSeen &lastSeen, QWidget *parent, const char *name, bool modal, Qt::WindowFlags f)
+:QDialog(parent, name, modal, f)
+{
+	kdebugf();
+	this->resize(800, this->height());
+	QVBoxLayout *mainLayout = new QVBoxLayout(this, 10, -1, "infos_dialog_main_layout");
+	QTreeWidget *listView = new QTreeWidget(this);
+	listView->setColumnCount(7);
+	listView->setSelectionMode(QAbstractItemView::NoSelection);
+	mainLayout->addWidget(listView);
+	QHBoxLayout *buttonsLayout = new QHBoxLayout(mainLayout, -1, "infos_dialog_buttons_layout");
+	listView->setAllColumnsShowFocus(true);
+	QStringList labels;
+	labels << "Contact" << "UIN" << "Nick" << "IP" << "Domain name" << "Description" << "Last time seen on";
+	listView->setHeaderLabels(labels);
+	QPushButton *closeButton = new QPushButton(tr("&Close"), this, "infos close button");
+	buttonsLayout->addStretch();
+	buttonsLayout->addWidget(closeButton);
+	
+	for(UserList::const_iterator it = userlist->begin(); it != userlist->end(); ++it)
+	{
+		if((*it).usesProtocol("Gadu"))
+		{
+			QString desc, ip;
+			if((*it).status("Gadu").hasDescription())
+				desc = (*it).status("Gadu").description();
+			desc.replace('\n', ' ');
+			if((*it).hasIP("Gadu"))
+				ip = (*it).IP("Gadu").toString();
+
+			QStringList labels;
+			labels << (*it).altNick() << (*it).ID("Gadu") << (*it).nickName() << ip <<
+					(*it).DNSName("Gadu") << desc << lastSeen[(*it).ID("Gadu")];
+
+			listView->addTopLevelItem(new QTreeWidgetItem(labels));
+		}
+	}
+	listView->sortItems(0, Qt::AscendingOrder);
+	
+	connect(closeButton, SIGNAL(clicked()), this, SLOT(close()));
+
+	kdebugf2();
+}
+
+InfosDialog::~InfosDialog()
+{
+	kdebugf();
+	kdebugf2();
+}
---
+++ modules/last_seen/translations/last_seen_pl.ts
@@ -0,0 +1,9 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>InfosDialog</name>
+    <message>
+        <source>&amp;Close</source>
+        <translation>&amp;Zamknij</translation>
+    </message>
+</context>
+</TS>
