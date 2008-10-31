caef1eaa0d21739bfc0912dbd4ba321b16dcf6e2
Tomasz Rostański <rozteck@interia.pl>
2008-10-31 17:58:53 +0000

    Module growl_notify added to .config
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@5763 fea5205d-6df5-0310-bef7-ea67e47da9a5

---
+++ modules/qt4_docking_notify/translations/qt4_docking_notify_pl.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="pl_PL">
+<defaultcodec></defaultcodec>
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="../qt4_docking_notify.cpp" line="59"/>
+        <source>Tray Icon Baloon</source>
+        <translation>Balonik ikony w zasobniku</translation>
+    </message>
+</context>
+<context>
+    <name>Qt4NotifyConfigurationWidget</name>
+    <message>
+        <location filename="../qt4_docking_notify_configuration_widget.cpp" line="26"/>
+        <source>
+%&amp;t - title (eg. New message) %&amp;m - notification text (eg. Message from Jim), %&amp;d - details (eg. message quotation),
+%&amp;i - notification icon</source>
+        <translation>%&amp;t - tytuł (np. Nowa wiadomość) %&amp;m - treść powiadomienia (np. Wiadomość od Jasia), %&amp;d - szczegóły (np. treść wiadomości),%&amp;i - ikona powiadomienia</translation>
+    </message>
+    <message>
+        <location filename="../qt4_docking_notify_configuration_widget.cpp" line="40"/>
+        <source>NoIcon</source>
+        <translation>Brak ikony</translation>
+    </message>
+    <message>
+        <location filename="../qt4_docking_notify_configuration_widget.cpp" line="40"/>
+        <source>Information</source>
+        <translation>Informacja</translation>
+    </message>
+    <message>
+        <location filename="../qt4_docking_notify_configuration_widget.cpp" line="40"/>
+        <source>Warning</source>
+        <translation>Ostrzeżenie</translation>
+    </message>
+    <message>
+        <location filename="../qt4_docking_notify_configuration_widget.cpp" line="40"/>
+        <source>Critical</source>
+        <translation>Błąd</translation>
+    </message>
+    <message>
+        <location filename="../qt4_docking_notify_configuration_widget.cpp" line="50"/>
+        <source>Timeout</source>
+        <translation>Czas zaniku</translation>
+    </message>
+    <message>
+        <location filename="../qt4_docking_notify_configuration_widget.cpp" line="52"/>
+        <source>Notification Icon</source>
+        <translation>Ikona powiadomienia</translation>
+    </message>
+    <message>
+        <location filename="../qt4_docking_notify_configuration_widget.cpp" line="54"/>
+        <source>Title</source>
+        <translation>Tytuł</translation>
+    </message>
+    <message>
+        <location filename="../qt4_docking_notify_configuration_widget.cpp" line="56"/>
+        <source>Syntax</source>
+        <translation>Treść</translation>
+    </message>
+</context>
+</TS>
