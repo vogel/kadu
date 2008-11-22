6c3dd793c78bf6a5bb0c3e4a4aadc63b298b6c42
Tomasz Rostański <rozteck@interia.pl>
2008-11-22 09:59:27 +0000

    Add translation to macosx docking module
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/branches/0.6.5@5842 fea5205d-6df5-0310-bef7-ea67e47da9a5

---
+++ modules/macosx_docking/translations/macosx_docking_it.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="4"/>
+        <source>Kadu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="7"/>
+        <source>Tray</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="10"/>
+        <source>MacOSX Dock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="13"/>
+        <source>Icon Notification</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
