1f5a46787164e4733a651141ff0b259589388ff5
Tomasz Rostański <rozteck@interia.pl>
2008-12-11 18:09:01 +0000

    Revert commit r5881 - Use coherent font size in entire chat window
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@5893 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- kadu-core/chat_messages_view.cpp
+++ kadu-core/chat_messages_view.cpp
@@ -244,11 +244,15 @@ void ChatMessagesView::configurationUpdated()
 	QString fontFamily = font.family();
 	QString fontSize;
 	if (font.pointSize() > 0)
-		/*  Dorr: On all systems this font is being displayed 3pts larger than
+#ifdef Q_OS_MAC
+		/*  Dorr: On MacOSX this font is being displayed 3pts larger than
 		 *  it really is, so reduce it's size to be coherent in entire
 		 *  application.
 		 */
 		fontSize = QString::number(font.pointSize()-3) + "pt";
+#else
+		fontSize = QString::number(font.pointSize()) + "pt";
+#endif
 	else
 		fontSize = QString::number(font.pixelSize()) + "px";
 	QString fontStyle = font.italic() ? "italic" : "normal";
