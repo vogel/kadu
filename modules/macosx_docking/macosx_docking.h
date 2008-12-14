877230d0c64b200b68fed772fbddea7d2d495a35
Tomasz Rostański <rozteck@interia.pl>
2008-12-14 20:17:24 +0000

    Bouncing dock icon and new message number indication on Mac OS X
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/branches/0.6.5@5914 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- modules/macosx_docking/macosx_docking.h
+++ modules/macosx_docking/macosx_docking.h
@@ -7,6 +7,9 @@
 #include "configuration_aware_object.h"
 #include "main_configuration_window.h"
 
+class UserListElement;
+class QPixmap;
+
 /**
  * @defgroup mac_os_docking MacOS docking
  * @{
@@ -14,16 +17,25 @@
 class MacOSXDocking : public ConfigurationUiHandler
 {
 	Q_OBJECT
+	private:
+		bool isBouncing;
+		bool overlayed;
+		QPixmap pixmap;
+
+		void startBounce();
+		void stopBounce();
+		void overlay(const QString& text);
 	private slots:
 		void blockSettingIcon(bool &);
 		void trayPixmapChanged(const QIcon &, const QString &);
 		void findTrayPosition(QPoint &);
 		void onCreateTabGeneral();
 		void toggleKaduVisibility();
+		void messageListChanged(UserListElement ule);
 	public:
 		MacOSXDocking(QObject *parent = 0, const char *name = 0);
 		~MacOSXDocking();
-		
+
 		virtual void mainConfigurationWindowCreated(MainConfigurationWindow *mainConfigurationWindow);
 };
 
--- modules/macosx_docking/translations/macosx_docking_pl.ts
+++ modules/macosx_docking/translations/macosx_docking_pl.ts
@@ -23,5 +23,15 @@
         <source>Icon Notification</source>
         <translation>Powiadamianie ikoną</translation>
     </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="13"/>
+        <source>Bounce Dock Icon on New Message</source>
+        <translation>Podskakiwanie ikony przy nadejsciu nowej wiadomosci</translation>
+    </message>
+    <message>
+        <location filename="../.configuration-ui-translations.cpp" line="13"/>
+        <source>Display New Messages Count in Dock Icon</source>
+        <translation>Pokazuj liczbe nowych wiadomosci</translation>
+    </message>
 </context>
 </TS>
