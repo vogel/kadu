9ee9065547d27acb215cb8848665ff75c2ad71bd
Tomasz Rostański <rozteck@interia.pl>
2008-12-14 14:36:02 +0000

    Do not allow the window to be displayed over the menu bar on Mac OS X
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/branches/0.6.5@5911 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- kadu-core/misc.cpp
+++ kadu-core/misc.cpp
@@ -80,6 +80,10 @@ void loadWindowGeometry(QWidget *w, const QString &section, const QString &name,
 	{
 		rect.setRect(defaultX, defaultY, defaultWidth, defaultHeight);
 	}
+#ifdef Q_OS_MAC
+	if (rect.y() < 20)
+		rect.setY(20);
+#endif
 	w->setGeometry(rect);
 }
 
