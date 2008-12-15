3b89e4ac7122ab5b790ce0a0084a95ec41bc2817
Tomasz Rostański <rozteck@interia.pl>
2008-12-15 22:39:22 +0000

    Updated translation for module auto_hide
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/branches/0.6.5@5921 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- modules/auto_hide/translations/auto_hide_pl.ts
+++ modules/auto_hide/translations/auto_hide_pl.ts
@@ -1,9 +1,23 @@
-<!DOCTYPE TS><TS>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="hi">
+<defaultcodec></defaultcodec>
 <context>
-    <name>AutoHide</name>
+    <name>@default</name>
     <message>
         <source>Dont hide</source>
         <translation>Nie ukrywaj</translation>
+    </message>
+    <message>
+        <source>Kadu&apos;s main window autohide</source>
+        <translation>Ukrywanie głównego okna</translation>
+    </message>
+    <message>
+        <source>Use autohide</source>
+        <translation>Włącz autoukrywanie</translation>
+    </message>
+    <message>
+        <source>Autohide idle time</source>
+        <translation>Czas bezczynności</translation>
     </message>
 </context>
 </TS>
