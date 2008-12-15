f2fb738d98c9862cff8da1718f5e672883a5c58f
Tomasz Rostański <rozteck@interia.pl>
2008-12-15 22:40:19 +0000

    Updated translation for module parser_extender
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/branches/0.6.5@5922 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- modules/parser_extender/translations/parser_extender_pl.ts
+++ modules/parser_extender/translations/parser_extender_pl.ts
@@ -1,2 +1,15 @@
-<!DOCTYPE TS><TS>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="pl_PL">
+<defaultcodec></defaultcodec>
+<context>
+    <name>@default</name>
+    <message>
+        <source>ParserExtender</source>
+        <translation>Rozszerzenie parsera</translation>
+    </message>
+    <message>
+        <source>Enable ParserExtender</source>
+        <translation>Włącz rozszerzenie parsera</translation>
+    </message>
+</context>
 </TS>
