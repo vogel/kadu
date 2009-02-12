8e9010df56398a7bbd34e28a470c4f0499b76cfc
Bartłomiej Zimoń <uzi18@go2.pl>
2009-02-12 13:14:12 +0000

    ContactAccountDataManager - one file left
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@6200 fea5205d-6df5-0310-bef7-ea67e47da9a5

---
+++ modules/gadu_protocol/gadu-contact-account-data-manager.cpp
@@ -0,0 +1,38 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "gadu-contact-account-data.h"
+
+#include "gadu-contact-account-data-manager.h"
+
+GaduContactAccountDataManager::GaduContactAccountDataManager(GaduContactAccountData* data)
+	: ContactAccountDataManager(data)
+{
+}
+
+void GaduContactAccountDataManager::writeEntry(const QString &section, const QString &name, const QVariant &value)
+{
+	if (section != "Gadu-Gadu")
+	{
+		ContactAccountDataManager::writeEntry(section, name, value);
+		return;
+	}
+
+	// other data
+}
+
+QVariant GaduContactAccountDataManager::readEntry(const QString &section, const QString &name)
+{
+	if (section != "Gadu-Gadu")
+		return ContactAccountDataManager::readEntry(section, name);
+
+	// other data
+
+	return QVariant(QString::null);
+}
