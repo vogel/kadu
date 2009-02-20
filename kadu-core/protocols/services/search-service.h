a66e836a6d7fd06baf80ab6b77f9cf82b16bec0b
Rafał Malinowski <malinowskirafal@wp.pl>
2009-02-20 12:23:26 +0000

    added missing file
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@6241 fea5205d-6df5-0310-bef7-ea67e47da9a5

---
+++ kadu-core/protocols/services/search-service.h
@@ -0,0 +1,33 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef SEARCH_SERVICE_H
+#define SEARCH_SERVICE_H
+
+#include <QtCore/QObject>
+
+#include "contacts/contact-list.h"
+
+class SearchService : public QObject
+{
+	Q_OBJECT
+
+public:
+	SearchService(QObject *parent = 0) : QObject(parent) {}
+
+	virtual void searchFirst(Contact contact) = 0;
+	virtual void searchNext() = 0;
+	virtual void stop() = 0;
+
+signals:
+	void newResults(ContactList constacts);
+
+};
+
+#endif // SEARCH_SERVICE_H
