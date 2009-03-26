70c2d296529505454b4a33965ed0362fa055fcdf
Piotr Galiszewski <piotrgaliszewski@gmail.com>
2009-03-26 10:06:41 +0000

    move notify translations and configuration
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@6458 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- translations/kadu_it.ts
+++ translations/kadu_it.ts
@@ -1092,6 +1092,81 @@ E un rischio per la sicurezza!</translation>
         <source>Display dates is nice format, like Today, Yesterday or Friday</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Notifications</source>
+        <translation>Notifiche</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>Generale</translation>
+    </message>
+    <message>
+        <source>Event</source>
+        <translation>Evento</translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>Opzioni</translation>
+    </message>
+    <message>
+        <source>New messages</source>
+        <translation>Nuovo messaggi</translation>
+    </message>
+    <message>
+        <source>Notify about new messages only when window is inactive</source>
+        <translation>Notifica nuovi messaggi per la finestra inattiva</translation>
+    </message>
+    <message>
+        <source>Status change</source>
+        <translation>Cambiamente dello status</translation>
+    </message>
+    <message>
+        <source>Ignore status changes from available / busy to busy / available</source>
+        <translation>Ignora i cambiament dello status da disponibile / occupato a disponibile / occupato</translation>
+    </message>
+    <message>
+        <source>Ignore changes right after connection to the server</source>
+        <translation>Non mostrare descrizioni dopo la conessione con server</translation>
+    </message>
+    <message>
+        <source>This option will supersede tooltips with users&apos; status
+ changes upon establishing connection to the server</source>
+        <translation>Questa opzione disattiva comparsa delle notifiche con status dei contatti
+durante primo colegamento Kadu con serwer</translation>
+    </message>
+    <message>
+        <source>Notify about all users</source>
+        <translation>Notifica tutti i contatti</translation>
+    </message>
+    <message>
+        <source>Connection error</source>
+        <translation>Errore conessione</translation>
+    </message>
+    <message>
+        <source>User changed status to online</source>
+        <translation>Contatto ha cambiato status a Accessibile</translation>
+    </message>
+    <message>
+        <source>User changed status to busy</source>
+        <translation>Contatto ha cambiato status a Occupato</translation>
+    </message>
+    <message>
+        <source>User changed status to invisible</source>
+        <translation>Contatto ha cambiato status a Invisibile</translation>
+    </message>
+    <message>
+        <source>User changed status to offline</source>
+        <translation>Contatto ha cambiato status a Fuori line</translation>
+    </message>
+    <message>
+        <source>New chat</source>
+        <translation>Nuovo chat</translation>
+    </message>
+    <message>
+        <source>New message</source>
+        <translation>Nuovo messaggio</translation>
+    </message>
+
 </context>
 <context>
     <name>About</name>
@@ -2626,6 +2701,66 @@ Cancellazione lista contatti su server GG fallita</translation>
     <message>
         <source>File exists. Are you sure you want to overwrite it?</source>
         <translation>File esiste. Vuoi davvero sovrascrivere?</translation>
+    </message>
+</context>
+<context>
+    <name>ConnectionErrorNotification</name>
+    <message>
+        <source>Connection error</source>
+        <translation>Errore conessione</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Error:&lt;/b&gt; (%1) %2</source>
+        <translation>&lt;b&gt;Errore:&lt;/b&gt; (%1) %2</translation>
+    </message>
+</context>
+<context>
+    <name>MessageNotification</name>
+    <message>
+        <source>Chat with &lt;b&gt;%1&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>New message from &lt;b&gt;%1&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Notify</name>
+    <message>
+        <source>Unable to find notifier for %1 event</source>
+        <translation>Non posso trovare notifier per evento %1</translation>
+    </message>
+    <message>
+        <source>Move to &apos;Notify list&apos;</source>
+        <translation>Riporta nella &apos;Lista Notifiche&apos;</translation>
+    </message>
+    <message>
+        <source>User list</source>
+        <translation>Lista contatti</translation>
+    </message>
+    <message>
+        <source>Move to &apos;User list&apos;</source>
+        <translation>Riporta nella &apos;Lista Contatti&apos;</translation>
+    </message>
+    <message>
+        <source>Notify list</source>
+        <translation>Lista notifiche</translation>
+    </message>
+</context>
+<context>
+    <name>StatusChangedNotification</name>
+    <message>
+        <source>Status changed</source>
+        <translation>Cambiamento dello status</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;%1&lt;/b&gt; changed status to &lt;i&gt;%2&lt;/i&gt;&lt;br/&gt; &lt;small&gt;%3&lt;/small&gt;</source>
+        <translation>&lt;b&gt;%1&lt;/b&gt; ha cambiato status a &lt;i&gt;%2&lt;/i&gt;&lt;br/&gt; &lt;small&gt;%3&lt;/small&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;%1&lt;/b&gt; changed status to &lt;i&gt;%2&lt;/i&gt;</source>
+        <translation>&lt;b&gt;%1&lt;/b&gt; ha cambiato status a &lt;i&gt;%2&lt;/i&gt;</translation>
     </message>
 </context>
 </TS>
--- translations/kadu_pl.ts
+++ translations/kadu_pl.ts
@@ -1105,6 +1105,79 @@ Jest to wysoce ryzykowne!</translation>
         <source>Display dates is nice format, like Today, Yesterday or Friday</source>
         <translation>Wyświetla daty w eleganckim formacie, na przykład: Dzisiaj, Wczoraj, Piątek</translation>
     </message>
+    <message>
+        <source>Notifications</source>
+        <translation>Powiadomienia</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>Ogólne</translation>
+    </message>
+    <message>
+        <source>Event</source>
+        <translation>Zdarzenie</translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>Opcje</translation>
+    </message>
+    <message>
+        <source>New messages</source>
+        <translation>Nowa wiadomość</translation>
+    </message>
+    <message>
+        <source>Notify about new messages only when window is inactive</source>
+        <translation>Informuj o nowych wiadomościach tylko gdy okno rozmowy jest nieaktywne</translation>
+    </message>
+    <message>
+        <source>Status change</source>
+        <translation>Zmiany statusów</translation>
+    </message>
+    <message>
+        <source>Ignore status changes from available / busy to busy / available</source>
+        <translation>Ignoruj zmiany statusów z dostępny / zajęty na zajęty / dostępny</translation>
+    </message>
+    <message>
+        <source>Ignore changes right after connection to the server</source>
+        <translation>Ignoruj zmiany statusów po połączeniu z serwerem</translation>
+    </message>
+    <message>
+        <source>This option will supersede tooltips with users&apos; status
+ changes upon establishing connection to the server</source>
+        <translation>Ta opcja zlikwiduje powiadomienia o zmianach statusów przy łączeniu do serwera</translation>
+    </message>
+    <message>
+        <source>Notify about all users</source>
+        <translation>Informuj o wszystkich kontaktach</translation>
+    </message>
+    <message>
+        <source>Connection error</source>
+        <translation>Błąd połączenia</translation>
+    </message>
+    <message>
+        <source>User changed status to online</source>
+        <translation>Kontakt zmienił status na &apos;Dostępny&apos;</translation>
+    </message>
+    <message>
+        <source>User changed status to busy</source>
+        <translation>Kontakt zmienił status na &apos;Zajęty&apos;</translation>
+    </message>
+    <message>
+        <source>User changed status to invisible</source>
+        <translation>Kontakt zmienił status na &apos;Ukryty&apos;</translation>
+    </message>
+    <message>
+        <source>User changed status to offline</source>
+        <translation>Kontakt zmienił status na &apos;Niedostępny&apos;</translation>
+    </message>
+    <message>
+        <source>New chat</source>
+        <translation>Nowa rozmowa</translation>
+    </message>
+    <message>
+        <source>New message</source>
+        <translation>Nowa wiadomość</translation>
+    </message>
 </context>
 <context>
     <name>About</name>
@@ -2644,6 +2717,66 @@ Jesteś pewny że tego chcesz?</translation>
     <message>
         <source>File exists. Are you sure you want to overwrite it?</source>
         <translation>Plik już istnieje. Czy na pewno go nadpisać?</translation>
+    </message>
+</context>
+<context>
+    <name>ConnectionErrorNotification</name>
+    <message>
+        <source>Connection error</source>
+        <translation>Błąd połączenia</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Error:&lt;/b&gt; (%1) %2</source>
+        <translation>&lt;b&gt;Błąd:&lt;/b&gt; (%1) %2</translation>
+    </message>
+</context>
+<context>
+    <name>MessageNotification</name>
+    <message>
+        <source>Chat with &lt;b&gt;%1&lt;/b&gt;</source>
+        <translation>Rozmowa z &lt;b&gt;%1&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <source>New message from &lt;b&gt;%1&lt;/b&gt;</source>
+        <translation>Nowa wiadomość od &lt;b&gt;%1&lt;/b&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>Notify</name>
+    <message>
+        <source>Unable to find notifier for %1 event</source>
+        <translation>Nie znaleziono powiadomienia dla zdarzenia %1</translation>
+    </message>
+    <message>
+        <source>Move to &apos;Notify list&apos;</source>
+        <translation>Przesuń do &quot;Listy powiadomień&quot;</translation>
+    </message>
+    <message>
+        <source>User list</source>
+        <translation>Lista kontaktów</translation>
+    </message>
+    <message>
+        <source>Move to &apos;User list&apos;</source>
+        <translation>Przesuń do &quot;Listy kontaktów&quot;</translation>
+    </message>
+    <message>
+        <source>Notify list</source>
+        <translation>Lista powiadomień</translation>
+    </message>
+</context>
+<context>
+    <name>StatusChangedNotification</name>
+    <message>
+        <source>Status changed</source>
+        <translation>Zmiany statusu</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;%1&lt;/b&gt; changed status to &lt;i&gt;%2&lt;/i&gt;&lt;br/&gt; &lt;small&gt;%3&lt;/small&gt;</source>
+        <translation>&lt;b&gt;%1&lt;/b&gt; zmienił status na &lt;i&gt;%2&lt;/i&gt;&lt;br/&gt; &lt;small&gt;%3&lt;/small&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;%1&lt;/b&gt; changed status to &lt;i&gt;%2&lt;/i&gt;</source>
+        <translation>&lt;b&gt;%1&lt;/b&gt; zmienił status na &lt;i&gt;%2&lt;/i&gt;</translation>
     </message>
 </context>
 </TS>
