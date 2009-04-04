d82ce6d762d3abbb4b23ecdf5648be503b3fc4d7
Rafał Malinowski <malinowskirafal@wp.pl>
2009-04-04 17:01:39 +0000

    cleanup
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@6501 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- kadu-core/gui/windows/contact-data-window.cpp
+++ kadu-core/gui/windows/contact-data-window.cpp
@@ -44,11 +44,8 @@
 
 #include "contact-data-window.h"
 
-ContactDataWindow::ContactDataWindow(Contact contact, QWidget *parent)
-	: QWidget(parent, Qt::Dialog), CurrentContact(contact),
-	e_firstname(0), e_lastname(0), e_nickname(0), e_display(0), e_mobile(0), e_id(0),
-	e_addr(0), e_ver(0), e_email(0), e_dnsname(0), c_blocking(0), c_offtouser(0),
-	c_notify(0), pb_addapply(0), tw_main(0), groups(), newGroup(0), groupsWidget(0), groupsLayout(0)
+ContactDataWindow::ContactDataWindow(Contact contact, QWidget *parent) :
+		QWidget(parent, Qt::Dialog), CurrentContact(contact)
 {
 	kdebugf();
 
@@ -57,74 +54,11 @@ ContactDataWindow::ContactDataWindow(Contact contact, QWidget *parent)
 
 	createGui();
 
-	UserAccount = CurrentContact.prefferedAccount();
-	return;
-
-	// create main QLabel widgets (icon and app info)
-	QWidget *left = new QWidget;
-
-	QLabel *l_icon = new QLabel;
-
-	QVBoxLayout *left_layout = new QVBoxLayout(left);
-	left_layout->addWidget(l_icon);
-	left_layout->addStretch();
-
-	QWidget *center = new QWidget();
-
-	QLabel *l_info = new QLabel;
-	l_info->setText(tr("This dialog box allows you to view and edit information about the selected contact."));
-	l_info->setWordWrap(true);
-#ifndef Q_OS_MAC
-	l_info->setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum));
-#endif
-	// end create main QLabel widgets (icon and app info)
-
-	tw_main = new QTabWidget;
-
-	QVBoxLayout *center_layout = new QVBoxLayout(center);
-	center_layout->addWidget(l_info);
-	center_layout->addWidget(tw_main);
-
-	// create our Tabs
-	setupTab1();
-	setupTab2();
-	setupTab3();
-
-	// create buttons and fill icon and app info
-	QWidget *bottom = new QWidget;
-
-	if (CurrentContact.isAnonymous())
-	{
-		setWindowTitle(tr("Add user"));
-		l_icon->setPixmap(icons_manager->loadPixmap("AddUserWindowIcon"));
-		pb_addapply = new QPushButton(icons_manager->loadIcon("AddUserButton"), tr("Add"));
-	}
-	else
-	{
-		setWindowTitle(tr("User info on %1").arg(CurrentContact.display()));
-		l_icon->setPixmap(icons_manager->loadPixmap("ManageUsersWindowIcon"));
-		pb_addapply = new QPushButton(icons_manager->loadIcon("UpdateUserButton"), tr("Update"));
-	}
-
-	QPushButton *pb_close = new QPushButton(icons_manager->loadIcon("CloseWindow"), tr("&Close"));
-	// end buttons
-
-	connect(pb_close, SIGNAL(clicked()), this, SLOT(close()));
-	connect(pb_addapply, SIGNAL(clicked()), this, SLOT(updateUserlist()));
-
-	QHBoxLayout *bottom_layout = new QHBoxLayout(bottom);
-	bottom_layout->addStretch();
-	bottom_layout->addWidget(pb_addapply);
-	bottom_layout->addWidget(pb_close);
-
-	center_layout->addWidget(bottom);
-
-	QHBoxLayout *layout = new QHBoxLayout(this);
-	layout->addWidget(left);
-	layout->addWidget(center);
+	setWindowTitle(CurrentContact.isAnonymous()
+			? tr("Add user")
+			: tr("User info on %1").arg(CurrentContact.display()));
 
 	loadWindowGeometry(this, "General", "ManageUsersDialogGeometry", 0, 50, 425, 500);
-	kdebugf2();
 }
 
 ContactDataWindow::~ContactDataWindow()
@@ -225,186 +159,27 @@ void ContactDataWindow::updateAndClose()
 	close();
 }
 
-void ContactDataWindow::setupTab1()
-{
-	kdebugf();
-
-	QWidget *generalWidget = new QWidget(tw_main);
-
-	tw_main->addTab(generalWidget, tr("General"));
-
-	// TODO: 0.6.6 we need it if we use only one account
-
-	// ID and disp
-	//e_id = new QLineEdit(generalWidget);
-	//e_id->setMaxLength(8);
-	// TODO: validateId from protocol
-	//e_id->setValidator(new QIntValidator(1, 99999999, this));
-
-	e_display = new QComboBox(generalWidget);
-	e_display->setEditable(true);
-	// end ID and disp
-
-	// name and nick
-	e_firstname = new QLineEdit(generalWidget);
-	e_nickname = new QLineEdit(generalWidget);
-	// end name and nick
-
-	// Surname & mobile
-	e_lastname = new QLineEdit(generalWidget);
-	e_mobile = new QLineEdit(generalWidget);
-	// end Surname & mobile
-
-	QLabel *emptyWidget = new QLabel("", generalWidget);
-	emptyWidget->setObjectName("space_for_advanced_userlist_label");
-
-	QWidget *emptyWidget2 = new QWidget(generalWidget);
-	emptyWidget2->setObjectName("space_for_advanced_userlist_spinbox");
-
-	// Email
-	e_email = new QLineEdit(generalWidget);
-	// end Email
-
-	//QFrame *line1 = new QFrame(generalWidget);
-	//line1->setFrameShape(QFrame::HLine);
-	//line1->setFrameShadow(QFrame::Sunken);
-	//line1->setFrameShape(QFrame::HLine);
-
-	// IP and DNS
-	//e_addr = new QLineEdit(generalWidget);
-	//e_addr->setBackgroundMode(Qt::PaletteButton);
-
-	//e_dnsname = new QLineEdit(generalWidget);
-	//e_dnsname->setBackgroundMode(Qt::PaletteButton);
-	// end IP and DNS
-
-	// Protocol Version and status
-	//e_ver = new QLineEdit(generalWidget);
-	//e_ver->setBackgroundMode(Qt::PaletteButton);
-
-	//QLineEdit *e_status = new QLineEdit(generalWidget);
-	//e_status->setBackgroundMode(Qt::PaletteButton);
-	// end Protocol Version and status
-
-	QGridLayout * generalLayout = new QGridLayout(generalWidget);
-	generalLayout->setSpacing(3);
-	generalLayout->addWidget(new QLabel(tr("Display"), generalWidget), 0, 0);
-	generalLayout->addWidget(e_display, 1, 0);
-	generalLayout->addWidget(new QLabel(tr("Nickname"), generalWidget), 0, 1);
-	generalLayout->addWidget(e_nickname, 1, 1);
-	generalLayout->addWidget(new QLabel(tr("First name"), generalWidget), 2, 0);
-	generalLayout->addWidget(e_firstname, 3, 0);
-	generalLayout->addWidget(new QLabel(tr("Surname"), generalWidget), 2, 1);
-	generalLayout->addWidget(e_lastname, 3, 1);
-	generalLayout->addWidget(new QLabel(tr("Email"), generalWidget), 4, 0);
-	generalLayout->addWidget(e_email, 5, 0);
-	generalLayout->addWidget(new QLabel(tr("Mobile"), generalWidget), 4, 1);
-	generalLayout->addWidget(e_mobile, 5, 1);
-	//generalLayout->addWidget(new QLabel(tr("Email"), generalWidget), 6, 0);
-	//generalLayout->addWidget(e_email, 7, 0);
-	generalLayout->addWidget(emptyWidget, 6, 1);
-	generalLayout->addWidget(emptyWidget2, 7, 1);
-	//generalLayout->addWidget(line1, 8, 0, 1, 2);
-	//generalLayout->addWidget(new QLabel(tr("Address IP and Port"), generalWidget), 9, 0);
-	//generalLayout->addWidget(e_addr, 10, 0);
-	//generalLayout->addWidget(new QLabel(tr("DNS name"), generalWidget), 9, 1);
-	//generalLayout->addWidget(e_dnsname, 10, 1);
-	//generalLayout->addWidget(new QLabel(tr("Protocol version"), generalWidget), 11, 0);
-	//generalLayout->addWidget(e_ver, 12, 0);
-	//generalLayout->addWidget(new QLabel(tr("Status"), generalWidget), 11, 1);
-	//generalLayout->addWidget(e_status, 12, 1);
-
-	//e_status->setReadOnly(true);
-	//e_addr->setReadOnly(true);
-	//e_ver->setReadOnly(true);
-	//e_dnsname->setReadOnly(true);
-
-	e_nickname->setText(CurrentContact.nickName());
-	e_firstname->setText(CurrentContact.firstName());
-	e_lastname->setText(CurrentContact.lastName());
-	e_mobile->setText(CurrentContact.mobile());
-	e_email->setText(CurrentContact.email());
-
-	//Protocol *userProtocol = UserAccount->protocol();
-
-	connect(e_nickname, SIGNAL(editingFinished()), this, SLOT(updateDisplay()));
-	connect(e_firstname, SIGNAL(editingFinished()), this, SLOT(updateDisplay()));
-	connect(e_lastname, SIGNAL(editingFinished()), this, SLOT(updateDisplay()));
-
-	e_display->setEditText(CurrentContact.display());
-	updateDisplay();
-	// TODO move to gadu.ui and add as contact account data widget ...
-	/*if (User.usesProtocol("Gadu"))
-	{
-		if (!userProtocol->isConnected())
-			e_status->setText(tr("(Unknown)"));
-
-		e_id->setText(User.id(UserAccount));
-
-		if (User.hasIP("Gadu"))
-		{
-			e_addr->setText(User.IP("Gadu").toString());
-
-			if (User.DNSName("Gadu").isEmpty())
-				QHostInfo::lookupHost(User.IP("Gadu").toString(), this, SLOT(resultsReady(QHostInfo)));
-			else
-				e_dnsname->setText(User.DNSName("Gadu"));
-		}
-		else
-		{
-			e_addr->setText(tr("(Unknown)"));
-			e_dnsname->setText(tr("(Unknown)"));
-		}
-
-		if (User.port("Gadu"))
-			e_addr->setText(e_addr->text() + ':' + QString::number(User.port("Gadu")));
-		else
-			e_addr->setText(e_addr->text() + ':' + tr("(Unknown)"));
-
-		unsigned int version = (User.protocolData("Gadu", "Version").toUInt() & 0x0000ffff);
-		if (version)
-		{
-			QString s_temp;
-			s_temp.sprintf("0x%02x", version);
-			e_ver->setText(s_temp + " (" + versionToName(version) + ")");
-		}
-		else
-			e_ver->setText(tr("(Unknown)"));
-
-		ContactAccountData *contactData = User.accountData(UserAccount);
-		Status status = contactData->status();
-
-// TODO: 0.6.6
-		e_status->setText(QString::number(status.type()));
-		e_status->setToolTip(status.description());
-
-		tw_main->setTabIconSet(generalWidget, account->statusPixmap(status));
-	}*/
-
-	kdebugf2();
-}
-
 void ContactDataWindow::updateDisplay()
 {
-	QStringList list;
-	if (!e_display->currentText().isEmpty())
-		list << e_display->currentText();
-	if (!e_nickname->text().isEmpty() && !list.contains(e_nickname->text()))
-		list << e_nickname->text();
-	if (!e_firstname->text().isEmpty())
-	{
-		if (!list.contains(e_firstname->text()))
-			list << e_firstname->text();
-		if (!e_lastname->text().isEmpty())
-		{
-			list << e_firstname->text() + " " + e_lastname->text();
-			list << e_lastname->text() + " " + e_firstname->text();
-		}
-	}
-	e_display->clear();
-	e_display->addItems(list);
+// 	QStringList list;
+// 	if (!e_display->currentText().isEmpty())
+// 		list << e_display->currentText();
+// 	if (!e_nickname->text().isEmpty() && !list.contains(e_nickname->text()))
+// 		list << e_nickname->text();
+// 	if (!e_firstname->text().isEmpty())
+// 	{
+// 		if (!list.contains(e_firstname->text()))
+// 			list << e_firstname->text();
+// 		if (!e_lastname->text().isEmpty())
+// 		{
+// 			list << e_firstname->text() + " " + e_lastname->text();
+// 			list << e_lastname->text() + " " + e_firstname->text();
+// 		}
+// 	}
+// 	e_display->clear();
+// 	e_display->addItems(list);
 }
-
+/*
 void ContactDataWindow::setupTab2()
 {
 	kdebugf();
@@ -477,146 +252,10 @@ void ContactDataWindow::newGroupClicked()
 	GroupManager::instance()->byName(groupName, true);
 
 	kdebugf2();
-}
-
-
-void ContactDataWindow::setupTab3()
-{
-	kdebugf();
-
-	foreach (Account *account, CurrentContact.accounts())
-	{
-		if (!account || !account->protocol())
-			continue;
-
-		ProtocolFactory *protocolFactory = account->protocol()->protocolFactory();
-
-		if (!CurrentContact.hasAccountData(account) || !protocolFactory)
-			continue;
-
-		ContactAccountData *contactAccountData = CurrentContact.accountData(account);
-
-		ContactAccountDataWidget *contactAccountDataWidget = protocolFactory->newContactAccountDataWidget(contactAccountData, tw_main);
-
-		if (!contactAccountDataWidget)
-			continue;
-
-		contactAccountDataWidget->setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Expanding));
-		contactAccountDataWidget->loadConfiguration();
-
-		tw_main->addTab(contactAccountDataWidget, account->name());
-
-		dataWidgets.append(contactAccountDataWidget);
-	}
-
-	kdebugf2();
-}
+}*/
 
 void ContactDataWindow::keyPressEvent(QKeyEvent *ke_event)
 {
 	if (ke_event->key() == Qt::Key_Escape)
 		close();
 }
-
-void ContactDataWindow::resultsReady(const QHostInfo &host)
-{
-    	if (host.error() == QHostInfo::NoError)
-		e_dnsname->setText(host.hostName());
-}
-
-void ContactDataWindow::updateUserlist()
-{
-	kdebugf();
-
-	foreach (ContactAccountDataWidget* widget, widgets())
-		widget->saveConfiguration();
-//TODO
-	foreach(QCheckBox *checkbox, groups)
-		if (checkbox->isChecked())
-		{
-			Group *group = GroupManager::instance()->byName(checkbox->text(), false);
-			CurrentContact.addToGroup(group);
-		}
-/*
-	QString id = QString::number(0);
-	if (!e_id->text().isEmpty())
-		id = e_id->text();
-
-	if (e_id->currentText().isEmpty())
-	{
-		MessageBox::msg(tr("Altnick field cannot be empty."), false, "Warning", this);
-
-		kdebugf2();
-		return;
-	}
-
-	if (userlist->contains("Gadu", id)) // je�eli istenieje ju� u�ytkownik o danym ID...
-	{
-		UserListElement user = userlist->byID("Gadu", id);
-		if (user != User) // ...i nie jest to ten, aktualnie edytowany...
-		{
-			if (user.isAnonymous()) // ...to je�li istniej�cy kontakt jest anonimem...
-			{
-				userlist->removeUser(User);
-				User = user; // ...to usuwamy edytowanego usera i zast�pujemy go anonimem
-			}
-			else
-			{
-				MessageBox::msg(tr("User known as %1 with that UIN is already in userlist.").arg(user.altNick()), false, "Warning", this);
-
-				kdebugf2();
-				return;
-			}
-		}
-	}
-
-	User.setFirstName(e_firstname->text());
-	User.setLastName(e_lastname->text());
-	User.setNickName(e_nickname->text());
-	User.setDisplay(e_display->currentText());
-	User.setMobile(e_mobile->text());
-
-	if (User.usesProtocol("Gadu")) // there was an UIN so far?
-	{
-		if (User.ID("Gadu") != id) // uin was changed
-		{
-			User.deleteProtocol("Gadu");
-			if (id.toUInt() != 0) // but it might be deleted
-				User.addProtocol("Gadu", id);
-		}
-	}
-	else // there was no UIN so far
-		if (id.toUInt() != 0) // if it was filled, then we add new protocol
-			User.addProtocol("Gadu", id);
-
-
-	User.setData("Groups", l);
-
-	User.setEmail(e_email->text());
-	User.setNotify(c_notify->isChecked());
-	if (User.usesProtocol("Gadu"))
-	{
-		User.setProtocolData("Gadu", "OfflineTo", c_offtouser->isChecked());
-		User.setProtocolData("Gadu", "Blocking", c_blocking->isChecked());
-	}
-	User.setAnonymous(false);
-	if (!userlist->contains(User))
-		userlist->addUser(User);
-
-	emit updateClicked(this);
-
-// TODO: 0.6.6
-// 	userlist->writeToConfig();
-	xml_config_file->sync();
-*/
-	close();
-
-	kdebugf2();
-}
-
-
-void ContactDataWindow::scrollToBottom()
-{
-	scrollArea->verticalScrollBar()->setValue(scrollArea->widget()->height());
-}
-
--- kadu-core/gui/windows/contact-data-window.h
+++ kadu-core/gui/windows/contact-data-window.h
@@ -46,31 +46,6 @@ class KADUAPI ContactDataWindow : public QWidget
 	Contact CurrentContact;
 	QList<ConfigurationWidget *> ConfigurationWidgets;
 
-	Account *UserAccount;
-	QLineEdit *e_firstname;
-	QLineEdit *e_lastname;
-	QLineEdit *e_nickname;
-	QComboBox *e_display;
-	QLineEdit *e_mobile;
-	QLineEdit *e_id;
-	QLineEdit *e_addr;
-	QLineEdit *e_ver;
-	QLineEdit *e_email;
-	QLineEdit *e_dnsname;
-	QCheckBox *c_blocking;
-	QCheckBox *c_offtouser;
-	QCheckBox *c_notify;
-	QPushButton *pb_addapply;
-	QTabWidget *tw_main;
-	QScrollArea* scrollArea;
-
-	QList<QCheckBox *> groups;
-	QLineEdit *newGroup;
-	QWidget *groupsWidget;
-	QVBoxLayout *groupsLayout;
-
-	QList<ContactAccountDataWidget *> dataWidgets;
-
 	void createGui();
 	void createTabs(QLayout *layout);
 	void createContactTab(QTabWidget *tabWidget);
@@ -85,79 +60,12 @@ private slots:
 	void update();
 	void updateAndClose();
 
-	/**
-		\fn void resultsReady(const QHostInfo &host)
-		Ustawia warto�� pola DNS na znalezion� nazw� domeny.
-	**/
-	void resultsReady(const QHostInfo &host);
-
-	/**
-		\fn void newGroupClicked()
-		wci�ni�to przycisk tworzenia nowej grupy
-	**/
-	void newGroupClicked();
-
-	/**
-		\fn void scrollToBottom()
-		po dodaniu nowej grupy przewija list� grup aby dodana by�a widoczna
-	**/
-	void scrollToBottom();
-		
-protected:
-
-	class Empty : public ConfigurationWindowDataManager
-	{
-
-	protected:
-		virtual QVariant readEntry(const QString &section, const QString &name) { return ""; }
-		virtual void writeEntry(const QString &section, const QString &name, const QVariant &value) {}
-
-	};
-
-	/**
-		\fn void setupTab1()
-		Tworzy pierwsz� zak�adk�.
-	**/
-	void setupTab1();
-
-	/**
-		\fn void setupTab2()
-		Tworzy drug� zak�adk�.
-	**/
-	void setupTab2();
-
-	/**
-		\fn void setupTab3()
-		Tworzy trzeci� zak�adk�.
-	**/
-	void setupTab3();
-
 public:
-	/**
-		\fn UserInfo(const QString &altnick, bool addUser = false, QDialog* parent=0, const char *name=0)
-		Konstruktor tworz�cy okno dialogowe z informacjami o danym kontakcie.
-		\param altnick pseudonim kontaktu wy�wietlany na li�cie.
-		\param addUser TRUE gdy dodajemy kontakt, FALSE gdy edytujemy dane istniej�cego. Domy�lnie FALSE.
-		\param parent wska�nik do obiektu stanowi�cego rodzica. Domy�lnie 0.
-		\param name nazwa kontrolki. Domy�lnie 0.
-	**/
 	ContactDataWindow(Contact contact, QWidget *parent = 0);
 	virtual ~ContactDataWindow();
 
-	QTabWidget *tabs() { return tw_main; }
-	QList<ContactAccountDataWidget *> widgets() { return dataWidgets; }
-	Contact contact() const { return CurrentContact; }
-
 public slots:
-	/**
-		\fn void updateUserlist()
-		Wci�ni�cie przycisku Dodaj/Uaktualnij
-	**/
-	void updateUserlist();
 	void updateDisplay();
-
-signals:
-	void updateClicked(ContactDataWindow *);
 
 };
 
