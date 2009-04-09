c2aa7ea82a5a97a761adaa5f898c103e7a162905
Tomasz Rostański <rozteck@interia.pl>
2009-04-09 14:57:11 +0000

    Changes to gg_avatars module (async, downloads avatars instead of using urls, version changed to 0.2)
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/branches/0.6.5@6524 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- modules/gg_avatars/gg_avatars.cpp
+++ modules/gg_avatars/gg_avatars.cpp
@@ -7,9 +7,13 @@
  *                                                                         *
  ***************************************************************************/
 
-#include <QtCore/QBuffer>
-#include <QtCore/QString>
+#include <QtCore/QUrl>
+#include <QtCore/QFile>
 #include <QtCore/QDebug>
+#include <QtCore/QString>
+#include <QtCore/QBuffer>
+#include <QtCore/QFileInfo>
+#include <QtNetwork/QHttp>
 
 #include "gg_avatars.h"
 
@@ -19,7 +23,6 @@
 #include "config_file.h"
 #include "userlistelement.h"
 
-QDateTime started;
 GaduAvatars *gaduAvatars;
 
 extern "C" KADU_EXPORT int gg_avatars_init()
@@ -56,6 +59,12 @@ QString get_avatar_url(const UserListElement &ule)
 
 GaduAvatars::GaduAvatars()
 {
+	linkDownloader = new QHttp("api.gadu-gadu.pl");
+	connect(linkDownloader, SIGNAL(requestFinished(int, bool)), this, SLOT(gotResponse(int, bool)));
+
+	fileDownloader = new QHttp();
+	connect(fileDownloader, SIGNAL(requestFinished(int, bool)), this, SLOT(fileDownloaded(int, bool)));
+
 	KaduParser::registerTag("avatar", &get_avatar);
 	KaduParser::registerTag("avatar_url", &get_avatar_url);
 }
@@ -64,34 +73,89 @@ GaduAvatars::~GaduAvatars()
 {
 	KaduParser::unregisterTag("avatar", &get_avatar);
 	KaduParser::registerTag("avatar_url", &get_avatar_url);
+
+	disconnect(linkDownloader, SIGNAL(requestFinished(int, bool)), this, SLOT(gotResponse(int, bool)));
+	delete linkDownloader;
+
+	disconnect(fileDownloader, SIGNAL(requestFinished(int, bool)), this, SLOT(fileDownloaded(int, bool)));
+	delete fileDownloader;
 }
 
 QString GaduAvatars::getAvatar(const UserListElement &ule)
 {
 	int uin = ule.ID("Gadu").toInt();
-	if (avatars.contains(uin))
+	QString filename = ggPath() + "/avatars/" + QString::number(uin);
+	if (QFileInfo(filename).size() > 0)
 	{
-		return avatars[uin];
+		return "file://" + filename;
 	}
 	else
 	{
-		SyncHTTP h("api.gadu-gadu.pl");
-		QBuffer buff;
-    		h.syncGet("/avatars/" + ule.ID("Gadu") + "/0.xml", &buff);
-
-		QString response(buff.data());
-
-		if (!response.isEmpty())
-		{
-			int begin = response.indexOf("<smallAvatar>") + 13;
-			int end = response.indexOf("</smallAvatar>");
-			
-			if ((begin > 0) && (end > begin))
-				response = response.mid(begin, end - begin);
-		}
-		avatars.insert(uin, response);
-		return response;
+		QBuffer *buffer = new QBuffer();
+    		int id = linkDownloader->get("/avatars/" + ule.ID("Gadu") + "/0.xml", buffer);
+		buffers.insert(id, buffer);
+		uins.insert(id, uin);
+		return "";
 	}
+}
+
+void GaduAvatars::gotResponse(int id, bool error)
+{
+	int uin = uins[id];
+
+	QBuffer *buffer = buffers[id];
+	QString response(buffer->data());
+	uins.remove(id);
+	buffers.remove(id);
+	delete buffer;
+	
+	if (!response.isEmpty())
+	{
+		int begin = response.indexOf("<smallAvatar>") + 13;
+		int end = response.indexOf("</smallAvatar>");
+	
+		if ((begin > 0) && (end > begin))
+			response = response.mid(begin, end - begin);
+	}
+
+	QDir dir;
+	QString path = ggPath() + "/avatars/";
+	dir.mkdir(path, true);
+	path += QString::number(uin);
+
+	if (QFile::exists(path))
+	{
+		return;
+	}
+
+	QFile *file = new QFile(path);
+	if (file->open(QIODevice::WriteOnly))
+	{
+		QUrl url(response);
+		fileDownloader->setHost(url.host());
+
+		QByteArray path = QUrl::toPercentEncoding(url.path(), "!$&'()*+,;=:@/");
+		int id = fileDownloader->get(path, file);
+		files.insert(id, file);
+	}
+}
+
+void GaduAvatars::fileDownloaded(int id, bool error)
+{
+	QFile *file = files[id];
+	if (file == NULL)
+	{
+		return;
+	}
+	
+	file->close();
+	if (error) {
+		printf("Error\n");
+		fflush(stdout);
+		file->remove();
+	}
+	files.remove(id);
+	delete file;
 }
 
 void GaduAvatars::configurationUpdated()
--- modules/gg_avatars/gg_avatars.h
+++ modules/gg_avatars/gg_avatars.h
@@ -3,65 +3,36 @@
 
 #include <QtCore/QObject>
 #include <QtCore/QEventLoop>
-#include <QtNetwork/QHttp>
 
 #include "configuration_aware_object.h"
 #include "main_configuration_window.h"
 
+class QFile;
 class QHttp;
+class QBuffer;
+
 class GaduAvatars : public ConfigurationUiHandler, ConfigurationAwareObject
 {
-    Q_OBJECT
+	Q_OBJECT
 	private:
-		QMap<int, QString> avatars;
+		QMap<int, QBuffer *> buffers;   /* buffers per link request */
+		QMap<int, int> uins;		/* uins per link/file request */
+		QMap<int, QFile *> files;	/* files per file request */
+		QHttp *linkDownloader;		/* downloader for avatar links */
+		QHttp *fileDownloader;		/* downloader for avatar images */
 
 	public:
 		GaduAvatars();
 		~GaduAvatars();
 		QString getAvatar(const UserListElement &ule);
-
 		virtual void mainConfigurationWindowCreated(MainConfigurationWindow *mainConfigurationWindow);
 
 	protected:
 		virtual void configurationUpdated();
-};
 
-/* 
-    Code taken from: 
-    http://www.erata.net/qt-boost/synchronous-http-request/
-*/
-class SyncHTTP: public QHttp
-{
-	Q_OBJECT
-	int requestID;
-	bool status;
-	QEventLoop loop;
- 
-public:
-
-	SyncHTTP(QObject * parent = 0) 
-		: QHttp(parent), requestID(-1), status(false) {}
-	
-	SyncHTTP(const QString & hostName, quint16 port = 80, QObject *parent = 0) 
-		: QHttp(hostName, port, parent), requestID(-1), status(false) {}
-	
-	virtual ~SyncHTTP() {}
-
-	bool syncGet(const QString & path, QIODevice *to = 0)
-	{
-		connect(this, SIGNAL(requestFinished(int,bool)), SLOT(finished(int,bool)));
-		requestID = get(path, to);
-		loop.exec();
-		return status;
-	}
-
-protected slots:
-	virtual void finished(int idx, bool err)
-	{
-		if (idx != requestID) return;
-		status = !err;
-		loop.exit();
-	}
+	private slots:
+		void gotResponse(int id, bool error);
+		void fileDownloaded(int id, bool error);
 };
 
 extern GaduAvatars *gaduAvatars;
