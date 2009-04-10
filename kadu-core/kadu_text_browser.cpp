ac24fb1da0345ef88f7eb79a5df51032fd5d1db8
Tomasz Rostański <rozteck@interia.pl>
2009-04-10 15:48:07 +0000

    Save instead of Open in Save image dialog
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/branches/0.6.5@6528 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- kadu-core/kadu_text_browser.cpp
+++ kadu-core/kadu_text_browser.cpp
@@ -138,6 +138,7 @@ void KaduTextBrowser::saveImage()
 
 	QFileDialog fd(this);
 	fd.setMode(QFileDialog::AnyFile);
+	fd.setAcceptMode(QFileDialog::AcceptSave);
 	fd.setDir(config_file.readEntry("Chat", "LastImagePath"));
 	fd.setFilter(QString("%1 (*%2)").arg(qApp->translate("ImageDialog", "Images"), fileExt));
 	fd.setLabelText(QFileDialog::FileName, image.section('/', -1));
