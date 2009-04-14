2c479ae5ad64207b2951762b381175f123c8e06c
Tomasz Rostański <rozteck@interia.pl>
2009-04-14 21:16:57 +0000

    Add translations for gg_avatars module
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/branches/0.6.5@6549 fea5205d-6df5-0310-bef7-ea67e47da9a5

---
+++ modules/gg_avatars/translations/gg_avatars_pl.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="pl_PL">
+<defaultcodec></defaultcodec>
+<context>
+    <name>GaduAvatars</name>
+    <message>
+        <location filename="../gg_avatars.cpp" line="77"/>
+        <source>Refresh Avatar</source>
+        <translation>Odśwież awatara</translation>
+    </message>
+</context>
+</TS>
