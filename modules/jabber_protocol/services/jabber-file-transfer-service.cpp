30dea79c7389cf6b6ee4db27fea8862767b83e67
Wojciech Treter <juzefwt@gmail.com>
2009-05-08 12:46:47 +0000

    Jabber file transfers - first step, part 2: missed files added
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@6615 fea5205d-6df5-0310-bef7-ea67e47da9a5

---
+++ modules/jabber_protocol/services/jabber-file-transfer-service.cpp
@@ -0,0 +1,42 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "misc/misc.h"
+
+#include "file-transfer/jabber-file-transfer.h"
+#include "jabber-contact-account-data.h"
+#include "jabber_protocol.h"
+
+#include "jabber-file-transfer-service.h"
+
+JabberFileTransferService::JabberFileTransferService(JabberProtocol *protocol) :
+		FileTransferService(protocol), Protocol(protocol)
+{
+}
+
+FileTransfer * JabberFileTransferService::loadFileTransferFromStorage(StoragePoint *storage)
+{
+	JabberFileTransfer *jft = new JabberFileTransfer(Protocol->account());
+	jft->setStorage(storage);
+	jft->load();
+
+	return jft;
+}
+
+FileTransfer * JabberFileTransferService::createOutgoingFileTransfer(Contact contact)
+{
+	return new JabberFileTransfer(Protocol->account(), contact, FileTransfer::TypeSend);
+}
+
+void JabberFileTransferService::incomingFile(JabberFileTransfer *transfer)
+{
+		emit incomingFileTransfer(transfer);
+}
+
+// kate: indent-mode cstyle; replace-tabs off; tab-width 4; 
---
+++ modules/jabber_protocol/services/jabber-file-transfer-service.h
@@ -0,0 +1,36 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef JABBER_FILE_TRANSFER_SERVICE_H
+#define JABBER_FILE_TRANSFER_SERVICE_H
+
+#include "protocols/services/file-transfer-service.h"
+#include "file-transfer/jabber-file-transfer.h"
+
+class JabberProtocol;
+
+class JabberFileTransferService : public FileTransferService
+{
+	Q_OBJECT
+
+	JabberProtocol *Protocol;
+
+public:
+	JabberFileTransferService(JabberProtocol *protocol);
+
+	virtual FileTransfer * loadFileTransferFromStorage(StoragePoint *storage);
+	virtual FileTransfer * createOutgoingFileTransfer(Contact contact);
+	
+  public slots:
+	void incomingFile(JabberFileTransfer *transfer);
+
+};
+
+#endif // JABBER_FILE_TRANSFER_SERVICE_H
+
