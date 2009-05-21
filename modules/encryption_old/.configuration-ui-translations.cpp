30026a508d10d3ed096951a520ff29a0e346f196
Tomasz Rostański <rozteck@interia.pl>
2009-05-21 14:55:04 +0000

    Reintroduce encryption based on openssl
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/branches/0.6.5@6665 fea5205d-6df5-0310-bef7-ea67e47da9a5

---
+++ modules/encryption_old/.configuration-ui-translations.cpp
@@ -0,0 +1,41 @@
+
+
+
+	
+			QT_TRANSLATE_NOOP("@default", "Look");
+		
+		
+			QT_TRANSLATE_NOOP("@default", "Colors");
+		
+			
+			QT_TRANSLATE_NOOP("@default", "Chat");
+		
+				
+			QT_TRANSLATE_NOOP("@default", "Encypted message");
+		
+			
+		
+    
+
+	
+			QT_TRANSLATE_NOOP("@default", "Chat");
+		
+		
+			QT_TRANSLATE_NOOP("@default", "Encryption");
+		
+			
+			QT_TRANSLATE_NOOP("@default", "General");
+		
+				
+			QT_TRANSLATE_NOOP("@default", "Encrypt by default");
+		
+				
+			QT_TRANSLATE_NOOP("@default", "Encrypt after receive encrypted message");
+		
+				
+			QT_TRANSLATE_NOOP("@default", "Generate keys");
+		
+			
+		
+	
+
