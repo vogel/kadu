98aed9d119d106b9574e25bb8806b0286dc9a009
Tomasz Rostański <rozteck@interia.pl>
2009-05-31 13:00:35 +0000

    Add modver.h for module speech
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/branches/0.6.5@6715 fea5205d-6df5-0310-bef7-ea67e47da9a5

---
+++ modules/speech/modver.h
@@ -0,0 +1,6 @@
+#ifndef _MODVER_H
+#define DESCRIPTION "Speech module
+#define DESCRIPTION_PL "Modu³ obs³ugi "G³o¶nego czytania" przez zewnêtrzny program 'Powiedz'"
+#define AUTHOR "Kadu Team"
+#define _MODVER_H
+#endif
