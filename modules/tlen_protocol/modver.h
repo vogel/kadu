b08ae656e79faf9002a968c4b14c87533a8f867e
Bartłomiej Zimoń <uzi18@go2.pl>
2009-07-18 20:51:43 +0000

    tlen: unify module description
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@6856 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- modules/tlen_protocol/modver.h
+++ modules/tlen_protocol/modver.h
@@ -1,6 +1,6 @@
 #ifndef _MODVER_H
-#define DESCRIPTION "Tlen protocol support module for Kadu."
-#define DESCRIPTION_PL "Modu³ wspracia protoko³u Tlen dla Kadu."
+#define DESCRIPTION "Tlen protocol support."
+#define DESCRIPTION_PL "Obs³uga protoko³u Tlen."
 #define AUTHOR "Bart³omiej Zimoñ uzi18 // o2 // pl"
 #define VERSION "0.0.1"
 #define _MODVER_H
