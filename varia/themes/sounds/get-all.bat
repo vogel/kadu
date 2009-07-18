7ca8fa2bd72642c932d4f39cbf23206ea8249077
Michał Podsiadlik <michal@kadu.net>
2009-07-18 15:49:04 +0000

    Scripts from branch 0.6.5
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/trunk@6847 fea5205d-6df5-0310-bef7-ea67e47da9a5

---
+++ varia/themes/sounds/get-all.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+SETLOCAL EnableDelayedExpansion 
+
+SET AVAILABLE_MODULES=
+
+FOR /D %%I IN (*) DO (
+	IF EXIST %%I/sound.conf (
+		CALL SET AVAILABLE_MODULES=%%AVAILABLE_MODULES%%;%%I
+	)
+)
+
+REM When echo called without parameter (or with empty one) it prints its status
+REM on screen so we have to avoid that simply adding ; to the end
+CALL SET AVAILABLE_MODULES=%%AVAILABLE_MODULES%%;
+
+ECHO %AVAILABLE_MODULES%
