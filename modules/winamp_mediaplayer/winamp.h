cf3d9d43edbdb23a50507797a3b02892155103d6
Michał Podsiadlik <michal@kadu.net>
2009-07-28 20:43:22 +0000

    Winamp media player module
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/branches/0.6.5@6888 fea5205d-6df5-0310-bef7-ea67e47da9a5

---
+++ modules/winamp_mediaplayer/winamp.h
@@ -0,0 +1,50 @@
+#ifndef FALF_H
+#define FALF_H
+
+#include <QtCore/QObject>
+
+#include "../mediaplayer/player_info.h"
+#include "../mediaplayer/player_commands.h"
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+class WinampMediaPlayer : public PlayerInfo, PlayerCommands
+{
+	public:
+		WinampMediaPlayer();
+		~WinampMediaPlayer();
+
+		// PlayerInfo
+		QString getTitle(int position = -1);
+		QString getAlbum(int position = -1);
+		QString getArtist(int position = -1);
+		QString getFile(int position = -1);
+		int getLength(int position = -1);
+		int getCurrentPos();
+		bool isPlaying();
+		bool isActive();
+		QStringList getPlayListTitles();
+		QStringList getPlayListFiles();
+		uint getPlayListLength();
+		QString getPlayerName();
+		QString getPlayerVersion();
+
+		// PlayerCommands
+		void nextTrack();
+		void prevTrack();
+		void play();
+		void stop();
+		void pause();
+		void setVolume(int vol);
+		void incrVolume();
+		void decrVolume();
+	private:
+		HWND findWinamp();
+		QString readWinampMemory(quint32 command, quint32 arg, bool unicode=false);
+		QString getFileTagW(int position, QString tag);
+		QString getFileTagA(int position, QString tag);		
+		HANDLE hProcess;
+};
+
+#endif
