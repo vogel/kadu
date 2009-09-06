4f8aeceda32119c90aed714dd1d9659bf20b3772
Rafał Malinowski <malinowskirafal@wp.pl>
2009-09-06 19:17:28 +0200

    added ChatType class and ChatTypeManager

---
+++ kadu-core/chat/type/chat-type-manager.h
@@ -0,0 +1,45 @@
+/***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
+#ifndef CHAT_TYPE_MANAGER
+#define CHAT_TYPE_MANAGER
+
+#include <QtCore/QList>
+#include <QtCore/QObject>
+
+#include "chat/type/chat-type.h"
+#include "exports.h"
+
+class KADUAPI ChatTypeManager : public QObject
+{
+	Q_OBJECT
+	Q_DISABLE_COPY(ChatTypeManager)
+
+	static ChatTypeManager * Instance;
+
+	QList<ChatType> ChatTypes;
+
+	ChatTypeManager();
+	virtual ~ChatTypeManager();
+
+public:
+	static ChatTypeManager * instance();
+
+	void addChatType(ChatType chatType);
+	void removeChatType(ChatType chatType);
+
+signals:
+	void chatTypeAboutToBeAdded(ChatType chatType);
+	void chatTypeAdded(ChatType chatType);
+	void chatTypeAboutToBeRemoved(ChatType chatType);
+	void chatTypeRemoved(ChatType chatType);
+
+};
+
+#endif // CHAT_TYPE_MANAGER
