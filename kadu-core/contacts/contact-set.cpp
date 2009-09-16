c5b7ad5b79c1de3468aa0c35b0b66d4a84e401c0
Bartlomiej Zimon <uzi18@o2.pl>
2009-09-16 15:36:57 +0200

    - implementation of Account * ContactSet::prefferedAccount()

--- kadu-core/contacts/contact-set.cpp
+++ kadu-core/contacts/contact-set.cpp
@@ -7,6 +7,7 @@
  *                                                                         *
  ***************************************************************************/
 
+#include "accounts/account-manager.h"
 #include "contacts/contact-list.h"
 
 #include "contact-set.h"
@@ -23,4 +24,36 @@ ContactSet::ContactSet(Contact contact)
 ContactList ContactSet::toContactList() const
 {
 	return toList();
+}
+
+Account * ContactSet::prefferedAccount()
+{
+	QList<Account *> accounts;
+	// TODO 0.6.6 - Rework it if more than 1 account on the same proto.
+
+	ContactList contacts = toList();
+	foreach(Contact contact, contacts)
+	{
+		Account *acc = contact.prefferedAccount();
+		// one contact have no account = no common account
+		if (!acc)
+			return 0;
+
+		accounts.append(acc);
+	}
+
+	Account * accountMaxCount;
+	int maxCount, tmp;
+
+	foreach(Account *account, AccountManager::instance()->accounts())
+	{
+		tmp = accounts.count(account);
+		if (maxCount < tmp)
+		{
+			maxCount = tmp;
+			accountMaxCount = account;
+		}
+	}
+	// TODO 0.6.6 - is it ok?
+	return maxCount == contacts.count() ? accountMaxCount : 0 ;
 }
