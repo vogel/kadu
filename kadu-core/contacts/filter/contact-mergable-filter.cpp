949ff0113f7b74393e719a9e1cd9d8dff9c890b1
Rafał Malinowski <malinowskirafal@wp.pl>
2009-10-16 23:29:35 +0200

    ContactMergableFilter created

---
+++ kadu-core/contacts/filter/contact-mergable-filter.cpp
@@ -0,0 +1,35 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "contact-mergable-filter.h"
+
+ContactMergableFilter::ContactMergableFilter(Contact contact, QObject *parent) :
+		AbstractContactFilter(parent), MyContact(contact)
+{
+}
+
+ContactMergableFilter::~ContactMergableFilter()
+{
+}
+
+void ContactMergableFilter::setContact(Contact contact)
+{
+	if (MyContact == contact)
+		return;
+
+	MyContact = contact;
+	Accounts = contact.accounts().toSet();
+
+	emit filterChanged();
+}
+
+bool ContactMergableFilter::acceptContact(Contact contact)
+{
+	return !Accounts.contains(contact.accounts().toSet());
+}
