5f67b25a02bf636293b7f2fb0ebc400693d86caa
Dawid Stawiarski <neeo@kadu.net>
2009-10-18 10:07:21 +0000

    r7118 revert
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/branches/0.6.5@7122 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- kadu-core/search.cpp
+++ kadu-core/search.cpp
@@ -112,7 +112,7 @@ SearchDialog::SearchDialog(QWidget *parent, UinType whoisSearchUin)
 	l_uin = new QLabel(tr("Uin"),qgrp1);
 	e_uin = new QLineEdit(qgrp1);
 	e_uin->setMaxLength(9);
-	e_uin->setValidator(new QIntValidator(1, 3999999999, centralWidget));
+	e_uin->setValidator(new QIntValidator(1, 999999999, centralWidget));
 	connect(e_uin, SIGNAL(textChanged(const QString &)), this, SLOT(uinTyped()));
 	connect(e_uin, SIGNAL(returnPressed()), this, SLOT(firstSearch()));
 	uinLayout->addWidget(l_uin);
