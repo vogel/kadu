d2ed6ef61daad081d83d1ad7b626414dea1f1a60
Bartlomiej Zimon <uzi18@o2.pl>
2009-11-11 14:22:29 +0100

    - tlen: fix module loading

--- modules/tlen_protocol/tlen-account-details.cpp
+++ modules/tlen_protocol/tlen-account-details.cpp
@@ -34,42 +34,10 @@ void TlenAccountDetails::load()
 		return;
 
 	AccountDetails::load();
-
-	/*
-	AllowDcc = loadValue<bool>("AllowDcC");
-	DccIpDetect = loadValue<bool>("DccIpDetect");
-	DccPort = loadValue<int>("DccPort");
-	DccExternalPort = loadValue<int>("DccExternalPort");
-	DccLocalPort = loadValue<int>("DccLocalPort");
-	RemoveCompletedTransfers = loadValue<bool>("RemoveCompletedTransfers");
-	DccForwarding = loadValue<bool>("DccForwarding");
-
-	QHostAddress host;
-	if (!host.setAddress(loadValue<QString>("DccExternalIp")))
-		host.setAddress("0.0.0.0");
-	DccExternalIP = host;
-	if (!host.setAddress(loadValue<QString>("DccIP")))
-		host.setAddress("0.0.0.0");
-	DccIP = host;
-	*/
 }
 
 void TlenAccountDetails::store()
 {
 	if (!isValidStorage())
 		return;
-
-	AccountDetails::store();
-
-	/*
-	storeValue("AllowDcc", AllowDcc);
-	storeValue("DccIP", DccIP.toString());
-	storeValue("DccIpDetect", DccIpDetect);
-	storeValue("DccPort", DccPort);
-	storeValue("DccExternalIp", DccExternalIP.toString());
-	storeValue("DccExternalPort", DccExternalPort);
-	storeValue("DccLocalPort", DccLocalPort);
-	storeValue("RemoveCompletedTransfers", RemoveCompletedTransfers);
-	storeValue("DccForwarding", DccForwarding);
-	*/
 }
