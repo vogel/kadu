02111a91ce87bbe79eee4f5a50e30b6f58134059
Bartlomiej Zimon <uzi18@o2.pl>
2009-11-23 14:46:59 +0100

    - tlen: add tlen copyright

--- modules/tlen_protocol/QtTlen/tlen.cpp
+++ modules/tlen_protocol/QtTlen/tlen.cpp
@@ -2,6 +2,9 @@
  *   Copyright (C) 2004-2005 by Naresh [Kamil Klimek]                      *
  *   naresh@tlen.pl                                                        *
  *                                                                         *
+ *   Copyright (C) 2008-2009 by uzi18 [Bartłomiej Zimoń]                   *
+ *   uzi18@tlen.pl                                                         *
+ *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
