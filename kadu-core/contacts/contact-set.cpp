9604787c01bc8af7ebc3b97074fad458044f0af4
Bartlomiej Zimon <uzi18@o2.pl>
2009-11-29 14:55:53 +0100

    - core: introducing ContactSet

---
+++ kadu-core/contacts/contact-set.cpp
@@ -0,0 +1,36 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "accounts/account-manager.h"
+#include "contacts/contact-manager.h"
+#include "buddies/buddy-list.h"
+
+#include "contact-set.h"
+
+ContactSet::ContactSet()
+{
+}
+
+ContactSet::ContactSet(Contact contact)
+{
+	insert(contact);
+}
+
+QList<Contact> ContactSet::toContactList() const
+{
+	return toList();
+}
+
+Account ContactSet::account() const
+{
+	if (isEmpty())
+		return Account::null;
+	
+	return toList()[0].contactAccount();
+}
