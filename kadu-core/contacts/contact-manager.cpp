e4a60d481c683037635addb069d63c499549050f
Piotr Galiszewski <piotrgaliszewski@gmail.com>
2009-12-06 19:18:31 +0100

    fix one connect in ContactManager

--- kadu-core/contacts/contact-manager.cpp
+++ kadu-core/contacts/contact-manager.cpp
@@ -50,13 +50,13 @@ void ContactManager::itemAboutToBeRegistered(Contact item)
 void ContactManager::itemRegisterd(Contact item)
 {
 	emit contactAdded(item);
-	connect(item, SIGNAL(contactIdChanged(const QString &)), this, SLOT(idChanged(const QString &)));
+	connect(item.data(), SIGNAL(idChanged(const QString &)), this, SLOT(idChanged(const QString &)));
 }
 
 void ContactManager::itemAboutToBeUnregisterd(Contact item)
 {
 	emit contactAboutToBeRemoved(item);
-	disconnect(item, SIGNAL(contactIdChanged(const QString &)), this, SLOT(idChanged(const QString &)));
+	disconnect(item.data(), SIGNAL(idChanged(const QString &)), this, SLOT(idChanged(const QString &)));
 }
 
 void ContactManager::itemUnregistered(Contact item)
