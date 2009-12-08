6f2e6a8a677f4e2ec1308f9f533b24d5e228c997
Rafał Malinowski <malinowskirafal@wp.pl>
2009-12-08 22:10:23 +0100

    doc: Shared class documented

--- kadu-core/storage/shared.cpp
+++ kadu-core/storage/shared.cpp
@@ -9,15 +9,36 @@
 
 #include "shared.h"
 
+/**
+ * @author Rafal 'Vogel' Malinowski
+ * @param uuid objects uuid
+ * @short Contructs empty object with given (or generated) uuid.
+ *
+ * Contructs empty object with given uuid. When uuid is invalid (NULL)
+ * new uuid is created and assigned to object.
+ */
 Shared::Shared(QUuid uuid) :
 		Uuid(uuid.isNull() ? QUuid::createUuid() : uuid)
 {
 }
 
+/**
+ * @author Rafal 'Vogel' Malinowski
+ * @short Destroys object.
+ *
+ * Destroys object.
+ */
 Shared::~Shared()
 {
 }
 
+/**
+ * @author Rafal 'Vogel' Malinowski
+ * @short Loads object from storage.
+ *
+ * Loads obejct from storage. Loads uuid from uuid attribute. Superclass method
+ * is also called. You must call this method in load methods of derivered class.
+ */
 void Shared::load()
 {
 	if (!isValidStorage())
@@ -27,6 +48,13 @@ void Shared::load()
 	Uuid = QUuid(loadAttribute<QString>("uuid"));
 }
 
+/**
+ * @author Rafal 'Vogel' Malinowski
+ * @short Stores object to storage.
+ *
+ * Storeas obejct to storage. Stored uuid to uuid attribute.
+ * You must call this method in load methods of derivered class.
+ */
 void Shared::store()
 {
 	if (!isValidStorage())
@@ -35,10 +63,28 @@ void Shared::store()
 	storeAttribute("uuid", Uuid.toString());
 }
 
+/**
+ * @author Rafal 'Vogel' Malinowski
+ * @short Method called just before this obejct is removed from storage.
+ *
+ * Method is called just before this obejct is removed from storage.
+ * Override it when you need to take special actions (like removing files from disk).
+ * If this object stored references to other SharedBase objects you must set them
+ * all to null value or remove them (if stored in list).
+ */
 void Shared::aboutToBeRemoved()
 {
 }
 
+/**
+ * @author Rafal 'Vogel' Malinowski
+ * @short Call this method to block calling of emitUpdated method.
+ *
+ * Calling this method blocks calling of emitUpdated when data stored in this object is
+ * changed. Homever, if data is changed after calling this method and unblockUpdatedSignal
+ * is called, emitUpdated is called. If blockUpdatedSignal is called more than once
+ * you have to call unblockUpdatedSignal the same amount to get emitUpdated called.
+ */
 void Shared::blockUpdatedSignal()
 {
 	if (0 == BlockUpdatedSignalCount)
@@ -46,19 +92,42 @@ void Shared::blockUpdatedSignal()
 	BlockUpdatedSignalCount++;
 }
 
+/**
+ * @author Rafal 'Vogel' Malinowski
+ * @short Call this method to unblock calling of emitUpdated method.
+ *
+ * Calling this method unblocks calling of emitUpdated when data stored in this object is
+ * changed. If data was changed after call of blockUpdatedSignal this method calls emitUpdated.
+ * If blockUpdatedSignal was called more than once this method must be calles the same
+ * amount to get emitUpdated called.
+ */
 void Shared::unblockUpdatedSignal()
 {
 	BlockUpdatedSignalCount--;
-	if (0 == BlockUpdatedSignalCount)
-		emitUpdated();
+	emitUpdated();
 }
 
+/**
+ * @author Rafal 'Vogel' Malinowski
+ * @short Call this method when any data was changed.
+ *
+ * Method should be called after any data in this object was changed. Setters implemented by
+ * KaduShared_PropertyWrite, KaduShared_Property, KaduShared_PropertyBoolWrite calls this
+ * method by default.
+ */
 void Shared::dataUpdated()
 {
 	Updated = true;
 	doEmitUpdated();
 }
 
+/**
+ * @author Rafal 'Vogel' Malinowski
+ * @short Calls emitUpdated and changed Updated flag.
+ *
+ * Calls emitUpdated when no blockUpdatedSignal was called without correspondent unblockUpdatedSignal
+ * and any data was changed (Updated flag is set to true). Then resets Updated flag to false.
+ */
 void Shared::doEmitUpdated()
 {
 	if (0 == BlockUpdatedSignalCount && Updated)
@@ -68,6 +137,12 @@ void Shared::doEmitUpdated()
 	}
 }
 
+/**
+ * @author Rafal 'Vogel' Malinowski
+ * @short Method called when any data was changed.
+ *
+ * Method called when any data was changed. Reimplement it to emit signal from you class when needed.
+ */
 void Shared::emitUpdated()
 {
 }
--- kadu-core/storage/shared.h
+++ kadu-core/storage/shared.h
@@ -18,22 +18,113 @@
 
 #include "storage/uuid-storable-object.h"
 
+/**
+ * @addtogroup Storage
+ * @{
+ */
+
+/**
+ * @author Rafal 'Vogel' Malinowski
+ * @short Defines getter for given property of StorableObject.
+ * @param type type of property
+ * @param name name of getter
+ * @param capitalized_name name of property
+ *
+ * Defines getter for given property of @link StorableObject @endlink. Value of property is always
+ * valid because this method calls @link<StorableObject::ensureLoaded @endLink, so object is always
+ * fully loaded before this method returns. Getter is called name.
+ */
 #define KaduShared_PropertyRead(type, name, capitalized_name) \
 	type name() { ensureLoaded(); return capitalized_name; }
+
+/**
+ * @author Rafal 'Vogel' Malinowski
+ * @short Defines setter for given property of StorableObject.
+ * @param type type of property
+ * @param name name of value parameter
+ * @param capitalized_name name of property (setter will be called set##capitalized_name)
+ *
+ * Defines setter for given property of @link StorableObject @endlink. Value of property will not be
+ * ovveriden by load, because this method calls @link<StorableObject::ensureLoaded @endLink, so object is always
+ * fully loaded before this method returns. Setter is called set##capitalized_name).
+ * Calls @link dataUpdated @endlink method after data is changed.
+ */
 #define KaduShared_PropertyWrite(type, name, capitalized_name) \
 	void set##capitalized_name(type name) { ensureLoaded(); capitalized_name = name; dataUpdated(); }
+
+/**
+ * @author Rafal 'Vogel' Malinowski
+ * @short Defines getter and setter for given property of StorableObject.
+ * @param type type of property
+ * @param name name of value parameter
+ * @param capitalized_name name of property.
+ *
+ * Defines both getter and setter for given property of @link StorableObject @endlink. Value of property will
+ * always be valid, because these methods calls @link<StorableObject::ensureLoaded @endLink, so object is always
+ * fully loaded before this method returns. Getter is called name, setter is called set##capitalized_name).
+ * Setter calls @link dataUpdated @endlink method after data is changed.
+ */
 #define KaduShared_Property(type, name, capitalized_name) \
 	KaduShared_PropertyRead(type, name, capitalized_name) \
 	KaduShared_PropertyWrite(type, name, capitalized_name)
 
+
+/**
+ * @author Rafal 'Vogel' Malinowski
+ * @short Defines getter for given boolean property of StorableObject.
+ * @param capitalized_name name of property
+ *
+ * Defines getter for given property of @link StorableObject @endlink. Value of property is always
+ * valid because this method calls @link<StorableObject::ensureLoaded @endLink, so object is always
+ * fully loaded before this method returns. Getter is called is##capitalized_name.
+ */
 #define KaduShared_PropertyBoolRead(capitalized_name) \
 	bool is##capitalized_name() { ensureLoaded(); return capitalized_name; }
+
+/**
+ * @author Rafal 'Vogel' Malinowski
+ * @short Defines setter for given boolean property of StorableObject.
+ * @param capitalized_name name of property (setter will be called set##capitalized_name)
+ *
+ * Defines setter for given property of @link StorableObject @endlink. Value of property will not be
+ * ovveriden by load, because this method calls @link<StorableObject::ensureLoaded @endLink, so object is always
+ * fully loaded before this method returns. Setter is called set##capitalized_name).
+ * Calls @link dataUpdated @endlink method after data is changed.
+ */
 #define KaduShared_PropertyBoolWrite(capitalized_name) \
 	void set##capitalized_name(bool name) { ensureLoaded(); capitalized_name = name; dataUpdated(); }
+
+/**
+ * @author Rafal 'Vogel' Malinowski
+ * @short Defines getter and setter for given boolean property of StorableObject.
+ * @param capitalized_name name of property.
+ *
+ * Defines both getter and setter for given property of @link StorableObject @endlink. Value of property will
+ * always be valid, because these methods calls @link<StorableObject::ensureLoaded @endLink, so object is always
+ * fully loaded before this method returns. Getter is called is##capitlized_name, setter is called set##capitalized_name).
+ * Setter calls @link dataUpdated @endlink method after data is changed.
+ */
 #define KaduShared_PropertyBool(capitalized_name) \
 	KaduShared_PropertyBoolRead(capitalized_name) \
 	KaduShared_PropertyBoolWrite(capitalized_name)
 
+/**
+ * @author Rafal 'Vogel' Malinowski
+ * @class Shared
+ * @short UuidStorableObject that acts like QSharedData - can be used as data for many SharedBase object.
+ *
+ * This class stores data for @link SharedBase @endlink objects that allows many places of code to access
+ * data of one object without concerning about copying, creating or destroying object. Many SharedBase objects
+ * can contain the same Shared object (so the same data) - especially copied objects, so every change
+ * in one of them is immediately visible in all other. Setter 
+ *
+ * Support for notyfiing of any changes to this object is also available. Every change calls
+ * @link dataUpdated @endlink method (that can be ovveriden and used to send a signal). To block
+ * this method (when multiple changes are performed at once) @link blockUpdatedSignal @endlink
+ * can be called. Then first change creates deffered call that is executed after call of
+ * @link unblockUpdatedSignal @endlink (multiple calls of blockUpdatedSignal requires as much calls
+ * of unblockUpdatedSignal to call doEmitUpdated).
+ */
 class Shared : public UuidStorableObject, public QSharedData
 {
 	QUuid Uuid;
@@ -63,5 +154,9 @@ public:
 	void unblockUpdatedSignal();
 
 };
+
+/**
+ * @}
+ */
 
 #endif // SHARED_H
