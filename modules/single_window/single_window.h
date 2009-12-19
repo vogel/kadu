fc648fdc2b5c6a26b8b1a0b8b260f04e631b8472
Tomasz Rostański <rozteck@interia.pl>
2009-12-19 11:15:33 +0000

    Enable shortcuts in single_window module
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/branches/0.6.5@7191 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- modules/single_window/single_window.h
+++ modules/single_window/single_window.h
@@ -1,17 +1,24 @@
 #ifndef SINGLE_WINDOW_H
 #define SINGLE_WINDOW_H
 
-#include <QtGui/QMainWindow>
+#include <QtCore/QList>
 #include <QtGui/QSplitter>
 #include <QtGui/QTabWidget>
+#include <QtGui/QMainWindow>
 
 #include "chat_manager.h"
+#include "main_configuration_window.h"
+#include "configuration_aware_object.h"
 
 class SingleWindow : public QMainWindow, public ChatContainer
+
 {
 	Q_OBJECT
+
 	QSplitter *split;
 	QTabWidget *tabs;
+	QList<int> splitSizes;
+	int rosterPos;
 
 protected:
 	void closeEvent(QCloseEvent *event);
@@ -21,15 +28,34 @@ public:
 	SingleWindow();
 	~SingleWindow();
 	void closeChatWidget(ChatWidget *);
+	int rosterPosition() { return rosterPos; }
 
 public slots:
-	void onOpenChat(ChatWidget *);
-	void onNewChat(ChatWidget *,bool &);
-	void onNewMessage(ChatWidget *);
+	void onOpenChat(ChatWidget *w);
+	void onNewChat(ChatWidget *w, bool &handled);
+	void onNewMessage(ChatWidget *w);
 	void onTabChange(int index);
+	void onChatKeyPressed(QKeyEvent *e, ChatWidget* w, bool &handled);
+	void onkaduKeyPressed(QKeyEvent *e);
 	void closeTab(int index);
 };
 
-extern SingleWindow *singleWindow;
+class SingleWindowManager : public ConfigurationUiHandler, public ConfigurationAwareObject
+{
+	Q_OBJECT
+
+	SingleWindow *singleWindow;
+
+	protected:
+		virtual void configurationUpdated();
+
+	public:
+		SingleWindowManager();
+		~SingleWindowManager();
+
+		virtual void mainConfigurationWindowCreated(MainConfigurationWindow *mainConfigurationWindow) {};
+};
+
+extern SingleWindowManager *singleWindowManager;
 
 #endif /* SingleWindow_H */
