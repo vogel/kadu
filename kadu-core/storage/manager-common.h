03d03453e542fb2f9f65c39c9f8534843bab6322
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2010-01-10 00:42:36 +0100

    doc: NotFoundAction documented
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- kadu-core/storage/manager-common.h
+++ kadu-core/storage/manager-common.h
@@ -10,11 +10,42 @@
 #ifndef MANAGER_COMMON_H
 #define MANAGER_COMMON_H
 
+/**
+ * @addtogroup Storage
+ * @{
+ */
+
+/**
+ * @enum NotFoundAction
+ * @author Rafal 'Vogel' Malinowski
+ *
+ * Action performed by manager when requested item was not found.
+ */
 enum NotFoundAction
 {
+	/**
+	 * @author Rafal 'Vogel' Malinowski
+	 *
+	 * When item is not found, Item::null value will be returned.
+	 */
 	ActionReturnNull,
+	/**
+	 * @author Rafal 'Vogel' Malinowski
+	 *
+	 * When item is not found, new item with given data will be created and returned.
+	 */
 	ActionCreate,
+	/**
+	 * @author Rafal 'Vogel' Malinowski
+	 *
+	 * When item is not found, new item with given data will be created,
+	 * added to manager and returned.
+	 */
 	ActionCreateAndAdd
 };
+
+/**
+ * @}
+ */
 
 #endif // MANAGER_COMMON_H
