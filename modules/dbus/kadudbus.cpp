7c746975af065495f9a75e9f9b9932934aa978d4
Bartlomiej Zimon <uzi18@o2.pl>
2010-01-09 21:26:47 +0100

    - dbus: some work

--- modules/dbus/kadudbus.cpp
+++ modules/dbus/kadudbus.cpp
@@ -7,8 +7,13 @@
  *                                                                         *
  ***************************************************************************/
 
-#include <QtDBus>
 
+#include <QtDBus/QDBusArgument>
+#include <QtDBus/QDBusMetaType>
+
+#include "accounts/account-manager.h"
+#include "buddies/buddy-manager.h"
+#include "contacts/contact-manager.h"
 #include "debug.h"
 
 #include "kadudbus.h"
@@ -26,16 +31,72 @@ extern "C" KADU_EXPORT void dbus_close()
 	dbus = NULL;
 }
 
-KaduDBus::KaduDBus() : QObject()
+QDBusArgument &operator<<(QDBusArgument &argument, const Contact &contact)
 {
+	argument.beginStructure();
+	argument << contact.uuid();
+	argument.endStructure();
+	return argument;
+}
+
+const QDBusArgument &operator>>(const QDBusArgument &argument, Contact &contact)
+{
+	argument.beginStructure();
+	contact = ContactManager::instance()->byUuid(QUuid(argument.asVariant().toString()));
+	argument.endStructure();
+	return argument;
+}
+
+QDBusArgument &operator<<(QDBusArgument &argument, const QList<Contact> &contacts)
+{
+	argument.beginStructure();
+	foreach (const Contact contact, contacts)
+		argument << contact.uuid();
+	argument.endStructure();
+	return argument;
+}
+
+const QDBusArgument &operator>>(const QDBusArgument &argument, QList<Contact> &contacts)
+{
+	argument.beginStructure();
+	// TODO
+	contacts.append(ContactManager::instance()->byUuid(QUuid(argument.asVariant().toString())));
+	argument.endStructure();
+	return argument;
+}
+
+QDBusArgument &operator<<(QDBusArgument &argument, const Account &account)
+{
+	argument.beginStructure();
+	argument << account.uuid();
+	argument.endStructure();
+	return argument;
+}
+
+const QDBusArgument &operator>>(const QDBusArgument &argument, Account &account)
+{
+	argument.beginStructure();
+	account = AccountManager::instance()->byUuid(QUuid(argument.asVariant().toString()));
+	argument.endStructure();
+	return argument;
+}
+
+KaduDBus::KaduDBus() : QObject(), connection(QDBusConnection::sessionBus())
+{
+	qDBusRegisterMetaType<Contact>();
+	qDBusRegisterMetaType<Account>();
+
 	KaduRootObject *root = new KaduRootObject(this);
-	QDBusConnection connection = QDBusConnection::sessionBus();
-	connection.registerObject("/", root, QDBusConnection::ExportAllContents);
+	//connection = QDBusConnection::sessionBus();
+	qDebug() << "Root Object = " << connection.registerObject("/", root, QDBusConnection::ExportAllContents);
+	//connection.registerObject("/Buddies", BuddyManager::instance(), QDBusConnection::ExportAllContents);
+	qDebug() << "Contacts Object = " << connection.registerObject("/Contacts", ContactManager::instance(), QDBusConnection::ExportAllContents | QDBusConnection::ExportChildObjects);
 	connection.registerService("im.kadu");
 }
 
 KaduDBus::~KaduDBus()
 {
+	connection.unregisterService("im.kadu");
 }
 
 KaduDBus* dbus;
