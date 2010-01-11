bcdd9adea160c138d78b253e68566fb0c0ff8554
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2010-01-11 07:19:55 +0100

    doc: ChatFilter documented
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- kadu-core/chat/filter/chat-filter.h
+++ kadu-core/chat/filter/chat-filter.h
@@ -27,18 +27,60 @@
 
 class Chat;
 
+/**
+ * @addtogroup Chat
+ * @{
+ */
+
+/**
+ * @class ChatFilter
+ * @author Rafał 'Vogel' Malinowski
+ * @short Abstract class for filtering chats.
+ *
+ * Abstract class for filtering chats. Derivered classes can be used in models
+ * to filter out some chats. Override acceptChat method to return false if
+ * chat should be filtered out. Emit filterChanged signal to inform model
+ * that setting of this filter has changed and model needs to be invalidated.
+ */
 class KADUAPI ChatFilter : public QObject
 {
 	Q_OBJECT
 
 public:
+
+	/**
+	* @author Rafał 'Vogel' Malinowski
+	* @short Contructs empty ChatFilter object.
+	* @param parent parent QObject
+	*
+	* Contructs empty ChatFilter object.
+	*/
 	ChatFilter(QObject *parent = 0) : QObject(parent) {}
 
+	/**
+	* @author Rafał 'Vogel' Malinowski
+	* @short Returns false if chat should be filtered out.
+	* @param chat Chat object to be tested
+	*
+	* Returns false if chat should be filtered out.
+	*/
 	virtual bool acceptChat(Chat chat) = 0;
 
 signals:
+	/**
+	* @author Rafał 'Vogel' Malinowski
+	* @short Emited when filter settings has changed.
+	*
+	* Signal should be emited every time filter settings has changed
+	* and model should be invalidated to filter items using these
+	* new settings.
+	*/
 	void filterChanged();
 
 };
+
+/**
+ * @}
+ */
 
 #endif // ABSTRACT_CHAT_FILTER
