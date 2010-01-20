c6f8f966d0fdaed15bb6f1af40e503bff3c12ee7
Juzef <juzefwt@tlen.pl>
2010-01-20 19:27:13 +0100

    Adding files missed before

---
+++ modules/jabber_protocol/certificates/trusted-certificates-manager.cpp
@@ -0,0 +1,89 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2009, 2010 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "configuration/configuration-file.h"
+#include "configuration/configuration-manager.h"
+#include "core/core.h"
+
+#include "trusted-certificates-manager.h"
+
+TrustedCertificatesManager * TrustedCertificatesManager::Instance = 0;
+
+KADUAPI TrustedCertificatesManager * TrustedCertificatesManager::instance()
+{
+	if (0 == Instance)
+		Instance = new TrustedCertificatesManager();
+	return Instance;
+}
+
+TrustedCertificatesManager::TrustedCertificatesManager()
+{
+	ConfigurationManager::instance()->registerStorableObject(this);
+	load();
+}
+
+TrustedCertificatesManager::~TrustedCertificatesManager()
+{
+}
+
+StorableObject * TrustedCertificatesManager::storageParent()
+{
+	return 0;
+}
+
+QString TrustedCertificatesManager::storageNodeName()
+{
+	return QLatin1String("JabberTrustedCertificates");
+}
+
+QString TrustedCertificatesManager::storageItemNodeName()
+{
+	return QLatin1String("JabberTrustedCertificate");
+}
+
+void TrustedCertificatesManager::addTrustedCertificate(const QString &certificate)
+{
+	if (certificate.isEmpty())
+		return;
+
+	if (StringList.contains(certificate))
+		removeTrustedCertificate(certificate);
+
+	emit certificateAboutToBeAdded(certificate);
+	StringList.prepend(certificate);
+	emit certificateAdded(certificate);
+}
+
+void TrustedCertificatesManager::removeTrustedCertificate(const QString& certificate)
+{
+	if (!StringList.contains(certificate))
+		return;
+
+	emit certificateAboutToBeRemoved(certificate);
+	StringList.removeAll(certificate);
+	emit certificateRemoved(certificate);
+}
+
+bool TrustedCertificatesManager::isTrusted(const QString &certificate)
+{
+	foreach (QString cert, StringList)
+		if (cert == certificate)
+			return true;
+	return false;
+}
---
+++ modules/jabber_protocol/certificates/trusted-certificates-manager.h
@@ -0,0 +1,54 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2009, 2010 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TRUSTED_CERTIFICATES_MANAGER_H
+#define TRUSTED_CERTIFICATES_MANAGER_H
+
+#include "storage/storable-string-list.h"
+
+class KADUAPI TrustedCertificatesManager : public QObject, public StorableStringList
+{
+	Q_OBJECT
+	Q_DISABLE_COPY(TrustedCertificatesManager)
+
+	static TrustedCertificatesManager *Instance;
+
+	TrustedCertificatesManager();
+	virtual ~TrustedCertificatesManager();
+
+public:
+	static TrustedCertificatesManager * instance();
+
+	virtual StorableObject * storageParent();
+	virtual QString storageNodeName();
+	virtual QString storageItemNodeName();
+
+	void addTrustedCertificate(const QString &certificate);
+	void removeTrustedCertificate(const QString &certificate);
+	bool isTrusted(const QString &certificate);
+
+signals:
+	void certificateAboutToBeAdded(const QString &certificate);
+	void certificateAdded(const QString &certificate);
+	void certificateAboutToBeRemoved(const QString &certificate);
+	void certificateRemoved(const QString &certificate);
+
+};
+
+#endif // TRUSTED_CERTIFICATES_MANAGER_H
