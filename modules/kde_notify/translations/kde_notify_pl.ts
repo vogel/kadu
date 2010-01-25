be07c339a3436cfdcf217957dffb58e70da988fe
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2010-01-25 02:23:08 +0100

    kde_notify: new module imported into GIT
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ modules/kde_notify/translations/kde_notify_pl.ts
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="pl">
+<defaultcodec></defaultcodec>
+<context>
+    <name>@default</name>
+    <message>
+        <source>Timeout</source>
+        <translation>Czas zaniku</translation>
+    </message>
+    <message>
+        <source>Show message content in hint</source>
+        <translation>Pokaż treść wiadomości w dymku</translation>
+    </message>
+    <message>
+        <source>Number of quoted characters</source>
+        <translation>Liczba cytowanych znaków</translation>
+    </message>
+</context>
+<context>
+    <name>KdeNotify</name>
+    <message>
+        <source>View</source>
+        <translation>Pokaż</translation>
+    </message>
+    <message>
+        <source>Chat</source>
+        <translation>Rozmowa</translation>
+    </message>
+    <message>
+        <source>Ignore</source>
+        <translation>Ignoruj</translation>
+    </message>
+</context>
+</TS>
