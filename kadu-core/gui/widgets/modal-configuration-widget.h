2b9099ef60ec21808da9601b11de60e2e8f7b907
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2010-02-10 23:20:37 +0100

    gui: ModalConfigurationWidget documented
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- kadu-core/gui/widgets/modal-configuration-widget.h
+++ kadu-core/gui/widgets/modal-configuration-widget.h
@@ -22,25 +22,91 @@
 
 #include <QtGui/QWidget>
 
+/**
+ * @addtogroup Gui
+ * @{
+ */
+
+/**
+ * @class ModalConfigurationWidget
+ * @author Rafal 'Vogel' Malinowski
+ * @short Widget that should not be hidden when it contains modified data.
+ *
+ * This widget allows to edit some data. When data is modified and not saved
+ * parent widget should not allow to hide it - it should either force saving
+ * data or rejecting it (by apply and cancel methods).
+ *
+ * Current state of widget can be determined by calling needsSave method -
+ * when it returns true, widget cannot be hidden or closed.
+ */
 class ModalConfigurationWidget : public QWidget
 {
 	Q_OBJECT
 
 	bool NeedsSave;
 
+protected:
+	/**
+	 * @author Rafal 'Vogel' Malinowski
+	 * @short Sets new value of needsSave.
+	 *
+	 * Sets new value of needsSave. When new value is different from
+	 * old one, needsSaveChanged signal is emited.
+	 */
+	void setNeedsSave(bool needsSave);
+
 public:
+	/**
+	 * @author Rafal 'Vogel' Malinowski
+	 * @short Creates new ModalConfigurationWidget.
+	 * @param parent parent widget
+	 *
+	 * Creates new ModalConfigurationWidget.
+	 */
 	explicit ModalConfigurationWidget(QWidget *parent = 0);
 	virtual ~ModalConfigurationWidget();
 
+	/**
+	 * @author Rafal 'Vogel' Malinowski
+	 * @short Stores widget data.
+	 *
+	 * This method have to be overloaded in derivered widgets. It stores
+	 * all widget data and marks it as 'unmodified'.
+	 */
 	virtual void apply() = 0;
+
+	/**
+	 * @author Rafal 'Vogel' Malinowski
+	 * @short Resets widget data to oryginal state.
+	 *
+	 * This method have to be overloaded in derivered widgets. It resets
+	 * all widget data to oryginal state and marks it as 'unmodified'.
+	 */
 	virtual void cancel() = 0;
 
-	void setNeedsSave(bool needsSave);
+	/**
+	 * @author Rafal 'Vogel' Malinowski
+	 * @short Returns true when data in widget has been modified.
+	 * @return true when data in widget has been modified
+	 *
+	 * Returns true when data in widget has been modified.
+	 */
 	bool needsSave();
 
 signals:
+	/**
+	 * @author Rafal 'Vogel' Malinowski
+	 * @short Emited when state of data modifiecation in widget has been changed.
+	 *
+	 * Emited when state of data modifiecation in widget has been changed. For example:
+	 * first bit of data is changed, data is saved, data is restored from saved state.
+	 */
 	void needsSaveChanged(bool newNeedsSave);
 
 };
+
+/**
+ * @}
+ */
 
 #endif // MODAL_CONFIGURATION_WIDGET_H
