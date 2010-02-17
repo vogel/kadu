bfc42345065a7d394d24928142feb027e042a88a
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2010-02-17 23:37:45 +0100

    screenshot: added missing files
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ modules/screenshot/gui/widgets/screenshot-tool-box.cpp
@@ -0,0 +1,54 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2009 Wojciech Treter (juzefwt@gmail.com)
+ * Copyright 2008, 2010 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QtGui/QLabel>
+#include <QtGui/QVBoxLayout>
+
+#include "screenshot-tool-box.h"
+
+ScreenshotToolBox::ScreenshotToolBox(QWidget *parent) :
+		QWidget(parent)
+{
+	setWindowFlags(Qt::Tool | Qt::CustomizeWindowHint | Qt::FramelessWindowHint
+			| Qt::WindowStaysOnTopHint | Qt::X11BypassWindowManagerHint);
+
+	QGridLayout *layout = new QGridLayout(this);
+	layout->setMargin(1);
+
+	GeometryLabel = new QLabel(this);
+	FileSizeLabel = new QLabel(tr("0 KB"), this);
+
+	layout->addWidget(GeometryLabel);
+	layout->addWidget(FileSizeLabel);
+}
+
+ScreenshotToolBox::~ScreenshotToolBox()
+{
+}
+
+void ScreenshotToolBox::setGeometry(const QString &geometry)
+{
+	GeometryLabel->setText(geometry);
+}
+
+void ScreenshotToolBox::setFileSize(const QString &fileSize)
+{
+	FileSizeLabel->setText(fileSize);
+}
---
+++ modules/screenshot/gui/widgets/screenshot-tool-box.h
@@ -0,0 +1,44 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2009 Wojciech Treter (juzefwt@gmail.com)
+ * Copyright 2008, 2010 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SCREENSHOT_TOOL_BOX_H
+#define SCREENSHOT_TOOL_BOX_H
+
+#include <QtGui/QWidget>
+
+class QLabel;
+
+class ScreenshotToolBox : public QWidget
+{
+	Q_OBJECT
+
+	QLabel *GeometryLabel;
+	QLabel *FileSizeLabel;
+
+public:
+	explicit ScreenshotToolBox(QWidget *parent = 0);
+	virtual ~ScreenshotToolBox();
+
+	void setGeometry(const QString &geometry);
+	void setFileSize(const QString &fileSize);
+
+};
+
+#endif // SCREENSHOT_TOOL_BOX_H
