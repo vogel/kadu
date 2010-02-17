e63c790f57f7b51e347418b715294c352cf46a74
Piotr Galiszewski <piotrgaliszewski@gmail.com>
2010-02-17 13:24:49 +0100

    core: use QCoreApplication::applicationDirPath instead of custom findMe method

--- kadu-core/misc/path-conversion.cpp
+++ kadu-core/misc/path-conversion.cpp
@@ -21,6 +21,7 @@
  */
 
 #include <QtCore/QtGlobal>
+#include <QtGui/QApplication>
 
 #ifdef Q_OS_WIN
 #include <windows.h>
@@ -35,193 +36,8 @@
 #include "path-conversion.h"
 
 #ifndef Q_OS_WIN
-
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
 #include <pwd.h>
-
-/**
-	sprawdza czy wskazana �cie�ka jest linkiem symbolicznym	i je�eli jest,
-	to pr�buje wyci�gn�� �cie�k� na kt�r� wskazuje ten link
-	zwraca b��d tylko gdy wyst�pi jaki� b��d przy wywo�ywaniu readlink (co wskazuje na jaki� powa�ny b��d)
-	uwaga: je�eli pliku nie ma, to funkcja zwraca _sukces_, bo plik nie jest linkiem
- **/
-static bool delinkify(char *path, int maxlen)
-{
-	kdebugmf(KDEBUG_FUNCTION_START, "%s\n", path);
-	struct stat st;
-	if (lstat(path, &st) == -1)
-	{
-		kdebugf2();
-		return true;
-	}
-	kdebugm(KDEBUG_INFO, "mode: %o\n", st.st_mode);
-	if (!S_ISLNK(st.st_mode))
-	{
-		kdebugf2();
-		return true;
-	}
-	char *path2 = new char[maxlen];
-	ssize_t bytesFilled = readlink(path, path2, maxlen - 1);
-	if (bytesFilled == -1)
-	{
-		fprintf(stderr, "readlink error: '%s'\n", strerror(errno));
-		fflush(stderr);
-		delete [] path2;
-		kdebugf2();
-		return false;
-	}
-	path2[bytesFilled] = 0;
-	memcpy(path, path2, bytesFilled + 1);
-	delete [] path2;
-	kdebugf2();
-	return true;
-}
-
-/**
-	funkcja poszukuje binarki programu na podstawie argv[0] oraz zmiennej PATH
-	je�eli j� znajdzie, to zapisuje �cie�k� pod adres wskazany przez path
-	(o maksymalnej d�ugo�ci len) oraz zwraca path, kt�ry zaka�czany jest znakiem '/'
-	je�eli binarka nie zostanie znaleziona, to zwracany jest NULL
-	w obu przypadkach gwarantowane jest, �e path ko�czy si� znakiem 0
-	(len musi by� > 2)
-**/
-static char *findMe(const char *argv0, char *path, int len)
-{
-	kdebugf();
-	struct stat buf;
-	char *lastslash;
-
-	char *current;
-	char *previous;
-	int l;
-
-
-	if (argv0[0] == '.' && argv0[1] == '/') //�cie�ka wzgl�dem bie��cego katalogu (./)
-	{
-		if (getcwd(path, len - 2) == NULL)
-		{
-			path[0] = 0;
-			kdebugf2();
-			return NULL;
-		}
-		strncat(path, argv0 + 1, len - 1);
-		path[len - 1] = 0;
-		if (!delinkify(path, len))
-		{
-			kdebugf2();
-			return NULL;
-		}
-		lastslash = strrchr(path, '/');
-		lastslash[1] = 0;
-		kdebugf2();
-		return path;
-	}
-
-	if (argv0[0] == '.' && argv0[1] == '.' && argv0[2] == '/') //�cie�ka wzgl�dem bie��cego katalogu (../)
-	{
-		if (getcwd(path, len - 2)==NULL)
-		{
-			path[0] = 0;
-			kdebugf2();
-			return NULL;
-		}
-		strncat(path, "/", len - 1);
-		strncat(path, argv0, len - 1);
-		path[len - 1] = 0;
-		if (!delinkify(path, len))
-		{
-			kdebugf2();
-			return NULL;
-		}
-		lastslash = strrchr(path, '/');
-		lastslash[1] = 0;
-		kdebugf2();
-		return path;
-	}
-
-	if (argv0[0] == '/') //�cie�ka bezwzgl�dna
-	{
-		strncpy(path, argv0, len - 1);
-		path[len - 1] = 0;
-		if (!delinkify(path, len))
-		{
-			kdebugf2();
-			return NULL;
-		}
-		lastslash = strrchr(path, '/');
-		lastslash[1] = 0;
-		kdebugf2();
-		return path;
-	}
-
-	previous = getenv("PATH"); //szukamy we wszystkich katalogach, kt�re s� w PATH
-	while ((current = strchr(previous, ':')))
-	{
-		l = current - previous;
-		if (l > len - 2)
-		{
-			path[0] = 0;
-			kdebugf2();
-			return NULL;
-		}
-
-		memcpy(path, previous, l);
-		path[l] = '/';
-		path[l + 1] = 0;
-		strncat(path, argv0, len);
-		path[len - 1] = 0;
-		if (!delinkify(path, len))
-		{
-			kdebugf2();
-			return NULL;
-		}
-		if (stat(path, &buf) != -1)
-		{
-			if (path[l - 1] == '/')
-				path[l] = 0;
-			else
-				path[l + 1] = 0;
-			kdebugf2();
-			return path;
-		}
-		previous = current + 1;
-	}
-	//nie znale�li�my dot�d (bo szukali�my ':'), wi�c mo�e w pozosta�ej cz��ci co� si� znajdzie?
-	strncpy(path, previous, len - 2);
-	path[len - 2] = 0;
-
-	l = strlen(path);
-	path[l] = '/';
-	path[l + 1] = 0;
-	strncat(path, argv0, len);
-	path[len - 1] = 0;
-	if (!delinkify(path, len))
-	{
-		kdebugf2();
-		return NULL;
-	}
-	if (stat(path, &buf) != -1)
-	{
-		if (path[l - 1] == '/')
-			path[l] = 0;
-		else
-			path[l + 1] = 0;
-		kdebugf2();
-		return path;
-	}
-	else
-	{
-		path[0] = 0;
-		kdebugf2();
-		return NULL;
-	}
-}
+#include <stdlib.h>
 #endif
 
 #ifdef HAVE_EXECINFO
@@ -327,13 +143,13 @@ QString libPath(const QString &f)
 
 QString dataPath(const QString &p, const char *argv0)
 {
-	QString path=p;
+	QString path = p;
 
 	if (argv0 != 0)
 	{
 #ifdef Q_OS_MACX
-		char cpath[1024];
-		if (findMe(argv0, cpath, 1024)==NULL)
+		QString appPath = qApp->applicationDirPath();
+		if (appPath.isEmpty())
 		{
 			fprintf(stderr, "we've got real problem here ;)\n");
 			fflush(stderr);
@@ -341,16 +157,16 @@ QString dataPath(const QString &p, const char *argv0)
 		}
 		else
 		{
-			data_path = QString(cpath) + "../../";
-			lib_path = QString(cpath) + "../../";
+			data_path = appPath + "/../../";
+			lib_path = appPath + "/../../";
 		}
 #elif defined(Q_OS_WIN)
 		WCHAR epath[MAX_PATH+1];
 		GetModuleFileNameW(NULL, epath, MAX_PATH);
 
-		data_path=QString::fromUtf16((const ushort*)epath);
-		data_path.resize(data_path.lastIndexOf('\\')+1);
-		lib_path=data_path;
+		data_path = QString::fromUtf16((const ushort*)epath);
+		data_path.resize(data_path.lastIndexOf('\\') + 1);
+		lib_path = data_path;
 #else
 		QString datadir(DATADIR);
 		QString bindir(BINDIR);
@@ -367,16 +183,16 @@ QString dataPath(const QString &p, const char *argv0)
 		}
 		else
 		{
-			char cpath[1024];
-			if (findMe(argv0, cpath, 1024) == NULL)
+			QString appPath = qApp->applicationDirPath();
+			if (appPath.isEmpty())
 			{
 				data_path = datadir + '/';
 				lib_path = libdir + '/';
 			}
 			else
 			{
-				data_path = QString(cpath) + "../share/";
-				lib_path = QString(cpath) + "../lib/";
+				data_path = appPath + "/../share/";
+				lib_path = appPath + "/../lib/";
 			}
 		}
 #endif
