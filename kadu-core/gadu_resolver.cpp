2ef7663717c5a93d329cea01e9a5c17b514b3059
Dawid Stawiarski <neeo@kadu.net>
2010-02-19 13:58:19 +0000

    freebsd fix
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/branches/0.6.5@7309 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- kadu-core/gadu_resolver.cpp
+++ kadu-core/gadu_resolver.cpp
@@ -1,3 +1,12 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include <QtCore/QTimer>
 #include <QtCore/QString>
 #include <QtNetwork/QHostAddress>
@@ -9,6 +18,7 @@
 #include <io.h>
 #else
 #include <sys/wait.h>
+#include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #endif
