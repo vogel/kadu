bb7124b5e94042776744fa07d0c0aafd52c98bd4
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2010-02-20 01:12:56 +0100

    core: added LongValidator class
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ kadu-core/qt/long-validator.cpp
@@ -0,0 +1,48 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2009 Wojciech Treter (juzefwt@gmail.com)
+ * Copyright 2009, 2009, 2010 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * Copyright 2009 Michał Podsiadlik (michal@kadu.net)
+ * Copyright 2009 Tomasz Rostański (rozteck@interia.pl)
+ * Copyright 2009, 2010 Piotr Galiszewski (piotrgaliszewski@gmail.com)
+ * Copyright 2009, 2009 Bartłomiej Zimoń (uzi18@o2.pl)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "long-validator.h"
+
+LongValidator::LongValidator(qlonglong bottom, qlonglong top, QObject* parent) :
+		QValidator(parent), Bottom(bottom), Top(top)
+{
+}
+
+LongValidator::~LongValidator()
+{
+}
+
+QValidator::State LongValidator::validate(QString &input, int &) const
+{
+	bool ok;
+	qlonglong result = input.toLongLong(&ok);
+
+	if (!ok)
+		return Invalid;
+
+	if (result < Bottom || result > Top)
+		return Invalid;
+
+	return Acceptable;
+}
