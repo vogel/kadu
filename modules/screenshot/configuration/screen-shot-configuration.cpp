43be5857394b38cbed5b38c4524537f2c031ec4b
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2010-02-23 23:51:40 +0100

    screenshot: ScreenShotConfiguration class introduced
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ modules/screenshot/configuration/screen-shot-configuration.cpp
@@ -0,0 +1,76 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2010 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "configuration/configuration-file.h"
+#include "misc/path-conversion.h"
+
+#include "screen-shot-configuration.h"
+
+ScreenShotConfiguration * ScreenShotConfiguration::Instance = 0;
+
+void ScreenShotConfiguration::createInstance()
+{
+	Instance = new ScreenShotConfiguration();
+}
+
+void ScreenShotConfiguration::destroyInstance()
+{
+	delete Instance;
+	Instance = 0;
+}
+
+ScreenShotConfiguration::ScreenShotConfiguration()
+{
+	configurationUpdated();
+
+	if (FileFormat.isEmpty())
+	{
+		createDefaultConfiguration();
+		configurationUpdated();
+	}
+}
+
+ScreenShotConfiguration::~ScreenShotConfiguration()
+{
+
+}
+
+void ScreenShotConfiguration::createDefaultConfiguration()
+{
+	config_file.addVariable("ScreenShot", "fileFormat", "PNG");
+	config_file.addVariable("ScreenShot", "use_short_jpg", true);
+	config_file.addVariable("ScreenShot", "quality", -1);
+	config_file.addVariable("ScreenShot", "path", profilePath("images/"));
+	config_file.addVariable("ScreenShot", "filenamePrefix", "shot");
+	config_file.addVariable("ScreenShot", "paste_clause", true);
+	config_file.addVariable("ScreenShot", "dir_size_warns", true);
+	config_file.addVariable("ScreenShot", "dir_size_limit", 10000);
+}
+
+void ScreenShotConfiguration::configurationUpdated()
+{
+	FileFormat = config_file.readEntry("ScreenShot", "fileFormat", "");
+	UseShortJpgExtension = config_file.readBoolEntry("ScreenShot", "use_short_jpg", true);
+	Quality = config_file.readNumEntry("ScreenShot", "quality", -1);
+	ImagePath = config_file.readEntry("ScreenShot", "path", profilePath("images/"));
+	FileNamePrefix = config_file.readEntry("ScreenShot", "filenamePrefix", "shot");
+	PasteImageClauseIntoChatWidget = config_file.readBoolEntry("ScreenShot", "paste_clause", true);
+	WarnAboutDirectorySize = config_file.readBoolEntry("ScreenShot", "dir_size_warns", true);
+	DirectorySizeLimit = config_file.readNumEntry("ScreenShot", "dir_size_limit", 10000);
+}
---
+++ modules/screenshot/configuration/screen-shot-configuration.h
@@ -0,0 +1,67 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2010 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SCREEN_SHOT_CONFIGURATION_H
+#define SCREEN_SHOT_CONFIGURATION_H
+
+#include <QtCore/QString>
+
+#include "configuration/configuration-aware-object.h"
+
+class ScreenShotConfiguration : private ConfigurationAwareObject
+{
+	Q_DISABLE_COPY(ScreenShotConfiguration)
+
+	static ScreenShotConfiguration * Instance;
+
+	QString FileFormat;
+	bool UseShortJpgExtension;
+	int Quality;
+	QString ImagePath;
+	QString FileNamePrefix;
+	bool PasteImageClauseIntoChatWidget;
+	bool WarnAboutDirectorySize;
+	long DirectorySizeLimit;
+
+	ScreenShotConfiguration();
+	virtual ~ScreenShotConfiguration();
+
+	void createDefaultConfiguration();
+
+protected:
+	virtual void configurationUpdated();
+
+public:
+	static ScreenShotConfiguration * instance();
+
+	static void createInstance();
+	static void destroyInstance();
+
+	QString fileFormat() { return FileFormat; }
+	bool useShortJpgExtension() { return UseShortJpgExtension; }
+	int quality() { return Quality; }
+	QString imagePath() { return ImagePath; }
+	QString fileNamePrefix() { return FileNamePrefix; }
+	bool pasteImageClauseIntoChatWidget() { return PasteImageClauseIntoChatWidget; }
+	bool warnAboutDirectorySize() { return WarnAboutDirectorySize; }
+	long directorySizeLimit() { return DirectorySizeLimit; }
+
+};
+
+#endif // SCREEN_SHOT_CONFIGURATION_H
