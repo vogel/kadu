3e7bbe45781619e664d3e171f91e7f20d2045734
Piotr Galiszewski <piotrgaliszewski@gmail.com>
2010-02-28 10:45:40 +0100

    typo

--- kadu-core/gui/widgets/path-list-edit.cpp
+++ kadu-core/gui/widgets/path-list-edit.cpp
@@ -107,7 +107,7 @@ PathListEditWindow::PathListEditWindow(const QStringList &pathList, QWidget *par
 	QWidget *hm = new QWidget;
 	hm->setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum));
 	QPushButton *ok = new QPushButton(qApp->style()->standardIcon(QStyle::SP_DialogOkButton), tr("OK"), this);
-	QPushButton *cancel = new QPushButton(qApp->style()->standardIcon(QStyle::SP_DialogCancelButton, tr("&Cancel"), this);
+	QPushButton *cancel = new QPushButton(qApp->style()->standardIcon(QStyle::SP_DialogCancelButton), tr("&Cancel"), this);
 	bottom_layout->addWidget(hm);
 	bottom_layout->addWidget(ok);
 	bottom_layout->addWidget(cancel);
