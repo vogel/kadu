417b1e07089fcc289b93f14395853e93a1fd7437
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2010-02-28 16:27:44 +0100

    core: use please-wait image for incoming images
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- kadu-core/chat/message/formatted-message-part.cpp
+++ kadu-core/chat/message/formatted-message-part.cpp
@@ -30,7 +30,7 @@
 QString FormattedMessagePart::loadingImageHtml(const QString &imageId)
 {
 	return QString("<img src=\"file:///%1\" id=\"%2\" />")
-			.arg(IconsManager::instance()->iconPath("32x32/image-loading.png"))
+			.arg(IconsManager::instance()->iconPath("kadu_icons/please-wait.gif"))
 			.arg(imageId);
 }
 
