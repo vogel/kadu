e7200fb0489abe4c924648769457bbfb6c93450f
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2010-03-03 23:53:18 +0100

    core: added MainConfiguration class
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ kadu-core/configuration/main-configuration.h
@@ -0,0 +1,51 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2010 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MAIN_CONFIGURATION_H
+#define MAIN_CONFIGURATION_H
+
+#include <QtCore/QString>
+
+#include "configuration/configuration-aware-object.h"
+
+class MainConfiguration : private ConfigurationAwareObject
+{
+	Q_DISABLE_COPY(MainConfiguration)
+
+	static MainConfiguration * Instance;
+
+	bool SimpleMode;
+
+	MainConfiguration();
+	virtual ~MainConfiguration();
+
+protected:
+	virtual void configurationUpdated();
+
+public:
+	static MainConfiguration * instance();
+
+	static void createInstance();
+	static void destroyInstance();
+
+	bool simpleMode() { return SimpleMode; }
+
+};
+
+#endif // MAIN_CONFIGURATION_H
