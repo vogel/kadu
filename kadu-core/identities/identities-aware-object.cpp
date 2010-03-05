7f9d9a8a427f8f78508bdb48b82cd8b7b554bae6
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2010-03-05 23:31:46 +0100

    identity: IdentitiesAwareObject introduced
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ kadu-core/identities/identities-aware-object.cpp
@@ -0,0 +1,48 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2009, 2010 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "identity-manager.h"
+
+#include "identities-aware-object.h"
+
+KADU_AWARE_CLASS(IdentitiesAwareObject)
+
+void IdentitiesAwareObject::notifyIdentityAdded(Identity identity)
+{
+	foreach (IdentitiesAwareObject *object, Objects)
+		object->identityAdded(identity);
+}
+
+void IdentitiesAwareObject::notifyIdentityRemoved(Identity identity)
+{
+	foreach (IdentitiesAwareObject *object, Objects)
+		object->identityRemoved(identity);
+}
+
+void IdentitiesAwareObject::triggerAllIdentitiesAdded()
+{
+	foreach (Identity identity, IdentityManager::instance()->items())
+		identityAdded(identity);
+}
+
+void IdentitiesAwareObject::triggerAllIdentitiesRemoved()
+{
+	foreach (Identity identity, IdentityManager::instance()->items())
+		identityRemoved(identity);
+}
