a083ca506d3f8d8499d698ef48ff8b3488aa2fbc
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2010-03-13 22:19:53 +0100

    gui: ProgressWindow class introduced
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ kadu-core/gui/windows/progress-window.cpp
@@ -0,0 +1,81 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2010 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QtGui/QApplication>
+#include <QtGui/QCloseEvent>
+#include <QtGui/QDialogButtonBox>
+#include <QtGui/QPushButton>
+#include <QtGui/QStyle>
+#include <QtGui/QVBoxLayout>
+
+#include "gui/widgets/progress-label.h"
+
+#include "progress-window.h"
+
+ProgressWindow::ProgressWindow(QWidget *parent) :
+		QDialog(parent), CanClose(false)
+{
+	createGui();
+}
+
+ProgressWindow::~ProgressWindow()
+{
+}
+
+void ProgressWindow::createGui()
+{
+	QVBoxLayout *mainLayout = new QVBoxLayout(this);
+
+	Progress = new ProgressLabel(tr("Plase wait. New Gadu-Gadu account is being registered."), this);
+	mainLayout->addWidget(Progress);
+
+	QDialogButtonBox *buttons = new QDialogButtonBox(this);
+	CloseButton = new QPushButton(qApp->style()->standardIcon(QStyle::SP_DialogCloseButton), tr("Close"));
+	CloseButton->setEnabled(false);
+	connect(CloseButton, SIGNAL(clicked(bool)), this, SLOT(close()));
+
+	buttons->addButton(CloseButton, QDialogButtonBox::DestructiveRole);
+
+	mainLayout->addWidget(buttons);
+}
+
+void ProgressWindow::enableClosing(bool enable)
+{
+	CanClose = enable;
+	CloseButton->setEnabled(enable);
+}
+
+void ProgressWindow::closeEvent(QCloseEvent *e)
+{
+	if (!CanClose)
+		e->ignore();
+	else
+		QDialog::closeEvent(e);
+}
+
+void ProgressWindow::setState(ProgressIcon::ProgressState state, const QString &text)
+{
+	enableClosing(ProgressIcon::StateInProgress != state);
+	Progress->setState(state, text);
+}
+
+void ProgressWindow::setText(const QString &text)
+{
+	Progress->setText(text);
+}
---
+++ kadu-core/gui/windows/progress-window.h
@@ -0,0 +1,53 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2010 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PROGRESS_WINDOW_H
+#define PROGRESS_WINDOW_H
+
+#include <QtGui/QDialog>
+
+#include "gui/widgets/progress-icon.h"
+
+class ProgressLabel;
+
+class ProgressWindow : public QDialog
+{
+	Q_OBJECT
+
+	bool CanClose;
+
+	ProgressLabel *Progress;
+	QPushButton *CloseButton;
+
+	void createGui();
+	void enableClosing(bool enable);
+
+protected:
+	virtual void closeEvent(QCloseEvent *);
+
+	void setState(ProgressIcon::ProgressState state, const QString &text);
+	void setText(const QString &text);
+
+public:
+	explicit ProgressWindow(QWidget *parent = 0);
+	virtual ~ProgressWindow();
+
+};
+
+#endif // PROGRESS_WINDOW_H
