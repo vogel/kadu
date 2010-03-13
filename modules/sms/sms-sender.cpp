af4817ac81ce63b09c1e83c5da4d85fba88db250
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2010-03-14 00:53:35 +0100

    sms: new version of SmsSender class created
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ modules/sms/sms-sender.cpp
@@ -0,0 +1,57 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2009 Wojciech Treter (juzefwt@gmail.com)
+ * Copyright 2010 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "sms-sender.h"
+
+SmsSender::SmsSender(const QString &number, QObject *parent) :
+		QObject(parent), Number(number)
+{
+	fixNumber();
+}
+
+SmsSender::~SmsSender()
+{
+}
+
+void SmsSender::fixNumber()
+{
+	if (Number.length() == 12 && Number.left(3) == "+48")
+		Number = Number.right(9);
+}
+
+bool SmsSender::validateNumber()
+{
+	return 9 == Number.length();
+}
+
+bool SmsSender::validateSignature()
+{
+	return !Signature.isEmpty();
+}
+
+void SmsSender::setContact(const QString &contact)
+{
+	Contact = contact;
+}
+
+void SmsSender::setSignature(const QString &signature)
+{
+	Signature = signature;
+}
---
+++ modules/sms/sms-sender.h
@@ -0,0 +1,53 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2009 Wojciech Treter (juzefwt@gmail.com)
+ * Copyright 2010 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SMS_SENDER_H
+#define SMS_SENDER_H
+
+#include <QtCore/QObject>
+
+class SmsSender : public QObject
+{
+	Q_OBJECT
+
+	QString Number;
+	QString Contact;
+	QString Signature;
+
+	void fixNumber();
+
+protected:
+	bool validateNumber();
+	bool validateSignature();
+
+public:
+	explicit SmsSender(const QString &number, QObject *parent = 0);
+	virtual ~SmsSender();
+
+	void setContact(const QString &contact);
+	void setSignature(const QString &signature);
+	void sendMessage(const QString &message);
+
+signals:
+	void finished(const QString &errorMessage);
+
+};
+
+#endif // SMS_SENDER_H
