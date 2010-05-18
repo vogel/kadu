b7a96771d48a367fe79d7a1290d7727a8d048628
tomek <tomek@bash.(none)>
2010-05-18 19:00:18 +0200

    Module Growl compilation fix part 1

--- modules/growl_notify/CMakeFiles/growl_notify-translations.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/growl_notify-translations"
-  "growl_notify_pl.qm"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/growl_notify-translations.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
