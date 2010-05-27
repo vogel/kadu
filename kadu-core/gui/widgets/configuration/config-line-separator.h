9d8897320c68429af5bef3dc821732951eeb8a26
Piotr Dąbrowski <ultr@ultra.tux-net>
2010-05-27 11:12:05 +0200

    ConfigLineSeparator widget

---
+++ kadu-core/gui/widgets/configuration/config-line-separator.h
@@ -0,0 +1,49 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2010 Piotr Dąbrowski (ultr@ultr.pl)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CONFIG_LINE_SEPARATOR_H
+#define CONFIG_LINE_SEPARATOR_H
+
+#include <QtGui/QFrame>
+
+#include "gui/widgets/configuration/config-widget.h"
+#include "configuration/configuration-window-data-manager.h"
+
+class ConfigGroupBox;
+
+/**
+	&lt;line-separator id="id" /&gt;
+ **/
+class KADUAPI ConfigLineSeparator : public QFrame, public ConfigWidget
+{
+protected:
+	virtual void createWidgets();
+
+public:
+	ConfigLineSeparator(ConfigGroupBox *parentConfigGroupBox, ConfigurationWindowDataManager *dataManager);
+	virtual ~ConfigLineSeparator() {}
+
+	virtual void loadConfiguration() {};
+	virtual void saveConfiguration() {};
+
+	virtual void show();
+	virtual void hide();
+};
+
+#endif
