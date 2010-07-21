b8f0e53b07d839bfb280cc69c8ed75682fd9b343
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2010-07-22 00:14:40 +0200

    core: new ContactListModel class added
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ kadu-core/contacts/model/contact-list-model.cpp
@@ -0,0 +1,85 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2010 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "contacts/model/contact-data-extractor.h"
+
+#include "contact-list-model.h"
+
+ContactListModel::ContactListModel(QList<Contact> list, QObject *parent) :
+		QAbstractItemModel(parent), List(list)
+{
+}
+
+ContactListModel::~ContactListModel()
+{
+}
+
+QModelIndex ContactListModel::index(int row, int column, const QModelIndex &parent) const
+{
+	return createIndex(row, column, parent.isValid() ? parent.row() : -1);
+}
+
+int ContactListModel::columnCount(const QModelIndex &parent) const
+{
+	Q_UNUSED(parent)
+
+	return 1;
+}
+
+int ContactListModel::rowCount(const QModelIndex &parentIndex) const
+{
+	if (parentIndex.isValid())
+		return 0;
+
+	return List.size();
+}
+
+QModelIndex ContactListModel::parent(const QModelIndex &child) const
+{
+	Q_UNUSED(child)
+
+	return QModelIndex();
+}
+
+QVariant ContactListModel::data(const QModelIndex &index, int role) const
+{
+	return ContactDataExtractor::data(contactAt(index), role, true);
+}
+
+Contact ContactListModel::contactAt(const QModelIndex &index) const
+{
+    int row = index.row();
+	if (row < 0 || row >= List.size())
+		return Contact::null;
+
+	return List.at(row);
+}
+
+const QModelIndex ContactListModel::buddyIndex(Buddy buddy) const
+{
+	int i = 0;
+	foreach (Contact contact, List)
+	{
+		if (contact.ownerBuddy() == buddy)
+			return createIndex(i, 0, 0);
+		i++;
+	}
+
+	return QModelIndex();
+}
