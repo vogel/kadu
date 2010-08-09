01c4e01c8a1296fec2749581e732cf11dccc129c
prudy <prudy1@o2.pl>
2010-08-09 21:46:52 +0200

    remove commented-out code

--- modules/simpleview/simpleview-config-ui.cpp
+++ modules/simpleview/simpleview-config-ui.cpp
@@ -16,10 +16,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-//#include <QtGui/QCheckBox>
 
 #include "configuration/configuration-file.h"
-//#include "gui/widgets/configuration/configuration-widget.h"
 
 #include "simpleview-config-ui.h"
 
