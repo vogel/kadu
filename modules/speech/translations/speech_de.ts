12ae6584e546b0e29271b3d5ac61f70a6f69c9a2
Piotr Galiszewski <piotr.galiszewski@kadu.im>
2010-09-21 16:45:21 +0200

    restore speech module from 0.6.5.x branch

---
+++ modules/speech/translations/speech_de.ts
@@ -0,0 +1,96 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>@default</name>
+    <message>
+        <source>Speech</source>
+        <translation>Sprache</translation>
+    </message>
+    <message>
+        <source>Notifications</source>
+        <translation>Benachrichtigung</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>Allgemein</translation>
+    </message>
+    <message>
+        <source>Maximum number of spoken letters</source>
+        <translation>Maximale Anzahl an Buchstaben, die vorgesagt werden sollen</translation>
+    </message>
+    <message>
+        <source>Message too long (male)</source>
+        <translation>Nachricht zu lang (männlich)</translation>
+    </message>
+    <message>
+        <source>Message too long (female)</source>
+        <translation>Nachricht zu lang (weiblich)</translation>
+    </message>
+    <message>
+        <source>Voice</source>
+        <translation>Stimme</translation>
+    </message>
+    <message>
+        <source>Frequency</source>
+        <translation>Frequenz</translation>
+    </message>
+    <message>
+        <source>Base frequency</source>
+        <translation>Grundfrequenz</translation>
+    </message>
+    <message>
+        <source>Tempo</source>
+        <translation>Tempo</translation>
+    </message>
+    <message>
+        <source>Melody</source>
+        <translation>Melodie</translation>
+    </message>
+    <message>
+        <source>Sound</source>
+        <translation>Klang</translation>
+    </message>
+    <message>
+        <source>Sound system</source>
+        <translation>System Audio</translation>
+    </message>
+    <message>
+        <source>aRts</source>
+        <translation>aRts</translation>
+    </message>
+    <message>
+        <source>Esd</source>
+        <translation>Esd</translation>
+    </message>
+    <message>
+        <source>Dsp</source>
+        <translation>Dsp</translation>
+    </message>
+    <message>
+        <source>Dsp device</source>
+        <translation>Gerät Dsp</translation>
+    </message>
+    <message>
+        <source>Klatt synthesizer (requires dsp)</source>
+        <translation>Klatt (braucht dsp)</translation>
+    </message>
+    <message>
+        <source>Speech program</source>
+        <translation>Programm Speech</translation>
+    </message>
+    <message>
+        <source>Test</source>
+        <translation>Test</translation>
+    </message>
+</context>
+<context>
+    <name>SpeechConfigurationWidget</name>
+    <message>
+        <source>Male format</source>
+        <translation>männliche Stimme</translation>
+    </message>
+    <message>
+        <source>Female format</source>
+        <translation>weibliche Stimme</translation>
+    </message>
+</context>
+</TS>
---
+++ modules/speech/translations/speech_it.ts
@@ -0,0 +1,96 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>@default</name>
+    <message>
+        <source>Speech</source>
+        <translation>Spech</translation>
+    </message>
+    <message>
+        <source>Notifications</source>
+        <translation>Notifiche</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>Generale</translation>
+    </message>
+    <message>
+        <source>Maximum number of spoken letters</source>
+        <translation>Quatntià massima delle lettere pronunciate</translation>
+    </message>
+    <message>
+        <source>Message too long (male)</source>
+        <translation>Messaggio troppo lungo (maschio)</translation>
+    </message>
+    <message>
+        <source>Message too long (female)</source>
+        <translation>Messaggio tropo lungo (femmina)</translation>
+    </message>
+    <message>
+        <source>Voice</source>
+        <translation>Voce</translation>
+    </message>
+    <message>
+        <source>Frequency</source>
+        <translation>Frequenza</translation>
+    </message>
+    <message>
+        <source>Base frequency</source>
+        <translation>Frequenza di base</translation>
+    </message>
+    <message>
+        <source>Tempo</source>
+        <translation>Temp</translation>
+    </message>
+    <message>
+        <source>Melody</source>
+        <translation>Melodia</translation>
+    </message>
+    <message>
+        <source>Sound</source>
+        <translation>Suono</translation>
+    </message>
+    <message>
+        <source>Sound system</source>
+        <translation>Sistema suono</translation>
+    </message>
+    <message>
+        <source>aRts</source>
+        <translation>aRts</translation>
+    </message>
+    <message>
+        <source>Esd</source>
+        <translation>ESD</translation>
+    </message>
+    <message>
+        <source>Dsp</source>
+        <translation>DSP</translation>
+    </message>
+    <message>
+        <source>Dsp device</source>
+        <translation>Dispositivo DSP</translation>
+    </message>
+    <message>
+        <source>Klatt synthesizer (requires dsp)</source>
+        <translation>Syntesi di Klatt (dsp richiesto)</translation>
+    </message>
+    <message>
+        <source>Speech program</source>
+        <translation>Speech program</translation>
+    </message>
+    <message>
+        <source>Test</source>
+        <translation>Prova</translation>
+    </message>
+</context>
+<context>
+    <name>SpeechConfigurationWidget</name>
+    <message>
+        <source>Male format</source>
+        <translation>Formato maschio</translation>
+    </message>
+    <message>
+        <source>Female format</source>
+        <translation>Formato femmina</translation>
+    </message>
+</context>
+</TS>
