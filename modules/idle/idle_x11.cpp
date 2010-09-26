c8c684ea973d8756a37d2e569024f0c2981c23a6
Bartosz Brachaczek <b.brachaczek@gmail.com>
2010-09-26 23:50:41 +0200

    idle/x11: Open and use own Display rather than QApplication's
    Rationale behind this change is the fact that XScreenSaver extension
    registers its close function in the given Display, for they to be called
    in XCloseDisplay(). However, since the idle can be a dynamically loaded
    module, it is unloaded along with -lXss (which kadu-core doesn't link
    to) before we want to actually close QApplication's Display. So, when
    doing XCloseDisplay() called from qt_cleanup(), libX11 will try to
    execute a function provided earlier by XScreenSaver, which no longer
    stays in memory. In such case, bad things will happen (actually, the
    stack gets corrupted and we get a segfault).
    
    Note that currently Kadu will never call QApplication's desctructor and
    the described trouble doesn't take place. Though, now we will be able to
    properly destroy QApplication instance.
    
    Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

--- modules/idle/idle_x11.cpp
+++ modules/idle/idle_x11.cpp
@@ -20,12 +20,11 @@
 
 #include "idle.h"
 
-#include <QtGui/QX11Info>
-
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/extensions/scrnsaver.h>
 
+static Display *display = 0;
 static XErrorHandler old_handler = 0;
 XScreenSaverInfo *ss_info = 0;
 
@@ -41,10 +40,12 @@ Idle::Idle()
 {
 	if (ss_info == 0)
 	{
+		display = XOpenDisplay(0);
+
 		old_handler = XSetErrorHandler(xerrhandler);
 
 		int event_base, error_base;
-		if(XScreenSaverQueryExtension(QX11Info::display(), &event_base, &error_base))
+		if(XScreenSaverQueryExtension(display, &event_base, &error_base))
 		{
 			ss_info = XScreenSaverAllocInfo();
 		}
@@ -58,10 +59,16 @@ Idle::~Idle()
 		XFree(ss_info);
 	}
 
-	if(old_handler) 
+	if(old_handler)
 	{
 		XSetErrorHandler(old_handler);
 		old_handler = 0;
+	}
+
+	if (display)
+	{
+		XCloseDisplay(display);
+		display = 0;
 	}
 }
 
@@ -75,7 +82,7 @@ int Idle::secondsIdle()
 	if(ss_info == 0)
 		return -1;
 
-	if (!XScreenSaverQueryInfo(QX11Info::display(), QX11Info::appRootWindow(), ss_info))
+	if (!XScreenSaverQueryInfo(display, DefaultRootWindow(display), ss_info))
 		return -1;
 
 	return ss_info->idle / 1000;
