1d080ba7adea4f28c12c76c2c7ac0cbf883262eb
Tomasz Rostanski <rozteck@interia.pl>
2010-11-03 09:44:47 +0100

    Initial version of plus gateway script for sms module

--- modules/sms/data/scripts/gateway-plus.js
+++ modules/sms/data/scripts/gateway-plus.js
@@ -27,7 +27,8 @@ GatewayPlus.prototype = {
 	},
 
 	sendSms: function(recipient, sender, signature, content, callbackObject) {
-		callbackObject.failure("Not implemented");
+		var sender = new PlusGatewaySmsSender();
+		sender.sendSms(recipient, sender, signature, content, callbackObject);
 	}
 };
 
@@ -36,3 +37,104 @@ function GatewayPlus() {
 }
 
 gatewayManager.addItem(new GatewayPlus());
+
+PlusGatewaySmsSender.prototype = {
+
+	failure: function(errorMessage) {
+		this.callbackObject.failure(errorMessage);
+	},
+
+	finished: function() {
+		this.callbackObject.result();
+	},
+
+	sendSms: function(recipient, sender, signature, content, callbackObject) {
+		this.callbackObject = callbackObject;
+
+		if (!network) {
+			this.failure("Network not available");
+			return;
+		}
+
+		var separator = String.fromCharCode(0xef, 0xbf, 0xbf);
+		var end = String.fromCharCode(0x0d, 0x0a, 0x0d, 0x0a);
+
+		var postUrl = "http://www1.plus.pl/bsm/service/SendSmsService";
+		var postData = 
+		"3" + separator + "0" + separator + "11" + separator +
+		"http://www1.plus.pl/bsm/" + separator +
+		"0C1263E2047195AA1A9BA7FDDF8A4B74" + separator +
+		"pl.plus.map.bsm.gwt.client.service.SendSmsService" + separator + "send" + separator +
+		"pl.plus.map.bsm.core.gwt.dto.MessageTO" + separator +
+		"pl.plus.map.bsm.core.gwt.dto.MessageTO/2025876352" + separator +
+		"638fe7871b8f5e6bcb4e3fc6a44a69ce" + separator +
+		content + separator +
+		recipient + separator +
+		signature + separator + separator + 
+		"1" + separator +
+		"2" + separator +
+		"3" + separator +
+		"4" + separator +
+		"1" + separator +
+		"5" + separator +
+		"6" + separator +
+		"0" + separator +
+		"0" + separator +
+		"7" + separator +
+		"0" + separator +
+		"0" + separator +
+		"8" + separator +
+		"0" + separator +
+		"0" + separator +
+		"1" + separator +
+		"2" + separator +
+		"0" + separator +
+		"0" + separator +
+		"0" + separator +
+		"0" + separator +
+		"0" + separator +
+		"0" + separator +
+		"0" + separator +
+		"0" + separator +
+		"0" + separator +
+		"0" + separator +
+		"0" + separator +
+		"0" + separator +
+		"0" + separator +
+		"0" + separator +
+		"0" + separator +
+		"9" + separator +
+		"0" + separator +
+		"0" + separator +
+		"0" + separator +
+		"10"+ separator +
+		"0" + separator +
+		"0" + separator +
+		"11"+ separator +
+		"0" + separator +
+		"0" + separator +
+		"0" + separator + end;
+
+		this.reply = network.post(postUrl, postData);
+		this.reply.finished.connect(this, this.smsSent);
+	},
+
+	smsSent: function() {
+		var content = this.reply.content();
+			this.failure(content);
+
+		if (content.indexOf("SMS zostaï¿½ wysï¿½any") >= 0)
+			this.finished();
+		else if (content.indexOf("wiadomo¶æ zosta³a wys³ana") >= 0)
+			this.finished();
+		else if (content.indexOf("wiadomoÅ›Ä‡ zostaÅ‚a wysÅ‚ana") >= 0)
+			this.finished();
+		else
+			this.failure("Provider gateway results page looks strange. SMS was probably NOT sent.");
+	}
+
+};
+
+function PlusGatewaySmsSender() {
+	return this;
+}
