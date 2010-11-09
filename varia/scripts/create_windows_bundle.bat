d2ece553a5017da2f49fb3a55141e035c26e0c56
unknown <tomek@.(none)>
2010-11-09 17:40:19 +0100

    Install Adium styles in create_windows_bundle.bat script

--- varia/scripts/create_windows_bundle.bat
+++ varia/scripts/create_windows_bundle.bat
@@ -19,46 +19,55 @@ ECHO Removing existing installation
 rd %DESTINATION% /S /Q
 
 ECHO Copying kadu files
-xcopy kadu-core\kadu*.exe %DESTINATION%\ /R /Y > install.log
-xcopy kadu-core\kadu*.dll %DESTINATION%\ /R /Y >> install.log
-xcopy kadu-core\kadu.ico  %DESTINATION%\ /R /Y >> install.log
-xcopy AUTHORS   %DESTINATION%\ /R /Y >> install.log
-xcopy ChangeLog %DESTINATION%\ /R /Y >> install.log
-xcopy COPYING   %DESTINATION%\ /R /Y >> install.log
-xcopy HISTORY   %DESTINATION%\ /R /Y >> install.log
-xcopy README    %DESTINATION%\ /R /Y >> install.log
-xcopy THANKS    %DESTINATION%\ /R /Y >> install.log
+xcopy kadu-core\kadu*.exe %DESTINATION%\ /R /Y /Q
+xcopy kadu-core\kadu*.dll %DESTINATION%\ /R /Y /Q
+xcopy kadu-core\kadu.ico  %DESTINATION%\ /R /Y /Q
+xcopy AUTHORS   %DESTINATION%\ /R /Y /Q
+xcopy ChangeLog %DESTINATION%\ /R /Y /Q
+xcopy COPYING   %DESTINATION%\ /R /Y /Q
+xcopy HISTORY   %DESTINATION%\ /R /Y /Q
+xcopy README    %DESTINATION%\ /R /Y /Q
+xcopy THANKS    %DESTINATION%\ /R /Y /Q
 
-xcopy  kadu-core\chat\style-engines\chat-scripts.js %DESTINATION%\scripts\ /C /H /R /Y  >> install.log
+xcopy  kadu-core\chat\style-engines\chat-scripts.js %DESTINATION%\scripts\ /C /H /R /Y /Q
 
-xcopy varia\configuration\*.ui        %DESTINATION%\configuration\ /C /H /R /Y  >> install.log
-xcopy varia\syntax\chat\*.syntax      %DESTINATION%\syntax\chat\ /C /H /R /Y  >> install.log
-xcopy varia\syntax\infopanel\*.syntax %DESTINATION%\syntax\infopanel\ /C /H /R /Y  >> install.log
-xcopy translations\*.qm               %DESTINATION%\translations\ /C /H /R /Y >> install.log
+xcopy varia\configuration\*.ui        %DESTINATION%\configuration\ /C /H /R /Y /Q
+xcopy varia\syntax\chat\*.syntax      %DESTINATION%\syntax\chat\ /C /H /R /Y /Q
 
-xcopy varia\themes\emoticons\penguins\* %DESTINATION%\themes\emoticons\penguins\ /C /H /R /Y >> install.log
-xcopy varia\themes\icons\default\*      %DESTINATION%\themes\icons\default\ /E /C /H /R /Y >> install.log
-xcopy varia\themes\sounds\default\*     %DESTINATION%\themes\sounds\default\ /C /H /R /Y >> install.log
+cd varia\syntax\chat\
+for /D %%F in (*) do (
+	IF EXIST %%F\Contents (
+		xcopy "%%F" %DESTINATION%\syntax\chat\"%%F" /C /H /R /Y /S /I /Q
+	)
+)
+cd ..\..\..
+
+xcopy varia\syntax\infopanel\*.syntax %DESTINATION%\syntax\infopanel\ /C /H /R /Y /Q
+xcopy translations\*.qm               %DESTINATION%\translations\ /C /H /R /Y /Q
+
+xcopy varia\themes\emoticons\penguins\* %DESTINATION%\themes\emoticons\penguins\ /C /H /R /Y /Q
+xcopy varia\themes\icons\default\*      %DESTINATION%\themes\icons\default\ /E /C /H /R /Y  /Q
+xcopy varia\themes\sounds\default\*     %DESTINATION%\themes\sounds\default\ /C /H /R /Y /Q
 
 ECHO Copying modules
 cd modules
 for /D %%F in (*) do (
 	IF EXIST %%F\*.a (
 		IF EXIST %%F\*.dll (
-		  xcopy %%F\*.dll  %DESTINATION%\modules\ /C /H /R /Y  >> install.log
+		  xcopy %%F\*.dll  %DESTINATION%\modules\ /C /H /R /Y /Q
 		)
-		xcopy %%F\*.desc %DESTINATION%\modules\ /C /H /R /Y  >> install.log
+		xcopy %%F\*.desc %DESTINATION%\modules\ /C /H /R /Y /Q
 		IF EXIST %%F\configuration\*.ui (
-		  xcopy %%F\configuration\*.ui %DESTINATION%\modules\configuration\ /C /H /R /Y  >> install.log
+		  xcopy %%F\configuration\*.ui %DESTINATION%\modules\configuration\ /C /H /R /Y /Q
 		)
 		IF EXIST %%F\data\configuration\*.ui (
-		  xcopy %%F\data\configuration\*.ui %DESTINATION%\modules\configuration\ /C /H /R /Y  >> install.log
+		  xcopy %%F\data\configuration\*.ui %DESTINATION%\modules\configuration\ /C /H /R /Y /Q
 		)
 		IF EXIST %%F\*.qm (
-		  xcopy %%F\*.qm   %DESTINATION%\modules\translations\ /C /H /R /Y  >> install.log
+		  xcopy %%F\*.qm   %DESTINATION%\modules\translations\ /C /H /R /Y /Q
 		)
 		IF EXIST %%F\data\* (
-		  xcopy %%F\data\* %DESTINATION%\modules\data\%%F\ /C /H /R /Y /E  >> install.log
+		  xcopy %%F\data\* %DESTINATION%\modules\data\%%F\ /C /H /R /Y /E /Q
 		)
 	)
 )
@@ -66,46 +75,45 @@ echo LoadByDefault=true >> %DESTINATION%\modules\qt4_sound.desc
 cd .. 
 
 ECHO Copying Qt
-REM xcopy %QT_DIR%Qt3Support4.dll    %DESTINATION%\ /C /H /R /Y  >> install.log
-xcopy %QT_DIR%QtCore4.dll        %DESTINATION%\ /C /H /R /Y  >> install.log
-xcopy %QT_DIR%QtGui4.dll         %DESTINATION%\ /C /H /R /Y  >> install.log
-xcopy %QT_DIR%QtNetwork4.dll     %DESTINATION%\ /C /H /R /Y  >> install.log
-xcopy %QT_DIR%QtSql4.dll         %DESTINATION%\ /C /H /R /Y  >> install.log
-xcopy %QT_DIR%QtSvg4.dll         %DESTINATION%\ /C /H /R /Y  >> install.log
-xcopy %QT_DIR%QtWebKit4.dll      %DESTINATION%\ /C /H /R /Y  >> install.log
-xcopy %QT_DIR%QtXml4.dll         %DESTINATION%\ /C /H /R /Y  >> install.log
-xcopy %QT_DIR%QtScript4.dll      %DESTINATION%\ /C /H /R /Y  >> install.log
-xcopy %QT_DIR%QtScriptTools4.dll %DESTINATION%\ /C /H /R /Y  >> install.log
-xcopy %QT_DIR%mingwm10.dll       %DESTINATION%\ /C /H /R /Y  >> install.log
-xcopy %QT_DIR%QtXmlPatterns4.dll %DESTINATION%\ /C /H /R /Y  >> install.log
-xcopy %QT_DIR%phonon4.dll        %DESTINATION%\ /C /H /R /Y  >> install.log
-xcopy %QT_DIR%libgcc_s_dw2-1.dll %DESTINATION%\ /C /H /R /Y  >> install.log
+xcopy %QT_DIR%QtCore4.dll        %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%QtGui4.dll         %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%QtNetwork4.dll     %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%QtSql4.dll         %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%QtSvg4.dll         %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%QtWebKit4.dll      %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%QtXml4.dll         %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%QtScript4.dll      %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%QtScriptTools4.dll %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%mingwm10.dll       %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%QtXmlPatterns4.dll %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%phonon4.dll        %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%libgcc_s_dw2-1.dll %DESTINATION%\ /C /H /R /Y /Q
 
 ECHO Copying Qt plugins
-xcopy %QT_DIR%..\plugins\imageformats\qgif4.dll    %DESTINATION%\plugins\imageformats\ /C /H /R /Y  >> install.log
-xcopy %QT_DIR%..\plugins\imageformats\qjpeg4.dll   %DESTINATION%\plugins\imageformats\ /C /H /R /Y  >> install.log
-xcopy %QT_DIR%..\plugins\imageformats\qmng4.dll    %DESTINATION%\plugins\imageformats\ /C /H /R /Y  >> install.log
-xcopy %QT_DIR%..\plugins\imageformats\qsvg4.dll    %DESTINATION%\plugins\imageformats\ /C /H /R /Y  >> install.log
-xcopy %QT_DIR%..\plugins\imageformats\qtiff4.dll   %DESTINATION%\plugins\imageformats\ /C /H /R /Y  >> install.log
-xcopy %QT_DIR%..\plugins\imageformats\qico4.dll    %DESTINATION%\plugins\imageformats\ /C /H /R /Y  >> install.log
-xcopy %QT_DIR%..\plugins\sqldrivers\qsqlite4.dll   %DESTINATION%\plugins\sqldrivers\ /C /H /R /Y  >> install.log
-xcopy %QT_DIR%..\plugins\iconengines\qsvgicon4.dll %DESTINATION%\plugins\iconengines\ /C /H /R /Y  >> install.log
+xcopy %QT_DIR%..\plugins\imageformats\qgif4.dll    %DESTINATION%\plugins\imageformats\ /C /H /R /Y /Q
+xcopy %QT_DIR%..\plugins\imageformats\qjpeg4.dll   %DESTINATION%\plugins\imageformats\ /C /H /R /Y /Q
+xcopy %QT_DIR%..\plugins\imageformats\qmng4.dll    %DESTINATION%\plugins\imageformats\ /C /H /R /Y /Q
+xcopy %QT_DIR%..\plugins\imageformats\qsvg4.dll    %DESTINATION%\plugins\imageformats\ /C /H /R /Y /Q
+xcopy %QT_DIR%..\plugins\imageformats\qtiff4.dll   %DESTINATION%\plugins\imageformats\ /C /H /R /Y /Q
+xcopy %QT_DIR%..\plugins\imageformats\qico4.dll    %DESTINATION%\plugins\imageformats\ /C /H /R /Y /Q
+xcopy %QT_DIR%..\plugins\sqldrivers\qsqlite4.dll   %DESTINATION%\plugins\sqldrivers\   /C /H /R /Y /Q
+xcopy %QT_DIR%..\plugins\iconengines\qsvgicon4.dll %DESTINATION%\plugins\iconengines\  /C /H /R /Y /Q
 
 
 ECHO [Paths] > %DESTINATION%\qt.conf
 ECHO Plugins = plugins >> %DESTINATION%\qt.conf
 
 ECHO Copying libgadu
-xcopy %LIBGADU_DIR%libgadu.dll %DESTINATION%\ /C /H /R /Y  >> install.log
+xcopy %LIBGADU_DIR%libgadu.dll %DESTINATION%\ /C /H /R /Y /Q
 
 ECHO Copying libsndfile
-xcopy %LIBSNDFILE_DIR%libsndfile-1.dll %DESTINATION%\ /C /H /R /Y  >> install.log
+xcopy %LIBSNDFILE_DIR%libsndfile-1.dll %DESTINATION%\ /C /H /R /Y /Q
 
 ECHO Copying zlib
-xcopy %ZLIB_DIR%zlib1.dll %DESTINATION%\ /C /H /R /Y  >> install.log
+xcopy %ZLIB_DIR%zlib1.dll %DESTINATION%\ /C /H /R /Y /Q
 
 ECHO Copying libidn
-xcopy %IDN_DIR%libidn.dll %DESTINATION%\ /C /H /R /Y  >> install.log
+xcopy %IDN_DIR%libidn.dll %DESTINATION%\ /C /H /R /Y /Q
 
 rem ECHO Stripping
 rem cd %DESTINATION%
@@ -119,17 +127,17 @@ rem   MOVE /Y "%%F.stripped" "%%F"
 rem )
 
 ECHO Copying libcrypto
-xcopy %OPENSSL_DIR%bin\libeay32.dll %DESTINATION%\ /C /H /R /Y  >> install.log
-xcopy %OPENSSL_DIR%bin\libssl32.dll %DESTINATION%\ /C /H /R /Y  >> install.log
+xcopy %OPENSSL_DIR%bin\libeay32.dll %DESTINATION%\ /C /H /R /Y /Q
+xcopy %OPENSSL_DIR%bin\libssl32.dll %DESTINATION%\ /C /H /R /Y /Q
 
 IF EXIST %QCA_DIR%qca2.dll (
 ECHO Copying QCA
-xcopy %QCA_DIR%qca2.dll %DESTINATION%\ /C /H /R /Y  >> install.log
+xcopy %QCA_DIR%qca2.dll %DESTINATION%\ /C /H /R /Y /Q
 )
 IF EXIST %QCA_OSSL_DIR%qca-ossl2.dll (
 ECHO Copying QCA Plugins
-xcopy %QCA_OSSL_DIR%qca-ossl2.dll %DESTINATION%\plugins\crypto\ /C /H /R /Y  >> install.log
-xcopy %QCA_OSSL_DIR%qca-gnupg2.dll %DESTINATION%\plugins\crypto\ /C /H /R /Y  >> install.log
+xcopy %QCA_OSSL_DIR%qca-ossl2.dll  %DESTINATION%\plugins\crypto\ /C /H /R /Y /Q
+xcopy %QCA_OSSL_DIR%qca-gnupg2.dll %DESTINATION%\plugins\crypto\ /C /H /R /Y /Q
 )
 
 ECHO Done
