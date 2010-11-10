b4445aa7f8484d1edf030cdf7721685848920638
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2010-11-10 23:45:26 +0100

    core: InvalidPasswordNotification added
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ kadu-core/protocols/invalid-password-notification.cpp
@@ -0,0 +1,73 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2007 Dawid Stawiarski (neeo@kadu.net)
+ * Copyright 2009 Wojciech Treter (juzefwt@gmail.com)
+ * Copyright 2007, 2009, 2010 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * Copyright 2009 Piotr Galiszewski (piotrgaliszewski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "notify/notification-manager.h"
+#include "notify/notify-event.h"
+#include "parser/parser.h"
+#include "debug.h"
+#include "icons-manager.h"
+
+#include "invalid-password-notification.h"
+
+NotifyEvent *InvalidPasswordNotification::InvalidPasswordNotifyEvent = 0;
+QSet<Account> InvalidPasswordNotification::ActiveErrors;
+
+void InvalidPasswordNotification::registerEvent()
+{
+	if (InvalidPasswordNotifyEvent)
+		return;
+
+	InvalidPasswordNotifyEvent = new NotifyEvent("InvalidPassword", NotifyEvent::CallbackRequired, QT_TRANSLATE_NOOP("@default", "Invalid password"));
+	NotificationManager::instance()->registerNotifyEvent(InvalidPasswordNotifyEvent);
+}
+
+void InvalidPasswordNotification::unregisterEvent()
+{
+	if (!InvalidPasswordNotifyEvent)
+		return;
+
+	NotificationManager::instance()->unregisterNotifyEvent(InvalidPasswordNotifyEvent);
+	delete InvalidPasswordNotifyEvent;
+	InvalidPasswordNotifyEvent = 0;
+}
+
+bool InvalidPasswordNotification::activeError(Account account)
+{
+	return ActiveErrors.contains(account);
+}
+
+InvalidPasswordNotification::InvalidPasswordNotification(Account account) :
+		AccountNotification(account, "InvalidPassword", "dialog-error")
+{
+	setTitle(tr("Invalid password"));
+	setText(tr("<b>Invalid password for:</b> %1").arg(account.accountIdentity().name()));
+
+	addCallback(tr("Enter new password"), SLOT(callbackAccept()), "callbackAccept()");
+	addCallback(tr("Ignore"), SLOT(callbackDiscard()), "callbackDiscard()");
+
+	ActiveErrors.insert(account);
+}
+
+InvalidPasswordNotification::~InvalidPasswordNotification()
+{
+	ActiveErrors.remove(account());
+}
--- kadu-core/protocols/protocols-manager.cpp
+++ kadu-core/protocols/protocols-manager.cpp
@@ -24,6 +24,7 @@
 #include "configuration/xml-configuration-file.h"
 #include "misc/misc.h"
 #include "protocols/connection-error-notification.h"
+#include "protocols/invalid-password-notification.h"
 #include "protocols/protocol-factory.h"
 #include "protocols/protocols-aware-object.h"
 
@@ -44,11 +45,13 @@ ProtocolsManager * ProtocolsManager::instance()
 ProtocolsManager::ProtocolsManager()
 {
 	ConnectionErrorNotification::registerEvent();
+	InvalidPasswordNotification::registerEvent();
 }
 
 ProtocolsManager::~ProtocolsManager()
 {
 	ConnectionErrorNotification::unregisterEvent();
+	InvalidPasswordNotification::unregisterEvent();
 }
 
 void ProtocolsManager::registerProtocolFactory(ProtocolFactory *factory)
