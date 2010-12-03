vogel/kadu

commit 3d1f028d762273c5a922b1cc3a3fb717178149dc

gadu: #1795 fixed: kadu wants to remove anonymous contacts after importing contacts from server

kadu-core/protocols/services/contact-list-service.cpp
@@ -118,17 +118,22 @@ void ContactListService::setBuddiesList(BuddyList buddies, bool removeOld)
 			unImportedContacts.removeAll(contact);
 	}
 
+	QStringList contactsList;
+	for (QList<Contact>::iterator i = unImportedContacts.begin(); i != unImportedContacts.end() ; ++i)
+	{
+		// TODO: why not Contact.ownerBuddy()?
+		Buddy buddy = BuddyManager::instance()->byId(CurrentProtocol->account(), (*i).id(), ActionCreate);
+		if (buddy.isAnonymous())
+		{
+			unImportedContacts.removeAll((*i));
+			continue;
+		}
+		if (!contactsList.contains(buddy.display()))
+			contactsList.append(buddy.display());
+	}
+
 	if (removeOld && !unImportedContacts.isEmpty())
 	{
-		// create names list
-		QStringList contactsList;
-		foreach (const Contact &c, unImportedContacts)
-		{
-			QString display = BuddyManager::instance()->byId(CurrentProtocol->account(), c.id(), ActionCreate).display();
-			if (!contactsList.contains(display))
-				contactsList.append(display);
-		}
-
 		if (MessageDialog::ask("", tr("Kadu"), tr("Following contacts from your list were not found on server: <b>%0</b>.\nDo you want to remove them from contacts list?").arg(contactsList.join("</b>, <b>"))))
 		{
 			foreach (const Contact &c, unImportedContacts)

commit d2b15ea7984313f94a3167245ad5e8cd84c32145

protocols/services: Fix bad crash after receiving contact list from server
Introduced in 3d1f028d762273c5a922b1cc3a3fb717178149dc. Please remember
that you have to be careful when removing items from list you iterate.
It's not the first time we have bug of this kind, see
136bfb882a488ab9b74ff4d4b679b9248c9d5e04.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

kadu-core/protocols/services/contact-list-service.cpp
@@ -119,17 +119,21 @@ void ContactListService::setBuddiesList(BuddyList buddies, bool removeOld)
 	}
 
 	QStringList contactsList;
-	for (QList<Contact>::iterator i = unImportedContacts.begin(); i != unImportedContacts.end() ; ++i)
+	QList<Contact>::iterator i = unImportedContacts.begin();
+	while (i != unImportedContacts.end())
 	{
 		// TODO: why not Contact.ownerBuddy()?
-		Buddy buddy = BuddyManager::instance()->byId(CurrentProtocol->account(), (*i).id(), ActionCreate);
+		Buddy buddy = BuddyManager::instance()->byId(CurrentProtocol->account(), i->id(), ActionCreate);
 		if (buddy.isAnonymous())
 		{
-			unImportedContacts.removeAll((*i));
+			i = unImportedContacts.erase(i);
 			continue;
 		}
+
 		if (!contactsList.contains(buddy.display()))
 			contactsList.append(buddy.display());
+
+		++i;
 	}
 
 	if (removeOld && !unImportedContacts.isEmpty())