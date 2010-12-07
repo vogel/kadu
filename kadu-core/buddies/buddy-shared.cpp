358685dd52a617811ca5f3612e24f2a8ce7387f8
Piotr Galiszewski <piotr.galiszewski@kadu.im>
2010-12-07 22:35:03 +0100

    core: #1766 fixed: save buddy's gender in configuration

--- kadu-core/buddies/buddy-shared.cpp
+++ kadu-core/buddies/buddy-shared.cpp
@@ -168,6 +168,7 @@ void BuddyShared::load()
 	Website = loadValue<QString>("Website");
 	Blocked = loadValue<bool>("Blocked", false);
 	OfflineTo = loadValue<bool>("OfflineTo", false);
+	Gender = (BuddyGender)loadValue<int>("Gender", 0);
 }
 
 void BuddyShared::store()
@@ -204,6 +205,7 @@ void BuddyShared::store()
 	storeValue("Anonymous", Anonymous);
 	storeValue("Blocked", Blocked);
 	storeValue("OfflineTo", OfflineTo);
+	storeValue("Gender", (int)Gender);
 
 	if (Groups.count())
 	{
