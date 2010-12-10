2bc0d8cc40460de688bdbc944b5431c3211d70a2
Piotr Galiszewski <piotrgaliszewski@gmail.com>
2010-12-10 18:17:42 +0100

    openChatWidth: send also left key to buddy list

--- kadu-core/gui/windows/open-chat-with/open-chat-with.cpp
+++ kadu-core/gui/windows/open-chat-with/open-chat-with.cpp
@@ -123,6 +123,7 @@ bool OpenChatWith::eventFilter(QObject *obj, QEvent *e)
 					key == Qt::Key_Up ||
 					key == Qt::Key_PageDown ||
 					key == Qt::Key_PageUp ||
+					(key == Qt::Key_Left && ContactID->cursorPosition() == 0) ||
 					(key == Qt::Key_Right && ContactID->cursorPosition() == ContactID->text().length()))
 			{
 				qApp->sendEvent(BuddiesWidget, e);
