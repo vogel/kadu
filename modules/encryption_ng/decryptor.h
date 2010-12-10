148b0e8e61d3ba31722462e36e592aeabcf10eed
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2010-12-10 18:54:59 +0100

    encryption_ng: added some abstract classes
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ modules/encryption_ng/decryptor.h
@@ -0,0 +1,34 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2010 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DECRYPTOR_H
+#define DECRYPTOR_H
+
+class QByteArray;
+
+class Decryptor
+{
+public:
+	virtual ~Decryptor() {}
+
+	QByteArray decrypt(const QByteArray &data) = 0;
+
+};
+
+#endif // DECRYPTOR_H
