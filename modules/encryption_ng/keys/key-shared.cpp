8288d3fc1dd42f62f2b5573ae4dfdf8fa0c4b828
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2010-12-24 14:46:08 +0100

    encryption-ng: more secure key storage
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- modules/encryption_ng/keys/key-shared.cpp
+++ modules/encryption_ng/keys/key-shared.cpp
@@ -103,13 +103,19 @@ void KeyShared::store()
 
 	QDir keysDir(KeysDir + KeyType);
 	if (!keysDir.exists())
+	{
 		keysDir.mkpath(KeysDir + KeyType);
+		QFile::setPermissions(KeysDir, QFile::ReadOwner | QFile::WriteOwner | QFile::ExeOwner);
+		QFile::setPermissions(KeysDir + KeyType, QFile::ReadOwner | QFile::WriteOwner | QFile::ExeOwner);
+	}
 
 	if (Key.isEmpty())
 		QFile::remove(filePath());
 	else
 	{
 		QFile keyFile(filePath());
+		keyFile.setPermissions(QFile::ReadOwner | QFile::WriteOwner);
+
 		if (keyFile.open(QFile::WriteOnly))
 		{
 			keyFile.write(Key);
