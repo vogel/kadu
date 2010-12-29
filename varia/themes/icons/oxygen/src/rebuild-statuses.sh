bf3686ff9a25c682cd38f116bf64f8c288f6b19f
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2010-12-29 17:17:48 +0100

    gui: added oxygen icon theme from ultr (Vogel)
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ varia/themes/icons/oxygen/src/rebuild-statuses.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+
+FILTER="Blackman"
+
+
+for size in "16x16" "22x22" "32x32" "48x48" "64x64"; do
+	mkdir -p ../${size}
+done
+for size in "16x16" "22x22" "32x32" "48x48" "64x64"; do
+	mkdir -p ../kadu_icons/${size}
+done
+
+
+mkdir -p ../kadu_icons/64x64
+for kaduicon in "blocked" "blocking"; do
+	xcf2png statuses.xcf ${kaduicon} > ../kadu_icons/64x64/kadu-${kaduicon}.png
+	for size in "16x16" "22x22" "32x32" "48x48"; do
+		mkdir -p ../kadu_icons/${size}
+		convert ../kadu_icons/64x64/kadu-${kaduicon}.png -resize ${size} -filter ${FILTER} ../kadu_icons/${size}/kadu-${kaduicon}.png
+	done;
+done;
+
+for protocol in "common" "gadu-gadu" "xmpp"; do
+	mkdir -p ../protocols/${protocol}/64x64
+	for status in "offline" "invisible" "do_not_disturb" "not_available" "away" "online" "free_for_chat"; do
+		for desc in "" "_d"; do
+			for mobile in "" "_m"; do
+				xcf2png statuses.xcf ${status}_ ${mobile} ${desc} _${protocol} > ../protocols/${protocol}/64x64/${status}${desc}${mobile}.png
+				for size in "16x16" "22x22" "32x32" "48x48"; do
+					mkdir -p ../protocols/${protocol}/${size}
+					convert ../protocols/${protocol}/64x64/${status}${desc}${mobile}.png -resize ${size} -filter ${FILTER} ../protocols/${protocol}/${size}/${status}${desc}${mobile}.png
+				done
+			done
+		done
+	done
+done
+
+
+for size in "16x16" "22x22" "32x32" "48x48" "64x64"; do
+	mkdir -p ../protocols/common/${size}
+	cp ../protocols/common/${size}/offline.png ../${size}/user-offline.png
+	cp ../protocols/common/${size}/online.png  ../${size}/user-online.png
+done
+
+
+xcf2png statuses.xcf offline50_ _d ___force_d > ../kadu_icons/64x64/kadu-showdescriptionusers_off.png
+for size in "16x16" "22x22" "32x32" "48x48"; do
+	convert ../kadu_icons/64x64/kadu-showdescriptionusers_off.png -resize ${size} -filter ${FILTER} ../kadu_icons/${size}/kadu-showdescriptionusers_off.png
+done
+xcf2png statuses.xcf offline50_ _d > ../kadu_icons/64x64/kadu-showdescriptionusers.png
+for size in "16x16" "22x22" "32x32" "48x48"; do
+	convert ../kadu_icons/64x64/kadu-showdescriptionusers.png -resize ${size} -filter ${FILTER} ../kadu_icons/${size}/kadu-showdescriptionusers.png
+done
+
+
+xcf2png statuses.xcf online_ _d ___force_online_d > ../kadu_icons/64x64/kadu-onoff_onlineandd_off.png
+for size in "16x16" "22x22" "32x32" "48x48"; do
+	convert ../kadu_icons/64x64/kadu-onoff_onlineandd_off.png -resize ${size} -filter ${FILTER} ../kadu_icons/${size}/kadu-onoff_onlineandd_off.png
+done
+xcf2png statuses.xcf online_ _d > ../kadu_icons/64x64/kadu-onoff_onlineandd.png
+for size in "16x16" "22x22" "32x32" "48x48"; do
+	convert ../kadu_icons/64x64/kadu-onoff_onlineandd.png -resize ${size} -filter ${FILTER} ../kadu_icons/${size}/kadu-onoff_onlineandd.png
+done
+
+
+echo "Oxygen status icons rebuilt."
