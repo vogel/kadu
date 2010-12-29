5e92bf9952e12f25cfa7d1ee0159f412d3bbf8b5
Bartosz Brachaczek <b.brachaczek@gmail.com>
2010-12-29 23:23:52 +0100

    encryption-ng: Fix crash on Kadu exit because of destruction of a QCA::Initializer object when QCA dynamic library is no more opened
    Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

--- modules/encryption_ng/encryption-ng-module.cpp
+++ modules/encryption_ng/encryption-ng-module.cpp
@@ -34,17 +34,24 @@
 
 namespace EncryptionNg
 {
-	QCA::Initializer InitObject;
+	static QCA::Initializer *InitObject;
 }
 
 extern "C" int encryption_ng_init(bool firstLoad)
 {
+	EncryptionNg::InitObject = new QCA::Initializer();
+
 	if (!QCA::isSupported("pkey") ||
 			!QCA::PKey::supportedIOTypes().contains(QCA::PKey::RSA) ||
 			!QCA::isSupported("sha1"))
 	{
 		MessageDialog::exec("dialog-error", QApplication::tr("Encryption"),
 				QApplication::tr("The QCA OSSL plugin for libqca2 is not present!"));
+
+		delete EncryptionNg::InitObject;
+		EncryptionNg::InitObject = 0;
+		qRemovePostRoutine(QCA::deinit);
+
 		return -1;
 	}
 
@@ -67,5 +74,7 @@ extern "C" void encryption_ng_close()
 	EncryptionNgConfigurationUiHandler::unregisterConfigurationUi();
 	EncryptionNgConfiguration::destroyInstance();
 
+	delete EncryptionNg::InitObject;
+	EncryptionNg::InitObject = 0;
 	qRemovePostRoutine(QCA::deinit);
 }
