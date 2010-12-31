vogel/kadu

commit d405e9e9d38e13a7c220ebe68e7a76faee2ac46e

gadu: Fix major bug in GaduFormatter::computeFormatsSize() (#1141, #1868)
Now it returns the same number of bytes that
GaduFormatter::createFormats() will need.

In 0.6.5 branch it is fixed with
6d7c26f76c38207f72da2a458801891d69e7ee70.

http://kadu.net/mantis/view.php?id=1141
http://kadu.net/mantis/view.php?id=1868

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

modules/gadu_protocol/helpers/gadu-formatter.cpp
@@ -47,24 +47,18 @@ unsigned int computeFormatsSize(const FormattedMessage &message)
 
 	foreach (const FormattedMessagePart &part, message.parts())
 	{
-		if (!first || part.isImage() || part.bold() || part.italic() || part.underline() || part.color().isValid())
+		if (first && !part.isImage() && !part.bold() && !part.italic() && !part.underline() && !part.color().isValid())
 		{
-			size += sizeof(struct gg_msg_richtext_format);
-			first = false;
-		}
-
-		if (part.isImage())
-		{
-			size += sizeof(struct gg_msg_richtext_image);
 			first = false;
 			continue;
 		}
 
-		if (part.color().isValid())
-		{
+		size += sizeof(struct gg_msg_richtext_format);
+
+		if (part.isImage())
+			size += sizeof(struct gg_msg_richtext_image);
+		else if (part.color().isValid())
 			size += sizeof(struct gg_msg_richtext_color);
-			first = false;
-		}
 	}
 
 	return first ? 0 : size;

commit 6dedaaa71da9cf8b86cea0fc13758aa302de21ce

gadu: fixed formatting logic broken in d405e9e9d38e13a7c220ebe68e7a76faee2ac46e
Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

modules/gadu_protocol/helpers/gadu-formatter.cpp
@@ -47,10 +47,10 @@ unsigned int computeFormatsSize(const FormattedMessage &message)
 
 	foreach (const FormattedMessagePart &part, message.parts())
 	{
-		if (first && !part.isImage() && !part.bold() && !part.italic() && !part.underline() && !part.color().isValid())
-			first = false;
-		else
+		if (!first || part.isImage() || part.bold() || part.italic() || part.underline() || part.color().isValid())
 		{
+			first = false;
+
 			size += sizeof(struct gg_msg_richtext_format);
 
 			if (part.isImage())
@@ -85,10 +85,10 @@ unsigned char * createFormats(Account account, const FormattedMessage &message, 
 
 	foreach (const FormattedMessagePart &part, message.parts())
 	{
-		if (first && !part.isImage() && !part.bold() && !part.italic() && !part.underline() && !part.color().isValid())
-			first = false;
-		else
+		if (!first || part.isImage() || part.bold() || part.italic() || part.underline() || part.color().isValid())
 		{
+			first = false;
+
 			format.position = gg_fix16(textPosition);
 			format.font = 0;
 