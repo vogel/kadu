056b1b49706b6c42fcb2aafd793b5f21c9fa97d7
Bartosz Brachaczek <b.brachaczek@gmail.com>
2010-12-31 02:24:46 +0100

    sms: Add copyright information to sms-configuration-ui-handler.h
    Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

--- modules/sms/sms-configuration-ui-handler.h
+++ modules/sms/sms-configuration-ui-handler.h
@@ -1,5 +1,32 @@
-#ifndef SMS_H
-#define SMS_H
+/*
+ * %kadu copyright begin%
+ * Copyright 2004, 2008 Michał Podsiadlik (michal@kadu.net)
+ * Copyright 2004, 2005 Adrian Smarzewski (adrian@kadu.net)
+ * Copyright 2004, 2005, 2006 Marcin Ślusarz (joi@kadu.net)
+ * Copyright 2005 Paweł Płuciennik (pawel_p@kadu.net)
+ * Copyright 2007 Dawid Stawiarski (neeo@kadu.net)
+ * Copyright 2007, 2008, 2010 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * Copyright 2008, 2010 Piotr Galiszewski (piotrgaliszewski@gmail.com)
+ * Copyright 2009, 2010 Wojciech Treter (juzefwt@gmail.com)
+ * Copyright 2010 Bartosz Brachaczek (b.brachaczek@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SMS_CONFIGURATION_UI_HANDLER_H
+#define SMS_CONFIGURATION_UI_HANDLER_H
 
 #include <QtCore/QBuffer>
 #include <QtCore/QMap>
@@ -51,4 +78,4 @@ public slots:
 
 extern SMSAPI SmsConfigurationUiHandler *smsConfigurationUiHandler;
 
-#endif
+#endif // SMS_CONFIGURATION_UI_HANDLER_H
