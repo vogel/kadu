3a4242ff2331ea1c59c74c48ba8d5264be297018
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2011-01-06 13:48:36 +0100

    profiles_import: stub of module added
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ modules/profiles_import/profiles-import-module.cpp
@@ -0,0 +1,28 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2010 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+extern "C" int profiles_import_init(bool firstLoad)
+{
+	(void)firstLoad;
+	return 0;
+}
+
+extern "C" void profiles_import_close()
+{
+}
