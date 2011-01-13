679ecc6eb40c94d1954d994ced9801852cea7817
Bartosz Brachaczek <b.brachaczek@gmail.com>
2011-01-13 01:05:38 +0100

    scripts: Improve create_windows_bundle.bat to support installing Qt translations and Qt debug files
    Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

--- varia/scripts/create_windows_bundle.bat
+++ varia/scripts/create_windows_bundle.bat
@@ -1,11 +1,12 @@
 @ECHO OFF
 
-set QT_VERSION="4.7.0"
 set DESTINATION="C:\Program Files\Kadu-0.6.6"
-set QT_DIR="C:\Qt\%QT_VERSION%\bin\"
+set QT_DIR="C:\Qt\4.7.0\bin\"
+set QT_TRANSLATIONS_DIR="C:\Qt\2010.05\qt\translations\"
+set QT_DEBUG_SUFFIX=""
 set LIBGADU_DIR="c:\Qt\libgadu-win32\"
 set LIBSNDFILE_DIR="C:\Qt\libsndfile\"
-set OPENSSL_DIR="C:\Qt\mingw32-openssl-0.9.8j\"
+set OPENSSL_DIR="C:\Qt\mingw32-openssl-0.9.8j\bin\"
 set ZLIB_DIR="C:\Qt\zlib\"
 set IDN_DIR="C:\Qt\libidn\"
 set QCA_DIR="C:\Qt\qca-2.0.1-mingw\bin\"
@@ -18,7 +19,7 @@ REM EXIT
 ECHO Removing existing installation
 rd %DESTINATION% /S /Q
 
-ECHO Copying kadu files
+ECHO Copying Kadu core
 xcopy kadu-core\kadu*.exe %DESTINATION%\ /R /Y /Q
 xcopy kadu-core\kadu*.dll %DESTINATION%\ /R /Y /Q
 xcopy kadu-core\kadu.ico  %DESTINATION%\ /R /Y /Q
@@ -29,7 +30,7 @@ xcopy HISTORY   %DESTINATION%\ /R /Y /Q
 xcopy README    %DESTINATION%\ /R /Y /Q
 xcopy THANKS    %DESTINATION%\ /R /Y /Q
 
-xcopy  kadu-core\chat\style-engines\chat-scripts.js %DESTINATION%\scripts\ /C /H /R /Y /Q
+xcopy kadu-core\chat\style-engines\chat-scripts.js %DESTINATION%\scripts\ /C /H /R /Y /Q
 
 xcopy varia\configuration\*.ui        %DESTINATION%\configuration\ /C /H /R /Y /Q
 xcopy varia\syntax\chat\*.syntax      %DESTINATION%\syntax\chat\ /C /H /R /Y /Q
@@ -68,7 +69,7 @@ for /D %%D in (*) do (
 )
 cd ..\..\..
 
-ECHO Copying modules
+ECHO Copying Kadu modules
 cd modules
 for /D %%F in (*) do (
 	IF EXIST %%F\*.a (
@@ -91,36 +92,42 @@ for /D %%F in (*) do (
 	)
 )
 echo LoadByDefault=true >> %DESTINATION%\modules\qt4_sound.desc
-cd .. 
+cd ..
 
 ECHO Copying Qt
-xcopy %QT_DIR%QtCore4.dll        %DESTINATION%\ /C /H /R /Y /Q
-xcopy %QT_DIR%QtGui4.dll         %DESTINATION%\ /C /H /R /Y /Q
-xcopy %QT_DIR%QtNetwork4.dll     %DESTINATION%\ /C /H /R /Y /Q
-xcopy %QT_DIR%QtSql4.dll         %DESTINATION%\ /C /H /R /Y /Q
-xcopy %QT_DIR%QtSvg4.dll         %DESTINATION%\ /C /H /R /Y /Q
-xcopy %QT_DIR%QtWebKit4.dll      %DESTINATION%\ /C /H /R /Y /Q
-xcopy %QT_DIR%QtXml4.dll         %DESTINATION%\ /C /H /R /Y /Q
-xcopy %QT_DIR%QtScript4.dll      %DESTINATION%\ /C /H /R /Y /Q
-xcopy %QT_DIR%QtScriptTools4.dll %DESTINATION%\ /C /H /R /Y /Q
-xcopy %QT_DIR%mingwm10.dll       %DESTINATION%\ /C /H /R /Y /Q
-xcopy %QT_DIR%QtXmlPatterns4.dll %DESTINATION%\ /C /H /R /Y /Q
-xcopy %QT_DIR%phonon4.dll        %DESTINATION%\ /C /H /R /Y /Q
-xcopy %QT_DIR%libgcc_s_dw2-1.dll %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%libgcc_s_dw2-1.dll                  %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%mingwm10.dll                        %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%QtCore%QT_DEBUG_SUFFIX%4.dll        %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%QtGui%QT_DEBUG_SUFFIX%4.dll         %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%QtNetwork%QT_DEBUG_SUFFIX%4.dll     %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%QtScript%QT_DEBUG_SUFFIX%4.dll      %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%QtScriptTools%QT_DEBUG_SUFFIX%4.dll %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%QtSql%QT_DEBUG_SUFFIX%4.dll         %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%QtSvg%QT_DEBUG_SUFFIX%4.dll         %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%QtWebKit%QT_DEBUG_SUFFIX%4.dll      %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%QtXml%QT_DEBUG_SUFFIX%4.dll         %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%QtXmlPatterns%QT_DEBUG_SUFFIX%4.dll %DESTINATION%\ /C /H /R /Y /Q
+xcopy %QT_DIR%phonon%QT_DEBUG_SUFFIX%4.dll        %DESTINATION%\ /C /H /R /Y /Q
 
 ECHO Copying Qt plugins
-xcopy %QT_DIR%..\plugins\imageformats\qgif4.dll    %DESTINATION%\plugins\imageformats\ /C /H /R /Y /Q
-xcopy %QT_DIR%..\plugins\imageformats\qjpeg4.dll   %DESTINATION%\plugins\imageformats\ /C /H /R /Y /Q
-xcopy %QT_DIR%..\plugins\imageformats\qmng4.dll    %DESTINATION%\plugins\imageformats\ /C /H /R /Y /Q
-xcopy %QT_DIR%..\plugins\imageformats\qsvg4.dll    %DESTINATION%\plugins\imageformats\ /C /H /R /Y /Q
-xcopy %QT_DIR%..\plugins\imageformats\qtiff4.dll   %DESTINATION%\plugins\imageformats\ /C /H /R /Y /Q
-xcopy %QT_DIR%..\plugins\imageformats\qico4.dll    %DESTINATION%\plugins\imageformats\ /C /H /R /Y /Q
-xcopy %QT_DIR%..\plugins\sqldrivers\qsqlite4.dll   %DESTINATION%\plugins\sqldrivers\   /C /H /R /Y /Q
-xcopy %QT_DIR%..\plugins\iconengines\qsvgicon4.dll %DESTINATION%\plugins\iconengines\  /C /H /R /Y /Q
+xcopy %QT_DIR%..\plugins\iconengines\qsvgicon%QT_DEBUG_SUFFIX%4.dll %DESTINATION%\plugins\iconengines\  /C /H /R /Y /Q
+xcopy %QT_DIR%..\plugins\imageformats\qgif%QT_DEBUG_SUFFIX%4.dll    %DESTINATION%\plugins\imageformats\ /C /H /R /Y /Q
+xcopy %QT_DIR%..\plugins\imageformats\qico%QT_DEBUG_SUFFIX%4.dll    %DESTINATION%\plugins\imageformats\ /C /H /R /Y /Q
+xcopy %QT_DIR%..\plugins\imageformats\qjpeg%QT_DEBUG_SUFFIX%4.dll   %DESTINATION%\plugins\imageformats\ /C /H /R /Y /Q
+xcopy %QT_DIR%..\plugins\imageformats\qmng%QT_DEBUG_SUFFIX%4.dll    %DESTINATION%\plugins\imageformats\ /C /H /R /Y /Q
+xcopy %QT_DIR%..\plugins\imageformats\qsvg%QT_DEBUG_SUFFIX%4.dll    %DESTINATION%\plugins\imageformats\ /C /H /R /Y /Q
+xcopy %QT_DIR%..\plugins\imageformats\qtiff%QT_DEBUG_SUFFIX%4.dll   %DESTINATION%\plugins\imageformats\ /C /H /R /Y /Q
+xcopy %QT_DIR%..\plugins\sqldrivers\qsqlite%QT_DEBUG_SUFFIX%4.dll   %DESTINATION%\plugins\sqldrivers\   /C /H /R /Y /Q
 
+ECHO Copying Qt translations
+xcopy %QT_TRANSLATIONS_DIR%qt_de.qm %DESTINATION%\translations\  /C /H /R /Y /Q
+xcopy %QT_TRANSLATIONS_DIR%qt_fr.qm %DESTINATION%\translations\  /C /H /R /Y /Q
+xcopy %QT_TRANSLATIONS_DIR%qt_it.qm %DESTINATION%\translations\  /C /H /R /Y /Q
+xcopy %QT_TRANSLATIONS_DIR%qt_pl.qm %DESTINATION%\translations\  /C /H /R /Y /Q
 
 ECHO [Paths] > %DESTINATION%\qt.conf
 ECHO Plugins = plugins >> %DESTINATION%\qt.conf
+ECHO Translations = translations >> %DESTINATION%\qt.conf
 
 ECHO Copying libgadu
 xcopy %LIBGADU_DIR%libgadu.dll %DESTINATION%\ /C /H /R /Y /Q
@@ -146,17 +153,14 @@ rem   MOVE /Y "%%F.stripped" "%%F"
 rem )
 
 ECHO Copying libcrypto
-xcopy %OPENSSL_DIR%bin\libeay32.dll %DESTINATION%\ /C /H /R /Y /Q
-xcopy %OPENSSL_DIR%bin\libssl32.dll %DESTINATION%\ /C /H /R /Y /Q
+xcopy %OPENSSL_DIR%libeay32.dll %DESTINATION%\ /C /H /R /Y /Q
+xcopy %OPENSSL_DIR%libssl32.dll %DESTINATION%\ /C /H /R /Y /Q
 
-IF EXIST %QCA_DIR%qca2.dll (
 ECHO Copying QCA
 xcopy %QCA_DIR%qca2.dll %DESTINATION%\ /C /H /R /Y /Q
-)
-IF EXIST %QCA_OSSL_DIR%qca-ossl2.dll (
+
 ECHO Copying QCA Plugins
 xcopy %QCA_OSSL_DIR%qca-ossl2.dll  %DESTINATION%\plugins\crypto\ /C /H /R /Y /Q
 xcopy %QCA_OSSL_DIR%qca-gnupg2.dll %DESTINATION%\plugins\crypto\ /C /H /R /Y /Q
-)
 
 ECHO Done
