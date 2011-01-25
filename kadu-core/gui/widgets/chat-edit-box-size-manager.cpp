fd1344543a3935eb836f7a161439db3c3169d056
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2011-01-25 15:06:24 +0100

    gui: missing files added
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ kadu-core/gui/widgets/chat-edit-box-size-manager.cpp
@@ -0,0 +1,48 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2011 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "chat-edit-box-size-manager.h"
+
+ChatEditBoxSizeManager * ChatEditBoxSizeManager::Instance = 0;
+
+ChatEditBoxSizeManager * ChatEditBoxSizeManager::instance()
+{
+	if (!Instance)
+		Instance = new ChatEditBoxSizeManager();
+
+	return Instance;
+}
+
+ChatEditBoxSizeManager::ChatEditBoxSizeManager() :
+		CommonHeight(0)
+{
+}
+
+ChatEditBoxSizeManager::~ChatEditBoxSizeManager()
+{
+}
+
+void ChatEditBoxSizeManager::setCommonHeight(int height)
+{
+	if (height != CommonHeight)
+	{
+		CommonHeight = height;
+		emit commonHeightChanged(CommonHeight);
+	}
+}
