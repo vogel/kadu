9f53d9be46608d41274510ea41f8aa3239ba4313
Bartosz Brachaczek <b.brachaczek@gmail.com>
2011-02-14 19:23:46 +0100

    icons: Workaround bug #1960
    get-all scripts need fixing.
    
    http://kadu.net/mantis/view.php?id=1960
    
    Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

--- varia/themes/icons/get-all.sh
+++ varia/themes/icons/get-all.sh
@@ -7,14 +7,15 @@ for dir in *; do
 	fi
 done
 
-if [ "$1" == "ON" ]; then
-	for file in *.web; do
-		dir=`basename $file .web`
-		if [ ! -d $dir/kadu_icons ]; then
-			available_icon_themes="$available_icon_themes $dir"
-		fi
-	done
-fi
+# workaround #1960
+#if [ "$1" == "ON" ]; then
+#	for file in *.web; do
+#		dir=`basename $file .web`
+#		if [ ! -d $dir/kadu_icons ]; then
+#			available_icon_themes="$available_icon_themes $dir"
+#		fi
+#	done
+#fi
 
 if [ ! -z "$available_icon_themes" ]; then
 	echo $available_icon_themes
