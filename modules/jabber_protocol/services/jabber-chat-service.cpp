2154af60ced09a5a447048adb0e6ad37ac8144fb
Piotr Galiszewski <piotrgaliszewski@gmail.com>
2011-02-20 19:59:27 +0100

    Revert "jabber: add support for receiving xhtml messages - part of XEP-0071: XHTML-IM"
    This reverts commit 98c7c06af3d909847908fd932b79b90d0e87de90.
    
    There are many security implications and we are too advaned in our release cycle to fix them all.
    Psi ignores them, but we are more paranoic ;)

--- modules/jabber_protocol/services/jabber-chat-service.cpp
+++ modules/jabber_protocol/services/jabber-chat-service.cpp
@@ -121,35 +121,24 @@ void JabberChatService::clientMessageReceived(const XMPP::Message &msg)
 	bool ignore = false;
 
 	time_t msgtime = msg.timeStamp().toTime_t();
-
 	QByteArray body = msg.body().toUtf8();
-
 	emit filterRawIncomingMessage(chat, contact, body, ignore);
-	if (ignore)
-		return;
 
-	QString plain = QString::fromUtf8(body);
+	FormattedMessage formattedMessage(QString::fromUtf8(body));
+
+	QString plain = formattedMessage.toPlain();
 
 	emit filterIncomingMessage(chat, contact, plain, msgtime, ignore);
 	if (ignore)
 		return;
 
-	Message message = Message::create();
-	if (msg.containsHTML() && !msg.html().text().isEmpty())
-	{
-		QString html = msg.html().toString("span");
-		html.remove("\n");
-		message.setContent(html);
-	}
-	else
-	{
-		HtmlDocument::escapeText(plain);
-		message.setContent(plain);
-	}
+	HtmlDocument::escapeText(plain);
 
+	Message message = Message::create();
 	message.setMessageChat(chat);
 	message.setType(Message::TypeReceived);
 	message.setMessageSender(contact);
+	message.setContent(plain);
 	message.setSendDate(msg.timeStamp());
 	message.setReceiveDate(QDateTime::currentDateTime());
 
