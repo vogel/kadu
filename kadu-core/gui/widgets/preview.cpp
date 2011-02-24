b992840c4de2cebc6fe5bb58671a65f3ca131323
Piotr Dąbrowski <ultr@ultr.pl>
2011-02-24 15:51:27 +0100

    added transparency to Preview, frame

--- kadu-core/gui/widgets/preview.cpp
+++ kadu-core/gui/widgets/preview.cpp
@@ -1,5 +1,6 @@
 /*
  * %kadu copyright begin%
+ * Copyright 2011 Piotr Dąbrowski (ultr@ultr.pl)
  * Copyright 2010 Bartosz Brachaczek (b.brachaczek@gmail.com)
  * Copyright 2008, 2009, 2010 Piotr Galiszewski (piotr.galiszewski@kadu.im)
  * Copyright 2007, 2008, 2009, 2010 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
@@ -22,6 +23,7 @@
  */
 
 #include <QtGui/QApplication>
+#include <QtGui/QFrame>
 #include <QtNetwork/QHostAddress>
 
 #include "accounts/account-manager.h"
@@ -35,10 +37,15 @@
 #include "preview.h"
 
 Preview::Preview(QWidget *parent)
-	: KaduWebView(parent), contact(Contact::null)
+	: KaduWebView(parent), contact(Contact::null), DrawFrame(true)
 {
 	setFixedHeight(PREVIEW_DEFAULT_HEIGHT);
 	setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
+
+	QPalette p = palette();
+	p.setBrush(QPalette::Base, Qt::transparent);
+	page()->setPalette(p);
+	setAttribute(Qt::WA_OpaquePaintEvent, false);
 }
 
 Preview::~Preview()
@@ -68,4 +75,26 @@ void Preview::syntaxChanged(const QString &content)
 	emit needFixup(text);
 
 	setHtml(text);
+}
+
+void Preview::paintEvent(QPaintEvent *event)
+{
+	if (DrawFrame)
+	{
+		QFrame frame;
+		frame.setFrameStyle(QFrame::StyledPanel | QFrame::Raised);
+		frame.resize(size());
+		QPainter painter(this);
+		QPixmap framepixmap = QPixmap::grabWidget(&frame);
+		painter.drawPixmap(0, 0, framepixmap);
+	}
+	KaduWebView::paintEvent(event);
+}
+
+void Preview::setDrawFrame(bool drawFrame)
+{
+	if (DrawFrame == drawFrame)
+		return;
+	DrawFrame = drawFrame;
+	repaint();
 }
