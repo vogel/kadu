vogel/kadu

commit 6759629a49e4b68064062d940d47d9c8e59171a4

gui: Unfold folded links when copying
Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

kadu-core/gui/widgets/kadu-web-view.cpp
@@ -190,7 +190,7 @@ void KaduWebView::mouseReleaseEvent(QMouseEvent *e)
 
 #ifdef Q_WS_X11
 	if (!page()->selectedText().isEmpty())
-		convertClipboardHtmlImages(QClipboard::Selection);
+		convertClipboardHtml(QClipboard::Selection);
 #endif
 }
 
@@ -330,16 +330,23 @@ void KaduWebView::saveImage()
 
 void KaduWebView::textCopied() const
 {
-	convertClipboardHtmlImages(QClipboard::Clipboard);
+	convertClipboardHtml(QClipboard::Clipboard);
 }
 
 // taken from Psi+'s webkit patch, SVN rev. 2638, and slightly modified
-void KaduWebView::convertClipboardHtmlImages(QClipboard::Mode mode)
+void KaduWebView::convertClipboardHtml(QClipboard::Mode mode)
 {
+	static QRegExp emotsRegExpApos("<img[^>]+title\\s*=\\s*'([^']+)'[^>]*>");
+	static QRegExp emotsRegExpQuot("<img[^>]+title\\s*=\\s*\"([^\"]+)\"[^>]*>");
+	static QRegExp linksRegExpApos("<a[^>]+href\\s*=\\s*'([^']+)'[^>]*>[^<]*<[^>]*>");
+	static QRegExp linksRegExpQuot("<a[^>]+href\\s*=\\s*\"([^\"]+)\"[^>]*>[^<]*<[^>]*>");
+
 	QClipboard *cb = QApplication::clipboard();
 	QString html = cb->mimeData(mode)->html();
-	html.replace(QRegExp("<img[^>]+title\\s*=\\s*'([^']+)'[^>]*>"), "\\1");
-	html.replace(QRegExp("<img[^>]+title\\s*=\\s*\"([^\"]+)\"[^>]*>"), "\\1");
+	html.replace(emotsRegExpApos, QLatin1String("\\1"));
+	html.replace(emotsRegExpQuot, QLatin1String("\\1"));
+	html.replace(linksRegExpApos, QLatin1String("<a href='\\1'>\\1</a>"));
+	html.replace(linksRegExpQuot, QLatin1String("<a href=\"\\1\">\\1</a>"));
 	QTextDocument htmlToPlainTextConverter;
 	htmlToPlainTextConverter.setHtml(html);
 	QMimeData *data = new QMimeData();

kadu-core/gui/widgets/kadu-web-view.h
@@ -44,7 +44,7 @@ class KADUAPI KaduWebView : public QWebView
 	QPoint DragStartPosition;
 	QTimer *RefreshTimer;
 
-	static void convertClipboardHtmlImages(QClipboard::Mode mode);
+	static void convertClipboardHtml(QClipboard::Mode mode);
 
 private slots:
 	void hyperlinkClicked(const QUrl &anchor) const;

commit ba1c0478d7795b381753be04d50bb9b33a97ce03

gui: Fix copying mail URL's
Copy "name@domain.org" rather than "mailto:name@domain.org". It was a
regression since 6759629a49e4b68064062d940d47d9c8e59171a4.

NOTE: This is a candidate for the 0.6.6.x branch.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>
Acked-by: Piotr Galiszewski <piotrgaliszewski@gmail.com>

kadu-core/gui/widgets/kadu-web-view.cpp
@@ -340,8 +340,8 @@ void KaduWebView::convertClipboardHtml(QClipboard::Mode mode)
 {
 	static QRegExp emotsRegExpApos("<img[^>]+title\\s*=\\s*'([^']+)'[^>]*>");
 	static QRegExp emotsRegExpQuot("<img[^>]+title\\s*=\\s*\"([^\"]+)\"[^>]*>");
-	static QRegExp linksRegExpApos("<a[^>]+href\\s*=\\s*'([^']+)'[^>]*>[^<]*<[^>]*>");
-	static QRegExp linksRegExpQuot("<a[^>]+href\\s*=\\s*\"([^\"]+)\"[^>]*>[^<]*<[^>]*>");
+	static QRegExp linksRegExpApos("<a[^>]+href\\s*=[^>]+title\\s*=\\s*'([^']+)'[^>]*>[^<]*<[^>]*>");
+	static QRegExp linksRegExpQuot("<a[^>]+href\\s*=[^>]+title\\s*=\\s*\"([^\"]+)\"[^>]*>[^<]*<[^>]*>");
 
 	QClipboard *cb = QApplication::clipboard();
 	QString html = cb->mimeData(mode)->html();

kadu-core/url-handlers/mail-url-handler.cpp
@@ -51,7 +51,7 @@ void MailUrlHandler::convertUrlsToHtml(HtmlDocument &document)
 		QString mail = Qt::escape(text.mid(index, length));
 
 		document.splitElement(i, index, length);
-		document.setElementValue(i, "<a href=\"mailto:" + mail + "\">" + mail + "</a>", true);
+		document.setElementValue(i, "<a href=\"mailto:" + mail + "\" title=\"" + mail +"\">" + mail + "</a>", true);
 	}
 }
 