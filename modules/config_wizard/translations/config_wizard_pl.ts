69128befd97315908f64dd43e19d5adc5abb818e
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2011-03-09 23:40:09 +0100

    config_wizard: translations updated
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- modules/config_wizard/translations/config_wizard_pl.ts
+++ modules/config_wizard/translations/config_wizard_pl.ts
@@ -4,23 +4,8 @@
 <context>
     <name>@default</name>
     <message>
-        <source>Kadu</source>
-        <translation>Kadu</translation>
-    </message>
-    <message>
-        <source>General</source>
-        <translation>Ogólne</translation>
-    </message>
-    <message>
-        <source>Config Wizard</source>
-        <translation>Kreator konfiguracji</translation>
-    </message>
-</context>
-<context>
-    <name>ConfigWizardConfigurationUiHandler</name>
-    <message>
-		<source>Start Configuration Wizard...</source>
-		<translation>Uruchom kreator konfiguracji...</translation>
+        <source>Kadu needs to be restarted before changes to the language settings will take effect.</source>
+        <translation>Aby zastosować zmiany języka Kadu musi zostać uruchomione ponownie</translation>
     </message>
 </context>
 <context>
@@ -49,6 +34,10 @@
         <source>I don&apos;t want to set up my account for Kadu now</source>
         <translation>Nie chcę teraz dodawać konta</translation>
     </message>
+    <message>
+        <source>&lt;p&gt;Please choose the network for the account that you would like to set up.&lt;/p&gt;&lt;p&gt;You can also create a new account in the wizard if you don&apos;t already have one&lt;/p&gt;</source>
+        <translation>&lt;p&gt;Wybierz sieć w której chcesz skonfigurować konto.&lt;/p&gt;&lt;p&gt;Kreator konfiguracji pozwala także na stworzenie nowego konto&lt;/p&gt;</translation>
+    </message>
 </context>
 <context>
     <name>ConfigWizardCompletedPage</name>
@@ -59,6 +48,17 @@
     <message>
         <source>Go to Accounts Setting after closing this window</source>
         <translation>Przejdź do okna konfiguracji kont po zamknięciu tego okna</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;Confirm your wizard setting to start using Kadu.&lt;/p&gt;</source>
+        <translation>&lt;p&gt;Potwierdź wprowadzone ustawienia aby rozpocząć korzystanie z Kadu.&lt;/p&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>ConfigWizardConfigurationUiHandler</name>
+    <message>
+        <source>Start Configuration Wizard...</source>
+        <translation>Uruchom kreator konfiguracji...</translation>
     </message>
 </context>
 <context>
@@ -85,6 +85,10 @@
     <message>
         <source>&lt;h3&gt;Account Setup&lt;/h3&gt;</source>
         <translation>&lt;h3&gt;Tworzenie konta&lt;/h3&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;Please enter your account data.&lt;/p&gt;&lt;p&gt;Go back if you want to select a different Account Setup option.&lt;/p&gt;</source>
+        <translation>&lt;p&gt;Wprowadź dane konta.&lt;/p&gt;&lt;p&gt;Cofnij się, jeżeli chcesz wybrać inną opcję ustawień konta.&lt;/p&gt;</translation>
     </message>
 </context>
 <context>
