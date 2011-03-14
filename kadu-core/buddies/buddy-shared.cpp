vogel/kadu

commit c1d34629dc716107957e0f93d1ec0b25c1440f7a

buddies: When adding Contact to Buddy, always add it to the end of list
We shouldn't take Contact's initialial Priority into account as it
doesn't mean anything useful to us.

NOTE: This is a candidate for the 0.6.6.x branch.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>
Acked-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

kadu-core/buddies/buddy-shared.cpp
@@ -264,17 +264,12 @@ void BuddyShared::addContact(Contact contact)
 
 	emit contactAboutToBeAdded(contact);
 
-	if (contact.priority() == -1)
-	{
-		int last = Contacts.count() > 1
-				? Contacts.at(Contacts.count() - 1).priority()
-				: 0;
-		contact.setPriority(last);
-	}
+	int newPriority = Contacts.isEmpty()
+			? 0
+			: Contacts.at(Contacts.count() - 1).priority() + 1;
+	contact.setPriority(newPriority);
 
 	Contacts.append(contact);
-
-	sortContacts();
 
 	emit contactAdded(contact);
 
@@ -329,7 +324,7 @@ QString BuddyShared::id(const Account &account)
 	return QString();
 }
 
-bool contactPriorityLessThan(const Contact &c1, const Contact &c2)
+static bool contactPriorityLessThan(const Contact &c1, const Contact &c2)
 {
 	return c1.priority() < c2.priority();
 }

commit ce70ec475d852b19ba86ca76b59f1d535fe34814

buddies: Fix priorities broken since c1d34629dc716107957e0f93d1ec0b25c1440f7a
NOTE: This is a candidate for the 0.6.6.x branch.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>
Acked-by: Piotr Galiszewski <piotrgaliszewski@gmail.com>

kadu-core/buddies/buddy-shared.cpp
@@ -264,12 +264,20 @@ void BuddyShared::addContact(Contact contact)
 
 	emit contactAboutToBeAdded(contact);
 
-	int newPriority = Contacts.isEmpty()
-			? 0
-			: Contacts.at(Contacts.count() - 1).priority() + 1;
-	contact.setPriority(newPriority);
+	// anonymous (default) buddies should have only contacts without priority
+	if (isAnonymous())
+		contact.setPriority(-1);
+	// if no priority (i.e., adding new contact), append at the end
+	else if (contact.priority() == -1)
+	{
+		int newPriority = Contacts.isEmpty()
+				? 0
+				: Contacts.at(Contacts.count() - 1).priority() + 1;
+		contact.setPriority(newPriority);
+	}
 
 	Contacts.append(contact);
+	sortContacts();
 
 	emit contactAdded(contact);
 