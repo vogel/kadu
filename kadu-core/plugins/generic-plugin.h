9246b47cae01810f54e999bc690c749ef06206e4
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2011-04-07 01:55:28 +0200

    doc: GenericPlugin documentation added
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- kadu-core/plugins/generic-plugin.h
+++ kadu-core/plugins/generic-plugin.h
@@ -21,16 +21,56 @@
 
 #include <QtCore/QtPlugin>
 
+/**
+ * @addtogroup Plugins
+ * @{
+ */
+
+/**
+ * @class GenericPlugin
+ * @author Rafał 'Vogel' Malinowski
+ * @short Base interface for all Kadu plugins.
+ *
+ * Every Kadu plugin has to have a class that inherits from GenericPlugin. Next this class has to
+ * be registered using Q_EXPORT_PLUGIN2 macro with plugin library name and class name as parameters.
+ *
+ * A new instance of given object is created when such plugin is loaded and then init() method is
+ * called. Before plugin gets unloaded done() method is called.
+ */
 class GenericPlugin
 {
 public:
 	virtual ~GenericPlugin() {}
 
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Method called after plugin activation.
+	 * @param firstLoad true, if this is first activation of current plugin
+	 *
+	 * This method is called every time a plugin is activated. Implementations should do all work
+	 * needed to properly run plugin, like registering protocols, notifications and do on, in this
+	 * method. Parameter firstLoad is set to true if it is first activation of plugin for given
+	 * Kadu instance. Plugin can then do additional work if reuired (like showing configuration
+	 * wizard window).
+	 */
 	virtual int init(bool firstLoad) = 0;
+
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Method called before plugin deactivation.
+	 *
+	 * This method is called before a plugin is deactivated. Implementations should do all work
+	 * needed to properly finalize plugin, like unregistering protocols, notifications and do on,
+	 * in this method. Every action run in init() has to have a counterpart in this method.
+	 */
 	virtual void done() = 0;
 
 };
 
 Q_DECLARE_INTERFACE(GenericPlugin, "im.kadu.GenericPlugin/0.1")
+
+/**
+ * @}
+ */
 
 #endif // GENERIC_PLUGIN_H
