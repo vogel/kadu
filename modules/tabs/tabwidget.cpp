vogel/kadu

commit 968574596024242013e76d3416a1f568c1e7adf8

tabs: Don't reserve space in tab bar if any of open/close chat buttons is disabled (#2093)
http://kadu.net/mantis/view.php?id=2093

NOTE: This is a candidate for the 0.9.x branch.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>
Acked-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

modules/tabs/tabwidget.cpp
@@ -66,14 +66,12 @@ TabWidget::TabWidget()
 	OpenChatButton = new QToolButton(this);
 	OpenChatButton->setIcon(IconsManager::instance()->iconByPath("kadu_icons/chat"));
 	OpenChatButton->setAutoRaise(true);
-	setCornerWidget(OpenChatButton, Qt::TopLeftCorner);
 	connect(OpenChatButton, SIGNAL(clicked()), SLOT(newChat()));
 
 	//przycisk zamkniecia aktywnej karty znajdujacy sie w prawym gornym rogu
 	CloseChatButton = new QToolButton(this);
 	CloseChatButton->setIcon(IconsManager::instance()->iconByPath("kadu_icons/tab-remove"));
 	CloseChatButton->setAutoRaise(true);
-	setCornerWidget(CloseChatButton, Qt::TopRightCorner);
 	connect(CloseChatButton, SIGNAL(clicked()), SLOT(deleteTab()));
 }
 
@@ -295,16 +293,28 @@ void TabWidget::configurationUpdated()
 {
 	triggerCompositingStateChanged();
 
-	// odswiezenie ikon
 	OpenChatButton->setIcon(IconsManager::instance()->iconByPath("internet-group-chat"));
 	CloseChatButton->setIcon(IconsManager::instance()->iconByPath("kadu_icons/tab-remove"));
 
 	setTabsClosable(config_file.readBoolEntry("Tabs", "CloseButtonOnTab"));
-
-	// uaktualniamy zmienne konfiguracyjne
-	CloseChatButton->setShown(config_file.readBoolEntry("Tabs", "CloseButton"));
-	OpenChatButton->setShown(config_file.readBoolEntry("Tabs", "OpenChatButton"));
 	config_oldStyleClosing = config_file.readBoolEntry("Tabs", "OldStyleClosing");
+
+	bool isOpenChatButtonEnabled = (cornerWidget(Qt::TopLeftCorner) == OpenChatButton);
+	bool shouldEnableOpenChatButton = config_file.readBoolEntry("Tabs", "OpenChatButton");
+	bool isCloseButtonEnabled = (cornerWidget(Qt::TopRightCorner) == CloseChatButton);
+	bool shouldEnableCloseButton = config_file.readBoolEntry("Tabs", "CloseButton");
+
+	if (isOpenChatButtonEnabled != shouldEnableOpenChatButton)
+	{
+		OpenChatButton->setVisible(shouldEnableOpenChatButton);
+		setCornerWidget(shouldEnableOpenChatButton ? OpenChatButton : 0, Qt::TopLeftCorner);
+	}
+
+	if (isCloseButtonEnabled != shouldEnableCloseButton)
+	{
+		CloseChatButton->setVisible(shouldEnableCloseButton);
+		setCornerWidget(shouldEnableCloseButton ? CloseChatButton : 0, Qt::TopRightCorner);
+	}
 }
 
 TabBar::TabBar(QWidget *parent) :

commit 5f54b3ceb3fbb6d5f6dbd67b0f30392117070976

tabs: Fix regression causing buttons to be shown when they shouldn't (#2129)
Regression since 968574596024242013e76d3416a1f568c1e7adf8.

http://kadu.net/mantis/view.php?id=2129

NOTE: This is a candidate for the 0.9.x branch.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

modules/tabs/tabwidget.cpp
@@ -66,12 +66,14 @@ TabWidget::TabWidget()
 	OpenChatButton = new QToolButton(this);
 	OpenChatButton->setIcon(KaduIcon("kadu_icons/chat").icon());
 	OpenChatButton->setAutoRaise(true);
+	OpenChatButton->setVisible(false);
 	connect(OpenChatButton, SIGNAL(clicked()), SLOT(newChat()));
 
 	//przycisk zamkniecia aktywnej karty znajdujacy sie w prawym gornym rogu
 	CloseChatButton = new QToolButton(this);
 	CloseChatButton->setIcon(KaduIcon("kadu_icons/tab-remove").icon());
 	CloseChatButton->setAutoRaise(true);
+	CloseChatButton->setVisible(false);
 	connect(CloseChatButton, SIGNAL(clicked()), SLOT(deleteTab()));
 }
 