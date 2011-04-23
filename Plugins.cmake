cf869e25b4004b99f560d0dc3217e0f12709da83
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2011-04-24 01:58:44 +0200

    gadu_protocol: moved to plugins
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- Plugins.cmake
+++ Plugins.cmake
@@ -1,6 +1,15 @@
 set (COMPILE_PLUGINS
+
+#protocols
+	# GaduGadu protocol suppot
+	gadu_protocol
+
+# notifiers
 	# Enables notifications about buddies presence and other in chat windows
 	chat_notifier
+
+# misc
 	# Shows image links and youtube links as images and videos in chat
 #	imagelink
+
 )
