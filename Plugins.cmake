204543c27a18e1d8e69a44d0829de65ea94e2e07
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2011-04-24 01:41:36 +0200

    plugins: moved plugins configuration to Plugins.cmake file
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ Plugins.cmake
@@ -0,0 +1,6 @@
+set (COMPILE_PLUGINS
+	# Enables notifications about buddies presence and other in chat windows
+	chat_notifier
+	# Shows image links and youtube links as images and videos in chat
+#	imagelink
+)
