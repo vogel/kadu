39272f5a827bc59d8eaab931e3be9d03fa898ee0
unknown <kolatzek@.sulb.uni-saarland.de>
2011-04-28 09:09:51 +0200

    qt4_docking_notify_de.ts ready

--- modules/qt4_docking_notify/translations/qt4_docking_notify_de.ts
+++ modules/qt4_docking_notify/translations/qt4_docking_notify_de.ts
@@ -5,71 +5,73 @@
     <name>@default</name>
     <message>
         <source>Notify</source>
-        <translation type="unfinished"></translation>
+        <translation>Benachrichtige</translation>
     </message>
     <message>
         <source>Notification</source>
-        <translation type="unfinished"></translation>
+        <translation>Benachrichtigung</translation>
     </message>
     <message>
         <source>Tray Icon Balloon</source>
-        <translation type="unfinished"></translation>
+        <translation>Sprechblase im Systemabschnitt der Kontrollleiste</translation>
     </message>
     <message>
         <source>Timeout</source>
-        <translation type="unfinished"></translation>
+        <translation>Timeout</translation>
     </message>
     <message numerus="yes">
         <source>%n second(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n Sekunde</numerusform>
+            <numerusform>%n Sekunden</numerusform>
         </translation>
     </message>
     <message>
         <source>Notification icon</source>
-        <translation type="unfinished"></translation>
+        <translation>Benachrichtigungssymbol</translation>
     </message>
     <message>
         <source>No icon</source>
-        <translation type="unfinished"></translation>
+        <translation>Kein Symbol</translation>
     </message>
     <message>
         <source>Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Information</translation>
     </message>
     <message>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation>Warnung</translation>
     </message>
     <message>
         <source>Critical</source>
-        <translation type="unfinished"></translation>
+        <translation>Kritisch</translation>
     </message>
     <message>
         <source>Title</source>
-        <translation type="unfinished"></translation>
+        <translation>Titel</translation>
     </message>
     <message>
         <source>Syntax</source>
-        <translation type="unfinished"></translation>
+        <translation>Syntax</translation>
     </message>
 </context>
 <context>
     <name>Qt4NotifyConfigurationWidget</name>
     <message>
         <source>Configure</source>
-        <translation type="unfinished"></translation>
+        <translation>Konfigurieren</translation>
     </message>
     <message>
         <source>Tray icon balloon&apos;s look configuration</source>
-        <translation type="unfinished"></translation>
+        <translation>Konfiguration der Sprechblase im Systemabschnitt der Kontrollleiste</translation>
     </message>
     <message>
         <source>
 %&amp;t - title (eg. New message) %&amp;m - notification text (eg. Message from Jim), %&amp;d - details (eg. message quotation),
 %&amp;i - notification icon</source>
-        <translation type="unfinished"></translation>
+        <translation>
+%&amp;t - Titel (z.B. Neue Nachricht) %&amp;m - Nachrichtentext (z.B. Nachricht vom Jim), %&amp;d - Details (z.B. Nachrichteninhalt),
+%&amp;i - Benachrichtigungssymbol</translation>
     </message>
 </context>
 </TS>
