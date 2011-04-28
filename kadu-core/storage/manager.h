vogel/kadu

commit 8e22a9c47b426bddd4f53657e03b533aca3c14f1

storage: Use signed int for count and indexes in managers, just like Qt does
For example, indexOf() method can return -1.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

kadu-core/storage/manager.h
@@ -345,7 +345,7 @@ public:
 	 *
 	 * This methods works on registered items.
 	 */
-	Item byIndex(unsigned int index)
+	Item byIndex(int index)
 	{
 		(void) QMutexLocker(&Mutex);
 
@@ -394,7 +394,7 @@ public:
 	 *
 	 * This methods works on registered items.
 	 */
-	unsigned int indexOf(Item item)
+	int indexOf(Item item)
 	{
 		(void) QMutexLocker(&Mutex);
 
@@ -411,7 +411,7 @@ public:
 	 *
 	 * This methods works on registered items.
 	 */
-	unsigned int count()
+	int count()
 	{
 		(void) QMutexLocker(&Mutex);
 

kadu-core/storage/simple-manager.h
@@ -242,7 +242,7 @@ public:
 	 *
 	 * When index is out of range Item::null value is returned.
 	 */
-	Item byIndex(unsigned int index)
+	Item byIndex(int index)
 	{
 		(void) QMutexLocker(&Mutex);
 
@@ -290,7 +290,7 @@ public:
 	 * or removed. This method returns the same index as
 	 * items().indexOf(item) would return.
 	 */
-	unsigned int indexOf(Item item)
+	int indexOf(Item item)
 	{
 		(void) QMutexLocker(&Mutex);
 
@@ -305,7 +305,7 @@ public:
 	 *
 	 * Return number of loaded items.
 	 */
-	unsigned int count()
+	int count()
 	{
 		(void) QMutexLocker(&Mutex);
 

kadu-core/buddies/model/buddies-model.cpp
@@ -106,7 +106,7 @@ int BuddiesModel::rowCount(const QModelIndex &parent) const
 Buddy BuddiesModel::buddyAt(const QModelIndex &index) const
 {
 	QModelIndex parent = index.parent();
-	unsigned int row = parent.isValid() ? parent.row() : index.row();
+	int row = parent.isValid() ? parent.row() : index.row();
 
 	if (IncludeMyself && (row == BuddyManager::instance()->count()))
 		return Core::instance()->myself();

commit e1041682a5f437e4aaaef4507b2148613a2ac70f

storage: Fix possible crash in byIndex() method in managers
It regressed in 8e22a9c47b426bddd4f53657e03b533aca3c14f1.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

kadu-core/storage/manager.h
@@ -351,7 +351,7 @@ public:
 
 		ensureLoaded();
 
-		if (index >= count())
+		if (index < 0 || index >= count())
 			return Item::null;
 
 		return ItemsWithDetails.at(index);

kadu-core/storage/simple-manager.h
@@ -248,7 +248,7 @@ public:
 
 		ensureLoaded();
 
-		if (index >= count())
+		if (index < 0 || index >= count())
 			return Item::null;
 
 		return Items.at(index);