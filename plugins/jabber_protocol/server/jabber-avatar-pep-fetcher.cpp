4cc484c868d92a8f6ee76d19e1ef9ef23b20bd16
Bartosz Brachaczek <b.brachaczek@gmail.com>
2011-05-03 13:05:07 +0200

    jabber: Do not create XMPP::Base64 object needlessly
    Missed that opportunity when fixing MSVC warning in
    8fc0e7697c83980188d0199823f8f6bc61345200.
    
    Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

--- plugins/jabber_protocol/server/jabber-avatar-pep-fetcher.cpp
+++ plugins/jabber_protocol/server/jabber-avatar-pep-fetcher.cpp
@@ -146,7 +146,7 @@ void JabberAvatarPepFetcher::avatarDataQueryFinished(const XMPP::Jid &jid, const
 	if (jabberProtocol)
 		disconnect(jabberProtocol->client()->pepManager(), SIGNAL(itemPublished(XMPP::Jid,QString,XMPP::PubSubItem)), this, SLOT(avatarDataQueryFinished(XMPP::Jid,QString,XMPP::PubSubItem)));
 
-	QByteArray imageData = XMPP::Base64().decode(item.payload().text());
+	QByteArray imageData = XMPP::Base64::decode(item.payload().text());
 
 	Avatar contactAvatar = AvatarManager::instance()->byContact(MyContact, ActionCreateAndAdd);
 	contactAvatar.setLastUpdated(QDateTime::currentDateTime());
