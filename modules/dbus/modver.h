798c00c32b4f62d87488a7c6749b266082297c66
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2011-05-04 02:32:33 +0200

    plugins: remove blacklisted plugins
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- modules/dbus/modver.h
+++
@@ -1,6 +0,0 @@
-#ifndef _MODVER_H
-#define DESCRIPTION "DBus module"
-#define DESCRIPTION_PL "Modu³ DBus"
-#define AUTHOR "Bart³omiej Zimoñ"
-#define _MODVER_H
-#endif
--- modules/weather/modver.h
+++
@@ -1,7 +0,0 @@
-#ifndef _MODVER_H
-#define DESCRIPTION "Weather checking module"
-#define DESCRIPTION_PL "Modu³ do sprawdzania pogody"
-#define AUTHOR "Micha³ Ma³ek"
-#define VERSION "3.15"
-#define _MODVER_H
-#endif
