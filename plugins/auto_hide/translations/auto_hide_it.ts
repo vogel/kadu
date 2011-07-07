0e6a2c11f8275d0be5339b5b2c8d1e84055296e0
Piotr Galiszewski <piotrgaliszewski@gmail.com>
2011-07-07 12:53:54 +0200

    translations: add missing it translation

--- plugins/auto_hide/translations/auto_hide_it.ts
+++ plugins/auto_hide/translations/auto_hide_it.ts
@@ -2,40 +2,37 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="it">
 <context>
-    <name>@default</name>
-    <message>
-        <source>Kadu</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Advanced</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Kadu&apos;s Main Window Autohide</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Use autohide</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Autohide idle time</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message numerus="yes">
-        <source>%n second(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+	<name>@default</name>
+	<message>
+		<source>Kadu</source>
+		<translation>Kadu</translation>
+	</message>
+	<message>
+		<source>Advanced</source>
+		<translation>Avanzato</translation>
+	</message>
+	<message>
+		<source>Kadu&apos;s Main Window Autohide</source>
+		<translation>Principale Finestra di Autonascondi</translation>
+	</message>
+	<message>
+		<source>Use autohide</source>
+		<translation>Usa autonascondi</translation>
+	</message>
+	<message>
+		<source>Autohide idle time</source>
+		<translation>Tempo di inattività per l&apos;autonascondi</translation>
+	</message>
+	<message numerus="yes">
+		<source>%n second(s)</source>
+	<translation><numerusform>%n secondo</numerusform><numerusform>%n secondi</numerusform></translation>
+	</message>
 </context>
 <context>
-    <name>AutoHide</name>
-    <message>
-        <source>Don&apos;t hide</source>
-        <translation type="unfinished"></translation>
-    </message>
+	<name>AutoHide</name>
+	<message>
+		<source>Don&apos;t hide</source>
+		<translation>Non nascondere</translation>
+	</message>
 </context>
 </TS>
