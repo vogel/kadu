vogel/kadu

commit 54b2249866f8a4eccc2080721320834dc60fdc30

hints: #2158 fixed: minimum size is now respected
Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

plugins/hints/hint_manager.cpp
@@ -74,7 +74,7 @@ HintManager::HintManager(QObject *parent) :
 #else
 	frame = new QFrame(0, Qt::FramelessWindowHint | Qt::Tool | Qt::X11BypassWindowManagerHint | Qt::WindowStaysOnTopHint |Qt::MSWindowsOwnDC);
 #endif
-	frame->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+	frame->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
 
 	Style = QString("QFrame {border-width: %1px; border-style: solid; border-color: %2; border-radius: %3px;}")
 			.arg(config_file.readNumEntry("Hints", "AllEvents_borderWidth", FRAME_WIDTH))
@@ -85,7 +85,7 @@ HintManager::HintManager(QObject *parent) :
 	layout = new QVBoxLayout(frame);
 	layout->setSpacing(0);
 	layout->setMargin(FRAME_WIDTH);
-	layout->setSizeConstraint(QLayout::SetFixedSize);
+// 	layout->setSizeConstraint(QLayout::SetFixedSize);
 
 	Opacity = config_file.readNumEntry("Hints", "AllEvents_transparency", 0);
 	Opacity = 1 - Opacity/100;
@@ -181,6 +181,10 @@ void HintManager::setHint()
 
 	frame->adjustSize();
 	QSize preferredSize = frame->sizeHint();
+	if (preferredSize.width() < config_file.readNumEntry("Hints", "MinimumWidth", 100))
+		preferredSize.setWidth(config_file.readNumEntry("Hints", "MinimumWidth", 100));
+	if (preferredSize.width() > config_file.readNumEntry("Hints", "MaximumWidth", 500))
+		preferredSize.setWidth(config_file.readNumEntry("Hints", "MaximumWidth", 500));
 	QSize desktopSize = QApplication::desktop()->screenGeometry(frame).size();
 
 	emit searchingForTrayPosition(trayPosition);
@@ -239,9 +243,9 @@ void HintManager::setHint()
 	}
 
 	frame->setGeometry(newPosition.x(), newPosition.y(), preferredSize.width(), preferredSize.height());
-
+	frame->setFixedWidth(preferredSize.width());
+	frame->setFixedHeight(preferredSize.height());
 	frame->setWindowOpacity(Opacity);
-
 	frame->update();
 
 	kdebugf2();

commit 83872c581c582c7b6d5c1a35764406a7b65a4166

hints: Fix computing hints size (#2258)
It partially regressed in 54b2249866f8a4eccc2080721320834dc60fdc30
(setting fixed height was incorrect) but this commit fixes more issues.

http://kadu.net/mantis/view.php?id=2258

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

plugins/hints/hint.cpp
@@ -95,7 +95,7 @@ Hint::Hint(QWidget *parent, Notification *notification)
 
 	connect(notification, SIGNAL(closed(Notification *)), this, SLOT(notificationClosed()));
 
-	setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+	setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
 
 	configurationUpdated();
 	show();
@@ -132,7 +132,6 @@ void Hint::createLabels(const QPixmap &pixmap)
 
 	vbox = new QVBoxLayout(this);
 	vbox->setSpacing(0);
-	vbox->setSizeConstraint(QLayout::SetMinimumSize);
 	labels = new QHBoxLayout();
 	labels->setSpacing(0);
 	labels->setContentsMargins(margin + 4, margin + 2, margin + 4, margin + 2);
@@ -148,8 +147,7 @@ void Hint::createLabels(const QPixmap &pixmap)
 	}
 
 	label = new QLabel(this);
-	label->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Expanding);
-	label->setMaximumWidth(config_file.readNumEntry("Hints", "MaximumWidth", 500));
+	label->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::MinimumExpanding);
 	label->setTextInteractionFlags(Qt::NoTextInteraction);
 	label->setAlignment(Qt::AlignVCenter | Qt::AlignLeft);
 	label->setContentsMargins(margin + 4, 0, 0, 0);
@@ -208,6 +206,7 @@ void Hint::updateText()
 		+ "</div>"
 		);
 
+	adjustSize();
 	emit updated(this);
 }
 

plugins/hints/hint_manager.cpp
@@ -76,12 +76,11 @@ HintManager::HintManager(QObject *parent) :
 #else
 	frame = new QFrame(0, Qt::FramelessWindowHint | Qt::Tool | Qt::X11BypassWindowManagerHint | Qt::WindowStaysOnTopHint |Qt::MSWindowsOwnDC);
 #endif
-	frame->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+	frame->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
 
 	layout = new QVBoxLayout(frame);
 	layout->setSpacing(0);
 	layout->setMargin(0);
-// 	layout->setSizeConstraint(QLayout::SetFixedSize);
 
 	connect(hint_timer, SIGNAL(timeout()), this, SLOT(oneSecond()));
 	connect(ChatWidgetManager::instance(), SIGNAL(chatWidgetActivated(ChatWidget *)), this, SLOT(chatWidgetActivated(ChatWidget *)));
@@ -240,7 +239,6 @@ void HintManager::setHint()
 
 	frame->setGeometry(newPosition.x(), newPosition.y(), preferredSize.width(), preferredSize.height());
 	frame->setFixedWidth(preferredSize.width());
-	frame->setFixedHeight(preferredSize.height());
 	frame->setWindowOpacity(Opacity);
 
 	if (frame->isVisible())