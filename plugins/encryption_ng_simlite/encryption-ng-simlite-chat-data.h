8d1593cb3f98cad3080bbdfac9ffb7e9938b5136
Bartosz Brachaczek <b.brachaczek@gmail.com>
2011-09-14 21:16:14 +0200

    encryption_ng_simlite: Introduce EncryptionNgSimliteChatData
    Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

--- plugins/encryption_ng_simlite/encryption-ng-simlite-chat-data.h
+++ plugins/encryption_ng_simlite/encryption-ng-simlite-chat-data.h
@@ -1,6 +1,6 @@
 /*
  * %kadu copyright begin%
- * Copyright 2010, 2011 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * Copyright 2011 Bartosz Brachaczek (b.brachaczek@gmail.com)
  * %kadu copyright end%
  *
  * This program is free software; you can redistribute it and/or
@@ -17,13 +17,9 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef ENCRYPTION_CHAT_DATA_H
-#define ENCRYPTION_CHAT_DATA_H
+#ifndef ENCRYPTION_NG_SIMLITE_CHAT_DATA_H
+#define ENCRYPTION_NG_SIMLITE_CHAT_DATA_H
 
-#include <QtCore/QObject>
-#include <QtGui/QAction>
-
-#include "chat/chat.h"
 #include "storage/module-data.h"
 
 #undef Property
@@ -31,44 +27,27 @@
 	type name() { ensureLoaded(); return capitalized_name; } \
 	void set##capitalized_name(type name) { ensureLoaded(); capitalized_name = name; }
 
-class Decryptor;
-class Encryptor;
-
-class EncryptionChatData : public ModuleData
+class EncryptionNgSimliteChatData : public ModuleData
 {
 	Q_OBJECT
 
-	Encryptor *ChatEncryptor;
-	Decryptor *ChatDecryptor;
-
-	bool Encrypt;
-
-	bool importEncrypt();
-
-private slots:
-	void encryptorDestroyed();
-	void decryptorDestroyed();
+	bool SupportUtf;
 
 protected:
 	virtual void load();
 	virtual void store();
+	virtual bool shouldStore();
 
 public:
-	explicit EncryptionChatData(const QString &moduleName, StorableObject *parent, QObject *qobjectParent);
-	virtual ~EncryptionChatData();
+	explicit EncryptionNgSimliteChatData(const QString &moduleName, StorableObject *parent, QObject *qobjectParent);
+	virtual ~EncryptionNgSimliteChatData();
 
 	virtual QString name() const;
 
-	Property(bool, encrypt, Encrypt)
-
-	void setEncryptor(Encryptor *encryptor);
-	Encryptor * encryptor();
-
-	void setDecryptor(Decryptor *decryptor);
-	Decryptor * decryptor();
+	Property(bool, supportUtf, SupportUtf)
 
 };
 
 #undef Property
 
-#endif // ENCRYPTION_CHAT_DATA_H
+#endif // ENCRYPTION_NG_SIMLITE_CHAT_DATA_H
