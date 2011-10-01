2b74f2dc20bced1870defdaef1cfe65089cf423b
Bartosz Brachaczek <b.brachaczek@gmail.com>
2011-10-01 21:30:53 +0200

    plugins: Fix checking plugin inter-dependencies
    Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

--- plugins/check-deps.sh
+++ plugins/check-deps.sh
@@ -3,49 +3,29 @@
 awk=$1
 shift
 
-function check_plugins
-{
-	type=$1
-	shift
-
-	for plugin in $*;
-	do
-		$awk -F = "/^Dependencies/ { printf \$2 }" $plugin/$plugin.desc > .tmp
-		deps=`cat .tmp`
-
-		for dep_plugin in $deps;
-		do
-			dep_ok=0
-			for check_dep_plugin in $*;
-			do
-				if [ "$dep_plugin" == "$check_dep_plugin" ];
-				then
-					dep_ok=1
-					break
-				fi
-			done
-
-			if [ $dep_ok -eq 0 ];
-			then
-				echo "Module $plugin dependency failed on $type plugin $dep_plugin"
-				exit 1
-			fi
-		done
-	done
-
-	rm -f .tmp
-}
-
-plugins=
 for plugin in $*;
 do
-	if [ "$plugin" == "-" ];
-	then
-		check_plugins "static" $plugins
-		continue
-	fi
+	$awk -F = "/^Dependencies/ { printf \$2 }" $plugin/$plugin.desc > .tmp
+	deps=`cat .tmp`
 
-	plugins="$plugins $plugin"
+	for dep_plugin in $deps;
+	do
+		dep_ok=0
+		for check_dep_plugin in $*;
+		do
+			if [ "$dep_plugin" == "$check_dep_plugin" ];
+			then
+				dep_ok=1
+				break
+			fi
+		done
+
+		if [ $dep_ok -eq 0 ];
+		then
+			echo "'$plugin' plugin dependency failed on '$dep_plugin' plugin"
+			exit 1
+		fi
+	done
 done
 
-check_plugins "dynamic" $plugins
+rm -f .tmp
