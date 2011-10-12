vogel/kadu

commit d396c02580b0515f6ba41a38239ea6fe06e50c94

build: Override COMPILE_PLUGINS variable in cache
It has at least two advantages:
- COMPILE_PLUGINS is always displayed in non-CLI CMake configurators.
- Allows to pass COMPILE_PLUGINS variable containing new line characters
  (CMake cache does not support new lines characters in variables so we
  need to override that variable to not contain new lines).

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

Plugins.cmake
@@ -172,3 +172,5 @@ else (NOT COMPILE_PLUGINS)
 	# Remove empty entries
 	list (REMOVE_ITEM COMPILE_PLUGINS "")
 endif (NOT COMPILE_PLUGINS)
+
+set (COMPILE_PLUGINS "${COMPILE_PLUGINS}" CACHE STRING "Plugins to compile" FORCE)

commit 40502b28019244518661114805102eaa01f2c02e

build: If the default plugins changed, take it into account in existing configurations
This fixes a regression from d396c02580b0515f6ba41a38239ea6fe06e50c94.
Please anyone who have run cmake on kadu since that commit at least
once, run cmake -DCOMPILE_PLUGINS= .. command, otherwise your
configuration will not be updated when the default plugins set changes.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

Plugins.cmake
@@ -1,169 +1,177 @@
-if (NOT COMPILE_PLUGINS)
-	set (COMPILE_PLUGINS
+set (DEFAULT_PLUGINS
 
-	# protocols
-		# GaduGadu protocol suppot
-		gadu_protocol
-		# Jabber/XMPP protocol support
-		jabber_protocol
+# protocols
+	# GaduGadu protocol suppot
+	gadu_protocol
+	# Jabber/XMPP protocol support
+	jabber_protocol
 
-	# notifiers
-		# Enables notifications about buddies presence and other in chat windows
-		chat_notify
-		# Notification by external commands module
-		exec_notify
-		# Hints near tray icon
-		hints
-		# Notifications by qt4_docking plugin
-		qt4_docking_notify
-		# Speech synthesis support
-		speech
-		# PC Speaker notification support
-		pcspeaker
+# notifiers
+	# Enables notifications about buddies presence and other in chat windows
+	chat_notify
+	# Notification by external commands module
+	exec_notify
+	# Hints near tray icon
+	hints
+	# Notifications by qt4_docking plugin
+	qt4_docking_notify
+	# Speech synthesis support
+	speech
+	# PC Speaker notification support
+	pcspeaker
 
-	# history
-		# General history plugin
-		history
-		# Chat history storing, browsing and searching support using sqlite
-		sql_history
-		# Migration from x <= 0.6.5 format
-		history_migration
+# history
+	# General history plugin
+	history
+	# Chat history storing, browsing and searching support using sqlite
+	sql_history
+	# Migration from x <= 0.6.5 format
+	history_migration
 
-	# encryption
-		# Encrypted chat support
-		encryption_ng
-		# OpenSSL encrypted chat support
-		encryption_ng_simlite
+# encryption
+	# Encrypted chat support
+	encryption_ng
+	# OpenSSL encrypted chat support
+	encryption_ng_simlite
 
-	# docking
-		# Tray icon support (common part of all docking modules)
-		docking
-		# Always on top window docking plugin
-		desktop_docking
-		# Qt tray docking techniques module (works everywhere)
-		qt4_docking
+# docking
+	# Tray icon support (common part of all docking modules)
+	docking
+	# Always on top window docking plugin
+	desktop_docking
+	# Qt tray docking techniques module (works everywhere)
+	qt4_docking
 
-	# sound
-		# General sound plugin
-		sound
-		# Phonon sound support
-		phonon_sound
-		# External sound player support
-		ext_sound
-		# QtSound sound support
-		qt4_sound
+# sound
+	# General sound plugin
+	sound
+	# Phonon sound support
+	phonon_sound
+	# External sound player support
+	ext_sound
+	# QtSound sound support
+	qt4_sound
+
+# mediaplayer
+	# Media players support plugin
+	mediaplayer
+
+# misc
+	# Antistring
+	antistring
+	# Auto away module
+	autoaway
+	# Auto hide Kadu window
+	auto_hide
+	# Autoresponder plugin
+	autoresponder
+	# Autostatus
+	autostatus
+	# Cenzor
+	cenzor
+	# Configuration wizard
+	config_wizard
+	# Loads presence status messages from file
+	filedesc
+	# Protection against unwanted chats
+	firewall
+	# Idle time counter
+	idle
+	# Shows image links and youtube links as images and videos in chat
+	imagelink
+	# Last status infos
+	last_seen
+	# Imports profiles from old Kadu
+	profiles_import
+	# Simple view
+	simpleview
+	# Single window mode
+	single_window
+	# Sms gateway support
+	sms
+	# Spellchecking in chat window
+	spellchecker
+	# Tabbed chat dialog
+	tabs
+	# Automatic mispelled word replacement
+	word_fix
+)
+
+# Platform-speficic plugins
+
+if (UNIX)
+	list (APPEND DEFAULT_PLUGINS
 
 	# mediaplayer
-		# Media players support plugin
-		mediaplayer
+		# MPD mediaplayer support
+		mpd_mediaplayer
+		# MPRIS Media Players support
+		mprisplayer_mediaplayer
+	)
+endif (UNIX)
+
+if (UNIX AND NOT APPLE)
+	list (APPEND DEFAULT_PLUGINS
+
+	# notifiers
+		# Freedesktop notification support
+		freedesktop_notify
+
+	# mediaplayer
+		# Amarok 1.x mediaplayer support
+		amarok1_mediaplayer
+		# FALF mediaplayer support
+		falf_mediaplayer
 
 	# misc
-		# Antistring
-		antistring
-		# Auto away module
-		autoaway
-		# Auto hide Kadu window
-		auto_hide
-		# Autoresponder plugin
-		autoresponder
-		# Autostatus
-		autostatus
-		# Cenzor
-		cenzor
-		# Configuration wizard
-		config_wizard
-		# Loads presence status messages from file
-		filedesc
-		# Protection against unwanted chats
-		firewall
-		# Idle time counter
-		idle
-		# Shows image links and youtube links as images and videos in chat
-		imagelink
-		# Last status infos
-		last_seen
-		# Imports profiles from old Kadu
-		profiles_import
-		# Simple view
-		simpleview
-		# Single window mode
-		single_window
-		# Sms gateway support
-		sms
-		# Spellchecking in chat window
-		spellchecker
-		# Tabbed chat dialog
-		tabs
-		# Automatic mispelled word replacement
-		word_fix
+		# Easily take screenshots and send as images
+		screenshot
 	)
 
-	# Platform-speficic plugins
+	if (WITH_LIBINDICATE_QT)
+		list (APPEND DEFAULT_PLUGINS
 
-	if (UNIX)
-		list (APPEND COMPILE_PLUGINS
-
-		# mediaplayer
-			# MPD mediaplayer support
-			mpd_mediaplayer
-			# MPRIS Media Players support
-			mprisplayer_mediaplayer
+		# docking
+			# Indicator docking support
+			indicator_docking
 		)
-	endif (UNIX)
+	endif (WITH_LIBINDICATE_QT)
+endif (UNIX AND NOT APPLE)
 
-	if (UNIX AND NOT APPLE)
-		list (APPEND COMPILE_PLUGINS
+if (APPLE)
+	list (APPEND DEFAULT_PLUGINS
 
-		# notifiers
-			# Freedesktop notification support
-			freedesktop_notify
+	# notifiers
+		# Growl notification support
+		growl_notify
 
-		# mediaplayer
-			# Amarok 1.x mediaplayer support
-			amarok1_mediaplayer
-			# FALF mediaplayer support
-			falf_mediaplayer
+	# mediaplayer
+		# Itunes Media Player support
+		itunes_mediaplayer
+	)
+endif (APPLE)
 
-		# misc
-			# Easily take screenshots and send as images
-			screenshot
-		)
+if (WIN32)
+	list (APPEND DEFAULT_PLUGINS
 
-		if (WITH_LIBINDICATE_QT)
-			list (APPEND COMPILE_PLUGINS
+	# mediaplayer
+		# Winamp Media Player support
+		winamp_mediaplayer
+	)
+endif (WIN32)
 
-			# docking
-				# Indicator docking support
-				indicator_docking
-			)
-		endif (WITH_LIBINDICATE_QT)
-	endif (UNIX AND NOT APPLE)
+# Sort the list so plugins will be built in alphabetical order
+list (SORT DEFAULT_PLUGINS)
 
-	if (APPLE)
-		list (APPEND COMPILE_PLUGINS
-
-		# notifiers
-			# Growl notification support
-			growl_notify
-
-		# mediaplayer
-			# Itunes Media Player support
-			itunes_mediaplayer
-		)
-	endif (APPLE)
-
-	if (WIN32)
-		list (APPEND COMPILE_PLUGINS
-
-		# mediaplayer
-			# Winamp Media Player support
-			winamp_mediaplayer
-		)
-	endif (WIN32)
-
-	# Sort the list so plugins will be built in alphabetical order
-	list (SORT COMPILE_PLUGINS)
-else (NOT COMPILE_PLUGINS)
+# We must remember that the defaults may change and we want all Git users who didn't set
+# COMPILE_PLUGINS by hand to always have current defaults.
+# So if it is the very first run (and the user didn't manually specify COMPILE_PLUGINS)
+# or last time default plugins were compiled and the user didn't change COMPILE_PLUGINS manually,
+# pick the default plugins.
+if ((NOT COMPILE_PLUGINS) OR (HAVE_DEFAULT_PLUGINS AND ("${OLD_COMPILE_PLUGINS}" STREQUAL "${COMPILE_PLUGINS}")))
+	set (COMPILE_PLUGINS ${DEFAULT_PLUGINS})
+	set (HAVE_DEFAULT_PLUGINS "TRUE" CACHE INTERNAL "" FORCE)
+else ((NOT COMPILE_PLUGINS) OR (HAVE_DEFAULT_PLUGINS AND ("${OLD_COMPILE_PLUGINS}" STREQUAL "${COMPILE_PLUGINS}")))
 	# Replace whitespace and commas with semicolons
 	string (REGEX REPLACE "[ \t\n\r,]" ";" COMPILE_PLUGINS "${COMPILE_PLUGINS}")
 	# Convert to list
@@ -171,6 +179,17 @@ else (NOT COMPILE_PLUGINS)
 
 	# Remove empty entries
 	list (REMOVE_ITEM COMPILE_PLUGINS "")
-endif (NOT COMPILE_PLUGINS)
+
+	# Sort the list locally so we can compare with the default
+	set (_compile_plugins ${COMPILE_PLUGINS})
+	list (SORT _compile_plugins)
+
+	if ("${_compile_plugins}" STREQUAL "${DEFAULT_PLUGINS}")
+		set (HAVE_DEFAULT_PLUGINS "TRUE" CACHE INTERNAL "" FORCE)
+	else ("${_compile_plugins}" STREQUAL "${DEFAULT_PLUGINS}")
+		set (HAVE_DEFAULT_PLUGINS "FALSE" CACHE INTERNAL "" FORCE)
+	endif ("${_compile_plugins}" STREQUAL "${DEFAULT_PLUGINS}")
+endif ((NOT COMPILE_PLUGINS) OR (HAVE_DEFAULT_PLUGINS AND ("${OLD_COMPILE_PLUGINS}" STREQUAL "${COMPILE_PLUGINS}")))
 
 set (COMPILE_PLUGINS "${COMPILE_PLUGINS}" CACHE STRING "Plugins to compile" FORCE)
+set (OLD_COMPILE_PLUGINS "${COMPILE_PLUGINS}" CACHE INTERNAL "" FORCE)