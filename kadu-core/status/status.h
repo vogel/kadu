694c52ef7ee3f5af43b04166b16439eecb2934c8
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2011-10-15 00:34:11 +0200

    status: Status class documented
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- kadu-core/status/status.h
+++ kadu-core/status/status.h
@@ -34,6 +34,21 @@
 
 #include "exports.h"
 
+/**
+ * @addtogroup Status
+ * @{
+ */
+
+/**
+ * @class Status
+ * @author Rafał 'Vogel' Malinowski
+ * @short Class representing current status of user.
+ * @see StatusType
+ * @see StatusTypeGroup
+ *
+ * This class describes current status of any user. Status contains two values - status type and description.
+ * Statuses can be ordered using StatusType enum (smaller value means greater availability).
+ */
 class KADUAPI Status
 {
 	StatusType Type;
@@ -42,28 +57,124 @@ class KADUAPI Status
 	QString DisplayName;
 
 public:
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Creates new status with given type and description.
+	 * @param statusType type of new status
+	 * @param description description of new status.
+	 *
+	 * Creates new status with given type and description.
+	 */
 	Status(StatusType statusType = StatusTypeOffline, const QString &description = QString());
 	Status(const Status &copyme);
 	~Status();
 
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Returns this status type.
+	 * @return this status type
+	 *
+	 * Returns this status type.
+	 */
 	StatusType type() const { return Type; }
+
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Updates this status type.
+	 * @param type new status type
+	 *
+	 * Updates this status type. Values of displayName and group are also updated.
+	 */
 	void setType(StatusType type);
 
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Returns this status type group.
+	 * @return this status type group
+	 *
+	 * Returns this status type group. This value depends only on current status type.
+	 */
 	StatusTypeGroup group() const { return Group; }
 
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Returns this status display name.
+	 * @return this status display name
+	 *
+	 * Returns this status display name. This value depends only on current status type.
+	 */
 	const QString & displayName() const { return DisplayName; }
 
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Returns this status description.
+	 * @return this status description
+	 *
+	 * Returns this status description.
+	 */
 	const QString & description() const { return Description; }
+
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Updates this status description.
+	 * @param escription new status description
+	 *
+	 * Updates this status description.
+	 */
 	void setDescription(const QString &description) { Description = description; }
+
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Returns true if this status description is not empty.
+	 * @return true if this status description is not empty
+	 *
+	 * Returns true if this status description is not empty.
+	 */
 	bool hasDescription() const;
 
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Returns true if this status if not connected (offline).
+	 * @return true if this status if not connected (offline)
+	 *
+	 * Returns true if this status if not connected (offline).
+	 */
 	bool isDisconnected() const;
 
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Compares two statuses.
+	 * @return true if left status is more available than right one
+	 *
+	 * This method compares two statuses. Status which is more available is considered 'less', so it returns
+	 * true when left status is more available than right one.
+	 */
 	bool operator < (const Status &compare) const;
+
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Compares two statuses.
+	 * @return true if both statuses are equal
+	 *
+	 * This method compares two statuses. Statuses are equal when its types and description are equal.
+	 */
 	bool operator == (const Status &compare) const;
+
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Compares two statuses.
+	 * @return true if statuses are not equal
+	 *
+	 * This method compares two statuses. Statuses are not equal when its types or description are not equal.
+	 */
 	bool operator != (const Status &compare) const;
 
 };
+
+/**
+ * @addtogroup Status
+ * @}
+ */
 
 Q_DECLARE_METATYPE(Status)
 
