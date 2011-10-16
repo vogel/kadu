9eb0daf11e2bb6fd7e47f5b924c36c0e22ce1aa1
Bartosz Brachaczek <b.brachaczek@gmail.com>
2011-10-16 16:11:44 +0200

    jabber: Do not switch XMPP::Client to "old mode" (#2329)
    This code was totally incorrect. Verified with Psi code. Psi manually
    takes local timezone offset into account only for XEP-0090 and XEP-0202
    which we do not support yet.
    
    Reported-by: Michał Ziąbkowski <mziab@o2.pl>
    Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

--- plugins/jabber_protocol/client/jabber-client.cpp
+++ plugins/jabber_protocol/client/jabber-client.cpp
@@ -89,7 +89,6 @@ JabberClient::JabberClient(JabberProtocol *protocol, QObject *parent) :
 
 	Client->setCapsVersion(capsVersion());
 	Client->setFeatures(Features(features));
-	Client->setTimeZone(timeZoneName(), timeZoneOffset());
 
 	serverInfoManager = new ServerInfoManager(Client, Client);
 	QObject::connect(serverInfoManager, SIGNAL(featuresChanged()),
@@ -194,8 +193,6 @@ void JabberClient::cleanUp()
 	setClientVersion(QString());
 	setOSName(QString());
 
-	setTimeZone("UTC", 0);
-
 	setIgnoreTLSWarnings(false);
 }
 
@@ -214,12 +211,6 @@ void JabberClient::setOverrideHost(bool flag, const QString &server, int port)
 	OverrideHost = flag;
 	Server = server;
 	Port = port;
-}
-
-void JabberClient::setTimeZone(const QString &timeZoneName, int timeZoneOffset)
-{
-	TimeZoneName = timeZoneName;
-	TimeZoneOffset = timeZoneOffset;
 }
 
 int JabberClient::getPenaltyTime()
