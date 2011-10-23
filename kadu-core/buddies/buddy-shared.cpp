vogel/kadu

commit 773f309fda47d8b37da8c29d9ca357c5c5c45d84

buddies: Remove support for importing 0.6.6-prealpha buddies
Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

kadu-core/buddies/buddy-shared.cpp
@@ -117,22 +117,13 @@ void BuddyShared::importConfiguration()
 
 void BuddyShared::load()
 {
-	QSharedPointer<StoragePoint> sp(storage());
-	if (!sp)
+	if (!isValidStorage())
 		return;
 
 	Shared::load();
 
-	XmlConfigFile *configurationStorage = sp->storage();
-	QDomElement parent = sp->point();
-
-	if (parent.hasAttribute("type"))
-	{
-		Anonymous = (1 == parent.attribute("type").toInt());
-		parent.removeAttribute("type");
-	}
-	else
-		Anonymous = loadValue<bool>("Anonymous", true);
+	XmlConfigFile *configurationStorage = storage()->storage();
+	QDomElement parent = storage()->point();
 
 	QDomElement customDataValues = configurationStorage->getNode(parent, "CustomDataValues", XmlConfigFile::ModeFind);
 	QDomNodeList customDataValuesList = customDataValues.elementsByTagName("CustomDataValue");
@@ -149,6 +140,8 @@ void BuddyShared::load()
 		if (!name.isEmpty())
 			CustomData[name] = customDataElement.text();
 	}
+
+	Anonymous = false;
 
 	Groups.clear();
 	QDomElement groupsNode = configurationStorage->getNode(parent, "ContactGroups", XmlConfigFile::ModeFind);
@@ -183,24 +176,23 @@ void BuddyShared::load()
 
 void BuddyShared::store()
 {
-	QSharedPointer<StoragePoint> sp(storage());
-	if (!sp)
+	if (!isValidStorage())
 		return;
 
 	Shared::store();
 
-	XmlConfigFile *configurationStorage = sp->storage();
-	QDomElement parent = sp->point();
+	XmlConfigFile *configurationStorage = storage()->storage();
+	QDomElement parent = storage()->point();
 
 	QDomElement customDataValues = configurationStorage->getNode(parent, "CustomDataValues", XmlConfigFile::ModeCreate);
 
 	foreach (const QString &key, CustomData.keys())
 		configurationStorage->createNamedTextNode(customDataValues, "CustomDataValue", key, CustomData.value(key));
 
-	if (BuddyAvatar->uuid().isNull())
-		removeValue("Avatar");
-	else
+	if (!BuddyAvatar->uuid().isNull())
 		storeValue("Avatar", BuddyAvatar->uuid().toString());
+	else
+		removeValue("Avatar");
 
 	storeValue("Display", Display);
 	storeValue("FirstName", FirstName);
@@ -210,11 +202,12 @@ void BuddyShared::store()
 	storeValue("Mobile", Mobile);
 	storeValue("Email", Email);
 	storeValue("Website", Website);
-	storeValue("Anonymous", Anonymous);
 	storeValue("Blocked", Blocked);
 	storeValue("OfflineTo", OfflineTo);
 	storeValue("Gender", (int)Gender);
 	storeValue("PreferHigherStatuses", PreferHigherStatuses);
+
+	removeValue("Anonymous");
 
 	if (!Groups.isEmpty())
 	{

commit 6de117813dad776b2eddfc3dd37c032206dbf011

buddies: Store "Anonymous" Buddy value to configuration
This fixes a regression from 773f309fda47d8b37da8c29d9ca357c5c5c45d84.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

kadu-core/buddies/buddy-shared.cpp
@@ -238,7 +238,10 @@ void BuddyShared::store()
 	storeValue("Gender", (int)Gender);
 	storeValue("PreferHigherStatuses", PreferHigherStatuses);
 
-	removeValue("Anonymous");
+	// This buddy can't be anonymous, otherwise we wouldn't be storing them. Though,
+	// we need to store Anonymous=false, otherwise we will break downgrade to Kadu <0.11.0.
+	// TODO when we change configuration format (or just file name): remove it
+	storeValue("Anonymous", false);
 
 	if (!Groups.isEmpty())
 	{