vogel/kadu

commit 1a1cb71e09b0709e7c0deb4da126281bba05a9fe

Better QMap usage in loops
Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

plugins/gadu_protocol/services/gadu-chat-service.cpp
@@ -368,16 +368,15 @@ void GaduChatService::handleEventAck(struct gg_event *e)
 void GaduChatService::removeTimeoutUndeliveredMessages()
 {
 	QDateTime now = QDateTime::currentDateTime();
-	QList<int> toRemove;
+	QHash<int, Message>::iterator it = UndeliveredMessages.begin();
 
-	foreach (int messageId, UndeliveredMessages.keys())
-		if (UndeliveredMessages[messageId].sendDate().addSecs(MAX_DELIVERY_TIME) < now)
-			toRemove.append(messageId);
-
-	foreach (int messageId, toRemove)
-	{
-		UndeliveredMessages[messageId].setStatus(MessageStatusWontDeliver);
-		emit messageStatusChanged(UndeliveredMessages[messageId], StatusRejectedTimeout);
-		UndeliveredMessages.remove(messageId);
-	}
+	while (it != UndeliveredMessages.end())
+		if (it.value().sendDate().addSecs(MAX_DELIVERY_TIME) < now)
+		{
+			it = UndeliveredMessages.erase(it);
+			it.value().setStatus(MessageStatusWontDeliver);
+			emit messageStatusChanged(it.value(), StatusRejectedTimeout);
+		}
+		else
+			++it;
 }

kadu-core/buddies/buddy-shared.cpp
@@ -186,8 +186,8 @@ void BuddyShared::store()
 
 	QDomElement customDataValues = configurationStorage->getNode(parent, "CustomDataValues", XmlConfigFile::ModeCreate);
 
-	foreach (const QString &key, CustomData.keys())
-		configurationStorage->createNamedTextNode(customDataValues, "CustomDataValue", key, CustomData.value(key));
+	for (QMap<QString, QString>::const_iterator it = CustomData.constBegin(), end = CustomData.constEnd(); it != end; ++it)
+		configurationStorage->createNamedTextNode(customDataValues, "CustomDataValue", it.key(), it.value());
 
 	if (!BuddyAvatar->uuid().isNull())
 		storeValue("Avatar", BuddyAvatar->uuid().toString());

kadu-core/gui/widgets/chat-widget-manager.cpp
@@ -180,13 +180,13 @@ void ChatWidgetManager::store()
 
 	if (config_file.readBoolEntry("Chat", "SaveOpenedWindows", true))
 	{
-		foreach (const Chat &chat, Chats.keys())
+		for (QHash<Chat , ChatWidget *>::const_iterator it = Chats.constBegin(), end = Chats.constEnd(); it != end; ++it)
 		{
-			Protocol *protocolHandler = chat.chatAccount().protocolHandler();
-			if (!protocolHandler || !protocolHandler->protocolFactory() || !qobject_cast<ChatWindow *>(Chats.value(chat)->window()))
+			Protocol *protocolHandler = it.key().chatAccount().protocolHandler();
+			if (!protocolHandler || !protocolHandler->protocolFactory() || !qobject_cast<ChatWindow *>(it.value()->window()))
 					continue;
 
-			StringList.append(chat.uuid().toString());
+			StringList.append(it.key().uuid().toString());
 		}
 	}
 

kadu-core/notify/notify-configuration-ui-handler.cpp
@@ -286,8 +286,8 @@ void NotifyConfigurationUiHandler::configurationWindowApplied()
 		if (gui.ConfigurationWidget)
 			gui.ConfigurationWidget->saveNotifyConfigurations();
 
-		foreach (const QString &eventKey, gui.Events.keys())
-			config_file.writeEntry("Notify", eventKey + '_' + notifier->name(), gui.Events[eventKey]);
+		for (QMap<QString, bool>::const_iterator it = gui.Events.constBegin(), end = gui.Events.constEnd(); it != end; ++it)
+			config_file.writeEntry("Notify", it.key() + '_' + notifier->name(), it.value());
 	}
 }
 

kadu-core/status/status-type-manager.cpp
@@ -65,9 +65,9 @@ StatusTypeManager::~StatusTypeManager()
 
 StatusType StatusTypeManager::fromName(const QString &name)
 {
-	foreach (StatusType statusType, StatusTypes.keys())
-		if (StatusTypes.value(statusType).name() == name)
-			return statusType;
+	for (QMap<StatusType, StatusTypeData>::const_iterator it = StatusTypes.constBegin(), end = StatusTypes.constEnd(); it != end; ++it)
+		if (it.value().name() == name)
+			return it.key();
 	return StatusTypeOffline;
 }
 

plugins/config_wizard/gui/widgets/config-wizard-profile-page.cpp
@@ -68,11 +68,8 @@ void ConfigWizardProfilePage::createGui()
 
 void ConfigWizardProfilePage::setLanguages()
 {
-	QStringList languageValues = LanguagesManager::languages().keys();
-	QStringList languageNames = LanguagesManager::languages().values();
-
-	for (int i = 0; i < languageValues.size(); i++)
-		LanguagesCombo->addItem(languageNames.at(i), languageValues.at(i));
+	for (QMap<QString, QString>::const_iterator it = LanguagesManager::languages().constBegin(), end = LanguagesManager::languages().constEnd(); it != end; ++it)
+		LanguagesCombo->addItem(it.value(), it.key());
 }
 
 void ConfigWizardProfilePage::initializePage()

plugins/exec_notify/exec_notify.cpp
@@ -75,8 +75,8 @@ void ExecConfigurationWidget::saveNotifyConfigurations()
 	if (!currentNotifyEvent.isEmpty())
 		Commands[currentNotifyEvent] = commandLineEdit->text();
 
-	foreach (const QString &eventName, Commands.keys())
-		config_file.writeEntry("Exec Notify", eventName + "Cmd", Commands[eventName]);
+	for (QMap<QString, QString>::const_iterator it = Commands.constBegin(), end = Commands.constEnd(); it != end; ++it)
+		config_file.writeEntry("Exec Notify", it.key() + "Cmd", it.value());
 }
 
 void ExecConfigurationWidget::switchToEvent(const QString &event)

plugins/pcspeaker/pcspeaker_configuration_widget.cpp
@@ -57,8 +57,8 @@ void PCSpeakerConfigurationWidget::saveNotifyConfigurations()
 	if (!CurrentNotifyEvent.isEmpty())
 		Sounds[CurrentNotifyEvent] = soundEdit->text();
 
-	foreach (const QString &key, Sounds.keys())
-		config_file.writeEntry("PC Speaker", key + "_Sound", Sounds[key]);
+	for (QMap<QString, QString>::const_iterator it = Sounds.constBegin(), end = Sounds.constEnd(); it != end; ++it)
+		config_file.writeEntry("PC Speaker", it.key() + "_Sound", it.value());
 }
 
 void PCSpeakerConfigurationWidget::switchToEvent(const QString &event)

commit fbb2e682d128e63c6484a1e72ebb9522d1b2c8dd

gadu: Fix very likely crash in GaduChatService::removeTimeoutUndeliveredMessages()
This a regression from 1a1cb71e09b0709e7c0deb4da126281bba05a9fe. My bad,
the erase() function should be called at the end of the block. Now this
code is as safe as it was before the problematic commit.

Reported-by: Piotr Galiszewski <piotrgaliszewski@gmail.com>
Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

plugins/gadu_protocol/services/gadu-chat-service.cpp
@@ -369,14 +369,20 @@ void GaduChatService::removeTimeoutUndeliveredMessages()
 {
 	QDateTime now = QDateTime::currentDateTime();
 	QHash<int, Message>::iterator it = UndeliveredMessages.begin();
+	QVector<Message> removedMessages;
 
 	while (it != UndeliveredMessages.end())
 		if (it.value().sendDate().addSecs(MAX_DELIVERY_TIME) < now)
 		{
+			removedMessages.append(it.value());
 			it = UndeliveredMessages.erase(it);
-			it.value().setStatus(MessageStatusWontDeliver);
-			emit messageStatusChanged(it.value(), StatusRejectedTimeout);
 		}
 		else
 			++it;
+
+	foreach (const Message &message, removedMessages)
+	{
+		message.setStatus(MessageStatusWontDeliver);
+		emit messageStatusChanged(message, StatusRejectedTimeout);
+	}
 }