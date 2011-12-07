vogel/kadu

commit ea1ff159871a5f6a33f9c8d861987f88ebbf4a20

spellchecker: use native os x spellchecker engine

plugins/spellchecker/spellchecker.cpp
@@ -24,10 +24,10 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifdef HAVE_ASPELL
+#if defined(HAVE_ASPELL)
 #define ASPELL_STATIC
 #include <aspell.h>
-#else
+#elif defined(HAVE_ENCHANT)
 #include <enchant++.h>
 #endif
 
@@ -37,6 +37,10 @@
 #include <QtGui/QLabel>
 #include <QtGui/QListWidget>
 #include <QtGui/QPushButton>
+
+#if defined(Q_WS_MAC)
+#include "macspellchecker.h"
+#endif
 
 #include "gui/widgets/chat-widget-manager.h"
 #include "gui/widgets/chat-widget.h"
@@ -53,7 +57,7 @@
 #include "configuration/spellchecker-configuration.h"
 #include "spellchecker.h"
 
-#ifdef HAVE_ENCHANT
+#if defined(HAVE_ENCHANT)
 typedef std::pair<SpellChecker::Checkers *, QStringList *> DescWrapper;
 
 static void enchantDictDescribe(const char * const langTag, const char * const providerName,
@@ -77,13 +81,13 @@ SpellChecker::SpellChecker(QObject *parent) :
 	connect(ChatWidgetManager::instance(), SIGNAL(chatWidgetCreated(ChatWidget *)),
 			this, SLOT(chatCreated(ChatWidget *)));
 
-#ifdef HAVE_ASPELL
+#if defined(HAVE_ASPELL)
 	// prepare configuration of spellchecker
 	SpellConfig = new_aspell_config();
 	aspell_config_replace(SpellConfig, "encoding", "utf-8");
 	aspell_config_replace(SpellConfig, "sug-mode", "ultra");
 
-#ifdef Q_OS_WIN32
+#if defined(Q_OS_WIN32)
 	aspell_config_replace(SpellConfig, "dict-dir", qPrintable(dataPath("aspell/dict")));
 	aspell_config_replace(SpellConfig, "data-dir", qPrintable(dataPath("aspell/data")));
 	aspell_config_replace(SpellConfig, "prefix", qPrintable(profilePath("dicts")));
@@ -98,7 +102,7 @@ SpellChecker::~SpellChecker()
 
 	Highlighter::removeAll();
 
-#ifdef HAVE_ASPELL
+#if defined(HAVE_ASPELL)
 	delete_aspell_config(SpellConfig);
 
 	foreach (AspellSpeller *speller, MyCheckers)
@@ -106,13 +110,14 @@ SpellChecker::~SpellChecker()
 #else
 	qDeleteAll(MyCheckers);
 #endif
+
 }
 
 QStringList SpellChecker::notCheckedLanguages()
 {
 	QStringList result;
 
-#ifdef HAVE_ASPELL
+#if defined(HAVE_ASPELL)
 	AspellDictInfoList *dlist;
 	AspellDictInfoEnumeration *dels;
 	const AspellDictInfo *entry;
@@ -125,7 +130,7 @@ QStringList SpellChecker::notCheckedLanguages()
 		if (!MyCheckers.contains(entry->name))
 			result.push_back(entry->name);
 	delete_aspell_dict_info_enumeration(dels);
-#else
+#elif defined(HAVE_ENCHANT)
 	DescWrapper aWrapper(&MyCheckers, &result);
 	enchant::Broker::instance()->list_dicts(enchantDictDescribe, &aWrapper);
 #endif
@@ -146,7 +151,7 @@ bool SpellChecker::addCheckedLang(const QString &name)
 	if (MyCheckers.contains(name))
 		return true;
 
-#ifdef HAVE_ASPELL
+#if defined(HAVE_ASPELL)
 	aspell_config_replace(SpellConfig, "lang", name.toAscii().constData());
 
 	// create spell checker using prepared configuration
@@ -158,7 +163,7 @@ bool SpellChecker::addCheckedLang(const QString &name)
 	}
 	else
 		MyCheckers[name] = to_aspell_speller(possibleErr);
-#else
+#elif defined(HAVE_ENCHANT)
 	try
 	{
 		MyCheckers[name] = enchant::Broker::instance()->request_dict(name.toStdString());
@@ -168,6 +173,8 @@ bool SpellChecker::addCheckedLang(const QString &name)
 		MessageDialog::show(KaduIcon("dialog-error"), tr("Kadu"), e.what());
 		return false;
 	}
+#elif defined(Q_WS_MAC)
+	MyCheckers[name] = new MacSpellChecker();
 #endif
 
 	if (MyCheckers.size() == 1)
@@ -182,7 +189,7 @@ void SpellChecker::removeCheckedLang(const QString &name)
 	Checkers::iterator checker = MyCheckers.find(name);
 	if (checker != MyCheckers.end())
 	{
-#ifdef HAVE_ASPELL
+#if defined(HAVE_ASPELL)
 		delete_aspell_speller(checker.value());
 #else
 		delete checker.value();
@@ -193,7 +200,7 @@ void SpellChecker::removeCheckedLang(const QString &name)
 
 void SpellChecker::buildCheckers()
 {
-#ifdef HAVE_ASPELL
+#if defined(HAVE_ASPELL)
 	foreach (AspellSpeller *speller, MyCheckers)
 		delete_aspell_speller(speller);
 #else
@@ -201,7 +208,7 @@ void SpellChecker::buildCheckers()
 #endif
 	MyCheckers.clear();
 
-#ifdef HAVE_ASPELL
+#if defined(HAVE_ASPELL)
 	if (SpellcheckerConfiguration::instance()->accents())
 		aspell_config_replace(SpellConfig, "ignore-accents", "true");
 	else
@@ -286,7 +293,7 @@ void SpellChecker::mainConfigurationWindowCreated(MainConfigurationWindow *mainC
 	connect(mainConfigurationWindow, SIGNAL(configurationWindowApplied()),
 			this, SLOT(configurationWindowApplied()));
 
-#ifndef HAVE_ASPELL
+#if defined(HAVE_ASPELL)
 	mainConfigurationWindow->widget()->widgetById("spellchecker/ignoreCase")->hide();
 #endif
 
@@ -331,31 +338,34 @@ void SpellChecker::configurationWindowApplied()
 
 bool SpellChecker::checkWord(const QString &word)
 {
+	bool isWordValid = false;
+
 	if (MyCheckers.isEmpty())
 		return true;
-
-	bool isWordValid = false;
+	
 	if (!word.contains(QRegExp("\\D")))
 		isWordValid = true;
 	else
 		for (Checkers::const_iterator it = MyCheckers.constBegin(); it != MyCheckers.constEnd(); ++it)
-#ifdef HAVE_ASPELL
+#if defined(HAVE_ASPELL)
 			if (aspell_speller_check(it.value(), word.toUtf8().constData(), -1))
-#else
+#elif defined(HAVE_ENCHANT)
 			if (it.value()->check(word.toUtf8().constData()))
+#elif defined(Q_WS_MAC)
+			if (it.value()->isCorrect(word.toUtf8().constData()))
 #endif
 			{
 				isWordValid = true;
 				break;
 			}
-
 	return isWordValid;
 }
 
 QStringList SpellChecker::buildSuggestList(const QString &word)
 {
 	QStringList suggestWordList;
-#ifdef HAVE_ASPELL
+
+#if defined(HAVE_ASPELL)
 	QTextCodec *codec = QTextCodec::codecForName("utf-8");
 #endif
 
@@ -369,7 +379,7 @@ QStringList SpellChecker::buildSuggestList(const QString &word)
 	for (Checkers::const_iterator it = MyCheckers.constBegin(); it != MyCheckers.constEnd(); ++it)
 	{
 		wordsForLanguage = suggesterWordCount;
-#ifdef HAVE_ASPELL
+#if defined(HAVE_ASPELL)
 		const AspellWordList *aspellTmpList = aspell_speller_suggest(it.value(), word.toUtf8().constData(), -1);
 
 		if (!aspell_word_list_empty(aspellTmpList))
@@ -388,7 +398,7 @@ QStringList SpellChecker::buildSuggestList(const QString &word)
 
 			delete_aspell_string_enumeration(aspellStringEnum);
 		}
-#else
+#elif defined(HAVE_ENCHANT)
 		size_t numberOfSuggs;
 		EnchantBroker *broker = enchant_broker_init();
 		EnchantDict *dict = enchant_broker_request_dict(broker, it.key().toUtf8().constData());
@@ -413,6 +423,8 @@ QStringList SpellChecker::buildSuggestList(const QString &word)
 		enchant_dict_free_string_list(dict, suggs);
 		enchant_broker_free_dict(broker, dict);
 		enchant_broker_free(broker);
+#elif defined(Q_WS_MAC)
+		suggestWordList.append(it.value()->suggestions(word));
 #endif
 	}
 

plugins/spellchecker/CMakeLists.txt
@@ -46,6 +46,10 @@ if (WIN32)
 		set (ASPELL_LIBRARIES aspell-15)
 	endif (NOT MINGW_ASPELL_DIR)
 else (WIN32)
+if (APPLE)
+	LIST(APPEND SOURCES macspellchecker.mm)
+	LIST(APPEND MOC_SOURCES macspellchecker.h)
+else (APPLE)
 	if (WITH_ENCHANT AND NOT WITH_ASPELL)
 		set (USE_ENCHANT TRUE)
 		set (FORCE_ENGINE TRUE)
@@ -82,6 +86,7 @@ else (WIN32)
 			message (FATAL_ERROR "enchant not found!")
 		endif (NOT ENCHANT_FOUND)
 	endif (USE_ENCHANT)
+endif (APPLE)
 endif (WIN32)
 
 if (USE_ASPELL)
@@ -92,6 +97,7 @@ if (USE_ASPELL)
 	link_directories (${ASPELL_LIBRARY_DIR})
 	include_directories (${ASPELL_INCLUDE_DIR})
 else (USE_ASPELL)
+if (NOT APPLE)
 	message (STATUS "Using enchant as backend")
 	add_definitions (-DHAVE_ENCHANT)
 
@@ -99,6 +105,7 @@ else (USE_ASPELL)
 	link_directories (${ENCHANT_LIBRARY_DIRS})
 	include_directories (${ENCHANT_INCLUDE_DIRS})
 	add_definitions (${ENCHANT_DEFINITIONS})
+endif (NOT APPLE)
 endif (USE_ASPELL)
 
 kadu_plugin (spellchecker

plugins/spellchecker/macspellchecker.h
@@ -1,0 +1,44 @@
+/*
+ * macspellchecker.h
+ *
+ * Copyright (C) 2006  Remko Troncon
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * You can also redistribute and/or modify this program under the
+ * terms of the Psi License, specified in the accompanied COPYING
+ * file, as published by the Psi Project; either dated January 1st,
+ * 2005, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef MACSPELLCHECKER_H
+#define MACSPELLCHECKER_H
+
+#include <QString>
+#include <QStringList>
+
+class MacSpellChecker : public QObject
+{
+	Q_OBJECT
+public:
+	MacSpellChecker();
+	~MacSpellChecker();
+	
+	QStringList suggestions(const QString&);
+	bool isCorrect(const QString&);
+};
+
+#endif

plugins/spellchecker/macspellchecker.mm
@@ -1,0 +1,58 @@
+/*
+ * macspellchecker.cpp
+ *
+ * Copyright (C) 2006  Remko Troncon
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * You can also redistribute and/or modify this program under the
+ * terms of the Psi License, specified in the accompanied COPYING
+ * file, as published by the Psi Project; either dated January 1st,
+ * 2005, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <Cocoa/Cocoa.h>
+
+#include "macspellchecker.h"
+
+MacSpellChecker::MacSpellChecker()
+{
+}
+
+MacSpellChecker::~MacSpellChecker()
+{
+}
+
+bool MacSpellChecker::isCorrect(const QString& word)
+{
+	NSString* ns_word = [NSString stringWithUTF8String: word.toUtf8().data()];
+	NSRange range = {0,0};
+	range = [[NSSpellChecker sharedSpellChecker] checkSpellingOfString:ns_word startingAt:0];
+	return (range.length == 0);
+}
+
+QStringList MacSpellChecker::suggestions(const QString& word)
+{
+	QStringList s;
+
+	NSString* ns_word = [NSString stringWithUTF8String: word.toUtf8().data()];
+	NSArray* ns_suggestions = [[NSSpellChecker sharedSpellChecker] guessesForWord:ns_word];
+	for(unsigned int i = 0; i < [ns_suggestions count]; i++) {
+		s += QString::fromUtf8([[ns_suggestions objectAtIndex:i] UTF8String]);
+	}
+
+	return s;
+}

plugins/spellchecker/spellchecker.h
@@ -30,6 +30,10 @@
 #include <QtCore/QString>
 #include <QtCore/QMap>
 
+#ifdef Q_WS_MAC
+#include "macspellchecker.h"
+#endif
+
 #include "gui/windows/main-configuration-window.h"
 
 class QListWidget;
@@ -52,15 +56,23 @@ class SpellChecker : public ConfigurationUiHandler
 	Q_OBJECT
 
 public:
+#ifdef Q_WS_MAC
+	typedef QMap<QString, MacSpellChecker *> Checkers;
+#else
 #ifdef HAVE_ASPELL
 	typedef QMap<QString, AspellSpeller *> Checkers;
 #else
 	typedef QMap<QString, enchant::Dict *> Checkers;
 #endif
+#endif // Q_WS_MAC
 
 private:
 #ifdef HAVE_ASPELL
 	AspellConfig *SpellConfig;
+#endif
+
+#ifdef Q_WS_MAC
+	MacSpellChecker *MacSpellCheck;
 #endif
 
 	Checkers MyCheckers;

commit be6003ff92c5d971997aee844c9d9501b8971362

spellchecker: Fix typo
Regression since ea1ff159871a5f6a33f9c8d861987f88ebbf4a20.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

plugins/spellchecker/spellchecker.cpp
@@ -294,7 +294,7 @@ void SpellChecker::mainConfigurationWindowCreated(MainConfigurationWindow *mainC
 	connect(mainConfigurationWindow, SIGNAL(configurationWindowApplied()),
 			this, SLOT(configurationWindowApplied()));
 
-#if defined(HAVE_ASPELL)
+#if !defined(HAVE_ASPELL)
 	mainConfigurationWindow->widget()->widgetById("spellchecker/ignoreCase")->hide();
 #endif
 