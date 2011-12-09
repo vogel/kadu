vogel/kadu

commit 69cabc10e302b082fe696609aac78b498c32e79d

model: chagned indexForValue to indexListForValue, as some models contains multiple copies of one object
Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

kadu-core/gui/widgets/select-buddy-popup.cpp
@@ -78,7 +78,10 @@ void SelectBuddyPopup::show(Buddy buddy)
 	filterWidget()->setFocus();
 #endif
 
-	QModelIndex index = View->chain()->indexForValue(buddy);
+	const QModelIndexList &indexes = View->chain()->indexListForValue(buddy);
+	Q_ASSERT(indexes.size() == 1);
+
+	const QModelIndex &index = indexes.at(0);
 	View->setCurrentIndex(index);
 	FilteredTreeView::show();
 }

kadu-core/accounts/model/accounts-model.cpp
@@ -113,15 +113,18 @@ int AccountsModel::accountIndex(Account account) const
 	return AccountManager::instance()->indexOf(account);
 }
 
-QModelIndex AccountsModel::indexForValue(const QVariant &value) const
+QModelIndexList AccountsModel::indexListForValue(const QVariant &value) const
 {
-	return createIndex(accountIndex(value.value<Account>()), 0, 0);
+	QModelIndexList result;
+	result.append(createIndex(accountIndex(value.value<Account>()), 0, 0));
+	return result;
 }
 
 void AccountsModel::accountUpdated(Account account)
 {
-	QModelIndex index = indexForValue(account);
-	emit dataChanged(index, index);
+	const QModelIndexList &indexes = indexListForValue(account);
+	foreach (const QModelIndex &index, indexes)
+		emit dataChanged(index, index);
 }
 
 void AccountsModel::accountAboutToBeRegistered(Account account)

kadu-core/accounts/model/accounts-model.h
@@ -51,7 +51,7 @@ public:
 
 	Account account(const QModelIndex &index) const;
 	int accountIndex(Account account) const;
-	virtual QModelIndex indexForValue(const QVariant &value) const;
+	virtual QModelIndexList indexListForValue(const QVariant &value) const;
 
 	void setIncludeIdInDisplay(bool includeIdInDisplay);
 

kadu-core/buddies/model/buddies-model-base.cpp
@@ -66,10 +66,10 @@ void BuddiesModelBase::buddyStatusChanged(Contact contact, Status oldStatus)
 {
 	Q_UNUSED(oldStatus)
 
-	QModelIndex index = indexForValue(contact.ownerBuddy());
-
-	if (index.isValid())
-		emit dataChanged(index, index);
+	const QModelIndexList& indexes = indexListForValue(contact.ownerBuddy());
+	foreach (const QModelIndex &index, indexes)
+		if (index.isValid())
+			emit dataChanged(index, index);
 }
 
 QModelIndex BuddiesModelBase::index(int row, int column, const QModelIndex &parent) const

kadu-core/buddies/model/buddies-model.cpp
@@ -114,16 +114,17 @@ Buddy BuddiesModel::buddyAt(int index) const
 		return BuddyManager::instance()->byIndex(index);
 }
 
-QModelIndex BuddiesModel::indexForValue(const QVariant &value) const
+QModelIndexList BuddiesModel::indexListForValue(const QVariant &value) const
 {
-	Buddy buddy = value.value<Buddy>();
-	int result;
-	if (IncludeMyself && buddy == Core::instance()->myself())
-		result = BuddyManager::instance()->count();
-	else
-		result = BuddyManager::instance()->indexOf(buddy);
+	QModelIndexList result;
 
-	return index(result, 0);
+	const Buddy &buddy = value.value<Buddy>();
+	if (IncludeMyself && buddy == Core::instance()->myself())
+		result.append(index(BuddyManager::instance()->count(), 0));
+	else
+		result.append(index(BuddyManager::instance()->indexOf(buddy), 0));
+
+	return result;
 }
 
 void BuddiesModel::buddyAboutToBeAdded(Buddy &buddy)
@@ -136,22 +137,27 @@ void BuddiesModel::buddyAboutToBeAdded(Buddy &buddy)
 
 void BuddiesModel::buddyAdded(Buddy &buddy)
 {
-	Q_UNUSED(buddy)
-
 	endInsertRows();
 
 	// force refresh on proxy sorring model
 	// setDynamicSortFilter does not work properly when adding/removing items, only when changing item data
 	// this is Qt bug
 	// see bug #2167
-	QModelIndex index = indexForValue(buddy);
+
+	const QModelIndexList &indexes = indexListForValue(buddy);
+	Q_ASSERT(indexes.size() == 1);
+
+	const QModelIndex &index = indexes.at(0);
 	emit dataChanged(index, index);
 }
 
 void BuddiesModel::buddyAboutToBeRemoved(Buddy &buddy)
 {
-	int index = indexForValue(buddy).row();
-	beginRemoveRows(QModelIndex(), index, index);
+	const QModelIndexList &indexes = indexListForValue(buddy);
+	Q_ASSERT(indexes.size() == 1);
+
+	const QModelIndex &index = indexes.at(0);
+	beginRemoveRows(QModelIndex(), index.row(), index.row());
 }
 
 void BuddiesModel::buddyRemoved(Buddy &buddy)
@@ -172,16 +178,21 @@ void BuddiesModel::myselfBuddyUpdated()
 
 void BuddiesModel::buddyUpdated(Buddy &buddy)
 {
-	QModelIndex index = indexForValue(buddy);
-	if (index.isValid())
-		emit dataChanged(index, index);
+	const QModelIndexList &indexes = indexListForValue(buddy);
+	Q_ASSERT(indexes.size() == 1);
+
+	const QModelIndex &index = indexes.at(0);
+	emit dataChanged(index, index);
 }
 
 void BuddiesModel::contactAboutToBeAttached(Contact contact, Buddy nearFutureBuddy)
 {
 	Q_UNUSED(contact)
 
-	QModelIndex index = indexForValue(nearFutureBuddy);
+	const QModelIndexList &indexes = indexListForValue(nearFutureBuddy);
+	Q_ASSERT(indexes.size() == 1);
+
+	const QModelIndex &index = indexes.at(0);
 	if (!index.isValid())
 		return;
 
@@ -191,9 +202,12 @@ void BuddiesModel::contactAboutToBeAttached(Contact contact, Buddy nearFutureBud
 
 void BuddiesModel::contactAttached(Contact contact)
 {
-	Buddy buddy = contact.ownerBuddy();
+	const Buddy &buddy = contact.ownerBuddy();
 
-	QModelIndex index = indexForValue(buddy);
+	const QModelIndexList &indexes = indexListForValue(buddy);
+	Q_ASSERT(indexes.size() == 1);
+
+	const QModelIndex &index = indexes.at(0);
 	if (!index.isValid())
 		return;
 
@@ -202,9 +216,12 @@ void BuddiesModel::contactAttached(Contact contact)
 
 void BuddiesModel::contactAboutToBeDetached(Contact contact)
 {
-	Buddy buddy = contact.ownerBuddy();
+	const Buddy &buddy = contact.ownerBuddy();
 
-	QModelIndex index = indexForValue(buddy);
+	const QModelIndexList &indexes = indexListForValue(buddy);
+	Q_ASSERT(indexes.size() == 1);
+
+	const QModelIndex &index = indexes.at(0);
 	if (!index.isValid())
 		return;
 
@@ -216,7 +233,10 @@ void BuddiesModel::contactDetached(Contact contact, Buddy previousBuddy)
 {
 	Q_UNUSED(contact)
 
-	QModelIndex index = indexForValue(previousBuddy);
+	const QModelIndexList &indexes = indexListForValue(previousBuddy);
+	Q_ASSERT(indexes.size() == 1);
+
+	const QModelIndex &index = indexes.at(0);
 	if (!index.isValid())
 		return;
 
@@ -225,18 +245,18 @@ void BuddiesModel::contactDetached(Contact contact, Buddy previousBuddy)
 
 void BuddiesModel::contactUpdated(Contact &contact)
 {
-	if (!contact)
-		return;
-
-	Buddy buddy = contact.ownerBuddy();
+	const Buddy &buddy = contact.ownerBuddy();
 	if (!buddy)
 		return;
 
-	QModelIndex indexOfBuddy = indexForValue(buddy);
+	const QModelIndexList &indexes = indexListForValue(buddy);
+	Q_ASSERT(indexes.size() == 1);
+
+	const QModelIndex &indexOfBuddy = indexes.at(0);
 	if (!indexOfBuddy.isValid())
 		return;
 
-	QModelIndex contactIndex = index(buddy.contacts().indexOf(contact), 0, indexOfBuddy);
+	const QModelIndex &contactIndex = index(buddy.contacts().indexOf(contact), 0, indexOfBuddy);
 
 	emit dataChanged(indexOfBuddy, indexOfBuddy);
 	emit dataChanged(contactIndex, contactIndex);

kadu-core/buddies/model/buddies-model.h
@@ -65,7 +65,7 @@ public:
 	virtual int rowCount(const QModelIndex &parent = QModelIndex()) const;
 
 	// AbstractContactsModel implementation
-	virtual QModelIndex indexForValue(const QVariant &value) const;
+	virtual QModelIndexList indexListForValue(const QVariant &value) const;
 
 	void setIncludeMyself(bool includeMyself);
 	bool includeMyself() const { return IncludeMyself; }

kadu-core/buddies/model/buddy-list-model.cpp
@@ -55,7 +55,9 @@ Buddy BuddyListModel::buddyAt(int index) const
 		return Buddy::null;
 }
 
-QModelIndex BuddyListModel::indexForValue(const QVariant &value) const
+QModelIndexList BuddyListModel::indexListForValue(const QVariant &value) const
 {
-	return index(List.indexOf(value.value<Buddy>()), 0);
+	QModelIndexList result;
+	result.append(index(List.indexOf(value.value<Buddy>()), 0));
+	return result;
 }

kadu-core/buddies/model/buddy-list-model.h
@@ -50,7 +50,7 @@ public:
 	virtual int rowCount(const QModelIndex &parent = QModelIndex()) const;
 
 	// AbstractContactsModel implementation
-	virtual QModelIndex indexForValue(const QVariant &value) const;
+	virtual QModelIndexList indexListForValue(const QVariant &value) const;
 
 };
 

commit 19cd27adebabec83ab0215f336150dccd1b773f0

gui: Fix opening SelectBuddyPopup when no buddy is selected in the combo
Q_ASSERT was hit incorrectly here. Regression since
69cabc10e302b082fe696609aac78b498c32e79d.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

kadu-core/gui/widgets/select-buddy-popup.cpp
@@ -75,11 +75,17 @@ void SelectBuddyPopup::show(Buddy buddy)
 	filterWidget()->setFocus();
 #endif
 
-	const QModelIndexList &indexes = View->chain()->indexListForValue(buddy);
-	Q_ASSERT(indexes.size() == 1);
+	if (buddy)
+	{
+		const QModelIndexList &indexes = View->chain()->indexListForValue(buddy);
+		Q_ASSERT(indexes.size() == 1);
 
-	const QModelIndex &index = indexes.at(0);
-	View->setCurrentIndex(index);
+		const QModelIndex &index = indexes.at(0);
+		View->setCurrentIndex(index);
+	}
+	else
+		View->setCurrentIndex(QModelIndex());
+
 	FilteredTreeView::show();
 }
 