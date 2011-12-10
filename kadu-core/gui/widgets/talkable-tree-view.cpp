vogel/kadu

commit eeeff37f7354d895fbbf806866e8a73c63bea573

gui: restored currentChanged signal in TalkableTreeView
Required for info panel.

Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

kadu-core/gui/widgets/talkable-tree-view.cpp
@@ -307,3 +307,10 @@ void TalkableTreeView::hideEvent(QHideEvent *event)
 	toolTipHide(false);
 	QTreeView::hideEvent(event);
 }
+
+void TalkableTreeView::currentChanged(const QModelIndex &current, const QModelIndex &previous)
+{
+	Q_UNUSED(previous);
+
+	emit currentChanged(talkableAt(current));
+}

kadu-core/gui/widgets/talkable-tree-view.h
@@ -95,6 +95,8 @@ protected:
 
 	virtual void hideEvent(QHideEvent *event);
 
+	virtual void currentChanged(const QModelIndex &current, const QModelIndex &previous);
+
 public:
 	explicit TalkableTreeView(QWidget *parent = 0);
 	virtual ~TalkableTreeView();
@@ -116,6 +118,7 @@ public:
 
 signals:
 	void talkableActivated(const Talkable &talkable);
+	void currentChanged(const Talkable &talkable);
 
 };
 

commit 4b914bbca2c3c0973e8cec156a9825731b95db40

gui: Fix navigation on buddies list with keyboard
Regression since eeeff37f7354d895fbbf806866e8a73c63bea573.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

kadu-core/gui/widgets/talkable-tree-view.cpp
@@ -315,7 +315,7 @@ void TalkableTreeView::hideEvent(QHideEvent *event)
 
 void TalkableTreeView::currentChanged(const QModelIndex &current, const QModelIndex &previous)
 {
-	Q_UNUSED(previous);
+	QTreeView::currentChanged(current, previous);
 
 	emit currentChanged(talkableAt(current));
 }