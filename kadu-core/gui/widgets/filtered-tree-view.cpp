vogel/kadu

commit e89879d32e01336053e70d1ea41f2191dc1a3d0d

gui: added stub of new Chats view UI
Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

kadu-core/gui/windows/kadu-window.cpp
@@ -27,6 +27,7 @@
 #include <QtGui/QApplication>
 #include <QtGui/QCloseEvent>
 #include <QtGui/QHBoxLayout>
+#include <QtGui/QLabel>
 #include <QtGui/QMenu>
 #include <QtGui/QMenuBar>
 #include <QtGui/QPushButton>
@@ -120,8 +121,18 @@ void KaduWindow::createGui()
 	MainLayout->setMargin(0);
 	MainLayout->setSpacing(0);
 
-	Split = new QSplitter(Qt::Vertical, this);
-	MainLayout->addWidget(Split);
+	QTabWidget *buddiesChatView = new QTabWidget(this);
+	MainLayout->addWidget(buddiesChatView);
+	buddiesChatView->addTab(createBuddiesWidget(buddiesChatView), tr("Buddies"));
+	buddiesChatView->addTab(createChatsWidget(buddiesChatView), tr("Chats"));
+
+	setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+	setCentralWidget(MainWidget);
+}
+
+QWidget * KaduWindow::createBuddiesWidget(QWidget *parent)
+{
+	Split = new QSplitter(Qt::Vertical, parent);
 
 	QWidget* hbox = new QWidget(Split);
 	QHBoxLayout *hboxLayout = new QHBoxLayout(hbox);
@@ -163,8 +174,12 @@ void KaduWindow::createGui()
 
 	Split->setSizes(splitSizes);
 
-	setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
-	setCentralWidget(MainWidget);
+	return Split;
+}
+
+QWidget * KaduWindow::createChatsWidget(QWidget *parent)
+{
+	return new QTreeView(parent);
 }
 
 void KaduWindow::createMenu()

kadu-core/gui/windows/kadu-window.h
@@ -101,6 +101,9 @@ private:
 	bool CompositingEnabled;
 
 	void createGui();
+	QWidget *createBuddiesWidget(QWidget *parent);
+	QWidget *createChatsWidget(QWidget *parent);
+
 	void createMenu();
 	void createKaduMenu();
 	void createContactsMenu();

commit 936b3986d3747487620631ffa2cb90a73cc21872

gui: Restore giving focus to buddies list at start
Regression since e89879d32e01336053e70d1ea41f2191dc1a3d0d.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

kadu-core/gui/widgets/filtered-tree-view.cpp
@@ -76,6 +76,8 @@ void FilteredTreeView::insertView()
 		Layout->insertWidget(1, View);
 	else
 		Layout->insertWidget(0, View);
+
+	setFocusProxy(View);
 }
 
 void FilteredTreeView::setPosition(FilterPosition filterPosition)

kadu-core/gui/widgets/roster-widget.cpp
@@ -74,6 +74,8 @@ void RosterWidget::createGui()
 
 	layout->setStretchFactor(GroupBar, 1);
 	layout->setStretchFactor(TalkableWidget, 100);
+
+	setFocusProxy(TalkableWidget);
 }
 
 void RosterWidget::configurationUpdated()

kadu-core/gui/windows/kadu-window.cpp
@@ -144,6 +144,8 @@ void KaduWindow::createGui()
 
 	setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
 	setCentralWidget(MainWidget);
+	setFocusProxy(Roster);
+	Roster->setFocus();
 }
 
 void KaduWindow::createMenu()