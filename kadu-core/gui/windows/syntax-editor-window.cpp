vogel/kadu

commit a1a2ce99fa7c849560021ef9e9436f3cc1b7a3d6

gui: big cleanup of Preview widget
Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

kadu-core/gui/windows/syntax-editor-window.cpp
@@ -99,7 +99,6 @@ SyntaxEditorWindow::SyntaxEditorWindow(const QSharedPointer<SyntaxList> &syntaxL
 	previewPanel->setMinimumHeight(0);
 	previewPanel->setMaximumHeight(QWIDGETSIZE_MAX);
 	previewPanel->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Expanding);
-	previewPanel->setResetBackgroundColor(config_file.readEntry("Look", category + "BgColor"));
 	previewPanel->setMinimumSize(PREVIEW_MINIMUM_SIZE);
 
 	QPushButton *previewbutton = new QPushButton(qApp->style()->standardIcon(QStyle::SP_BrowserReload), tr("Refresh Preview"), this);

kadu-core/chat/chat-styles-manager.cpp
@@ -413,7 +413,7 @@ void ChatStylesManager::preparePreview(Preview *preview)
 
 	MessageRenderInfo *messageRenderInfo = new MessageRenderInfo(sentMessage);
 	messageRenderInfo->setSeparatorSize(CfgHeaderSeparatorHeight);
-	preview->addObjectToParse(BuddyPreferredManager::instance()->preferredContact(Core::instance()->myself()), messageRenderInfo);
+	preview->addMessage(messageRenderInfo);
 
 	Message receivedMessage = Message::create();
 	receivedMessage.setMessageChat(chat);
@@ -425,7 +425,7 @@ void ChatStylesManager::preparePreview(Preview *preview)
 
 	messageRenderInfo = new MessageRenderInfo(receivedMessage);
 	messageRenderInfo->setSeparatorSize(CfgHeaderSeparatorHeight);
-	preview->addObjectToParse(BuddyPreferredManager::instance()->preferredContact(example), messageRenderInfo);
+	preview->addMessage(messageRenderInfo);
 }
 
 void ChatStylesManager::styleChangedSlot(const QString &styleName)

kadu-core/chat/style-engines/chat-engine-adium/chat-engine-adium.cpp
@@ -279,10 +279,10 @@ void AdiumChatStyleEngine::prepareStylePreview(Preview *preview, QString styleNa
 	AdiumStyle style(styleName);
 
 	style.setCurrentVariant(variantName);
-	if (preview->getObjectsToParse().count() != 2)
+	if (preview->messages().count() != 2)
 		return;
 
-	MessageRenderInfo *message = qobject_cast<MessageRenderInfo *>(preview->getObjectsToParse().at(0));
+	MessageRenderInfo *message = preview->messages().at(0);
 	if (!message)
 		return;
 	Message msg = message->message();
@@ -319,7 +319,7 @@ void AdiumChatStyleEngine::prepareStylePreview(Preview *preview, QString styleNa
 	outgoingHtml.append("</span>");
 	preview->page()->mainFrame()->evaluateJavaScript("appendMessage(\'" + outgoingHtml + "\')");
 
-	message = qobject_cast<MessageRenderInfo *>(preview->getObjectsToParse().at(1));
+	message = preview->messages().at(1);
 	QString incomingHtml(replacedNewLine(replaceKeywords(style.baseHref(), style.incomingHtml(), message), QLatin1String(" ")));
 	incomingHtml.replace('\'', QLatin1String("\\'"));
 	if (!message->message().id().isEmpty())

kadu-core/chat/style-engines/chat-engine-kadu/chat-engine-kadu.cpp
@@ -256,16 +256,15 @@ void KaduChatStyleEngine::prepareStylePreview(Preview *preview, QString styleNam
 
 	KaduChatSyntax syntax(SyntaxList::readSyntax("chat", styleName, QString()));
 
-	Contact contact = preview->getContactList().count() == 1 ? *(preview->getContactList().constBegin()) : Contact();
-	QString text = Parser::parse(syntax.top(), Talkable(contact), true);
+	QString text = Parser::parse(syntax.top(), Talkable(), true);
 
-	int count = preview->getObjectsToParse().count();
+	int count = preview->messages().count();
 	if (count)
 	{
 		MessageRenderInfo *message;
 		for (int i = 0; i < count; i++)
 		{
-			message = qobject_cast<MessageRenderInfo *>(preview->getObjectsToParse().at(i));
+			message = preview->messages().at(i);
 			Contact sender = message->message().messageSender();
 			text += Parser::parse(syntax.withHeader(), Talkable(sender), message);
 		}

kadu-core/gui/widgets/preview.cpp
@@ -28,6 +28,7 @@
 #include "accounts/account-manager.h"
 #include "chat/style-engines/chat-engine-kadu/kadu-chat-syntax.h"
 #include "configuration/chat-configuration-holder.h"
+#include "message/message-render-info.h"
 #include "parser/parser.h"
 #include "status/status-type-manager.h"
 #include "status/status.h"
@@ -36,8 +37,8 @@
 
 #include "preview.h"
 
-Preview::Preview(QWidget *parent)
-	: KaduWebView(parent), contact(Contact::null), DrawFrame(true)
+Preview::Preview(QWidget *parent) :
+		KaduWebView(parent)
 {
 	setFixedHeight(PREVIEW_DEFAULT_HEIGHT);
 	setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
@@ -52,7 +53,17 @@ Preview::Preview(QWidget *parent)
 
 Preview::~Preview()
 {
-	qDeleteAll(objectsToParse);
+	qDeleteAll(Messages);
+}
+
+void Preview::addMessage(MessageRenderInfo *messageRenderInfo)
+{
+	 Messages.append(messageRenderInfo);
+}
+
+const QList<MessageRenderInfo *> & Preview::messages() const
+{
+	return Messages;
 }
 
 void Preview::syntaxChanged(const QString &content)
@@ -64,18 +75,14 @@ void Preview::syntaxChanged(const QString &content)
 
 	QString text;
 
-// 	setHtml("<body bgcolor=\"" + resetBackgroundColor + "\"></body>");
-
-	int count = objectsToParse.count();
-	if (count)
+	if (!Messages.isEmpty())
 	{
 		KaduChatSyntax syntax(content);
 
-		text = Parser::parse(syntax.top(), Talkable(contact), true);
+		text = Parser::parse(syntax.top(), Talkable(), true);
 
-		Contact contact = *contacts.constBegin();
-		for (int i = 0; i < count; i++)
-			text += Parser::parse(syntax.withHeader(), Talkable(contact), objectsToParse.at(i));
+		foreach (MessageRenderInfo *message, Messages)
+			text += Parser::parse(syntax.withHeader(), message->message().messageSender(), message);
 	}
 	else
 		text = Parser::parse(syntax, Talkable(Buddy::dummy()));
@@ -87,24 +94,14 @@ void Preview::syntaxChanged(const QString &content)
 
 void Preview::paintEvent(QPaintEvent *event)
 {
-	if (DrawFrame)
-	{
-		QFrame frame;
-		frame.setFrameStyle(QFrame::StyledPanel | QFrame::Raised);
-		frame.resize(size());
-		QPainter painter(this);
-		QPixmap framepixmap = QPixmap::grabWidget(&frame);
-		painter.drawPixmap(0, 0, framepixmap);
-	}
-	KaduWebView::paintEvent(event);
-}
+	QFrame frame;
+	frame.setFrameStyle(QFrame::StyledPanel | QFrame::Raised);
+	frame.resize(size());
+	QPainter painter(this);
+	QPixmap framepixmap = QPixmap::grabWidget(&frame);
+	painter.drawPixmap(0, 0, framepixmap);
 
-void Preview::setDrawFrame(bool drawFrame)
-{
-	if (DrawFrame == drawFrame)
-		return;
-	DrawFrame = drawFrame;
-	repaint();
+	KaduWebView::paintEvent(event);
 }
 
 void Preview::configurationUpdated()

kadu-core/gui/widgets/preview.h
@@ -32,32 +32,24 @@
 
 #define PREVIEW_DEFAULT_HEIGHT 250
 
+class MessageRenderInfo;
+
 class Preview : public KaduWebView, public ConfigurationAwareObject
 {
 	Q_OBJECT
 
-	Contact contact;
-	QObjectList objectsToParse;
-	ContactSet contacts;
-	QString resetBackgroundColor;
-	bool DrawFrame;
+	QList<MessageRenderInfo *> Messages;
 
 protected:
 	virtual void configurationUpdated();
 	void paintEvent(QPaintEvent *event);
 
 public:
-	Preview(QWidget *parent = 0);
-	~Preview();
+	explicit Preview(QWidget *parent = 0);
+	virtual ~Preview();
 
-	void setResetBackgroundColor(const QString &resetBackgroundColor) { this->resetBackgroundColor = resetBackgroundColor; }
-	void addObjectToParse(Contact contact, QObject *object) { contacts.insert(contact); objectsToParse.append(object); }
-
-	const QObjectList & getObjectsToParse() const { return objectsToParse; }
-	const ContactSet & getContactList() const { return contacts; }
-
-	bool drawFrame() { return DrawFrame; };
-	void setDrawFrame(bool drawFrame);
+	void addMessage(MessageRenderInfo *messageRenderInfo);
+	const QList<MessageRenderInfo *> & messages() const;
 
 public slots:
 	void syntaxChanged(const QString &content);

kadu-core/gui/windows/main-configuration-window.cpp
@@ -250,7 +250,6 @@ MainConfigurationWindow::MainConfigurationWindow() :
 	connect(widget()->widgetById("lookChatAdvanced"), SIGNAL(clicked()), this, SLOT(showLookChatAdvanced()));
 
 	Preview *infoPanelSyntaxPreview = static_cast<Preview *>(widget()->widgetById("infoPanelSyntaxPreview"));
-	infoPanelSyntaxPreview->setResetBackgroundColor(config_file.readEntry("Look", "InfoPanelBgColor"));
 	connect(infoPanelSyntaxPreview, SIGNAL(needFixup(QString &)), Core::instance()->kaduWindow()->infoPanel(), SLOT(styleFixup(QString &)));
 	connect(widget()->widgetById("infoPanelSyntax"), SIGNAL(syntaxChanged(const QString &)), infoPanelSyntaxPreview, SLOT(syntaxChanged(const QString &)));
 	connect(widget()->widgetById("infoPanelSyntax"), SIGNAL(onSyntaxEditorWindowCreated(SyntaxEditorWindow *)),

commit 73cb03c9c8af4a60a39f5f491ce1685e6a6b613e

gui: Fix compilation with -Werror
Regression since a1a2ce99fa7c849560021ef9e9436f3cc1b7a3d6.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

kadu-core/gui/windows/syntax-editor-window.cpp
@@ -49,6 +49,9 @@ SyntaxEditorWindow::SyntaxEditorWindow(const QSharedPointer<SyntaxList> &syntaxL
 		const QString &category, const QString &syntaxHint, QWidget *parent) :
 		QWidget(parent), DesktopAwareObject(this), syntaxList(syntaxList), syntaxName(syntaxName)
 {
+	// TODO: unused since a1a2ce99fa7c849560021ef9e9436f3cc1b7a3d6, fix API
+	Q_UNUSED(category);
+
 	setWindowRole("kadu-syntax-editor");
 
 	setWindowTitle(tr("Kadu syntax editor"));