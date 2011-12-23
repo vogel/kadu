vogel/kadu

commit 32b199bdb8f63c3336da80e10a39806653a6792d

mediaplayer: remove unused parameters and some unneccessary methods
Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

plugins/winamp_mediaplayer/winamp.cpp
@@ -70,7 +70,7 @@ QString WinampMediaPlayer::readWinampMemory(quint32 command, quint32 arg, bool u
 			return QString::fromUtf16((const ushort*)lpBuffer);
 		}
 		else{
-			kdebug("ret = %s\n", lpBuffer);		
+			kdebug("ret = %s\n", lpBuffer);
 			return QString::fromLocal8Bit(lpBuffer);
 		}
 	}
@@ -119,7 +119,7 @@ QString WinampMediaPlayer::getFileTagW(int position, QString tag)
 		ReadProcessMemory(hWinampProcess, pForeightRet, lpReturn, 256, NULL);
 
 		VirtualFreeEx(hWinampProcess, pForeightRet, 0, MEM_RELEASE);
-		VirtualFreeEx(hWinampProcess, pForeightTag, 0, MEM_RELEASE);		
+		VirtualFreeEx(hWinampProcess, pForeightTag, 0, MEM_RELEASE);
 		VirtualFreeEx(hWinampProcess, pFileInfo, 0, MEM_RELEASE);
 
 		CloseHandle(hWinampProcess);
@@ -171,7 +171,7 @@ QString WinampMediaPlayer::getFileTagA(int position, QString tag)
 		ReadProcessMemory(hWinampProcess, pForeightRet, lpReturn, 128, NULL);
 
 		VirtualFreeEx(hWinampProcess, pForeightRet, 0, MEM_RELEASE);
-		VirtualFreeEx(hWinampProcess, pForeightTag, 0, MEM_RELEASE);		
+		VirtualFreeEx(hWinampProcess, pForeightTag, 0, MEM_RELEASE);
 		VirtualFreeEx(hWinampProcess, pFileInfo, 0, MEM_RELEASE);
 
 		CloseHandle(hWinampProcess);
@@ -242,14 +242,12 @@ uint WinampMediaPlayer::getPlayListLength()
 	return 0;
 }
 
-QString WinampMediaPlayer::getTitle(int position)
+QString WinampMediaPlayer::getTitle()
 {
 	kdebugf();
 	HWND hWinamp=findWinamp();
 	if(hWinamp){
-		if(position<0){
-			position=SendMessage(hWinamp, WM_WA_IPC, 0, IPC_GETLISTPOS);
-		}
+		int position=SendMessage(hWinamp, WM_WA_IPC, 0, IPC_GETLISTPOS);
 
 		QString title = getFileTagW(position, "title");
 
@@ -262,14 +260,12 @@ QString WinampMediaPlayer::getTitle(int position)
 	return QString();
 }
 
-QString WinampMediaPlayer::getAlbum(int position)
+QString WinampMediaPlayer::getAlbum()
 {
 	kdebugf();
 	HWND hWinamp=findWinamp();
 	if(hWinamp){
-		if(position<0){
-			position=SendMessage(hWinamp, WM_WA_IPC, 0, IPC_GETLISTPOS);
-		}
+		int position=SendMessage(hWinamp, WM_WA_IPC, 0, IPC_GETLISTPOS);
 
 		QString album = getFileTagW(position, "album");
 
@@ -282,14 +278,12 @@ QString WinampMediaPlayer::getAlbum(int position)
 	return QString();
 }
 
-QString WinampMediaPlayer::getArtist(int position)
+QString WinampMediaPlayer::getArtist()
 {
 	kdebugf();
 	HWND hWinamp=findWinamp();
 	if(hWinamp){
-		if(position<0){
-			position=SendMessage(hWinamp, WM_WA_IPC, 0, IPC_GETLISTPOS);
-		}
+		int position=SendMessage(hWinamp, WM_WA_IPC, 0, IPC_GETLISTPOS);
 
 		QString artist = getFileTagW(position, "artist");
 
@@ -302,14 +296,12 @@ QString WinampMediaPlayer::getArtist(int position)
 	return "";
 }
 
-QString WinampMediaPlayer::getFile(int position)
+QString WinampMediaPlayer::getFile()
 {
 	kdebugf();
 	HWND hWinamp=findWinamp();
 	if(hWinamp){
-		if(position<0){
-			position=SendMessage(hWinamp, WM_WA_IPC, 0, IPC_GETLISTPOS);
-		}
+		int position=SendMessage(hWinamp, WM_WA_IPC, 0, IPC_GETLISTPOS);
 
 		QString file=readWinampMemory(IPC_GETPLAYLISTFILEW, position, true);
 		if(file.isEmpty()){
@@ -323,54 +315,14 @@ QString WinampMediaPlayer::getFile(int position)
 	return QString();
 }
 
-int WinampMediaPlayer::getLength(int position)
+int WinampMediaPlayer::getLength()
 {
 	kdebugf();
 	HWND hWinamp=findWinamp();
 
 	if(hWinamp){
-		if(position<0)
-			return SendMessage(hWinamp, WM_WA_IPC, 2, IPC_GETOUTPUTTIME);
-		else{
-			DWORD processId;
-			GetWindowThreadProcessId(hWinamp, &processId);
-
-			HANDLE hWinampProcess=OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE, 0, processId);
-			if(!SUCCEEDED(hWinampProcess)){
-				kdebugm(KDEBUG_WARNING, "unable to open winamp process\n");
-				return 0;
-			}
-
-			basicFileInfoStructW* pFileInfo = (basicFileInfoStructW*)VirtualAllocEx(hWinampProcess, NULL, sizeof(basicFileInfoStructW), MEM_COMMIT, PAGE_READWRITE);
-			if(!pFileInfo){
-				kdebugm(KDEBUG_WARNING, "unable to allocate foreight memory\n");
-				CloseHandle(hWinampProcess);
-				return 0;
-			}
-
-			basicFileInfoStructW fis;
-			fis.filename=(const wchar_t*)SendMessage(hWinamp, WM_WA_IPC, position, IPC_GETPLAYLISTFILEW);
-			fis.quickCheck=0;
-
-			WriteProcessMemory(hWinampProcess, pFileInfo, &fis, sizeof(fis), NULL);
-
-			if(SendMessage(hWinamp, WM_WA_IPC, (WPARAM)pFileInfo, IPC_GET_BASIC_FILE_INFOW)==1){
-				kdebug("unicode not supported\n");
-				fis.filename=(const wchar_t*)SendMessage(hWinamp, WM_WA_IPC, position, IPC_GETPLAYLISTFILE);
-				WriteProcessMemory(hWinampProcess, pFileInfo, &fis, sizeof(fis), NULL);
-				SendMessage(hWinamp, WM_WA_IPC, (WPARAM)pFileInfo, IPC_GET_BASIC_FILE_INFO);
-			}
-
-			ReadProcessMemory(hWinampProcess, pFileInfo, &fis, sizeof(fis), NULL);
-
-			VirtualFreeEx(hWinampProcess, pFileInfo, 0, MEM_RELEASE);
-
-			CloseHandle(hWinampProcess);
-
-			return fis.length * 1000;
-		}
+		return SendMessage(hWinamp, WM_WA_IPC, 2, IPC_GETOUTPUTTIME);
 	}
-
 
 	return 0;
 }
@@ -399,7 +351,7 @@ bool WinampMediaPlayer::isPlaying()
 bool WinampMediaPlayer::isActive()
 {
 	kdebugf();
-	
+
 	return findWinamp();
 }
 

plugins/winamp_mediaplayer/winamp.h
@@ -23,11 +23,11 @@ class WinampMediaPlayer : public PlayerCommands, public PlayerInfo
 		~WinampMediaPlayer();
 
 		// PlayerInfo
-		QString getTitle(int position = -1);
-		QString getAlbum(int position = -1);
-		QString getArtist(int position = -1);
-		QString getFile(int position = -1);
-		int getLength(int position = -1);
+		QString getTitle();
+		QString getAlbum();
+		QString getArtist();
+		QString getFile();
+		int getLength();
 		int getCurrentPos();
 		bool isPlaying();
 		bool isActive();

plugins/amarok1_mediaplayer/amarok.cpp
@@ -174,53 +174,36 @@ QStringList AmarokMediaPlayer::getPlayListFiles()
 	kdebugf2();
 }
 
-uint AmarokMediaPlayer::getPlayListLength()
+QString AmarokMediaPlayer::getTitle()
 {
-	kdebugf();
-	return getUint("playlist", "getTotalTrackCount");
-	kdebugf2();
-}
-
-QString AmarokMediaPlayer::getTitle(int position)
-{
-	Q_UNUSED(position)
-
 	kdebugf();
 	return getString("player", "title");
 	kdebugf2();
 }
 
-QString AmarokMediaPlayer::getAlbum(int position)
+QString AmarokMediaPlayer::getAlbum()
 {
-	Q_UNUSED(position)
-
 	kdebugf();
 	return getString("player", "album");
 	kdebugf2();
 }
 
-QString AmarokMediaPlayer::getArtist(int position)
+QString AmarokMediaPlayer::getArtist()
 {
-	Q_UNUSED(position)
-
 	kdebugf();
 	return getString("player", "artist");
 	kdebugf2();
 }
 
-QString AmarokMediaPlayer::getFile(int position)
+QString AmarokMediaPlayer::getFile()
 {
-	Q_UNUSED(position)
-
 	kdebugf();
 	return getString("player", "path");
 	kdebugf2();
 }
 
-int AmarokMediaPlayer::getLength(int position)
+int AmarokMediaPlayer::getLength()
 {
-	Q_UNUSED(position)
-
 	kdebugf();
 	return getInt("player", "trackTotalTime") * 1000;
 	kdebugf2();

plugins/amarok1_mediaplayer/amarok.h
@@ -27,17 +27,16 @@ public:
 	virtual void done();
 
 	// PlayerInfo implementation
-	virtual QString getTitle(int position = -1);
-	virtual QString getAlbum(int position = -1);
-	virtual QString getArtist(int position = -1);
-	virtual QString getFile(int position = -1);
-	virtual int getLength(int position = -1);
+	virtual QString getTitle();
+	virtual QString getAlbum();
+	virtual QString getArtist();
+	virtual QString getFile();
+	virtual int getLength();
 	virtual int getCurrentPos();
 	virtual bool isPlaying();
 	virtual bool isActive();
 	virtual QStringList getPlayListTitles();
 	virtual QStringList getPlayListFiles();
-	virtual uint getPlayListLength();
 	virtual QString getPlayerName();
 	virtual QString getPlayerVersion();
 

plugins/falf_mediaplayer/falf.cpp
@@ -125,43 +125,32 @@ QStringList FalfMediaPlayer::getPlayListFiles()
 	return QStringList();
 }
 
-uint FalfMediaPlayer::getPlayListLength()
+QString FalfMediaPlayer::getTitle()
 {
-	kdebugf();
-	return 0;
-}
-
-QString FalfMediaPlayer::getTitle(int position)
-{
-	Q_UNUSED(position)
 	kdebugf();
 	return getData(TypeTitle);
 }
 
-QString FalfMediaPlayer::getAlbum(int position)
+QString FalfMediaPlayer::getAlbum()
 {
-	Q_UNUSED(position)
 	kdebugf();
 	return getData(TypeAlbum);
 }
 
-QString FalfMediaPlayer::getArtist(int position)
+QString FalfMediaPlayer::getArtist()
 {
-	Q_UNUSED(position)
 	kdebugf();
 	return getData(TypeArtist);
 }
 
-QString FalfMediaPlayer::getFile(int position)
+QString FalfMediaPlayer::getFile()
 {
-	Q_UNUSED(position)
 	kdebugf();
 	return QString();
 }
 
-int FalfMediaPlayer::getLength(int position)
+int FalfMediaPlayer::getLength()
 {
-	Q_UNUSED(position)
 	kdebugf();
 	return 0;
 }

plugins/falf_mediaplayer/falf.h
@@ -32,17 +32,16 @@ public:
 	virtual int init(bool firstLoad);
 	virtual void done();
 
-	virtual QString getTitle(int position = -1);
-	virtual QString getAlbum(int position = -1);
-	virtual QString getArtist(int position = -1);
-	virtual QString getFile(int position = -1);
-	virtual int getLength(int position = -1);
+	virtual QString getTitle();
+	virtual QString getAlbum();
+	virtual QString getArtist();
+	virtual QString getFile();
+	virtual int getLength();
 	virtual int getCurrentPos();
 	virtual bool isPlaying();
 	virtual bool isActive();
 	virtual QStringList getPlayListTitles();
 	virtual QStringList getPlayListFiles();
-	virtual uint getPlayListLength();
 	virtual QString getPlayerName();
 	virtual QString getPlayerVersion();
 

plugins/itunes_mediaplayer/itunes.cpp
@@ -75,22 +75,14 @@ QStringList ITunesMediaPlayer::getPlayListFiles()
 	return list;
 }
 
-uint ITunesMediaPlayer::getPlayListLength()
+QString ITunesMediaPlayer::getTitle()
 {
-	kdebugf();
-	QByteArray reply = executeCommand("tell application \"iTunes\" to get duration of current playlist");
-	return reply.toInt();
-}
-
-QString ITunesMediaPlayer::getTitle(int position)
-{
-	Q_UNUSED(position)
 	kdebugf();
 	Tune t = controller->currentTune();
 	return t.name();
 }
 
-QString ITunesMediaPlayer::getAlbum(int position)
+QString ITunesMediaPlayer::getAlbum()
 {
 	Q_UNUSED(position)
 	kdebugf();
@@ -98,25 +90,22 @@ QString ITunesMediaPlayer::getAlbum(int position)
 	return t.album();
 }
 
-QString ITunesMediaPlayer::getArtist(int position)
+QString ITunesMediaPlayer::getArtist()
 {
-	Q_UNUSED(position)
 	kdebugf();
 	Tune t = controller->currentTune();
 	return t.artist();
 }
 
-QString ITunesMediaPlayer::getFile(int position)
+QString ITunesMediaPlayer::getFile()
 {
-	Q_UNUSED(position)
 	kdebugf();
 	Tune t = controller->currentTune();
 	return t.location();
 }
 
-int ITunesMediaPlayer::getLength(int position)
+int ITunesMediaPlayer::getLength()
 {
-	Q_UNUSED(position)
 	kdebugf();
 	Tune t = controller->currentTune();
 	return t.time();

plugins/itunes_mediaplayer/itunes.h
@@ -22,17 +22,16 @@ public:
 	ITunesMediaPlayer();
 	~ITunesMediaPlayer();
 
-	QString getTitle(int position = -1);
-	QString getAlbum(int position = -1);
-	QString getArtist(int position = -1);
-	QString getFile(int position = -1);
-	int getLength(int position = -1);
+	QString getTitle();
+	QString getAlbum();
+	QString getArtist();
+	QString getFile();
+	int getLength();
 	int getCurrentPos();
 	bool isPlaying();
 	bool isActive();
 	QStringList getPlayListTitles();
 	QStringList getPlayListFiles();
-	uint getPlayListLength();
 	QString getPlayerName();
 	QString getPlayerVersion();
 

commit 4481ca081da750db0f7c52e0788a82fe981479c0

winamp_mediaplayer: Fix compilation
Regression since 32b199bdb8f63c3336da80e10a39806653a6792d.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

plugins/winamp_mediaplayer/winamp.cpp
@@ -227,7 +227,7 @@ QStringList WinampMediaPlayer::getPlayListFiles()
 	if(hWinamp){
 		int items=getPlayListLength();
 		for(int i=0;i<items;i++)
-			ret.append(getFile(i));
+			ret.append(getFileImpl(i));
 	}
 	return ret;
 }
@@ -296,12 +296,13 @@ QString WinampMediaPlayer::getArtist()
 	return "";
 }
 
-QString WinampMediaPlayer::getFile()
+QString WinampMediaPlayer::getFileImpl(int position)
 {
 	kdebugf();
 	HWND hWinamp=findWinamp();
 	if(hWinamp){
-		int position=SendMessage(hWinamp, WM_WA_IPC, 0, IPC_GETLISTPOS);
+		if (position < 0)
+			position=SendMessage(hWinamp, WM_WA_IPC, 0, IPC_GETLISTPOS);
 
 		QString file=readWinampMemory(IPC_GETPLAYLISTFILEW, position, true);
 		if(file.isEmpty()){
@@ -313,6 +314,11 @@ QString WinampMediaPlayer::getFile()
 	}
 
 	return QString();
+}
+
+QString WinampMediaPlayer::getFile()
+{
+	return getFileImpl(-1);
 }
 
 int WinampMediaPlayer::getLength()

plugins/winamp_mediaplayer/winamp.h
@@ -48,6 +48,7 @@ class WinampMediaPlayer : public PlayerCommands, public PlayerInfo
 		void decrVolume();
 	private:
 		HWND findWinamp();
+		QString getFileImpl(int position);
 		QString readWinampMemory(quint32 command, quint32 arg, bool unicode=false);
 		QString getFileTagW(int position, QString tag);
 		QString getFileTagA(int position, QString tag);