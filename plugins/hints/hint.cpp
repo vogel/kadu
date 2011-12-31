vogel/kadu

commit 0416ab79c8b3214a7396a1975363d6f741fa1f06

hints: #2266 fixed: restored word wraping in hints
Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

plugins/hints/hint.cpp
@@ -153,6 +153,7 @@ void Hint::createLabels(const QPixmap &pixmap)
 	label->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::MinimumExpanding);
 	label->setTextInteractionFlags(Qt::NoTextInteraction);
 	label->setAlignment(Qt::AlignVCenter | Qt::AlignLeft);
+	label->setWordWrap(true);
 	labels->addWidget(label);
 }
 
@@ -214,6 +215,7 @@ void Hint::updateText()
 		);
 
 	adjustSize();
+	updateGeometry();
 	emit updated(this);
 }
 

commit 2deb1e1b40de9e8bafe47fb31348df1e109f545f

hints: Fix issues with incorrect positioning hints frame on Qt 4.8
Probably still problematic with Qt 4.7, I will check it in a moment.

Regression since 0416ab79c8b3214a7396a1975363d6f741fa1f06.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

plugins/hints/hint.cpp
@@ -66,7 +66,6 @@ Hint::Hint(QWidget *parent, Notification *notification)
 	startSecs = secs = config_file.readNumEntry("Hints", "Event_" + notification->key() + "_timeout", 10);
 
 	createLabels(notification->icon().icon().pixmap(config_file.readNumEntry("Hints", "AllEvents_iconSize", 32)));
-	updateText();
 
 	const QList<Notification::Callback> callbacks = notification->getCallbacks();
 	bool showButtons = !callbacks.isEmpty();
@@ -95,9 +94,10 @@ Hint::Hint(QWidget *parent, Notification *notification)
 
 	connect(notification, SIGNAL(closed(Notification *)), this, SLOT(notificationClosed()));
 
-	setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
+	setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::Ignored);
 
 	configurationUpdated();
+	updateText();
 	show();
 
 	kdebugf2();
@@ -124,6 +124,7 @@ void Hint::configurationUpdated()
 	setMinimumWidth(config_file.readNumEntry("Hints", "MinimumWidth", 100));
 	setMaximumWidth(config_file.readNumEntry("Hints", "MaximumWidth", 500));
 	mouseOut();
+	updateText();
 }
 
 void Hint::createLabels(const QPixmap &pixmap)
@@ -150,7 +151,7 @@ void Hint::createLabels(const QPixmap &pixmap)
 	}
 
 	label = new QLabel(this);
-	label->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::MinimumExpanding);
+	label->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::Ignored);
 	label->setTextInteractionFlags(Qt::NoTextInteraction);
 	label->setAlignment(Qt::AlignVCenter | Qt::AlignLeft);
 	label->setWordWrap(true);

plugins/hints/hint_manager.cpp
@@ -177,7 +177,6 @@ void HintManager::setHint()
 	QPoint newPosition;
 	QPoint trayPosition;
 
-	frame->adjustSize();
 	QSize preferredSize = frame->sizeHint();
 	if (preferredSize.width() < minimumWidth)
 		preferredSize.setWidth(minimumWidth);
@@ -241,7 +240,8 @@ void HintManager::setHint()
 	}
 
 	frame->setGeometry(newPosition.x(), newPosition.y(), preferredSize.width(), preferredSize.height());
-	frame->setFixedWidth(preferredSize.width());
+	// Without this sometimes frames is bigger than hints inside it and it looks really bad.
+	frame->setFixedSize(preferredSize);
 	frame->setWindowOpacity(Opacity);
 
 	if (frame->isVisible())