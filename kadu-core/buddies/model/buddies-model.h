59aa0aa8abda5dff416f88f0a5f715bb9a23e91f
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2012-01-06 21:17:25 +0100

    buddies: use new buddyContact(AboutToBe)Added/Removed signals instead of contact(AboutToBe)Attached/Detached from ContactManager
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- kadu-core/buddies/model/buddies-model.h
+++ kadu-core/buddies/model/buddies-model.h
@@ -46,13 +46,13 @@ private slots:
 	void buddyAboutToBeRemoved(Buddy &buddy);
 	void buddyRemoved(Buddy &buddy);
 
+	void buddyContactAboutToBeAdded(Buddy &buddy, const Contact &contact);
+	void buddyContactAdded(Buddy &buddy, const Contact &contact);
+	void buddyContactAboutToBeRemoved(Buddy &buddy, const Contact &contact);
+	void buddyContactRemoved(Buddy &buddy, const Contact &contact);
+
 	void myselfBuddyUpdated();
 	void buddyUpdated(Buddy &buddy);
-
-	void contactAboutToBeAttached(Contact contact, Buddy nearFutureBuddy);
-	void contactAttached(Contact contact);
-	void contactAboutToBeDetached(Contact contact);
-	void contactDetached(Contact contact, Buddy previousBuddy);
 
 	void contactUpdated(Contact &contact);
 
