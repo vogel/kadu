347066510c5369526d73fd15e099214e031aff6d
Bartosz Brachaczek <b.brachaczek@gmail.com>
2012-01-09 17:09:39 +0100

    buddies: Do not use Contact::setId()
    Contact::create() does what we want. And now we hit assertion in
    ContactShared.
    
    Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

--- kadu-core/buddies/buddy.cpp
+++ kadu-core/buddies/buddy.cpp
@@ -238,7 +238,6 @@ Buddy Buddy::dummy()
 	Contact contact = Contact::create("999999");
 	contact.setContactAccount(account);
 	contact.setOwnerBuddy(example);
-	contact.setId("999999");
 	contact.setCurrentStatus(Status(StatusTypeAway, example.data()->tr("Example description")));
 	contact.setAddress(QHostAddress(2130706433));
 	contact.setPort(80);
