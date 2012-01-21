55904f82199afac10cc13337f45d2221d1662505
Juzef <juzefwt@gmail.com>
2012-01-21 19:41:49 +0100

    Add buddy: restore window title

--- kadu-core/gui/windows/add-buddy-window.cpp
+++ kadu-core/gui/windows/add-buddy-window.cpp
@@ -70,7 +70,7 @@ AddBuddyWindow::AddBuddyWindow(QWidget *parent, const Buddy &buddy, bool forceBu
 		ForceBuddyAccount(forceBuddyAccount)
 {
 	setWindowRole("kadu-add-buddy");
-
+	setWindowTitle(tr("Add buddy"));
 	setAttribute(Qt::WA_DeleteOnClose);
 
 	if (MyBuddy)
