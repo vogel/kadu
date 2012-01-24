vogel/kadu

commit be2234924002db92dbe473ae31c78c68d5490fb6

protocol: #2433 fixed: incorrect status behaviour when no passwors is available
Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

kadu-core/protocols/protocol.cpp
@@ -241,6 +241,9 @@ void Protocol::loggedOutAnyStateEntered()
 {
 	emit disconnected(CurrentAccount);
 
+	// this is different from all other methods because we want to preserve description
+	LoginStatus.setType(StatusTypeOffline);
+
 	disconnectedCleanup();
 	statusChanged(loginStatus());
 }
@@ -257,6 +260,11 @@ void Protocol::wantToLogInStateEntered()
 
 void Protocol::passwordRequiredStateEntered()
 {
+	emit disconnected(CurrentAccount);
+
+	disconnectedCleanup();
+	statusChanged(Status());
+
 	emit invalidPassword(CurrentAccount);
 }
 

kadu-core/gui/windows/password-window.cpp
@@ -66,8 +66,11 @@ PasswordWindow::PasswordWindow(const QString &message, QVariant data, QWidget *p
 	QPushButton *cancelButton = new QPushButton(qApp->style()->standardIcon(QStyle::SP_DialogCancelButton), tr("Cancel"), this);
 	buttons->addButton(cancelButton, QDialogButtonBox::RejectRole);
 
-	connect(okButton, SIGNAL(clicked(bool)), this, SLOT(accepted()));
-	connect(cancelButton, SIGNAL(clicked(bool)), this, SLOT(canceled()));
+	connect(okButton, SIGNAL(clicked(bool)), this, SLOT(accept()));
+	connect(cancelButton, SIGNAL(clicked(bool)), this, SLOT(reject()));
+
+	connect(this, SIGNAL(accepted()), this, SLOT(dialogAccepted()));
+	connect(this, SIGNAL(rejected()), this, SLOT(dialogRejected()));
 
 	layout->addWidget(messageLabel);
 	layout->addWidget(Password);
@@ -79,13 +82,13 @@ PasswordWindow::~PasswordWindow()
 {
 }
 
-void PasswordWindow::accepted()
+void PasswordWindow::dialogAccepted()
 {
 	emit passwordEntered(Data, Password->text(), Store->isChecked());
 	close();
 }
 
-void PasswordWindow::canceled()
+void PasswordWindow::dialogRejected()
 {
 	emit passwordEntered(Data, QString(), false);
 	close();

kadu-core/gui/windows/password-window.h
@@ -44,8 +44,8 @@ class KADUAPI PasswordWindow : public QDialog, DesktopAwareObject
 	QCheckBox *Store;
 
 private slots:
-	void accepted();
-	void canceled();
+	void dialogAccepted();
+	void dialogRejected();
 
 public:
 	static void getPassword(const QString &message, QVariant data, QObject *receiver, const char *slot);

commit f4e64aeb62913ec33dcce0a6981b257a726810eb

protocol: fixed regression from be2234924002db92dbe473ae31c78c68d5490fb6 that preventer auto-login
Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

kadu-core/protocols/protocol-state-machine.cpp
@@ -126,7 +126,7 @@ ProtocolStateMachine::~ProtocolStateMachine()
 void ProtocolStateMachine::printConfiguration()
 {
 	QStringList states;
-	
+
 	if (configuration().contains(LoggingOutState))
 		states.append("logging-out");
 	if (configuration().contains(LoggedOutOnlineState))

kadu-core/protocols/protocol.cpp
@@ -87,9 +87,13 @@ void Protocol::prepareStateMachine()
 void Protocol::passwordProvided()
 {
 	if (CurrentAccount.hasPassword())
+	{
 		emit stateMachinePasswordAvailable();
-	else
-		emit stateMachinePasswordNotAvailable();
+		return;
+	}
+
+	LoginStatus = Status();
+	emit stateMachinePasswordNotAvailable();
 }
 
 void Protocol::setAllOffline()
@@ -240,9 +244,6 @@ void Protocol::loggingOutStateEntered()
 void Protocol::loggedOutAnyStateEntered()
 {
 	emit disconnected(CurrentAccount);
-
-	// this is different from all other methods because we want to preserve description
-	LoginStatus.setType(StatusTypeOffline);
 
 	disconnectedCleanup();
 	statusChanged(loginStatus());