vogel/kadu

commit 11347af3d2c0ec34ae7902207841ea3843a96453

chat-styles: ensure that no two hack in adium style engines are run at the same time
Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

kadu-core/chat/style-engines/chat-engine-adium/chat-engine-adium.cpp
@@ -117,7 +117,7 @@ void PreviewHack::loadFinished()
 
 
 AdiumChatStyleEngine::AdiumChatStyleEngine(QObject *parent) :
-		QObject(parent)
+		QObject(parent), CurrentRefreshHack(0), CurrentPreviewHack(0)
 {
 	// Load required javascript functions
 	QFile file(dataPath("kadu") + "/scripts/chat-scripts.js");
@@ -268,6 +268,16 @@ void AdiumChatStyleEngine::appendChatMessage(HtmlMessagesRenderer *renderer, Mes
 	renderer->setLastMessage(message);
 }
 
+void AdiumChatStyleEngine::currentPreviewHackDestroyed()
+{
+	CurrentPreviewHack = 0;
+}
+
+void AdiumChatStyleEngine::currentRefreshHackDestroyed()
+{
+	CurrentRefreshHack = 0;
+}
+
 void AdiumChatStyleEngine::refreshView(HtmlMessagesRenderer *renderer, bool useTransparency)
 {
 	QString styleBaseHtml = CurrentStyle.templateHtml();
@@ -292,12 +302,17 @@ void AdiumChatStyleEngine::refreshView(HtmlMessagesRenderer *renderer, bool useT
 	if (useTransparency && !CurrentStyle.defaultBackgroundIsTransparent())
 		styleBaseHtml.replace(styleBaseHtml.lastIndexOf("==bodyBackground=="), 18, "background-image: none; background: none; background-color: rgba(0, 0, 0, 0)");
 
-	RefreshViewHack *refreshViewHack = new RefreshViewHack(this, renderer, this);
+	if (CurrentRefreshHack)
+		delete CurrentRefreshHack;
+
+	CurrentRefreshHack = new RefreshViewHack(this, renderer, this);
+	connect(CurrentRefreshHack, SIGNAL(destroyed(QObject*)),
+	        this, SLOT(currentRefreshHackDestroyed()));
 
 	// lets wait a while for all javascript to resolve and execute
 	// we dont want to get to the party too early
 	connect(renderer->webPage()->mainFrame(), SIGNAL(loadFinished(bool)),
-	        refreshViewHack, SLOT(loadFinished()), Qt::QueuedConnection);
+	        CurrentRefreshHack, SLOT(loadFinished()), Qt::QueuedConnection);
 
 	renderer->webPage()->mainFrame()->setHtml(styleBaseHtml);
 	renderer->webPage()->mainFrame()->evaluateJavaScript(jsCode);
@@ -373,12 +388,15 @@ void AdiumChatStyleEngine::prepareStylePreview(Preview *preview, QString styleNa
 		styleBaseHtml.replace(styleBaseHtml.lastIndexOf("%@"), 2, (style.styleViewVersion() < 3) ? "s" : QString("@import url( \"" + style.mainHref() + "\" );"));
 	}
 
-	PreviewHack *previewHack = new PreviewHack(this, preview, style.baseHref(), style.outgoingHtml(), style.incomingHtml(), this);
+	if (CurrentPreviewHack)
+		delete CurrentPreviewHack;
+
+	CurrentPreviewHack = new PreviewHack(this, preview, style.baseHref(), style.outgoingHtml(), style.incomingHtml(), this);
 
 	// lets wait a while for all javascript to resolve and execute
 	// we dont want to get to the party too early
 	connect(preview->webView()->page()->mainFrame(), SIGNAL(loadFinished(bool)),
-	        previewHack, SLOT(loadFinished()),  Qt::QueuedConnection);
+	        CurrentPreviewHack, SLOT(loadFinished()),  Qt::QueuedConnection);
 
 	preview->webView()->page()->mainFrame()->setHtml(styleBaseHtml);
 	preview->webView()->page()->mainFrame()->evaluateJavaScript(jsCode);

kadu-core/chat/style-engines/chat-engine-adium/chat-engine-adium.h
@@ -76,6 +76,8 @@ class AdiumChatStyleEngine : public QObject, public ChatStyleEngine
 	friend class PreviewHack;
 
 	AdiumStyle CurrentStyle;
+	RefreshViewHack *CurrentRefreshHack;
+	PreviewHack *CurrentPreviewHack;
 
 	QString jsCode;
 
@@ -85,6 +87,10 @@ class AdiumChatStyleEngine : public QObject, public ChatStyleEngine
 	bool clearDirectory(const QString &directory);
 
 	void appendChatMessage(HtmlMessagesRenderer *renderer, MessageRenderInfo *message);
+
+private slots:
+	void currentRefreshHackDestroyed();
+	void currentPreviewHackDestroyed();
 
 public:
 	explicit AdiumChatStyleEngine(QObject *parent = 0);
@@ -116,6 +122,7 @@ public:
 	virtual void styleEditionRequested(QString ) {} //do nothing. Adium styles don't support editing
 
 	virtual bool removeStyle(const QString &styleName);
+
 };
 
 #endif // CHAT_ENGINE_ADIUM_H

commit bbb29fe3a3805834c030c7c33cbbb3e37803f688

fix regression introduced in 11347af3d2c0ec34ae7902207841ea3843a96453
Only first chat view has been refreshed

kadu-core/chat/style-engines/chat-engine-adium/chat-engine-adium.cpp
@@ -72,7 +72,10 @@ void RefreshViewHack::cancel()
 void RefreshViewHack::loadFinished()
 {
 	if (!Engine || !Renderer)
+	{
+		deleteLater();
 		return;
+	}
 
 	// We need to clear messages in case something was rendered before this slot was called.
 	Engine->clearMessages(Renderer);
@@ -80,6 +83,8 @@ void RefreshViewHack::loadFinished()
 
 	foreach (MessageRenderInfo *message, Renderer->messages())
 		Engine->appendChatMessage(Renderer, message);
+
+	emit finished(Renderer);
 
 	deleteLater();
 }
@@ -142,7 +147,7 @@ void PreviewHack::loadFinished()
 
 
 AdiumChatStyleEngine::AdiumChatStyleEngine(QObject *parent) :
-		QObject(parent), CurrentRefreshHack(0), CurrentPreviewHack(0)
+		QObject(parent), CurrentPreviewHack(0)
 {
 	// Load required javascript functions
 	QFile file(dataPath("kadu") + "/scripts/chat-scripts.js");
@@ -298,9 +303,12 @@ void AdiumChatStyleEngine::currentPreviewHackDestroyed()
 	CurrentPreviewHack = 0;
 }
 
-void AdiumChatStyleEngine::currentRefreshHackDestroyed()
+void AdiumChatStyleEngine::refreshHackFinished(HtmlMessagesRenderer *renderer)
 {
-	CurrentRefreshHack = 0;
+	if (!CurrentRefreshHacks.contains(renderer))
+		return;
+
+	CurrentRefreshHacks.remove(renderer);
 }
 
 void AdiumChatStyleEngine::refreshView(HtmlMessagesRenderer *renderer, bool useTransparency)
@@ -327,17 +335,18 @@ void AdiumChatStyleEngine::refreshView(HtmlMessagesRenderer *renderer, bool useT
 	if (useTransparency && !CurrentStyle.defaultBackgroundIsTransparent())
 		styleBaseHtml.replace(styleBaseHtml.lastIndexOf("==bodyBackground=="), 18, "background-image: none; background: none; background-color: rgba(0, 0, 0, 0)");
 
-	if (CurrentRefreshHack)
-		delete CurrentRefreshHack;
+	if (CurrentRefreshHacks.contains(renderer))
+		CurrentRefreshHacks.value(renderer)->cancel();
 
-	CurrentRefreshHack = new RefreshViewHack(this, renderer, this);
-	connect(CurrentRefreshHack, SIGNAL(destroyed(QObject*)),
-	        this, SLOT(currentRefreshHackDestroyed()));
+	RefreshViewHack *currentHack = new RefreshViewHack(this, renderer, this);
+	CurrentRefreshHacks.insert(renderer, currentHack);
+	connect(currentHack, SIGNAL(finished(HtmlMessagesRenderer*)),
+			this, SLOT(refreshHackFinished(HtmlMessagesRenderer *)));
 
 	// lets wait a while for all javascript to resolve and execute
 	// we dont want to get to the party too early
 	connect(renderer->webPage()->mainFrame(), SIGNAL(loadFinished(bool)),
-	        CurrentRefreshHack, SLOT(loadFinished()), Qt::QueuedConnection);
+			currentHack, SLOT(loadFinished()), Qt::QueuedConnection);
 
 	renderer->webPage()->mainFrame()->setHtml(styleBaseHtml);
 	renderer->webPage()->mainFrame()->evaluateJavaScript(jsCode);

kadu-core/chat/style-engines/chat-engine-adium/chat-engine-adium.h
@@ -39,16 +39,16 @@ class RefreshViewHack : public QObject
 	AdiumChatStyleEngine *Engine;
 	HtmlMessagesRenderer *Renderer;
 
-private slots:
-	void cancel();
-
 public:
 	explicit RefreshViewHack(AdiumChatStyleEngine *engine, HtmlMessagesRenderer *renderer, QObject *parent = 0);
 	virtual ~RefreshViewHack();
 
 public slots:
+	void cancel();
 	void loadFinished();
 
+signals:
+	void finished(HtmlMessagesRenderer *);
 };
 
 class PreviewHack : public QObject
@@ -82,7 +82,7 @@ class AdiumChatStyleEngine : public QObject, public ChatStyleEngine
 	friend class PreviewHack;
 
 	AdiumStyle CurrentStyle;
-	RefreshViewHack *CurrentRefreshHack;
+	QMap<HtmlMessagesRenderer *, RefreshViewHack*> CurrentRefreshHacks;
 	PreviewHack *CurrentPreviewHack;
 
 	QString jsCode;
@@ -95,7 +95,7 @@ class AdiumChatStyleEngine : public QObject, public ChatStyleEngine
 	void appendChatMessage(HtmlMessagesRenderer *renderer, MessageRenderInfo *message);
 
 private slots:
-	void currentRefreshHackDestroyed();
+	void refreshHackFinished(HtmlMessagesRenderer *);
 	void currentPreviewHackDestroyed();
 
 public: