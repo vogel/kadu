5764b4f4845d393e98760d16a4fddb331fe6aad8
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2012-02-04 13:37:17 +0100

    history: HistoryQueryResult class documented
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- plugins/history/history-query-result.h
+++ plugins/history/history-query-result.h
@@ -24,6 +24,20 @@
 
 #include "talkable/talkable.h"
 
+/**
+ * @addtogroup History
+ * @{
+ */
+
+/**
+ * @class HistoryQueryResult
+ * @author Rafał 'Vogel' Malinowski
+ * @short This class represents one result item of query for messages in history.
+ *
+ * This class represents one result item of query for messages in history. Each results consists
+ * of originating talkable item, date of this item, title (that is in general content value of first
+ * item in given day) and count of found items for given date.
+ */
 class HistoryQueryResult
 {
 	Talkable ResultTalkable;
@@ -32,24 +46,87 @@ class HistoryQueryResult
 	quint32 Count;
 
 public:
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Create empty query result.
+	 */
 	HistoryQueryResult();
+
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Create copy of existing query result.
+	 * @param copyMe existing query result to copy
+	 */
 	HistoryQueryResult(const HistoryQueryResult &copyMe);
 
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Copy existing query resylt into this query result.
+	 * @param copyMe existing query result to copy
+	 */
 	HistoryQueryResult & operator = (const HistoryQueryResult &copyMe);
 
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Sets originating talkable.
+	 * @param talkable originating talkable
+	 */
 	void setTalkable(const Talkable &talkable);
+
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Returns originating talkable.
+	 * @return originating talkable
+	 */
 	Talkable talkable() const;
 
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Sets date of query result.
+	 * @param date of query result
+	 */
 	void setDate(const QDate &date);
+
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Returns date of query result.
+	 * @return date of query result
+	 */
 	QDate date() const;
 
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Sets title of query result.
+	 * @param title title of query result
+	 */
 	void setTitle(const QString &title);
+
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Returns title of query result.
+	 * @return title of query result
+	 */
 	QString title() const;
 
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Sets count of found items.
+	 * @param count count of found items
+	 */
 	void setCount(const quint32 count);
+
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Returns count of found items.
+	 * @return count of found items
+	 */
 	quint32 count() const;
 
 };
+
+/**
+ * @}
+ */
 
 Q_DECLARE_METATYPE(HistoryQueryResult);
 
