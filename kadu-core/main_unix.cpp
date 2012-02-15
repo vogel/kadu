vogel/kadu

commit 30b1daf02fb1bb85d00e12844b75add73483c374

misc: New KaduPaths class which replaces old path-conversion global functions
Small guide how to update Kadu <=0.11-compatible code:
- Replace homePath() calls with KaduPaths::homePath().
- Replace webKitPath(path) calls with KaduPaths::webKitPath(path).
- Replace desktopFilePath() calls with KaduPaths::instance()->desktopFilePath().
- Replace dataPath("kadu/path") calls as well as dataPath() + "kadu/path"
  and other variants with
  KaduPaths::instance()->dataPath() + QLatin1String("path").
- Replace libPath("kadu/plugins/path") calls as well as
  libPath() + "kadu/plugins/path" and other variants with
  KaduPaths::instance()->pluginsLibPath() + QLatin1String("path").
- Replace profilePath("path") calls with
  KaduPaths::instance()->profilePath() + QLatin1String("path").

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

kadu-core/main_unix.cpp
@@ -31,7 +31,7 @@
 #include "core/core.h"
 #include "core/crash-aware-object.h"
 #include "gui/windows/kadu-window.h"
-#include "misc/path-conversion.h"
+#include "misc/kadu-paths.h"
 #include "plugins/plugin.h"
 #include "plugins/plugins-manager.h"
 #include "activate.h"
@@ -92,7 +92,7 @@ static void kadu_signal_handler(int signal)
 		fprintf(stderr, "======= END OF BACKTRACE  ======\n");
 		fflush(stderr);
 
-		FILE *backtraceFile = fopen(qPrintable(profilePath(backtraceFileName)), "w");
+		FILE *backtraceFile = fopen(qPrintable(KaduPaths::instance()->profilePath() + backtraceFileName), "w");
 		if (backtraceFile)
 		{
 			fprintf(backtraceFile, "======= BEGIN OF BACKTRACE =====\n");
@@ -121,7 +121,7 @@ static void kadu_signal_handler(int signal)
 		kdebugm(KDEBUG_PANIC, "backtrace not available\n");
 #endif // HAVE_EXECINFO
 
-		xml_config_file->saveTo(profilePath(backupFileName));
+		xml_config_file->saveTo(KaduPaths::instance()->profilePath() + backupFileName);
 		abort();
 	}
 	else if (signal == SIGUSR1)

commit 2564fb399d33a191d327b1b06452d85440b94cc4

core: Fix compilation error
main_unix.cpp:95:31: error: ‘class QStringBuilder<QString, QString>’ has
no member named ‘toLocal8Bit’

I didn't test it as I cannot reproduce this error. But it should help.

Regression since 30b1daf02fb1bb85d00e12844b75add73483c374.

Reported-by: Piotr Dąbrowski <ultr@ultr.pl>
Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

kadu-core/main_unix.cpp
@@ -92,7 +92,7 @@ static void kadu_signal_handler(int signal)
 		fprintf(stderr, "======= END OF BACKTRACE  ======\n");
 		fflush(stderr);
 
-		FILE *backtraceFile = fopen(qPrintable(KaduPaths::instance()->profilePath() + backtraceFileName), "w");
+		FILE *backtraceFile = fopen(qPrintable(QString(KaduPaths::instance()->profilePath() + backtraceFileName)), "w");
 		if (backtraceFile)
 		{
 			fprintf(backtraceFile, "======= BEGIN OF BACKTRACE =====\n");