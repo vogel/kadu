55a071d29974fae38537f562b826f0308cd77395
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2012-03-22 23:06:21 +0100

    gui: added Token Window
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ kadu-core/gui/windows/token-window.cpp
@@ -0,0 +1,86 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2012 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QtGui/QApplication>
+#include <QtGui/QDialogButtonBox>
+#include <QtGui/QFormLayout>
+#include <QtGui/QLabel>
+#include <QtGui/QLineEdit>
+#include <QtGui/QPushButton>
+#include <QtGui/QStyle>
+
+#include "token-window.h"
+
+TokenWindow::TokenWindow(const QPixmap &tokenPixmap, QWidget *parent) :
+		QDialog(parent)
+{
+	setAttribute(Qt::WA_DeleteOnClose);
+	setWindowTitle(tr("Enter Token Value"));
+
+	createGui(tokenPixmap);
+}
+
+TokenWindow::~TokenWindow()
+{
+}
+
+void TokenWindow::createGui(const QPixmap &tokenPixmap)
+{
+	QVBoxLayout *layout = new QVBoxLayout(this);
+
+	QLabel *imageLabel = new QLabel(this);
+	imageLabel->setPixmap(tokenPixmap);
+
+	TokenValue = new QLineEdit(this);
+
+	QWidget *formWidget = new QWidget(this);
+	layout->addWidget(formWidget);
+
+	QFormLayout *formLayout = new QFormLayout(formWidget);
+	formLayout->setRowWrapPolicy(QFormLayout::WrapAllRows);
+	formLayout->addRow(0, imageLabel);
+	formLayout->addRow(tr("Enter text from the picture:"), TokenValue);
+
+	QDialogButtonBox *buttons = new QDialogButtonBox(this);
+
+	QPushButton *okButton = new QPushButton(qApp->style()->standardIcon(QStyle::SP_DialogOkButton), tr("Ok"), buttons);
+	QPushButton *cancelButton = new QPushButton(qApp->style()->standardIcon(QStyle::SP_DialogCancelButton), tr("Cancel"), buttons);
+
+	buttons->addButton(okButton, QDialogButtonBox::AcceptRole);
+	buttons->addButton(cancelButton, QDialogButtonBox::DestructiveRole);
+
+	layout->addSpacing(16);
+	layout->addWidget(buttons);
+
+	connect(TokenValue, SIGNAL(returnPressed()), this, SLOT(accept()));
+	connect(okButton, SIGNAL(clicked()), this, SLOT(accept()));
+	connect(cancelButton, SIGNAL(clicked()), this, SLOT(reject()));
+}
+
+void TokenWindow::accept()
+{
+	emit tokenValueEntered(TokenValue->text());
+	QDialog::accept();
+}
+
+void TokenWindow::reject()
+{
+	emit tokenValueEntered(QString());
+	QDialog::reject();
+}
---
+++ kadu-core/gui/windows/token-window.h
@@ -0,0 +1,78 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2012 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TOKEN_WINDOW_H
+#define TOKEN_WINDOW_H
+
+#include <QtGui/QDialog>
+
+class QLineEdit;
+
+/**
+ * @addtogroup Gui
+ * @{
+ */
+
+/**
+ * @class TokenWindow
+ * @author Rafał 'Vogel' Malinowski
+ * @short Window with picture and text field to enter text from picture.
+ *
+ * This window displays a picture and text field to enter text from picture. When user enters texts or closes window
+ * signal @link tokenValueEntered @endlink is emited.
+ */
+class TokenWindow : public QDialog
+{
+	Q_OBJECT
+
+	QLineEdit *TokenValue;
+
+	void createGui(const QPixmap &tokenPixmap);
+	void result(const QString &value);
+
+public:
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Create new window with given picture.
+	 * @param tokenPixmap pixmap to present to user
+	 */
+	explicit TokenWindow(const QPixmap &tokenPixmap, QWidget *parent = 0);
+	virtual ~TokenWindow();
+
+public slots:
+	virtual void accept();
+	virtual void reject();
+
+signals:
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Signal emited when user enters text from picture or closes window.
+	 * @param tokenValue value entered by user
+	 *
+	 * If user closed window without entering value or with Escape key then tokenValue parameter is empty.
+	 */
+	void tokenValueEntered(const QString &tokenValue);
+
+};
+
+/**
+ * @}
+ */
+
+#endif // TOKEN_WINDOW_H
