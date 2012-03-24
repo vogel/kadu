2fdd7e357ad28d83a8c786687c1cea260af7fa17
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2012-03-24 01:51:49 +0100

    sms: added script to extract translatable texts from scripts
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ plugins/sms/translations/extract-custom-strings.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+grep "translator\\.tr(\\\".*\\\")" ../data/scripts/*.js -o | grep ""\\\".*\\\" -o | awk '{ print "QT_TRANSLATE_NOOP(\"@default\", "$0");"  }' | sort | uniq > .sms-translations.cpp
