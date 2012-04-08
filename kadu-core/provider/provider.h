0d94c495d5c812f1c1d90005cd4f716c3fa82f3c
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2012-04-08 03:32:16 +0200

    core: added Provider interface
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ kadu-core/provider/provider.h
@@ -0,0 +1,52 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2012 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PROVIDER_H
+#define PROVIDER_H
+
+/**
+ * @addtogroup Provider
+ * @{
+ */
+
+/**
+ * @class Provider
+ * @author Rafał 'Vogel' Malinowski
+ * @short Interface used to get instance of given type.
+ * @param T type of value to provide
+ *
+ * This interface should be used in places where an existing object of given type is required.
+ */
+template<typename T>
+class Provider
+{
+public:
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Provide value of given type.
+	 */
+	virtual T provide() = 0;
+
+};
+
+/**
+ * @}
+ */
+
+#endif // PROVIDER_H
