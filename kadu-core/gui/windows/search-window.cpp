vogel/kadu

commit 5c132da0f4482d0cb6885d72975b3f2b674ab192

chats: update ChatManager::findChat methods to use NotFoundAction
Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

kadu-core/gui/windows/search-window.cpp
@@ -333,8 +333,8 @@ void SearchWindow::chatFound()
 	const ContactSet &contacts = selectedContacts();
 	if (!contacts.isEmpty())
 	{
-		const Chat &chat = ChatManager::instance()->findChat(contacts, true);
-		ChatWidget * const chatWidget = ChatWidgetManager::instance()->byChat(chat, true);
+		const Chat &chat = ChatManager::instance()->findChat(contacts, ActionCreateAndAdd);
+		ChatWidget * const chatWidget = ChatWidgetManager::instance()->byChat(chat, ActionCreateAndAdd);
 		if (chatWidget)
 			chatWidget->activate();
 	}

kadu-core/chat/buddy-chat-manager.cpp
@@ -87,7 +87,7 @@ Chat BuddyChatManager::createAndInsertBuddyChat(const Buddy &buddy)
 	QVector<Chat> chats;
 	foreach (const Contact &contact, buddy.contacts())
 	{
-		const Chat &contactChat = ChatManager::instance()->findChat(ContactSet(contact), false);
+		const Chat &contactChat = ChatManager::instance()->findChat(ContactSet(contact), ActionReturnNull);
 		if (contactChat)
 			chats.append(contactChat);
 	}
@@ -105,7 +105,7 @@ void BuddyChatManager::buddyContactAdded(const Buddy &buddy, const Contact &cont
 	ChatDetailsBuddy *buddyDetails = qobject_cast<ChatDetailsBuddy *>(chat.details());
 	Q_ASSERT(buddyDetails);
 
-	buddyDetails->addChat(ChatManager::instance()->findChat(ContactSet(contact), false));
+	buddyDetails->addChat(ChatManager::instance()->findChat(ContactSet(contact), ActionReturnNull));
 }
 
 void BuddyChatManager::buddyContactRemoved(const Buddy &buddy, const Contact &contact)
@@ -117,7 +117,7 @@ void BuddyChatManager::buddyContactRemoved(const Buddy &buddy, const Contact &co
 	ChatDetailsBuddy *buddyDetails = qobject_cast<ChatDetailsBuddy *>(chat.details());
 	Q_ASSERT(buddyDetails);
 
-	buddyDetails->removeChat(ChatManager::instance()->findChat(ContactSet(contact), false));
+	buddyDetails->removeChat(ChatManager::instance()->findChat(ContactSet(contact), ActionReturnNull));
 }
 
 void BuddyChatManager::chatAdded(const Chat &addedChat)

kadu-core/chat/chat-manager.cpp
@@ -121,7 +121,7 @@ Account ChatManager::getCommonAccount(const BuddySet &buddies)
 	return Account::null;
 }
 
-Chat ChatManager::findChat(const BuddySet &buddies, bool create)
+Chat ChatManager::findChat(const BuddySet &buddies, NotFoundAction action)
 {
 	QMutexLocker locker(&mutex());
 
@@ -131,7 +131,7 @@ Chat ChatManager::findChat(const BuddySet &buddies, bool create)
 		if (!contact)
 			contact = BuddyPreferredManager::instance()->preferredContact(*buddies.constBegin());
 
-		return findChat(ContactSet(contact), create);
+		return findChat(ContactSet(contact), action);
 	}
 
 	Account commonAccount = getCommonAccount(buddies);
@@ -146,14 +146,14 @@ Chat ChatManager::findChat(const BuddySet &buddies, bool create)
 	if (contacts.size() != buddies.size())
 		return Chat::null;
 
-	return findChat(contacts, create);
+	return findChat(contacts, action);
 }
 
 /**
  * @author Rafal 'Vogel' Malinowski
  * @short Finds chat for given lsit of contacts.
  * @param contacts set of contacts that builds chat
- * @param create when true new chat will be returned when none was found with contacts
+ * @param action action executed when do such chat was found
  * @return chat for given lsit of contacts
  *
  * Every contact in contacts parameters needs to be in the same account. When there
@@ -170,7 +170,7 @@ Chat ChatManager::findChat(const BuddySet &buddies, bool create)
  * Do not manually create chats of type "Contact" and "ContactSet" - use this
  * method instead.
  */
-Chat ChatManager::findChat(const ContactSet &contacts, bool create)
+Chat ChatManager::findChat(const ContactSet &contacts, NotFoundAction action)
 {
 	QMutexLocker locker(&mutex());
 
@@ -195,10 +195,8 @@ Chat ChatManager::findChat(const ContactSet &contacts, bool create)
 	// for some users that have self on user list
 	// this should not be possible, and prevented on other level (like in ContactManager)
 	foreach (const Contact &contact, contacts)
-	{
 		if (contact.id() == account.id())
 			return Chat::null;
-	}
 
 	foreach (const Chat &c, allItems()) // search allItems, chats can be not loaded yet
 		if ((c.type() == QLatin1String("Contact") || c.type() == QLatin1String("Simple") ||
@@ -209,7 +207,7 @@ Chat ChatManager::findChat(const ContactSet &contacts, bool create)
 			return c;
 		}
 
-	if (!create)
+	if (ActionReturnNull == action)
 		return Chat::null;
 
 	Chat chat = Chat::create();
@@ -240,7 +238,10 @@ Chat ChatManager::findChat(const ContactSet &contacts, bool create)
 	else
 		return Chat::null;
 
-	addItem(chat);
+
+	if (ActionCreateAndAdd == action)
+		addItem(chat);
+
 	return chat;
 }
 

kadu-core/chat/chat-manager.h
@@ -133,8 +133,8 @@ public:
 	bool isAccountCommon(const Account &account, const BuddySet &buddies);
 	Account getCommonAccount(const BuddySet &buddies);
 
-	Chat findChat(const BuddySet &buddies, bool create = true);
-	Chat findChat(const ContactSet &contacts, bool create = true);
+	Chat findChat(const BuddySet &buddies, NotFoundAction action);
+	Chat findChat(const ContactSet &contacts, NotFoundAction action);
 
 	Chat byDisplay(const QString &display);
 

kadu-core/file-transfer/file-transfer-manager.cpp
@@ -192,7 +192,7 @@ void FileTransferManager::acceptFileTransfer(FileTransfer transfer)
 		if (!haveFileName && fi.exists())
 		{
 			QWidget *parent = 0;
-			Chat chat = ChatManager::instance()->findChat(ContactSet(transfer.peer()), false);
+			Chat chat = ChatManager::instance()->findChat(ContactSet(transfer.peer()), ActionReturnNull);
 			if (chat)
 				parent = ChatWidgetManager::instance()->byChat(chat, false);
 
@@ -292,7 +292,7 @@ void FileTransferManager::incomingFileTransfer(FileTransfer fileTransfer)
 			fileTransfer.setLocalFileName(alreadyTransferred.localFileName());
 	}
 
-	Chat chat = ChatManager::instance()->findChat(ContactSet(fileTransfer.peer()));
+	Chat chat = ChatManager::instance()->findChat(ContactSet(fileTransfer.peer()), ActionCreateAndAdd);
 	NewFileTransferNotification *notification = new NewFileTransferNotification("FileTransfer/IncomingFile", fileTransfer,
 			chat, fileTransfer.localFileName().isEmpty() ? StartNew : StartRestore);
 	notification->setTitle(tr("Incoming transfer"));

kadu-core/gui/widgets/chat-widget-manager.cpp
@@ -262,7 +262,7 @@ void ChatWidgetManager::closeAllChats(const Buddy &buddy)
 {
 	foreach (const Contact &contact, buddy.contacts())
 	{
-		const Chat &chat = ChatManager::instance()->findChat(ContactSet(contact), false);
+		const Chat &chat = ChatManager::instance()->findChat(ContactSet(contact), ActionReturnNull);
 		if (chat)
 			closeChat(chat);
 	}

kadu-core/gui/windows/add-conference-window.cpp
@@ -236,7 +236,7 @@ Chat AddConferenceWindow::computeChat() const
 	foreach (const Buddy &buddy, conferenceBuddies)
 		conferenceContacts.insert(buddy.contacts(account).first());
 
-	return ChatManager::instance()->findChat(conferenceContacts, true);
+	return ChatManager::instance()->findChat(conferenceContacts, ActionCreateAndAdd);
 }
 
 // we need to filter by account here because CheckableBuddiesProxyModel does not do filtering at all

kadu-core/gui/windows/open-chat-with/open-chat-with.cpp
@@ -247,7 +247,7 @@ void OpenChatWith::openChat()
 
 	BuddySet buddies = contacts.toBuddySet();
 
-	const Chat &chat = ChatManager::instance()->findChat(contacts);
+	const Chat &chat = ChatManager::instance()->findChat(contacts, ActionCreateAndAdd);
 	if (chat)
 	{
 		ChatWidget * const chatWidget = ChatWidgetManager::instance()->byChat(chat, true);

commit c66f4f1d885763a14ae2f8a85c253f86d5bafdb5

gui: Fix typo (spotted by MSVC warning)
Introduced in 5c132da0f4482d0cb6885d72975b3f2b674ab192.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

kadu-core/gui/windows/search-window.cpp
@@ -338,7 +338,7 @@ void SearchWindow::chatFound()
 		const Chat &chat = 1 == contacts.size()
 				? ChatTypeContact::findChat(*contacts.constBegin(), ActionCreateAndAdd)
 				: ChatTypeContactSet::findChat(contacts, ActionCreateAndAdd);
-		ChatWidget * const chatWidget = ChatWidgetManager::instance()->byChat(chat, ActionCreateAndAdd);
+		ChatWidget * const chatWidget = ChatWidgetManager::instance()->byChat(chat, true);
 		if (chatWidget)
 			chatWidget->activate();
 	}