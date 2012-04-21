17c09fb416d5373411e78d0e059b36bedb4d6117
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2012-04-21 13:12:28 +0200

    contacts: use better condition for requiresSynchronization in shouldSave method
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- kadu-core/contacts/contact-shared.cpp
+++ kadu-core/contacts/contact-shared.cpp
@@ -170,7 +170,7 @@ bool ContactShared::shouldStore()
 			&& !Id.isEmpty()
 			&& !ContactAccount->uuid().isNull()
 			&& !isAnonymous())
-			|| (!rosterEntry()->detached() && RosterEntrySynchronized != rosterEntry()->state())
+			|| (rosterEntry()->requiresSynchronization())
 			|| customProperties()->shouldStore();
 }
 
