vogel/kadu

commit 34084cb7deb8dd64c688b5939c75e7c5e5197a37

gui: do not show 'Select identity' entry in identities combo box
This makes creating new accounts easier!

Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

plugins/gadu_protocol/gui/widgets/gadu-add-account-widget.cpp
@@ -90,7 +90,7 @@ void GaduAddAccountWidget::createGui(bool showButtons)
 	layout->addRow(0, RemindPassword);
 	connect(RemindPassword, SIGNAL(linkActivated(QString)), this, SLOT(remindPasssword()));
 
-	Identity = new IdentitiesComboBox(true, this);
+	Identity = new IdentitiesComboBox(this);
 	connect(Identity, SIGNAL(currentIndexChanged(int)), this, SLOT(dataChanged()));
 	layout->addRow(tr("Account Identity") + ':', Identity);
 

plugins/gadu_protocol/gui/widgets/gadu-create-account-widget.cpp
@@ -88,7 +88,7 @@ void GaduCreateAccountWidget::createGui(bool showButtons)
 	connect(EMail, SIGNAL(textEdited(const QString &)), this, SLOT(dataChanged()));
 	layout->addRow(tr("E-Mail Address") + ':', EMail);
 
-	IdentityCombo = new IdentitiesComboBox(true, this);
+	IdentityCombo = new IdentitiesComboBox(this);
 	connect(IdentityCombo, SIGNAL(currentIndexChanged(int)), this, SLOT(dataChanged()));
 	layout->addRow(tr("Account Identity") + ':', IdentityCombo);
 

plugins/jabber_protocol/gui/widgets/jabber-add-account-widget.cpp
@@ -119,7 +119,7 @@ void JabberAddAccountWidget::createGui(bool showButtons)
 	RememberPassword = new QCheckBox(tr("Remember Password"), this);
 	layout->addRow(0, RememberPassword);
 
-	Identity = new IdentitiesComboBox(true, this);
+	Identity = new IdentitiesComboBox(this);
 	connect(Identity, SIGNAL(currentIndexChanged(int)), this, SLOT(dataChanged()));
 	layout->addRow(tr("Account Identity") + ':', Identity);
 

plugins/jabber_protocol/gui/widgets/jabber-create-account-widget.cpp
@@ -113,7 +113,7 @@ void JabberCreateAccountWidget::createGui(bool showButtons)
 	RememberPassword = new QCheckBox(tr("Remember password"), this);
 	layout->addWidget(RememberPassword);
 
-	IdentityCombo = new IdentitiesComboBox(true, this);
+	IdentityCombo = new IdentitiesComboBox(this);
 	connect(IdentityCombo, SIGNAL(currentIndexChanged(int)), this, SLOT(dataChanged()));
 	layout->addRow(tr("Account Identity") + ':', IdentityCombo);
 

kadu-core/gui/widgets/identities-combo-box.cpp
@@ -32,13 +32,10 @@
 
 #include "identities-combo-box.h"
 
-IdentitiesComboBox::IdentitiesComboBox(bool includeSelectIdentity, QWidget *parent) :
+IdentitiesComboBox::IdentitiesComboBox(QWidget *parent) :
 		ActionsComboBox(parent)
 {
 	IdentityManager::instance()->removeUnused();
-
-	if (includeSelectIdentity)
-		addBeforeAction(new QAction(tr(" - Select identity - "), this));
 
 	CreateNewIdentityAction = new QAction(tr("Create a new identity..."), this);
 	QFont createNewIdentityActionFont = CreateNewIdentityAction->font();

kadu-core/gui/widgets/identities-combo-box.h
@@ -40,7 +40,7 @@ private slots:
 	void createNewIdentity();
 
 public:
-	explicit IdentitiesComboBox(bool includeSelectIdentity, QWidget *parent = 0);
+	explicit IdentitiesComboBox(QWidget *parent = 0);
 	virtual ~IdentitiesComboBox();
 
 	void setCurrentIdentity(Identity identity);

plugins/gadu_protocol/gui/widgets/gadu-edit-account-widget.cpp
@@ -143,7 +143,7 @@ void GaduEditAccountWidget::createGeneralTab(QTabWidget *tabWidget)
 	formLayout->addRow(0, changePasswordLabel);
 	connect(changePasswordLabel, SIGNAL(linkActivated(QString)), this, SLOT(changePasssword()));
 
-	Identities = new IdentitiesComboBox(false, this);
+	Identities = new IdentitiesComboBox(this);
 	connect(Identities, SIGNAL(currentIndexChanged(int)), this, SLOT(dataChanged()));
 	formLayout->addRow(tr("Account Identity") + ':', Identities);
 

plugins/jabber_protocol/gui/widgets/jabber-edit-account-widget.cpp
@@ -129,7 +129,7 @@ void JabberEditAccountWidget::createGeneralTab(QTabWidget *tabWidget)
 	formLayout->addRow(0, changePasswordLabel);
 	connect(changePasswordLabel, SIGNAL(linkActivated(QString)), this, SLOT(changePasssword()));
 
-	Identities = new IdentitiesComboBox(false, this);
+	Identities = new IdentitiesComboBox(this);
 	connect(Identities, SIGNAL(currentIndexChanged(int)), this, SLOT(dataChanged()));
 	formLayout->addRow(tr("Account Identity") + ':', Identities);
 

commit 513eca65a8d1f0ae7686d42a60e747e4e994d182

gadu, jabber: Correct checking whether user changed something (#2584)
This is a sign of a bad design, sigh.

Regression since 34084cb7deb8dd64c688b5939c75e7c5e5197a37.

http://www.kadu.im/redmine/issues/2584

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

plugins/gadu_protocol/gui/widgets/gadu-add-account-widget.cpp
@@ -123,9 +123,9 @@ void GaduAddAccountWidget::resetGui()
 	AccountId->clear();
 	AccountPassword->clear();
 	RememberPassword->setChecked(true);
-	Identity->setCurrentIdentity(Identity::null);
-
 	IdentityManager::instance()->removeUnused();
+	Identity->setCurrentIndex(0);
+
 	dataChanged();
 }
 
@@ -176,7 +176,7 @@ void GaduAddAccountWidget::dataChanged()
 	if (AccountId->text().isEmpty()
 			&& AccountPassword->text().isEmpty()
 			&& RememberPassword->isChecked()
-			&& !Identity->currentIdentity())
+			&& 0 == Identity->currentIndex())
 		setState(StateNotChanged);
 	else
 		setState(valid ? StateChangedDataValid : StateChangedDataInvalid);

plugins/gadu_protocol/gui/widgets/gadu-create-account-widget.cpp
@@ -132,11 +132,11 @@ void GaduCreateAccountWidget::resetGui()
 	ReNewPassword->clear();
 	RememberPassword->setChecked(true);
 	EMail->clear();
-	IdentityCombo->setCurrentIdentity(Identity::null);
+	IdentityManager::instance()->removeUnused();
+	IdentityCombo->setCurrentIndex(0);
 	MyTokenWidget->setTokenValue(QString());
 	RegisterAccountButton->setEnabled(false);
 
-	IdentityManager::instance()->removeUnused();
 	setState(StateNotChanged);
 }
 
@@ -154,7 +154,7 @@ void GaduCreateAccountWidget::dataChanged()
 			&& ReNewPassword->text().isEmpty()
 			&& RememberPassword->isChecked()
 			&& EMail->text().isEmpty()
-			&& !IdentityCombo->currentIdentity()
+			&& 0 == IdentityCombo->currentIndex()
 			&& MyTokenWidget->tokenValue().isEmpty())
 		setState(StateNotChanged);
 	else

plugins/jabber_protocol/gui/widgets/jabber-add-account-widget.cpp
@@ -161,7 +161,7 @@ void JabberAddAccountWidget::dataChanged()
 			&& AccountPassword->text().isEmpty()
 			&& RememberPassword->isChecked()
 			&& Domain->currentText() == Factory->defaultServer()
-			&& !Identity->currentIdentity())
+			&& 0 == Identity->currentIndex())
 		setState(StateNotChanged);
 	else
 		setState(valid ? StateChangedDataValid : StateChangedDataInvalid);
@@ -216,10 +216,10 @@ void JabberAddAccountWidget::resetGui()
 	Username->clear();
 	Domain->setEditText(Factory->defaultServer());
 	RememberPassword->setChecked(true);
-	Identity->setCurrentIdentity(Identity::null);
+	IdentityManager::instance()->removeUnused();
+	Identity->setCurrentIndex(0);
 	AddAccountButton->setDisabled(true);
 
-	IdentityManager::instance()->removeUnused();
 	setState(StateNotChanged);
 }
 

plugins/jabber_protocol/gui/widgets/jabber-create-account-widget.cpp
@@ -274,7 +274,7 @@ void JabberCreateAccountWidget::dataChanged()
 			&& NewPassword->text().isEmpty()
 			&& ReNewPassword->text().isEmpty()
 			&& RememberPassword->isChecked()
-			&& !IdentityCombo->currentIdentity()
+			&& 0 == IdentityCombo->currentIndex()
 			&& !CustomHostPort->isChecked()
 			&& CustomHost->text().isEmpty()
 			&& CustomPort->text().toUInt() == port_
@@ -320,7 +320,8 @@ void JabberCreateAccountWidget::resetGui()
 	NewPassword->clear();
 	ReNewPassword->clear();
 	RememberPassword->setChecked(true);
-	IdentityCombo->setCurrentIdentity(Identity::null);
+	IdentityManager::instance()->removeUnused();
+	IdentityCombo->setCurrentIndex(0);
 	ShowConnectionOptions = false;
 	ExpandConnectionOptionsButton->setText(">");
 	OptionsWidget->setVisible(false);
@@ -332,7 +333,6 @@ void JabberCreateAccountWidget::resetGui()
 	LegacySSLProbe->setChecked(true);
 	RegisterAccountButton->setEnabled(false);
 
-	IdentityManager::instance()->removeUnused();
 	setState(StateNotChanged);
 }
 