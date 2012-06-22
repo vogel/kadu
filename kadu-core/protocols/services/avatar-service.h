df60c471f7f9824bb19cc7bef1f76cf06707d342
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2012-06-23 00:22:48 +0200

    avatars: AvatarService documented
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- kadu-core/protocols/services/avatar-service.h
+++ kadu-core/protocols/services/avatar-service.h
@@ -30,22 +30,93 @@
 
 class Account;
 
+/**
+ * @addtogroup Protocol
+ * @{
+ */
+
+/**
+ * @class AvatarService
+ * @short Service for feteching and uploading avatars.
+ * @author Rafał 'Vogel' Malinowski
+ *
+ * This service has two functions. It allows fetching avatars of known buddies and uploading own avatar.
+ *
+ * To fetch avatar of known buddy call fetchAvatar() method and pass an object that has avatarFetched(bool ok, QPixmap avatar)
+ * slot. When fetching is done this slot will be called. First parameter is success state - if true, then fetching
+ * was successfull. Second argument is fetched avatar data.
+ *
+ * To upload own avatar call uploadAvatar() method. It requires login id and login password parameters, as well as
+ * avatar image data. When uploading is done avatarUploaded() signal will be emitted.
+ */
 class KADUAPI AvatarService : public QObject
 {
 	Q_OBJECT
 
-public:
-	static AvatarService * fromAccount(Account account);
-
+protected:
 	explicit AvatarService(QObject *parent = 0);
 	virtual ~AvatarService();
 
+public:
+	/**
+	 * @short Return avatar service for given account.
+	 * @author Rafał 'Vogel' Malinowski
+	 * @param account account to get avatar service from
+	 * @return avatar service for given account
+	 */
+	static AvatarService * fromAccount(Account account);
+
+	/**
+	 * @short Fetch avatar for buddy with given id.
+	 * @author Rafał 'Vogel' Malinowski
+	 * @param id id of buddy to get avatar for
+	 * @param receiver object with avatarFetched(bool ok,QPixmap avatar) that will be notified of finished task
+	 *
+	 * This method will start fetching avatar for buddy with given id. When this task is finished receiver object
+	 * will be informed of that by calling its avatarFetched(bool ok, QPixmap avatar). First parameter of this slot
+	 * is success state - if true, then fetching was successfull. Second argument is fetched avatar data.
+	 *
+	 * Nothing will happen when receiver object is null or when id is empty. When given buddy does not have an
+	 * avatar then ok parameter will be true and avatar will be empty. If fetching fails for whatever reason
+	 * then ok parameter will be false.
+	 *
+	 * It is not possible for now to get reason of failure.
+	 */
 	virtual void fetchAvatar(const QString &id, QObject *receiver) = 0;
+
+	/**
+	 * @short Uploads avatar for self.
+	 * @author Rafał 'Vogel' Malinowski
+	 * @param id login id
+	 * @param password login password
+	 * @param avatar avatar to upload
+	 *
+	 * This method will start uploading avatar for account owner. When this task is finished avatarUploaded() slot
+	 * will be emitted. If no id provided then nothing will happen.
+	 *
+	 * When avatar is empty then it will be removed from server. In other cases, it will be updated. Some protocols
+	 * may resize or modify avatar in other way before uploading. Modified version will be returned in avatarUploaded()
+	 * signal.
+	 */
 	virtual void uploadAvatar(const QString &id, const QString &password, QImage avatar) = 0;
 
 signals:
+	/**
+	 * @short Signal emitted when uploading of avatar has finished.
+	 * @author Rafał 'Vogel' Malinowski
+	 * @param ok success flag
+	 * @param avatar uploaded avatar
+	 *
+	 * This Signal emitted when uploading of avatar has finished. If ok parameter is true then uploading was successfull.
+	 * Uploaded image is passed back as avatar parameter. It can be different than avatar passed in uploadAvatar() method
+	 * if resizing or other modifications were required.
+	 */
 	void avatarUploaded(bool ok, QImage avatar);
 
 };
+
+/**
+ * @}
+ */
 
 #endif // AVATAR_SERVICE_H
