a696078d42b9a0ed9c4c3e96055495e0035bb4f6
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2012-06-27 00:26:59 +0200

    jabber: added missing files
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ plugins/jabber_protocol/services/jabber-vcard-downloader.cpp
@@ -0,0 +1,66 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2012 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <xmpp_tasks.h>
+
+#include "jabber-vcard-downloader.h"
+
+JabberVCardDownloader::JabberVCardDownloader(XMPP::Client *client, QObject *parent) :
+		QObject(parent), XmppClient(client)
+{
+}
+
+JabberVCardDownloader::~JabberVCardDownloader()
+{
+}
+
+void JabberVCardDownloader::done(XMPP::VCard vCard)
+{
+	emit vCardDownloaded(true, vCard);
+	deleteLater();
+}
+
+void JabberVCardDownloader::failed()
+{
+	emit vCardDownloaded(false, XMPP::VCard());
+	deleteLater();
+}
+
+void JabberVCardDownloader::taskFinished()
+{
+	if (!Task || !Task.data()->success())
+		failed();
+	else
+		done(Task.data()->vcard());
+}
+
+void JabberVCardDownloader::downloadVCard(const QString &id)
+{
+	if (!XmppClient || !XmppClient.data()->rootTask())
+	{
+		failed();
+		return;
+	}
+
+	Task = new XMPP::JT_VCard(XmppClient.data()->rootTask());
+
+	connect(Task.data(), SIGNAL(finished()), this, SLOT(taskFinished()));
+	Task.data()->get(id);
+	Task.data()->go(true);
+}
---
+++ plugins/jabber_protocol/services/jabber-vcard-downloader.h
@@ -0,0 +1,96 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2012 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef JABBER_VCARD_DOWNLOADER_H
+#define JABBER_VCARD_DOWNLOADER_H
+
+#include <QtCore/QWeakPointer>
+
+#include <xmpp_vcard.h>
+
+namespace XMPP
+{
+	class Client;
+	class JT_VCard;
+}
+
+/**
+ * @addtogroup Jabber
+ * @{
+ */
+
+/**
+ * @class JabberVCardUploader
+ * @short Class for downloading VCard.
+ * @author Rafał 'Vogel' Malinowski
+ *
+ * This class allows for downloading VCard. To do that attach slot to vCardDownloaded() signal and call downloadVCard()
+ * method. After VCard is downloaded vCardDownloaded() signal is emitted and this object deletes itself.
+ */
+class JabberVCardDownloader : public QObject
+{
+	Q_OBJECT
+
+	QWeakPointer<XMPP::Client> XmppClient;
+	QWeakPointer<XMPP::JT_VCard> Task;
+
+	void done(XMPP::VCard vCard);
+	void failed();
+
+private slots:
+	void taskFinished();
+
+public:
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Create instance attached to given XMPP::Client.
+	 * @param client instance of XMPP::Client
+	 * @param parent QObject parent
+	 */
+	explicit JabberVCardDownloader(XMPP::Client *client, QObject *parent = 0);
+	virtual ~JabberVCardDownloader();
+
+	/**
+	 * @short Downloads VCard for given id.
+	 * @author Rafał 'Vogel' Malinowski
+	 * @param id id of contact to download VCard for
+	 *
+	 * Before calling this method attach to vCardDownloaded() signal to get informed about result. Please
+	 * note that this method can be only called once. After that this object emits vCardDownloaded() and
+	 * deletes itself.
+	 */
+	virtual void downloadVCard(const QString &id);
+
+signals:
+	/**
+	 * @short Signal emitted when job of this class is done.
+	 * @author Rafał 'Vogel' Malinowski
+	 * @param ok success flag
+	 * @param vCard downloaded VCard
+	 *
+	 * If ok is true then VCard downloading was successfull. If ok is false then operation failed.
+	 */
+	void vCardDownloaded(bool ok, XMPP::VCard vCard);
+};
+
+/**
+ * @}
+ */
+
+#endif // JABBER_VCARD_DOWNLOADER_H
