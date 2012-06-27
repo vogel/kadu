vogel/kadu

commit 4f08bec4e6b270dd9e83fb3d09a07e9006d19830

protocol: refactored ChatImageService to have reasonable API
Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

plugins/gadu_protocol/services/gadu-chat-image-service.h
@@ -37,6 +37,8 @@ class GaduChatImageService : public ChatImageService
 {
 	Q_OBJECT
 
+	static const qint64 RECOMMENDED_MAXIMUM_SIZE = 255 * 104;
+
 	struct ImageToSend
 	{
 		QString fileName;
@@ -66,9 +68,7 @@ public:
 	bool sendImageRequest(Contact contact, int size, quint32 crc32);
 	void prepareImageToSend(const QString &imageFileName, quint32 &size, quint32 &crc32);
 
-	virtual qint64 softSizeLimit();
-	virtual qint64 hardSizeLimit();
-	virtual bool showSoftSizeWarning();
+	virtual Error checkImageSize(qint64 size) const;
 
 };
 

kadu-core/gui/widgets/chat-edit-box.cpp
@@ -47,6 +47,7 @@
 #include "gui/widgets/talkable-tree-view.h"
 #include "gui/windows/message-dialog.h"
 #include "identities/identity.h"
+#include "misc/error.h"
 #include "protocols/protocol.h"
 #include "protocols/services/chat-image-service.h"
 #include "status/status-container-manager.h"
@@ -235,6 +236,10 @@ void ChatEditBox::openColorSelector(const QWidget *activatingWidget)
 
 void ChatEditBox::openInsertImageDialog()
 {
+	ChatImageService *chatImageService = CurrentChat.chatAccount().protocolHandler()->chatImageService();
+	if (!chatImageService)
+		return;
+
 	// QTBUG-849
 	QString selectedFile = QFileDialog::getOpenFileName(this, tr("Insert image"), config_file.readEntry("Chat", "LastImagePath"),
 							tr("Images (*.png *.PNG *.jpg *.JPG *.jpeg *.JPEG *.gif *.GIF *.bmp *.BMP);;All Files (*)"));
@@ -250,28 +255,22 @@ void ChatEditBox::openInsertImageDialog()
 			return;
 		}
 
-		ChatImageService *chatImageService = CurrentChat.chatAccount().protocolHandler()->chatImageService();
-		if (chatImageService)
+		Error imageSizeError = chatImageService->checkImageSize(f.size());
+		if (!imageSizeError.message().isEmpty())
 		{
-			if (!chatImageService->fitsHardSizeLimit(f.size()))
+			switch (imageSizeError.severity())
 			{
-				MessageDialog::show(
-						KaduIcon("dialog-error"), tr("Kadu"),
-						tr("This image has %1 KiB and exceeds the protocol limit of %2 KiB.")
-								.arg((f.size() + 1023) / 1024).arg(chatImageService->hardSizeLimit() / 1024),
-						QMessageBox::Ok, this);
-				return;
-			}
-			if (!chatImageService->fitsSoftSizeLimit(f.size()))
-			{
-				if (chatImageService->showSoftSizeWarning())
-				{
-					QString message = tr("This image has %1 KiB and exceeds recommended maximum size of %2 KiB.")
-							+ '\n' + tr("Do you really want to send this image?");
-					message = message.arg((f.size() + 1023) / 1024).arg(chatImageService->softSizeLimit() / 1024);
-					if (!MessageDialog::ask(KaduIcon("dialog-warning"), tr("Kadu"), message, this))
-						return;
-				}
+				case NoError:
+					break;
+				case ErrorLow:
+						if (!MessageDialog::ask(KaduIcon("dialog-warning"), tr("Kadu"), imageSizeError.message(), this))
+							return;
+					break;
+				case ErrorHigh:
+					MessageDialog::show(KaduIcon("dialog-error"), tr("Kadu"), imageSizeError.message(), QMessageBox::Ok, this);
+					return;
+				default:
+					break;
 			}
 		}
 

kadu-core/gui/widgets/chat-messages-view.cpp
@@ -122,8 +122,8 @@ void ChatMessagesView::connectChat()
 
 	ChatImageService *chatImageService = CurrentChat.chatAccount().protocolHandler()->chatImageService();
 	if (chatImageService)
-		connect(chatImageService, SIGNAL(imageReceived(const QString &, const QString &)),
-				this, SLOT(imageReceived(const QString &, const QString &)));
+		connect(chatImageService, SIGNAL(imageReceivedAndSaved(const QString &, const QString &)),
+				this, SLOT(imageReceivedAndSaved(const QString &, const QString &)));
 
 	ChatService *chatService = CurrentChat.chatAccount().protocolHandler()->chatService();
 	if (chatService)
@@ -182,7 +182,7 @@ void ChatMessagesView::pageDown()
 	keyPressEvent(&event);
 }
 
-void ChatMessagesView::imageReceived(const QString &imageId, const QString &imageFileName)
+void ChatMessagesView::imageReceivedAndSaved(const QString &imageId, const QString &imageFileName)
 {
 	Renderer->replaceLoadingImages(imageId, imageFileName);
 }

kadu-core/gui/widgets/chat-messages-view.h
@@ -66,7 +66,7 @@ class KADUAPI ChatMessagesView : public KaduWebView, public ConfigurationAwareOb
 
 private slots:
 	void repaintMessages();
-	void imageReceived(const QString &imageId, const QString &imageFileName);
+	void imageReceivedAndSaved(const QString &imageId, const QString &imageFileName);
 	void sentMessageStatusChanged(const Message &message);
 
 protected:

kadu-core/protocols/services/chat-image-service.cpp
@@ -39,19 +39,3 @@ ChatImageService::ChatImageService(Account account, QObject *parent) :
 ChatImageService::~ChatImageService()
 {
 }
-
-bool ChatImageService::fitsHardSizeLimit(qint64 size)
-{
-	if (NoSizeLimit == hardSizeLimit())
-		return true;
-	return size <= hardSizeLimit();
-}
-
-bool ChatImageService::fitsSoftSizeLimit(qint64 size)
-{
-	if (!fitsHardSizeLimit(size))
-		return false;
-	if (NoSizeLimit == softSizeLimit())
-		return true;
-	return size <= softSizeLimit();
-}

kadu-core/protocols/services/chat-image-service.h
@@ -31,6 +31,8 @@
 #include "protocols/services/account-service.h"
 #include "exports.h"
 
+class Error;
+
 class KADUAPI ChatImageService : public AccountService
 {
 	Q_OBJECT
@@ -43,18 +45,11 @@ public:
 	explicit ChatImageService(Account account, QObject *parent = 0);
 	virtual ~ChatImageService();
 
-	virtual qint64 hardSizeLimit() { return NoSizeLimit; }
-	virtual qint64 softSizeLimit() { return NoSizeLimit; }
-	bool fitsHardSizeLimit(qint64 size);
-	bool fitsSoftSizeLimit(qint64 size);
-	virtual bool showSoftSizeWarning() { return true; }
+	virtual Error checkImageSize(qint64 size) const = 0;
 
 signals:
-	void imageReceived(const QString &id, const QString &fileName);
+	void imageReceivedAndSaved(const QString &id, const QString &fileName);
 
 };
-
-// for MOC
-#include <QtCore/QString>
 
 #endif // CHAT_IMAGE_SERVICE_H

plugins/gadu_protocol/services/gadu-chat-image-service.cpp
@@ -27,6 +27,7 @@
 #include <QtCore/QFileInfo>
 
 #include "configuration/configuration-file.h"
+#include "misc/error.h"
 #include "debug.h"
 
 #include "helpers/gadu-formatter.h"
@@ -140,7 +141,7 @@ void GaduChatImageService::handleEventImageReply(struct gg_event *e)
 	if (fileName.isEmpty())
 		return;
 
-	emit imageReceived(GaduFormatter::createImageId(e->event.image_reply.sender,
+	emit imageReceivedAndSaved(GaduFormatter::createImageId(e->event.image_reply.sender,
 			e->event.image_reply.size, e->event.image_reply.crc32), fileName);
 }
 
@@ -183,20 +184,15 @@ void GaduChatImageService::prepareImageToSend(const QString &imageFileName, quin
 	ImagesToSend[qMakePair(size, crc32)] = imageToSend;
 }
 
-qint64 GaduChatImageService::softSizeLimit()
-{
-	return 255 * 1024;
-}
-
-qint64 GaduChatImageService::hardSizeLimit()
-{
-	return ChatImageService::NoSizeLimit;
-}
-
-bool GaduChatImageService::showSoftSizeWarning()
+Error GaduChatImageService::checkImageSize(qint64 size) const
 {
 	GaduAccountDetails *details = dynamic_cast<GaduAccountDetails *>(account().details());
-	if (!details)
-		return true;
-	return details->chatImageSizeWarning();
+	if (!details || !details->chatImageSizeWarning() || size <= RECOMMENDED_MAXIMUM_SIZE)
+		return Error(NoError, QString());
+
+	QString message = tr("This image has %1 KiB and exceeds recommended maximum size of %2 KiB.") + '\n' + tr("Do you really want to send this image?");
+	message = message.arg((size + 1023) / 1024).arg(RECOMMENDED_MAXIMUM_SIZE / 1024);
+
+	return Error(ErrorLow, message);
 }
+

commit 630b4ce619f3712b8202b27c1b4d9d23bb4e6d67

gadu: Fix typo bug introduced in 4f08bec4e6b270dd9e83fb3d09a07e9006d19830
Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

plugins/gadu_protocol/services/gadu-chat-image-service.h
@@ -53,7 +53,7 @@ class GaduChatImageService : public ChatImageService
 {
 	Q_OBJECT
 
-	static const qint64 RECOMMENDED_MAXIMUM_SIZE = 255 * 104;
+	static const qint64 RECOMMENDED_MAXIMUM_SIZE = 255 * 1024;
 
 	struct ImageToSend
 	{