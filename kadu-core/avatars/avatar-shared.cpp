vogel/kadu

commit 9a832556a572def040e9ef32d2da0a03b72fe3cd

gui: do not try to display or load non-existing avatars
Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

kadu-core/avatars/avatar-shared.cpp
@@ -73,7 +73,7 @@ QString AvatarShared::storageNodeName()
 
 QString AvatarShared::filePath()
 {
-	return FilePath.isEmpty() ? AvatarsDir + uuid().toString() : FilePath;
+	return FilePath.isEmpty() && !uuid().toString().isEmpty() ? AvatarsDir + uuid().toString() : FilePath;
 }
 
 QString AvatarShared::smallFilePath()
@@ -116,8 +116,13 @@ void AvatarShared::load()
 	LastUpdated = loadValue<QDateTime>("LastUpdated");
 	NextUpdate = loadValue<QDateTime>("NextUpdate");
 
-	QImageReader imageReader(filePath());
-	Pixmap = QPixmap::fromImageReader(&imageReader);
+	QFileInfo avatarFile(filePath());
+
+	if (avatarFile.exists() && avatarFile.isReadable() && avatarFile.isFile())
+	{
+		QImageReader imageReader(avatarFile.path());
+		Pixmap = QPixmap::fromImageReader(&imageReader);
+	}
 
 	ensureSmallPixmapExists();
 }

kadu-core/contacts/model/contact-data-extractor.cpp
@@ -20,6 +20,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <QtCore/QFileInfo>
 #include <QtCore/QVariant>
 #include <QtGui/QIcon>
 
@@ -71,7 +72,14 @@ QVariant ContactDataExtractor::data(const Contact &contact, int role, bool useBu
 		case AvatarRole:
 			return contact.avatar(useBuddyData).pixmap();
 		case AvatarPathRole:
-			return contact.avatar(useBuddyData).filePath();
+		{
+			QFileInfo avatarInfo(contact.avatar(useBuddyData).filePath());
+
+			if (avatarInfo.exists() && avatarInfo.isReadable() && avatarInfo.isFile())
+				return avatarInfo.filePath();
+			else
+				return QString();
+		}
 		case ItemTypeRole:
 			return ContactRole;
 		case TalkableRole:

commit 0d94c28d8f55371e5dfcb42ff02a39b898bbd582

avatars: Fix non-loading avatars at startup (typo fix)
Regression since 9a832556a572def040e9ef32d2da0a03b72fe3cd.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

kadu-core/avatars/avatar-shared.cpp
@@ -120,7 +120,7 @@ void AvatarShared::load()
 
 	if (avatarFile.exists() && avatarFile.isReadable() && avatarFile.isFile())
 	{
-		QImageReader imageReader(avatarFile.path());
+		QImageReader imageReader(avatarFile.filePath());
 		Pixmap = QPixmap::fromImageReader(&imageReader);
 	}
 