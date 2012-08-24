e58968e36c7092f8b3345de5503a7741869b90e1
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2012-08-24 23:51:12 +0200

    formatted-string: added FormattedStringCloneVisitor
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ kadu-core/formatted-string/formatted-string-clone-visitor.cpp
@@ -0,0 +1,89 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2012 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QtCore/QFileInfo>
+
+#include "formatted-string/composite-formatted-string.h"
+#include "formatted-string/formatted-string-image-block.h"
+#include "formatted-string/formatted-string-text-block.h"
+#include "misc/misc.h"
+
+#include "formatted-string-clone-visitor.h"
+
+FormattedStringCloneVisitor::FormattedStringCloneVisitor()
+{
+}
+
+FormattedStringCloneVisitor::~FormattedStringCloneVisitor()
+{
+	// in case something is left
+	qDeleteAll(ItemsStack);
+}
+
+void FormattedStringCloneVisitor::cloned(FormattedString *clonedFormattedString)
+{
+	ItemsStack.push(clonedFormattedString);
+}
+
+void FormattedStringCloneVisitor::beginVisit(const CompositeFormattedString * const compositeFormattedString)
+{
+	ItemsStack.push(0); // mark composite begin
+}
+
+void FormattedStringCloneVisitor::endVisit(const CompositeFormattedString * const compositeFormattedString)
+{
+	QList<FormattedString *> items;
+	while (!ItemsStack.isEmpty())
+	{
+		FormattedString *item = ItemsStack.pop();
+		if (item)
+			items.prepend(item);
+		else
+			break;
+	}
+
+	CompositeFormattedString *cloned = new CompositeFormattedString(items.toVector());
+
+	ItemsStack.push(cloned);
+}
+
+void FormattedStringCloneVisitor::visit(const FormattedStringImageBlock * const formattedStringImageBlock)
+{
+	cloned(formattedStringImageBlock->imageKey().isNull()
+			? new FormattedStringImageBlock(formattedStringImageBlock->imagePath())
+			: new FormattedStringImageBlock(formattedStringImageBlock->imageKey()));
+}
+
+void FormattedStringCloneVisitor::visit(const FormattedStringTextBlock * const formattedStringTextBlock)
+{
+	cloned(new FormattedStringTextBlock(
+		formattedStringTextBlock->content(),
+		formattedStringTextBlock->bold(),
+		formattedStringTextBlock->italic(),
+		formattedStringTextBlock->underline(),
+		formattedStringTextBlock->color()
+	));
+}
+
+FormattedString * FormattedStringCloneVisitor::result()
+{
+	Q_ASSERT(ItemsStack.size() == 1);
+
+	return ItemsStack.pop();
+}
---
+++ kadu-core/formatted-string/formatted-string-clone-visitor.h
@@ -0,0 +1,77 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2012 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef FORMATTED_STRING_CLONE_VISITOR_H
+#define FORMATTED_STRING_CLONE_VISITOR_H
+
+#include <QtCore/QScopedPointer>
+#include <QtCore/QStack>
+#include <QtCore/QString>
+
+#include "exports.h"
+#include "formatted-string/formatted-string-visitor.h"
+
+class FormattedString;
+
+/**
+ * @addtogroup FormattedString
+ * @{
+ */
+
+/**
+ * @class FormattedStringCloneVisitor
+ * @short This visitor creates new FormattedString identical to visited FormattedString.
+ * @author Rafał 'Vogel' Malinowski
+ *
+ * After being accepted by a FormattedString this visitor returns new FormattedString view identical to visited FormattedString.
+ */
+class KADUAPI FormattedStringCloneVisitor : public FormattedStringVisitor
+{
+	Q_DISABLE_COPY(FormattedStringCloneVisitor);
+
+	QStack<FormattedString *> ItemsStack;
+
+protected:
+	void cloned(FormattedString *clonedFormattedString);
+
+public:
+	FormattedStringCloneVisitor();
+	virtual ~FormattedStringCloneVisitor();
+
+	virtual void beginVisit(const CompositeFormattedString * const compositeFormattedString);
+	virtual void endVisit(const CompositeFormattedString * const compositeFormattedString);
+	virtual void visit(const FormattedStringImageBlock * const formattedStringImageBlock);
+	virtual void visit(const FormattedStringTextBlock * const formattedStringTextBlock);
+
+	/**
+	* @short Returns new FormattedString instance identical to visited one.
+	* @author Rafał 'Vogel' Malinowski
+	* @return new FormattedString instance identical to visited one
+	*
+	* Caller gains ownership of the returned object. This method can only be called once.
+	*/
+	FormattedString * result();
+
+};
+
+/**
+ * @}
+ */
+
+#endif // FORMATTED_STRING_CLONE_VISITOR_H
