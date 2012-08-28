6e8f6ed2793ea6c344798902f5b77b3a6a8affad
Bartosz Brachaczek <b.brachaczek@gmail.com>
2012-08-28 18:20:08 +0200

    configuration: After crash save backup configuration file to kadu-0.12.conf.xml.backup*, not kadu.conf.xml.backup*
    Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>
    (backport of commit d7a1c83355a4728a1f7b89902efef34140c9d6f9)

--- kadu-core/main_win32.cpp
+++ kadu-core/main_win32.cpp
@@ -91,8 +91,7 @@ LONG WINAPI exception_handler(struct _EXCEPTION_POINTERS *e)
 	}
 
 	// if we cannot make crash dump only save config file to backup
-	QString f = QString("kadu.conf.xml.backup.%1").arg(QDateTime::currentDateTime().toString("yyyy.MM.dd.hh.mm.ss"));
-	xml_config_file->saveTo(KaduPaths::instance()->profilePath() + f);
+	xml_config_file->makeBackup();
 	return ret;
 #else
 	MessageBoxW(NULL, L"Mini dumps are not available in this build.", L"Kadu crashed", MB_OK | MB_ICONERROR);
