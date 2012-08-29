vogel/kadu

commit 0c68878464f9ebad472f2412c30eca2878d4b41b

sms: use ProgressWindow2 instead of SmsProgressWindow
Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

plugins/sms/gui/windows/sms-dialog.cpp
@@ -26,6 +26,7 @@
 #include <QtGui/QFormLayout>
 #include <QtGui/QHBoxLayout>
 #include <QtGui/QKeyEvent>
+#include <QtGui/QLabel>
 #include <QtGui/QLineEdit>
 #include <QtGui/QPushButton>
 #include <QtGui/QTextEdit>
@@ -37,13 +38,13 @@
 #include "configuration/configuration-file.h"
 #include "gui/widgets/select-talkable-combo-box.h"
 #include "gui/windows/message-dialog.h"
+#include "gui/windows/progress-window2.h"
 #include "icons/kadu-icon.h"
 #include "misc/misc.h"
 #include "plugins/plugins-manager.h"
 #include "talkable/filter/mobile-talkable-filter.h"
 #include "debug.h"
 
-#include "gui/windows/sms-progress-window.h"
 #include "mobile-number-manager.h"
 #include "sms-external-sender.h"
 #include "sms-gateway.h"
@@ -290,8 +291,11 @@ void SmsDialog::sendSms()
 	connect(sender, SIGNAL(gatewayAssigned(QString, QString)), this, SLOT(gatewayAssigned(QString, QString)));
 	sender->setSignature(SignatureEdit->text());
 
-	SmsProgressWindow *window = new SmsProgressWindow(sender);
+	ProgressWindow2 *window = new ProgressWindow2(tr("Sending SMS..."));
 	window->show();
+
+	connect(sender, SIGNAL(progress(QString,QString)), window, SLOT(addProgressEntry(QString,QString)));
+	connect(sender, SIGNAL(finished(bool,QString,QString)), window, SLOT(progressFinished(bool,QString,QString)));
 
 	sender->sendMessage(ContentEdit->toPlainText());
 

plugins/sms/sms-external-sender.cpp
@@ -65,7 +65,7 @@ void SmsExternalSender::sendMessage(const QString &message)
 
 	if (!Process->waitForStarted())
 	{
-		emit failed(tr("Could not spawn child process. Check if the program is functional"));
+		emit finished(false, "dialog-error", tr("Could not spawn child process. Check if the program is functional"));
 		delete Process;
 		Process = 0;
 		return;
@@ -77,9 +77,9 @@ void SmsExternalSender::sendMessage(const QString &message)
 void SmsExternalSender::processFinished()
 {
 	if (QProcess::NormalExit == Process->exitStatus())
-		emit succeed(Message);
+		emit finished(true, "dialog-information", tr("SMS sent"));
 	else
-		emit failed(tr("The process exited abnormally. The SMS may not be sent"));
+		emit finished(false, "dialog-error", tr("The process exited abnormally. The SMS may not be sent"));
 
 	Process->deleteLater();
 	Process = 0;

plugins/sms/sms-internal-sender.cpp
@@ -51,7 +51,7 @@ void SmsInternalSender::sendMessage(const QString &message)
 
 	if (Gateway.signatureRequired() && !validateSignature())
 	{
-		emit failed(tr("Signature can't be empty"));
+		emit finished(false, "dialog-error", tr("Signature can't be empty"));
 		kdebugf2();
 		return;
 	}
@@ -73,12 +73,14 @@ void SmsInternalSender::gatewayQueryDone(const QString &gatewayId)
 {
 	if (gatewayId.isEmpty())
 	{
-		emit failed(tr("Automatic gateway selection is not available. Please select SMS gateway manually."));
+		emit finished(false, "dialog-error", tr("Automatic gateway selection is not available. Please select SMS gateway manually."));
 		kdebugf2();
 		return;
 	}
 
 	Gateway = SmsGatewayManager::instance()->byId(gatewayId);
+
+	emit progress("dialog-information", tr("Detected gateway: %1.").arg(Gateway.name()));
 
 	sendSms();
 }
@@ -108,10 +110,10 @@ void SmsInternalSender::sendSms()
 
 void SmsInternalSender::result()
 {
-	emit succeed(Message);
+	emit finished(true, "dialog-information", tr("SMS sent"));
 }
 
 void SmsInternalSender::failure(const QString &errorMessage)
 {
-	emit failed(errorMessage);
+	emit finished(false, "dialog-error", errorMessage);
 }

plugins/sms/sms-sender.h
@@ -54,8 +54,9 @@ public:
 
 signals:
 	void gatewayAssigned(const QString &number, const QString &gatewayId);
-	void succeed(const QString &message);
-	void failed(const QString &errorMessage);
+
+	void progress(const QString &entryIcon, const QString &entryMessage);
+	void finished(bool ok, const QString &entryIcon, const QString &entryMessage);
 
 };
 

commit 151c246f2d2ce664735eab9d1e4afd3565992960

sms: Restore saving sent SMS'es in history
Regression since 0c68878464f9ebad472f2412c30eca2878d4b41b.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

plugins/sms/gui/windows/sms-dialog.cpp
@@ -47,6 +47,8 @@
 #include "talkable/filter/mobile-talkable-filter.h"
 #include "debug.h"
 
+#include "plugins/history/history.h"
+
 #include "mobile-number-manager.h"
 #include "sms-external-sender.h"
 #include "sms-gateway.h"
@@ -301,6 +303,9 @@ void SmsDialog::sendSms()
 	connect(sender, SIGNAL(progress(QString,QString)), window, SLOT(addProgressEntry(QString,QString)));
 	connect(sender, SIGNAL(finished(bool,QString,QString)), window, SLOT(progressFinished(bool,QString,QString)));
 
+	if (SaveInHistoryCheckBox->isChecked())
+		connect(sender, SIGNAL(smsSent(QString,QString)), this, SLOT(saveSmsInHistory(QString,QString)));
+
 	sender->sendMessage(ContentEdit->toPlainText());
 
 	kdebugf2();
@@ -311,6 +316,12 @@ void SmsDialog::updateCounter()
 	LengthLabel->setText(QString::number(ContentEdit->toPlainText().length()) + MaxLengthSuffixText);
 
 	validate();
+}
+
+void SmsDialog::saveSmsInHistory(const QString &number, const QString &message)
+{
+	if (History::instance()->currentStorage())
+		History::instance()->currentStorage()->appendSms(number, message);
 }
 
 void SmsDialog::clear()

plugins/sms/gui/windows/sms-dialog.h
@@ -68,6 +68,8 @@ private slots:
 	void gatewayAssigned(const QString &number, const QString &gatewayId);
 	void sendSms();
 
+	void saveSmsInHistory(const QString &number, const QString &message);
+
 	void clear();
 
 protected:

plugins/sms/sms-external-sender.cpp
@@ -78,7 +78,10 @@ void SmsExternalSender::sendMessage(const QString &message)
 void SmsExternalSender::processFinished()
 {
 	if (QProcess::NormalExit == Process->exitStatus())
+	{
+		emit smsSent(number(), Message);
 		emit finished(true, "dialog-information", tr("SMS sent"));
+	}
 	else
 		emit finished(false, "dialog-error", tr("The process exited abnormally. The SMS may not be sent"));
 

plugins/sms/sms-internal-sender.cpp
@@ -153,6 +153,7 @@ void SmsInternalSender::cancel()
 
 void SmsInternalSender::result()
 {
+	emit smsSent(number(), Message);
 	emit finished(true, "dialog-information", tr("SMS sent"));
 
 	deleteLater();

plugins/sms/sms-internal-sender.h
@@ -46,10 +46,10 @@ private slots:
 	void jobFinished(bool ok, const QString &entryIcon, const QString &entryMessage);
 
 public:
-	explicit SmsInternalSender(const QString &number, const SmsGateway &gateway = SmsGateway(), QObject *parent = 0);
+	explicit SmsInternalSender(const QString &number, const SmsGateway &gateway, QObject *parent = 0);
 	virtual ~SmsInternalSender();
 
-	virtual void sendMessage(const QString& message);
+	virtual void sendMessage(const QString &message);
 
 	void findGatewayForNumber(const QString &number);
 

plugins/sms/sms-sender.h
@@ -58,6 +58,8 @@ public slots:
 signals:
 	void gatewayAssigned(const QString &number, const QString &gatewayId);
 
+	void smsSent(const QString &number, const QString &message);
+
 	void progress(const QString &entryIcon, const QString &entryMessage);
 	void finished(bool ok, const QString &entryIcon, const QString &entryMessage);
 	void canceled();