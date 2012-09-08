9327761a764a20064106fd54c0b0335ca76d6589
Bartosz Brachaczek <b.brachaczek@gmail.com>
2012-09-08 20:25:43 +0200

    tests: Setup first automatic tests
    To build tests, ensure BUILD_TESTING CMake option is on. It is
    automatically enabled if you have ENABLE_DEVELOPER_BUILD set to on,
    which you really should if you are doing development.
    
    To run tests, just type "ctest" in the build directory. Logs from failed
    tests will be available in the Testing directory (probably CTest can be
    tweaked to dump them also immediately to stdout/stderr). You can also
    run the test binaries directly.
    
    About QtTest:
      http://techbase.kde.org/Development/Tutorials/Unittests
      http://qt-project.org/doc/qt-4.8/qttest.html
    
    The first tests here are just stupid examples. We'll make this test
    suite big and useful!
    
    Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

---
+++ tests/auto/kadu-core/misc/tst-string-utils.cpp
@@ -0,0 +1,62 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2012 Bartosz Brachaczek (b.brachaczek@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QtTest/QtTest>
+
+#include "misc/string-utils.h"
+
+class tst_StringUtils : public QObject
+{
+	Q_OBJECT
+
+private slots:
+	void ellipsisTest_data();
+	void ellipsisTest();
+
+};
+
+void tst_StringUtils::ellipsisTest_data()
+{
+	QTest::addColumn<QString>("input");
+	QTest::addColumn<int>("maxLen");
+	QTest::addColumn<QString>("expected");
+
+	// TODO: omg this function is unintuitive
+// 	QTest::newRow(",0") << "" << 0 << "";
+// 	QTest::newRow(",10") << "" << 10 << "";
+// 	QTest::newRow("string,0") << "string" << 0 << "";
+// 	QTest::newRow("string,1") << "string" << 0 << "s";
+// 	QTest::newRow("string,5") << "string" << 5 << "st...";
+// 	QTest::newRow("string,6") << "string" << 6 << "string";
+
+	// this is how actually it works:
+	QTest::newRow("string,3") << "string" << 3 << "str...";
+}
+
+void tst_StringUtils::ellipsisTest()
+{
+	QFETCH(QString, input);
+	QFETCH(int, maxLen);
+	QFETCH(QString, expected);
+
+	QCOMPARE(StringUtils::ellipsis(input, maxLen), expected);
+}
+
+QTEST_APPLESS_MAIN(tst_StringUtils)
+#include "tst-string-utils.moc"
