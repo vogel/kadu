vogel/kadu

commit 0f771aac1b4ac1279f57d9cb7b394c23540e1019

jabber: Fix more crashes in JabberConnectionService
Now it seems pretty stable for me.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

plugins/jabber_protocol/services/jabber-connection-service.cpp
@@ -57,21 +57,30 @@ JabberConnectionService::~JabberConnectionService()
 void JabberConnectionService::cleanUp()
 {
 	// Destroy in order opposite to creation. Expect crashes otherwise.
+
+	// Just deinit. JabberProtocols owns it.
+	if (XmppClient)
+		XmppClient.data()->close();
+
+	// We need to delete iris objects immediately (normally we would do that
+	// with deleteLater()) because deferred deletion apparently does not
+	// guarantee to preserve order of deleteLater() calls. This can be changed
+	// if Qt starts to guarantee that at some point.
 	if (Stream)
 	{
-		Stream.data()->deleteLater();
+		delete Stream.data();
 		Stream.clear();
 	}
 	if (TLSHandler)
 	{
 		Q_ASSERT(TLSHandler.data()->parent());
 		Q_ASSERT(this != TLSHandler.data()->parent());
-		TLSHandler.data()->parent()->deleteLater();
+		delete TLSHandler.data()->parent();
 		TLSHandler.clear();
 	}
 	if (Connector)
 	{
-		Connector.data()->deleteLater();
+		delete Connector.data();
 		Connector.clear();
 	}
 
@@ -345,6 +354,10 @@ void JabberConnectionService::connectToServer()
 
 void JabberConnectionService::disconnectFromServer(const ::Status &status)
 {
+	// XmppClient depends on Stream
+	if (!Stream)
+		return;
+
 	XMPP::Status presence = IrisStatusAdapter::toIrisStatus(status);
 	XmppClient.data()->setPresence(presence);
 

plugins/jabber_protocol/jabber-protocol.cpp
@@ -260,9 +260,6 @@ void JabberProtocol::logout()
 	CurrentConnectionService->disconnectFromServer(status());
 
 	loggedOut();
-
-	if (XmppClient)
-		XmppClient->close();
 }
 
 void JabberProtocol::sendStatusToServer()

commit 438a27af0359150c1f7305c5e8b29da081fc95b6

jabber: Fix connecting to server
Regression since 0f771aac1b4ac1279f57d9cb7b394c23540e1019.

iris is stupid enough to emit disconnected() signal from
XMPP::Client::close() unconditionally. And because we call
JabberConnectionService::cleanUp() from
JabberConnectionService::connectToServer() to make sure nothing leaks
and CleanUpTimer stops, we had some real problems there, as the state
machine was thinking we were always getting connection errors when
trying to connect.

Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

plugins/jabber_protocol/services/jabber-connection-service.cpp
@@ -59,7 +59,7 @@ void JabberConnectionService::cleanUp()
 	// Destroy in order opposite to creation. Expect crashes otherwise.
 
 	// Just deinit. JabberProtocols owns it.
-	if (XmppClient)
+	if (XmppClient && XmppClient.data()->isActive())
 		XmppClient.data()->close();
 
 	// We need to delete iris objects immediately (normally we would do that