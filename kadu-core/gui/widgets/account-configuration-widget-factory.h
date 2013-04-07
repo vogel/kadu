b281658be20a785285b13f0381e9d5aac3745600
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2013-04-07 22:03:58 +0200

    gui: added AccountConfigurationWidgetFactory class
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ kadu-core/gui/widgets/account-configuration-widget-factory.h
@@ -0,0 +1,38 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2013 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ACCOUNT_CONFIGURATION_WIDGET_FACTORY_H
+#define ACCOUNT_CONFIGURATION_WIDGET_FACTORY_H
+
+class QWidget;
+
+class Account;
+class AccountConfigurationWidget;
+
+class AccountConfigurationWidgetFactory
+{
+
+public:
+	virtual ~AccountConfigurationWidgetFactory() {}
+
+	virtual AccountConfigurationWidget * createWidget(const Account &account, QWidget *parent) = 0;
+
+};
+
+#endif // ACCOUNT_CONFIGURATION_WIDGET_FACTORY_H
