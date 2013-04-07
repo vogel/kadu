c6e30eb951f1b6e46f9516f6c8207e539e57115b
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2013-04-07 22:02:15 +0200

    gui: added AccountConfigurationWidget class
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ kadu-core/gui/widgets/account-configuration-widget.h
@@ -0,0 +1,41 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2013 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ACCOUNT_CONFIGURATION_WIDGET_H
+#define ACCOUNT_CONFIGURATION_WIDGET_H
+
+#include "accounts/account.h"
+
+#include "modal-configuration-widget.h"
+
+class AccountConfigurationWidget : public ModalConfigurationWidget
+{
+	Q_OBJECT
+
+	Account MyAccount;
+
+public:
+	explicit AccountConfigurationWidget(const Account &account, QWidget *parent = 0);
+	virtual ~AccountConfigurationWidget();
+
+	Account account() const;
+
+};
+
+#endif // ACCOUNT_CONFIGURATION_WIDGET_H
