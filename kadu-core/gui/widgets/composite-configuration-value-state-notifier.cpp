a5ffbbcb2ea11f5c6cf9568a55d85616e5692a83
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2013-05-12 22:43:40 +0000

    gui: add CompositeConfigurationValueStateNotifier
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ kadu-core/gui/widgets/composite-configuration-value-state-notifier.cpp
@@ -0,0 +1,92 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2013 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "composite-configuration-value-state-notifier.h"
+
+CompositeConfigurationValueStateNotifier::CompositeConfigurationValueStateNotifier(QObject *parent) :
+		ConfigurationValueStateNotifier(parent), CurrentState(StateNotChanged)
+{
+}
+
+CompositeConfigurationValueStateNotifier::~CompositeConfigurationValueStateNotifier()
+{
+}
+
+void CompositeConfigurationValueStateNotifier::addConfigurationValueStateNotifier(const ConfigurationValueStateNotifier *configurationValueStateNotifier)
+{
+	if (!configurationValueStateNotifier)
+		return;
+	if (StateNotifiers.contains(configurationValueStateNotifier))
+		return;
+
+	StateNotifiers.append(configurationValueStateNotifier);
+	connect(configurationValueStateNotifier, SIGNAL(stateChanged(ConfigurationValueState)), this, SLOT(recomputeState()));
+	recomputeState();
+}
+
+void CompositeConfigurationValueStateNotifier::removeConfigurationValueStateNotifier(const ConfigurationValueStateNotifier *configurationValueStateNotifier)
+{
+	if (!configurationValueStateNotifier)
+		return;
+	if (!StateNotifiers.contains(configurationValueStateNotifier))
+		return;
+
+	StateNotifiers.removeAll(configurationValueStateNotifier);
+	disconnect(configurationValueStateNotifier, SIGNAL(stateChanged(ConfigurationValueState)), this, SLOT(recomputeState()));
+	recomputeState();
+}
+
+ConfigurationValueState CompositeConfigurationValueStateNotifier::computeState()
+{
+	bool anyChanged = false;
+
+	foreach (const ConfigurationValueStateNotifier * const stateNotifier, StateNotifiers)
+	{
+		switch (stateNotifier->state())
+		{
+			case StateChangedDataInvalid:
+				return StateChangedDataInvalid;
+			case StateNotChanged:
+				break;
+			case StateChangedDataValid:
+				anyChanged = true;
+				break;
+		}
+	}
+
+	if (anyChanged)
+		return StateChangedDataValid;
+	else
+		return StateNotChanged;
+}
+
+void CompositeConfigurationValueStateNotifier::recomputeState()
+{
+	ConfigurationValueState newState = computeState();
+	if (CurrentState == newState)
+		return;
+
+	CurrentState = newState;
+	emit stateChanged(CurrentState);
+}
+
+ConfigurationValueState CompositeConfigurationValueStateNotifier::state() const
+{
+	return CurrentState;
+}
---
+++ kadu-core/gui/widgets/composite-configuration-value-state-notifier.h
@@ -0,0 +1,48 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2013 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef COMPOSITE_CONFIGURATION_VALUE_STATE_NOTIFIER_H
+#define COMPOSITE_CONFIGURATION_VALUE_STATE_NOTIFIER_H
+
+#include "configuration-value-state-notifier.h"
+
+class KADUAPI CompositeConfigurationValueStateNotifier : public ConfigurationValueStateNotifier
+{
+	Q_OBJECT
+
+	QList<const ConfigurationValueStateNotifier *> StateNotifiers;
+	ConfigurationValueState CurrentState;
+
+	ConfigurationValueState computeState();
+
+private slots:
+	void recomputeState();
+
+public:
+	explicit CompositeConfigurationValueStateNotifier(QObject *parent = 0);
+	virtual ~CompositeConfigurationValueStateNotifier();
+
+	void addConfigurationValueStateNotifier(const ConfigurationValueStateNotifier *configurationValueStateNotifier);
+	void removeConfigurationValueStateNotifier(const ConfigurationValueStateNotifier *configurationValueStateNotifier);
+
+	virtual ConfigurationValueState state() const;
+
+};
+
+#endif // COMPOSITE_CONFIGURATION_VALUE_STATE_NOTIFIER_H
