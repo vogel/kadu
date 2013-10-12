db77621532b430dcc8ec8e0e0bb931ce169d34ae
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2013-10-12 09:00:05 +0200

    encryption_ng_simlite: regenerated translations
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- plugins/encryption_ng_simlite/translations/encryption_ng_simlite_cs.ts
+++ plugins/encryption_ng_simlite/translations/encryption_ng_simlite_cs.ts
@@ -1,4 +1,25 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="cs" version="2.0">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs">
+<context>
+    <name>@default</name>
+    <message>
+        <source>Chat</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Encryption</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enable encrytpion after receiving encrypted message</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 <context>
     <name>EncryptioNgSimliteEncryptor</name>
     <message>
@@ -28,6 +49,48 @@
     <message>
         <source>Cannot encrypt: unknown error</source>
         <translation>Nelze zašifrovat: neznámá chyba</translation>
+    </message>
+</context>
+<context>
+    <name>EncryptioNgSimliteProvider</name>
+    <message>
+        <source>Simlite</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SimliteSendPublicKeyActionDescription</name>
+    <message>
+        <source>Send My Public Key (Simlite)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Encryption</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cannot send keys. Check if encryption_ng_simlite plugin is loaded</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Public key dont exist. Do you want to create new one?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error generating key</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No public key available</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
--- plugins/encryption_ng_simlite/translations/encryption_ng_simlite_en.ts
+++ plugins/encryption_ng_simlite/translations/encryption_ng_simlite_en.ts
@@ -2,6 +2,25 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="en_US">
 <context>
+    <name>@default</name>
+    <message>
+        <source>Chat</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Encryption</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enable encrytpion after receiving encrypted message</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>EncryptioNgSimliteEncryptor</name>
     <message>
         <source>Cannot use public key: not a valid RSA key</source>
@@ -29,6 +48,48 @@
     </message>
     <message>
         <source>Cannot encrypt: unknown error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>EncryptioNgSimliteProvider</name>
+    <message>
+        <source>Simlite</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SimliteSendPublicKeyActionDescription</name>
+    <message>
+        <source>Send My Public Key (Simlite)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Encryption</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cannot send keys. Check if encryption_ng_simlite plugin is loaded</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Public key dont exist. Do you want to create new one?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error generating key</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No public key available</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
