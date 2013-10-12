8af88a51c184628f8de47d498dec961b6fbe943f
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2013-10-13 00:31:32 +0200

    filedesc: translations updated from transifex
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- plugins/filedesc/translations/filedesc_en.ts
+++ plugins/filedesc/translations/filedesc_en.ts
@@ -1,43 +1,41 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="en" version="2.0">
 <context>
     <name>@default</name>
     <message>
         <source>FileDesc</source>
-        <translation type="unfinished"></translation>
+        <translation>FileDesc</translation>
     </message>
     <message>
         <source>File with description to synchronize</source>
-        <translation type="unfinished"></translation>
+        <translation>File with description to synchronize</translation>
     </message>
     <message>
         <source>Enter here a file, which will contain descriptions to refresh by module.</source>
-        <translation type="unfinished"></translation>
+        <translation>Enter here a file, which will contain descriptions to refresh by module.</translation>
     </message>
     <message>
         <source>If you choose status without description, module will set it automatically to similar but with description</source>
-        <translation type="unfinished"></translation>
+        <translation>If you choose status without description, module will set it automatically to similar but with description</translation>
     </message>
     <message>
         <source>Allow other descriptions</source>
-        <translation type="unfinished"></translation>
+        <translation>Allow other descriptions</translation>
     </message>
     <message>
         <source>Allows you to set some custom description manualy, until file contents does not change</source>
-        <translation type="unfinished"></translation>
+        <translation>Allows you to set some custom description manualy, until file contents does not change</translation>
     </message>
     <message>
         <source>Behaviour</source>
-        <translation type="unfinished"></translation>
+        <translation>Behaviour</translation>
     </message>
     <message>
         <source>Description</source>
-        <translation type="unfinished"></translation>
+        <translation>Description</translation>
     </message>
     <message>
         <source>Always add description to status</source>
-        <translation type="unfinished"></translation>
+        <translation>Always add description to status</translation>
     </message>
 </context>
 </TS>
