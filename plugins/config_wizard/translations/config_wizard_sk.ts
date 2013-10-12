ed4c8c7e972956755a3a330f6da98d4113047320
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2013-10-13 01:55:03 +0200

    config_wizard: translations updated from transifex
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- plugins/config_wizard/translations/config_wizard_sk.ts
+++ plugins/config_wizard/translations/config_wizard_sk.ts
@@ -1,95 +1,95 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="en" version="2.0">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="sk" version="2.0">
 <context>
     <name>@default</name>
     <message>
         <source>Kadu needs to be restarted before changes to the language settings will take effect.</source>
-        <translation>Kadu needs to be restarted before changes to the language settings will take effect.</translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 <context>
     <name>ConfigWizardChooseNetworkPage</name>
     <message>
         <source>&lt;h3&gt;Account Setup&lt;/h3&gt;</source>
-        <translation>&lt;h3&gt;Account Setup&lt;/h3&gt;</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>IM Network</source>
-        <translation>IM Network</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>I want to set up existing account for Kadu</source>
-        <translation>I want to set up existing account for Kadu</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>I want to create new account for Kadu</source>
-        <translation>I want to create new account for Kadu</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>I don&apos;t want to set up my account for Kadu now</source>
-        <translation>I don&apos;t want to set up my account for Kadu now</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>&lt;p&gt;Please choose the network for the account that you would like to set up.&lt;/p&gt;&lt;p&gt;You can also create a new account in the wizard if you don&apos;t already have one&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Please choose the network for the account that you would like to set up.&lt;/p&gt;&lt;p&gt;You can also create a new account in the wizard if you don&apos;t already have one&lt;/p&gt;</translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 <context>
     <name>ConfigWizardCompletedPage</name>
     <message>
         <source>&lt;h3&gt;Configuration Wizard Completed&lt;/h3&gt;</source>
-        <translation>&lt;h3&gt;Configuration Wizard Completed&lt;/h3&gt;</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Go to Accounts Setting after closing this window</source>
-        <translation>Go to Accounts Setting after closing this window</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>&lt;p&gt;Confirm your wizard setting to start using Kadu.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Confirm your wizard setting to start using Kadu.&lt;/p&gt;</translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 <context>
     <name>ConfigWizardConfigurationUiHandler</name>
     <message>
         <source>Start Configuration Wizard</source>
-        <translation>Start Configuration Wizard</translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 <context>
     <name>ConfigWizardProfilePage</name>
     <message>
         <source>&lt;h3&gt;Welcome to Kadu Instant Messenger&lt;/h3&gt;&lt;p&gt;This wizard will help you to configure the basic settings of Kadu.&lt;/p&gt;&lt;p&gt;Please choose a preferred language and create a nickname&lt;/p&gt;</source>
-        <translation>&lt;h3&gt;Welcome to Kadu Instant Messenger&lt;/h3&gt;&lt;p&gt;This wizard will help you to configure the basic settings of Kadu.&lt;/p&gt;&lt;p&gt;Please choose a preferred language and create a nickname&lt;/p&gt;</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>&lt;h3&gt;Profile setup&lt;/h3&gt;</source>
-        <translation>&lt;h3&gt;Profile setup&lt;/h3&gt;</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Language</source>
-        <translation>Language</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Nickname</source>
-        <translation>Nickname</translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 <context>
     <name>ConfigWizardSetUpAccountPage</name>
     <message>
         <source>&lt;h3&gt;Account Setup&lt;/h3&gt;</source>
-        <translation>&lt;h3&gt;Account Setup&lt;/h3&gt;</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>&lt;p&gt;Please enter your account data.&lt;/p&gt;&lt;p&gt;Go back if you want to select a different Account Setup option.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Please enter your account data.&lt;/p&gt;&lt;p&gt;Go back if you want to select a different Account Setup option.&lt;/p&gt;</translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 <context>
     <name>ConfigWizardWindow</name>
     <message>
         <source>Kadu Wizard</source>
-        <translation>Kadu Wizard</translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 </TS>
