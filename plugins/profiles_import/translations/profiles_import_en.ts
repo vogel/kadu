d0659be819822c0df9af159ce7ebafa172c8f9ee
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2013-10-13 01:58:18 +0200

    profiles_import: translations updated from transifex
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- plugins/profiles_import/translations/profiles_import_en.ts
+++ plugins/profiles_import/translations/profiles_import_en.ts
@@ -1,121 +1,120 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="en" version="2.0">
 <context>
     <name>ImportProfileWindow</name>
     <message>
         <source>Import external Kadu 0.6.5 profile</source>
-        <translation type="unfinished"></translation>
+        <translation>Import external Kadu 0.6.5 profile</translation>
     </message>
     <message>
         <source>Select profile path</source>
-        <translation type="unfinished"></translation>
+        <translation>Select profile path</translation>
     </message>
     <message>
         <source>Select profile path:</source>
-        <translation type="unfinished"></translation>
+        <translation>Select profile path:</translation>
     </message>
     <message>
         <source>Select imported account identity:</source>
-        <translation type="unfinished"></translation>
+        <translation>Select imported account identity:</translation>
     </message>
     <message>
         <source>Import history</source>
-        <translation type="unfinished"></translation>
+        <translation>Import history</translation>
     </message>
     <message>
         <source>Import</source>
-        <translation type="unfinished"></translation>
+        <translation>Import</translation>
     </message>
     <message>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Close</translation>
     </message>
     <message>
         <source>&lt;b&gt;Identity not selected&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Identity not selected&lt;/b&gt;</translation>
     </message>
     <message>
         <source>&lt;b&gt;Selected directory does not contain kadu.conf.xml file&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Selected directory does not contain kadu.conf.xml file&lt;/b&gt;</translation>
     </message>
     <message>
         <source>Import external profile...</source>
-        <translation type="unfinished"></translation>
+        <translation>Import external profile...</translation>
     </message>
     <message>
         <source>This directory is not a Kadu profile directory.
 File kadu.conf.xml not found</source>
-        <translation type="unfinished"></translation>
+        <translation>This directory is not a Kadu profile directory.
+File kadu.conf.xml not found</translation>
     </message>
     <message>
         <source>Profile successfully imported!</source>
-        <translation type="unfinished"></translation>
+        <translation>Profile successfully imported!</translation>
     </message>
     <message>
         <source>Unable to import profile: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Unable to import profile: %1</translation>
     </message>
 </context>
 <context>
     <name>ImportProfilesWindow</name>
     <message>
         <source>&lt;p&gt;Current version of Kadu does not support user profiles.&lt;br /&gt;Instead, multiple account are supported in one instances of kadu.&lt;/p&gt;&lt;p&gt;Please select profiles that you would like to import as&lt;br /&gt;account into this instance of Kadu.&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;Current version of Kadu does not support user profiles.&lt;br /&gt;Instead, multiple account are supported in one instances of kadu.&lt;/p&gt;&lt;p&gt;Please select profiles that you would like to import as&lt;br /&gt;account into this instance of Kadu.&lt;/p&gt;</translation>
     </message>
     <message>
         <source>Import</source>
-        <translation type="unfinished"></translation>
+        <translation>Import</translation>
     </message>
     <message>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Close</translation>
     </message>
     <message>
         <source>Import history</source>
-        <translation type="unfinished"></translation>
+        <translation>Import history</translation>
     </message>
     <message>
         <source>Import external profile...</source>
-        <translation type="unfinished"></translation>
+        <translation>Import external profile...</translation>
     </message>
     <message>
         <source>Profile %1 successfully imported!</source>
-        <translation type="unfinished"></translation>
+        <translation>Profile %1 successfully imported!</translation>
     </message>
     <message>
         <source>Import profile...</source>
-        <translation type="unfinished"></translation>
+        <translation>Import profile...</translation>
     </message>
     <message>
         <source>Unable to import profile: %1: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Unable to import profile: %1: %2</translation>
     </message>
 </context>
 <context>
     <name>ProfileImporter</name>
     <message>
         <source>Unable to open profile file [%1].</source>
-        <translation type="unfinished"></translation>
+        <translation>Unable to open profile file [%1].</translation>
     </message>
     <message>
         <source>Account already exists.</source>
-        <translation type="unfinished"></translation>
+        <translation>Account already exists.</translation>
     </message>
     <message>
         <source>Imported account has no ID</source>
-        <translation type="unfinished"></translation>
+        <translation>Imported account has no ID</translation>
     </message>
 </context>
 <context>
     <name>ProfilesImportActions</name>
     <message>
         <source>Import profiles...</source>
-        <translation type="unfinished"></translation>
+        <translation>Import profiles...</translation>
     </message>
     <message>
         <source>Import external profile...</source>
-        <translation type="unfinished"></translation>
+        <translation>Import external profile...</translation>
     </message>
 </context>
 </TS>
--- plugins/profiles_import/translations/profiles_import_vi_VN.ts
+++ plugins/profiles_import/translations/profiles_import_vi_VN.ts
@@ -1,120 +1,119 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="cs" version="2.0">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="vi_VN" version="2.0">
 <context>
     <name>ImportProfileWindow</name>
     <message>
         <source>Import external Kadu 0.6.5 profile</source>
-        <translation>Zavést vnější profil Kadu 0.6.5</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Select profile path</source>
-        <translation>Vybrat cestu k profilu</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Select profile path:</source>
-        <translation>Vybrat cestu k profilu:</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Select imported account identity:</source>
-        <translation>Vybrat totožnost zavedeného účtu:</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Import history</source>
-        <translation>Zavést historii</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Import</source>
-        <translation>Zavést</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Close</source>
-        <translation>Zavřít</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>&lt;b&gt;Identity not selected&lt;/b&gt;</source>
-        <translation>&lt;b&gt;Totožnost nevybrána&lt;/b&gt;</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>&lt;b&gt;Selected directory does not contain kadu.conf.xml file&lt;/b&gt;</source>
-        <translation>&lt;b&gt;Vybraný adresář neobsahuje soubor kadu.conf.xml&lt;/b&gt;</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Import external profile...</source>
-        <translation>Zavést vnější profil...</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>This directory is not a Kadu profile directory.
 File kadu.conf.xml not found</source>
-        <translation>Tento adresář není adresářem s profilem Kadu.
-Soubor kadu.conf.xml nebyl nalezen</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Profile successfully imported!</source>
-        <translation>Profil úspěšně zaveden!</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Unable to import profile: %1</source>
-        <translation>Nelze zavést profil: %1</translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 <context>
     <name>ImportProfilesWindow</name>
     <message>
         <source>&lt;p&gt;Current version of Kadu does not support user profiles.&lt;br /&gt;Instead, multiple account are supported in one instances of kadu.&lt;/p&gt;&lt;p&gt;Please select profiles that you would like to import as&lt;br /&gt;account into this instance of Kadu.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Současná verze Kadu nepodporuje uživatelské profily.&lt;br /&gt;Místo toho je v jednom běhu Kadu podporováno více účtů.&lt;/p&gt;&lt;p&gt;Vyberte, prosím, profily, které chcete zavést jako&lt;br /&gt;účet pro tento běh Kadu.&lt;/p&gt;</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Import</source>
-        <translation>Zavést</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Close</source>
-        <translation>Zavřít</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Import history</source>
-        <translation>Zavést historii</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Import external profile...</source>
-        <translation>Zavést vnější profil...</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Profile %1 successfully imported!</source>
-        <translation>Profil %1 úspěšně zaveden!</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Import profile...</source>
-        <translation>Zavést profil...</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Unable to import profile: %1: %2</source>
-        <translation>Nelze zavést profil: %1: %2</translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 <context>
     <name>ProfileImporter</name>
     <message>
         <source>Unable to open profile file [%1].</source>
-        <translation>Nelze otevřít soubor s profilem [%1].</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Account already exists.</source>
-        <translation>Účet již existuje.</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Imported account has no ID</source>
-        <translation>Zavedený účet nemá žádné ID</translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 <context>
     <name>ProfilesImportActions</name>
     <message>
         <source>Import profiles...</source>
-        <translation>Zavést profily...</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>Import external profile...</source>
-        <translation>Zavést vnější profil...</translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 </TS>
