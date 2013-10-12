d1acd24be2c29596c4b450d19a9b9c0242a58e24
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2013-10-13 01:58:43 +0200

    qt4_sound: translations updated from transifex
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- plugins/qt4_sound/translations/qt4_sound_en.ts
+++ plugins/qt4_sound/translations/qt4_sound_en.ts
@@ -1,15 +1,13 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="en" version="2.0">
 <context>
     <name>QObject</name>
     <message>
         <source>Kadu</source>
-        <translation type="unfinished"></translation>
+        <translation>Kadu</translation>
     </message>
     <message>
         <source>QSound API is not available on this platform</source>
-        <translation type="unfinished"></translation>
+        <translation>QSound API is not available on this platform</translation>
     </message>
 </context>
 </TS>
--- plugins/qt4_sound/translations/qt4_sound_vi_VN.ts
+++ plugins/qt4_sound/translations/qt4_sound_vi_VN.ts
@@ -1,13 +1,13 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="cs" version="2.0">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="vi_VN" version="2.0">
 <context>
     <name>QObject</name>
     <message>
         <source>Kadu</source>
-        <translation>Kadu</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <source>QSound API is not available on this platform</source>
-        <translation>QSound API v tomto systému není dostupné</translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 </TS>
