a41f4087886ea8a86d8fe9451eb7c57f05c90ff9
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2013-10-14 00:01:29 +0200

    gui: GroupTabType enum added
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ kadu-core/gui/widgets/group-tab-bar/group-tab-type.h
@@ -0,0 +1,30 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2013 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GROUP_TAB_TYPE_H
+#define GROUP_TAB_TYPE_H
+
+enum GroupTabType
+{
+	GroupTabRegular,
+	GroupTabEverybody,
+	GroupTabUngroupped
+};
+
+#endif // GROUP_TAB_TYPE_H
