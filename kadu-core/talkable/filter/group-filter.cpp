1dc3936e1a63016da0d643917cf76a2fbe0658d3
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2013-10-18 00:11:30 +0200

    talkable: GroupFilter class added
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ kadu-core/talkable/filter/group-filter.cpp
@@ -0,0 +1,59 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2013 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "talkable/filter/group-filter-type.h"
+
+#include "group-filter.h"
+
+GroupFilter::GroupFilter() :
+		FilterType(GroupFilterInvalid)
+{
+}
+
+GroupFilter::GroupFilter(GroupFilterType filterType) :
+		FilterType(filterType)
+{
+}
+
+GroupFilter::GroupFilter(const Group &group) :
+		FilterType(GroupFilterRegular), MyGroup(group)
+{
+}
+
+bool GroupFilter::operator == (const GroupFilter &groupFilter) const
+{
+	if (groupFilter.filterType() != FilterType)
+		return false;
+	return groupFilter.group() == MyGroup;
+}
+
+bool GroupFilter::isValid() const
+{
+	return GroupFilterInvalid != FilterType;
+}
+
+GroupFilterType GroupFilter::filterType() const
+{
+	return FilterType;
+}
+
+Group GroupFilter::group() const
+{
+	return MyGroup;
+}
---
+++ kadu-core/talkable/filter/group-filter.h
@@ -0,0 +1,49 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2013 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GROUP_FILTER_H
+#define GROUP_FILTER_H
+
+#include "buddies/group.h"
+#include "talkable/filter/group-filter-type.h"
+
+class GroupFilter
+{
+	GroupFilterType FilterType;
+	Group MyGroup;
+
+public:
+	GroupFilter();
+	GroupFilter(const GroupFilter &) = default;
+	explicit GroupFilter(GroupFilterType filterType);
+	explicit GroupFilter(const Group &group);
+
+	GroupFilter & operator = (const GroupFilter &groupFilter) = default;
+	bool operator == (const GroupFilter &groupFilter) const;
+
+	bool isValid() const;
+
+	GroupFilterType filterType() const;
+	Group group() const;
+
+};
+
+Q_DECLARE_METATYPE(GroupFilter)
+
+#endif // GROUP_FILTER_H
