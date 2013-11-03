d571271bbb37c23ad552a22d3a5ca6160f5edfdf
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2013-11-03 13:18:37 +0100

    gui: add copyright notice to chat-window.h file
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- kadu-core/gui/windows/chat-window/chat-window.h
+++ kadu-core/gui/windows/chat-window/chat-window.h
@@ -1,5 +1,31 @@
-#ifndef KADU_CHAT_WINDOW_H
-#define KADU_CHAT_WINDOW_H
+/*
+ * %kadu copyright begin%
+ * Copyright 2008, 2009, 2010, 2010, 2011 Piotr Galiszewski (piotr.galiszewski@kadu.im)
+ * Copyright 2009, 2011, 2012 Wojciech Treter (juzefwt@gmail.com)
+ * Copyright 2008 Tomasz Rostański (rozteck@interia.pl)
+ * Copyright 2010, 2011 Piotr Dąbrowski (ultr@ultr.pl)
+ * Copyright 2008, 2009 Michał Podsiadlik (michal@kadu.net)
+ * Copyright 2009 Bartłomiej Zimoń (uzi18@o2.pl)
+ * Copyright 2007, 2008, 2009, 2009, 2010, 2011, 2012 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * Copyright 2010, 2011, 2012, 2013 Bartosz Brachaczek (b.brachaczek@gmail.com)
+ * Copyright 2007 Dawid Stawiarski (neeo@kadu.net)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
 
 #include <QtGui/QWidget>
 
@@ -67,5 +93,3 @@ public slots:
 	void showNewMessagesNumInTitle();
 
 };
-
-#endif
