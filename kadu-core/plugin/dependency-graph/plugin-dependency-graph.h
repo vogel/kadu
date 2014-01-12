d431ffda0eb6893957186b9b113072df57e6ea78
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2014-01-12 12:21:27 +0100

    plugin: documentation for PluginDependencyGraph added
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- kadu-core/plugin/dependency-graph/plugin-dependency-graph.h
+++ kadu-core/plugin/dependency-graph/plugin-dependency-graph.h
@@ -25,6 +25,23 @@
 #include <QtCore/QSet>
 #include <QtCore/QVector>
 
+/**
+ * @addtogroup Plugin
+ * @{
+ */
+
+/**
+ * @class PluginDependencyGraph
+ * @short Graph of dependencies between plugins.
+ *
+ * Dependencies are two directional - each plugin has list of dependencies and list of
+ * dependents. For each 'dependency' relation there is corresponding 'dependent' one.
+ *
+ * Dependency graph is created by adding plugins and relations between them. If dependency
+ * relation is created for plugins that are not yet in the graph - they are added to it.
+ *
+ * All plugin that have dependency cycle can be obtained by calling @see findPluginsInDependencyCycle.
+ */
 class KADUAPI PluginDependencyGraph
 {
 	struct PluginDependencyTag {};
@@ -38,15 +55,63 @@ public:
 	PluginDependencyGraph & operator = (const PluginDependencyGraph &) = delete;
 	PluginDependencyGraph & operator = (PluginDependencyGraph &&) = default;
 
+	/**
+	 * @short Add plugin to graph.
+	 * @param pluginName name of plugin to add
+	 *
+	 * If plugin is already in dependency graph, nothing is done. In other case it is added
+	 * to it without any dependencies.
+	 */
 	void addPlugin(const QString &pluginName);
+
+	/**
+	 * @short Add dependency relation to graph.
+	 * @param dependentPluginName name of dependent plugin
+	 * @param dependencyPluginName name of dependency plugin
+	 *
+	 * Add dependency relation fo two given plugins. Also adds dependent relation in the other way.
+	 * If given plugins were not in the graph already then they are added.
+	 */
 	void addDependency(const QString &dependentPluginName, const QString &dependencyPluginName);
 
+	/**
+	 * @return Number of plugins in graph.
+	 */
 	int size() const;
+
+	/**
+	 * @param pluginName plugin to get dependencies for
+	 * @return Set of direct dependencies of given plugin.
+	 */
 	QSet<QString> directDependencies(const QString &pluginName) const;
+
+	/**
+	 * @param pluginName plugin to get dependents for
+	 * @return Set of direct dependents of given plugin.
+	 */
 	QSet<QString> directDependents(const QString &pluginName) const;
 
+	/**
+	 * @return set of plugins that have dependency cycle
+	 */
 	QSet<QString> findPluginsInDependencyCycle() const;
+
+	/**
+	 * @param pluginName name of plugin to find all dependencies for
+	 * @return return sorted list of dependencies for given plugin
+	 *
+	 * This method returns topological-sorted list of dependencies for given plugin. It means that
+	 * plugins can be properly activated in order of this list.
+	 */
 	QVector<QString> findDependencies(const QString &pluginName) const noexcept(false);
+
+	/**
+	 * @param pluginName name of plugin to find all dependents for
+	 * @return return sorted list of dependents for given plugin
+	 *
+	 * This method returns topological-sorted list of dependents for given plugin. It means that
+	 * plugins can be properly deactivated in order of this list.
+	 */
 	QVector<QString> findDependents(const QString &pluginName) const noexcept(false);
 
 private:
@@ -59,3 +124,7 @@ private:
 	QVector<QString> findSuccessors(const QString &pluginName) const noexcept(false);
 
 };
+
+/**
+ * @}
+ */
