08a4c60cd916058de7710dc6a8c6c47d4bb7b8fa
Bartosz Brachaczek <b.brachaczek@gmail.com>
2014-01-13 21:20:11 +0100

    qt5: Port to new QTabletEvent c-tor
    Signed-off-by: Bartosz Brachaczek <b.brachaczek@gmail.com>

--- kadu-core/gui/widgets/plugin-list/plugin-list-view-delegate-widgets.cpp
+++ kadu-core/gui/widgets/plugin-list/plugin-list-view-delegate-widgets.cpp
@@ -222,8 +222,15 @@ bool PluginListWidgetDelegateEventListener::eventFilter(QObject *watched, QEvent
                         case QEvent::TabletLeaveProximity:
                         {
                                 QTabletEvent *tabletEvent = static_cast<QTabletEvent*>(event);
-                                QTabletEvent evt(event->type(), viewport->mapFromGlobal(tabletEvent->globalPos()),
-                                                 tabletEvent->globalPos(), tabletEvent->hiResGlobalPos(), tabletEvent->device(),
+                                QTabletEvent evt(event->type(),
+#if QT_VERSION >= 0x050000
+                                                 QPointF(viewport->mapFromGlobal(tabletEvent->globalPos())),
+                                                 tabletEvent->globalPosF(),
+#else
+                                                 viewport->mapFromGlobal(tabletEvent->globalPos()), tabletEvent->globalPos(),
+                                                 tabletEvent->hiResGlobalPos(),
+#endif
+                                                 tabletEvent->device(),
                                                  tabletEvent->pointerType(), tabletEvent->pressure(), tabletEvent->xTilt(),
                                                  tabletEvent->yTilt(), tabletEvent->tangentialPressure(), tabletEvent->rotation(),
                                                  tabletEvent->z(), tabletEvent->modifiers(), tabletEvent->uniqueId());
