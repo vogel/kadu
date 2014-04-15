1c52c24ae7a48e8ff5db913fc3d02e5000c23bf3
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2014-04-16 00:53:41 +0200

    message: add RawMessage class
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ kadu-core/message/raw-message.cpp
@@ -0,0 +1,53 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2014 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "raw-message.h"
+
+RawMessage::RawMessage()
+{
+}
+
+RawMessage::RawMessage(QByteArray rawPlainContent, QByteArray rawXmlContent) :
+		m_rawPlainContent{std::move(rawPlainContent)},
+		m_rawXmlContent{std::move(rawXmlContent)}
+{
+}
+
+RawMessage::RawMessage(const QByteArray &plainContent) :
+		m_rawPlainContent{plainContent},
+		m_rawXmlContent{plainContent}
+{
+}
+
+QByteArray RawMessage::rawContent() const
+{
+	return m_rawXmlContent.isEmpty()
+		? m_rawPlainContent
+		: m_rawXmlContent;
+}
+
+QByteArray RawMessage::rawPlainContent() const
+{
+	return m_rawPlainContent;
+}
+
+QByteArray RawMessage::rawXmlContent() const
+{
+	return m_rawXmlContent;
+}
---
+++ kadu-core/message/raw-message.h
@@ -0,0 +1,42 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2014 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "exports.h"
+
+#include <QtCore/QByteArray>
+
+class KADUAPI RawMessage
+{
+
+public:
+	RawMessage();
+	RawMessage(const QByteArray &plainContent);
+	RawMessage(QByteArray rawPlainContent, QByteArray rawXmlContent);
+
+	QByteArray rawContent() const;
+	QByteArray rawPlainContent() const;
+	QByteArray rawXmlContent() const;
+
+private:
+	QByteArray m_rawPlainContent;
+	QByteArray m_rawXmlContent;
+
+};
