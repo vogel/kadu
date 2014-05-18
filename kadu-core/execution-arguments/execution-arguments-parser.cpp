2a4e02b6944abae54eff2ec61a3a7d8cdf28a715
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2014-05-19 00:12:11 +0200

    core: add ExecutionArgumentsParser class
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ kadu-core/execution-arguments/execution-arguments-parser.cpp
@@ -0,0 +1,56 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2014 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "execution-arguments-parser.h"
+
+#include "execution-arguments/execution-arguments.h"
+#include "execution-arguments/execution-arguments-builder.h"
+
+ExecutionArguments ExecutionArgumentsParser::parse(const QStringList &arguments) const
+{
+	auto queryVersion = false;
+	auto queryUsage = false;
+	auto debugMask = QString{};
+	auto configurationDirectory = QString{};
+	auto openIds = QStringList{};
+
+	for (auto it = arguments.constBegin(); it != arguments.constEnd(); ++it)
+	{
+		if (*it == QLatin1String("--version"))
+			queryVersion = true;
+		else if (*it == QLatin1String("--help"))
+			queryUsage = true;
+		else if (*it == QLatin1String("--debug") && (it + 1) != arguments.constEnd())
+			debugMask = *(++it);
+		else if (*it == QLatin1String("--config-dir") && (it + 1) != arguments.constEnd())
+			configurationDirectory = *(++it);
+		else if (QRegExp("^[a-zA-Z]*:(/){0,3}.*").exactMatch(*it))
+			openIds.append(*it);
+		else
+			fprintf(stderr, "Ignoring unknown parameter '%s'\n", it->toUtf8().constData());
+	}
+
+	return ExecutionArgumentsBuilder{}
+		.setQueryVersion(queryVersion)
+		.setQueryUsage(queryUsage)
+		.setDebugMask(std::move(debugMask))
+		.setConfigurationDirectory(std::move(configurationDirectory))
+		.setOpenIds(std::move(openIds))
+		.build();
+}
