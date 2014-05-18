d9469524980cc6b469bb7f1a9aee370debc477b0
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2014-05-19 00:00:32 +0200

    core: add ExecutionArgumentsBuilder class
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ kadu-core/execution-arguments/execution-arguments-builder.cpp
@@ -0,0 +1,69 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2014 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "execution-arguments-builder.h"
+
+#include "execution-arguments/execution-arguments.h"
+
+ExecutionArgumentsBuilder::ExecutionArgumentsBuilder() :
+		m_queryVersion{false},
+		m_queryUsage{false}
+{
+}
+
+ExecutionArgumentsBuilder & ExecutionArgumentsBuilder::setQueryVersion(bool queryVersion)
+{
+	m_queryVersion = queryVersion;
+	return *this;
+}
+
+ExecutionArgumentsBuilder & ExecutionArgumentsBuilder::setQueryUsage(bool queryUsage)
+{
+	m_queryUsage = queryUsage;
+	return *this;
+}
+
+ExecutionArgumentsBuilder & ExecutionArgumentsBuilder::setDebugMask(QString debugMask)
+{
+	m_debugMask = std::move(debugMask);
+	return *this;
+}
+
+ExecutionArgumentsBuilder & ExecutionArgumentsBuilder::setConfigurationDirectory(QString configurationDirectory)
+{
+	m_configurationDirectory = std::move(configurationDirectory);
+	return *this;
+}
+
+ExecutionArgumentsBuilder & ExecutionArgumentsBuilder::setOpenIds(QStringList openIds)
+{
+	m_openIds = std::move(openIds);
+	return *this;
+}
+
+ExecutionArguments ExecutionArgumentsBuilder::build() const
+{
+	return {
+		m_queryVersion,
+		m_queryUsage,
+		m_debugMask,
+		m_configurationDirectory,
+		m_openIds
+	};
+}
