25a342bb77705132b9d5be39a77048f1f556baa3
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2014-06-29 18:00:49 +0200

    plugins: fix installing external plugins
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- cmake/KaduMacros.cmake
+++ cmake/KaduMacros.cmake
@@ -172,6 +172,7 @@ function (kadu_plugin KADU_PLUGIN_NAME)
 		foreach (_plugin_dependency ${KADU_PLUGIN_DEPENDENCIES})
 			set (KaduPlugin_${_plugin_dependency}_DIR "${Kadu_DIR}")
 			find_package (KaduPlugin_${_plugin_dependency} REQUIRED)
+			include_directories (${KADU_INCLUDE_DIR}/plugins/${_plugin_dependency})
 		endforeach ()
 	endif ()
 
