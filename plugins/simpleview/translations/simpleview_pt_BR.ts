06712354267035b23b6fddf61b61d29d8589611f
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2014-07-13 23:25:48 +0200

    translations: update from transifex
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ plugins/simpleview/translations/simpleview_pt_BR.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="pt_BR" version="2.0">
+<context>
+    <name>@default</name>
+    <message>
+        <source>Keep contact list size</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Hide scroll bar</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Extras</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Simple View</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Hide window borders</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>SimpleView</name>
+    <message>
+        <source>Simple view</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+</TS>
