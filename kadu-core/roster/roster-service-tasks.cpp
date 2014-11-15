fbf88f66b3b50ddbc93220773d0669ac32e4b3b6
Rafał Przemysław Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2014-11-16 00:30:04 +0100

    roster: add RosterServiceTasks class
    Signed-off-by: Rafał Przemysław Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ kadu-core/roster/roster-service-tasks.cpp
@@ -0,0 +1,117 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2014 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "roster-service-tasks.h"
+
+#include "contacts/contact.h"
+#include "roster/roster-entry.h"
+#include "roster/roster-entry-state.h"
+#include "roster/roster-task-type.h"
+
+QVector<RosterTask> RosterServiceTasks::updateTasksForContacts(const QVector<Contact> &contacts)
+{
+	auto result = QVector<RosterTask>{};
+	for (auto &&contact : contacts)
+	{
+		if (!contact.rosterEntry())
+			continue;
+		if (contact.rosterEntry()->requiresSynchronization())
+			result.append(RosterTask{RosterTaskType::Update, contact.id()});
+	}
+	return result;
+}
+
+RosterServiceTasks::RosterServiceTasks(QObject *parent) :
+		QObject{parent}
+{
+}
+
+RosterServiceTasks::~RosterServiceTasks()
+{
+}
+
+bool RosterServiceTasks::containsTask(const QString &id) const
+{
+	return m_idToTask.contains(id);
+}
+
+QVector<RosterTask> RosterServiceTasks::tasks()
+{
+	return m_tasks.toVector();
+}
+
+bool RosterServiceTasks::shouldReplaceTask(RosterTaskType taskType, RosterTaskType replacementType)
+{
+	Q_ASSERT(RosterTaskType::None != taskType);
+	Q_ASSERT(RosterTaskType::None != replacementType);
+
+	if (RosterTaskType::Delete == taskType)
+		return true;
+
+	if (RosterTaskType::Add == taskType)
+		return RosterTaskType::Delete == replacementType;
+
+	return RosterTaskType::Update != replacementType;
+}
+
+void RosterServiceTasks::addTask(const RosterTask &task)
+{
+	if (!m_idToTask.contains(task.id()))
+	{
+		m_tasks.enqueue(task);
+		return;
+	}
+
+	auto existingTask = m_idToTask.value(task.id());
+	if (shouldReplaceTask(existingTask.type(), task.type()))
+	{
+		m_tasks.removeAll(existingTask);
+		m_idToTask.remove(task.id());
+		m_idToTask.insert(task.id(), task);
+		m_tasks.enqueue(task);
+	}
+}
+
+void RosterServiceTasks::addTasks(const QVector<RosterTask> &tasks)
+{
+	for (auto const &task : tasks)
+		addTask(task);
+}
+
+RosterTaskType RosterServiceTasks::taskType(const QString &id)
+{
+	if (!m_idToTask.contains(id))
+		return RosterTaskType::None;
+	else
+		return m_idToTask.value(id).type();
+}
+
+bool RosterServiceTasks::isEmpty() const
+{
+	return m_tasks.isEmpty();
+}
+
+RosterTask RosterServiceTasks::dequeue()
+{
+	auto result = m_tasks.dequeue();
+	m_idToTask.remove(result.id());
+	return result;
+}
+
+#include "moc_roster-service-tasks.cpp"
---
+++ kadu-core/roster/roster-service-tasks.h
@@ -0,0 +1,97 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2014 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "roster/roster-task.h"
+#include "exports.h"
+
+#include <QtCore/QMap>
+#include <QtCore/QObject>
+#include <QtCore/QQueue>
+#include <QtCore/QVector>
+
+class Contact;
+
+/**
+ * @addtogroup Protocol
+ * @{
+ */
+
+/**
+ * @class RosterServiceTasks
+ */
+class KADUAPI RosterServiceTasks : public QObject
+{
+	Q_OBJECT
+
+public:
+	/**
+	 * @return List of tasks required to update @p contacts
+	 * 
+	 * For each contact that has a RosterEntry and is in desynchronized state a new RosterTask with Update mode is returned.
+	 */
+	static QVector<RosterTask> updateTasksForContacts(const QVector<Contact> &contacts);
+
+	explicit RosterServiceTasks(QObject *parent = nullptr);
+	virtual ~RosterServiceTasks();
+
+	QVector<RosterTask> tasks();
+
+	void addTasks(const QVector<RosterTask> &tasks);
+
+	/**
+	 * @return type of task for given id
+	 *
+	 * If no task for given id is found then RosterTaskType::None is returned.
+	 */
+	RosterTaskType taskType(const QString &id);
+
+	bool isEmpty() const;
+
+	RosterTask dequeue();
+
+	bool containsTask(const QString &id) const;
+
+private:
+	QQueue<RosterTask> m_tasks;
+	QMap<QString, RosterTask> m_idToTask;
+
+	/**
+	 * @short Add new task for later execution.
+	 * @param task new task
+	 *
+	 * If existing task for given id is available then this service decides which one to use and which one to ignore.
+	 * If existing task is deletion, then it is always replaced. Addition task can be only replaced by deletion task. Update task
+	 * can be replaced by any non-update task.
+	 */
+	void addTask(const RosterTask &task);
+
+	/**
+	 * @author Rafał 'Vogel' Malinowski
+	 * @short Return true if task of second type should replace task of first type on list of task to execute.
+	 * @return true if task of second type should replace task of first type on list of task to execute
+	 */
+	bool shouldReplaceTask(RosterTaskType taskType, RosterTaskType replacementType);
+
+};
+
+/**
+ * @}
+ */
