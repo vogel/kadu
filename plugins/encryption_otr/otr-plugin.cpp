000b902ebf743bf1044b29fa4b424f945e99b295
Rafał Przemysław Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2015-01-08 23:06:40 +0100

    otr: really fix issue with new libotr
    Signed-off-by: Rafał Przemysław Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- plugins/encryption_otr/otr-plugin.cpp
+++ plugins/encryption_otr/otr-plugin.cpp
@@ -66,10 +66,7 @@ OtrPlugin::~OtrPlugin()
 
 bool OtrPlugin::fragmentsFixAvailable() const
 {
-	return false;
-
-	// seems like there is bug in 4.1.0 that makes it unstable
-	// return (OTRL_VERSION_MAJOR > 4) || (OTRL_VERSION_MINOR > 0) || (OTRL_VERSION_SUB > 0);
+	return (OTRL_VERSION_MAJOR > 4) || (OTRL_VERSION_MINOR > 0) || (OTRL_VERSION_SUB > 0);
 }
 
 bool OtrPlugin::init(bool firstLoad)
