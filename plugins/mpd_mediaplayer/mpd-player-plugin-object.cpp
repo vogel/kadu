93d427660851cb147e3919dd9aeb7cf5d29ed442
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2016-01-02 14:25:04 +0100

    mpd_player: port plugin to injectors
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ plugins/mpd_mediaplayer/mpd-player-plugin-object.cpp
@@ -0,0 +1,59 @@
+/*
+ * %kadu copyright begin%
+ * Copyright 2015 Rafał Przemysław Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+ * %kadu copyright end%
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "mpd-player-plugin-object.h"
+
+#include "mpd-mediaplayer.h"
+
+#include "plugins/mediaplayer/mediaplayer.h"
+
+#include "misc/paths-provider.h"
+
+MpdPlayerPluginObject::MpdPlayerPluginObject(QObject *parent) :
+		PluginObject{parent}
+{
+}
+
+MpdPlayerPluginObject::~MpdPlayerPluginObject()
+{
+}
+
+void MpdPlayerPluginObject::setMPDMediaPlayer(MPDMediaPlayer *mpdMediaPlayer)
+{
+	m_mpdMediaPlayer = mpdMediaPlayer;
+}
+
+void MpdPlayerPluginObject::setPathsProvider(PathsProvider *pathsProvider)
+{
+	m_pathsProvider = pathsProvider;
+}
+
+void MpdPlayerPluginObject::init()
+{
+	MainConfigurationWindow::registerUiFile(m_pathsProvider->dataPath() + QLatin1String("plugins/configuration/mpd_config.ui"));
+	MediaPlayer::instance()->registerMediaPlayer(m_mpdMediaPlayer, m_mpdMediaPlayer);
+}
+
+void MpdPlayerPluginObject::done()
+{
+	MediaPlayer::instance()->unregisterMediaPlayer();
+	MainConfigurationWindow::unregisterUiFile(m_pathsProvider->dataPath() + QLatin1String("plugins/configuration/mpd_config.ui"));
+}
+
+#include "moc_mpd-player-plugin-object.cpp"
