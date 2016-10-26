a873488e7dba2e29b9bf9c483c9f26b49aac71d3
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2016-10-27 00:09:45 +0200

    pcspeaker: #3111 fixed: usage of __useconds_t type removed
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- plugins/pcspeaker/pcspeaker-notifier.cpp
+++ plugins/pcspeaker/pcspeaker-notifier.cpp
@@ -68,7 +68,7 @@ void PCSpeakerNotifier::beep(int pitch, int duration)
 void PCSpeakerNotifier::beep(int pitch, int duration)
 {
 	if (pitch == 0)
-		usleep(static_cast<__useconds_t>(duration * 200));
+		usleep(static_cast<useconds_t>(duration * 200));
 	else
 	{
 		XKeyboardState s;			//save previous sound config
@@ -80,7 +80,7 @@ void PCSpeakerNotifier::beep(int pitch, int duration)
 		XChangeKeyboardControl(xdisplay, (KBBellPitch | KBBellDuration | KBBellPercent), &v); //set sound config
 		XBell(xdisplay, volume);  		//put sound to buffer
 		XFlush(xdisplay);			//flush buffer (beep)
-		usleep(static_cast<__useconds_t>(pitch * 100));			//wait until sound is played
+		usleep(static_cast<useconds_t>(pitch * 100));			//wait until sound is played
 		v.bell_pitch = static_cast<int>(s.bell_pitch);		//restore previous sound config
 		v.bell_duration = static_cast<int>(s.bell_duration);
 		v.bell_percent = static_cast<int>(s.bell_percent);
