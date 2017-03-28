4f7f619e6d3273872e8c077687867d7e85915e01
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2017-03-28 23:48:00 +0200

    facebook: add thanks comments for bitlbee-facebook authors
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- plugins/facebook_protocol/qfacebook/messages/qfacebook-connect-ack.h
+++ plugins/facebook_protocol/qfacebook/messages/qfacebook-connect-ack.h
@@ -3,6 +3,9 @@
  * Copyright 2017 Rafał Przemysław Malinowski (rafal.przemyslaw.malinowski@gmail.com)
  * %kadu copyright end%
  *
+ * Based on bitlbee-facebook by James Geboski (jgeboski@gmail.com) and
+ * dequis <dx@dxzone.com.ar>.
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of
